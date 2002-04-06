016bd1dd532ce8df102eb0305cbb9543592de0e7
Arnaud Delorme <arno@ucsd.edu>
2002-04-06 03:48:07 +0000

    changing input for 1 channel for topoplot
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@26 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/timefreqfunc/timef.m
+++ functions/timefreqfunc/timef.m
@@ -113,6 +113,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.2  2002/04/06 03:40:58  arno
+% modifying location file check
+%
 % Revision 1.1  2002/04/05 17:36:45  jorn
 % Initial revision
 %
@@ -868,8 +871,13 @@ switch lower(g.plotitc)
     %
 	if (~isempty(g.topovec))
 		h(12) = subplot('Position',[-.1 .43 .2 .14].*s+q);
-		topoplot(g.topovec,g.elocs,'electrodes','off')
-		axis('square')
+		if length(g.topovec) == 1
+		  topoplot(g.topovec,g.elocs,'electrodes','off', ...
+			   'style', 'blank', 'emarkersize1chan', 10);
+		else
+		  topoplot(g.topovec,g.elocs,'electrodes','off');
+		end;
+		    axis('square')
 	end
 end; %switch
 
