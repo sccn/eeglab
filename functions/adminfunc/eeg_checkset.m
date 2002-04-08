27b5d4a0cbac379d70141a3f2e49effd10adc0a4
Arnaud Delorme <arno@ucsd.edu>
2002-04-08 20:49:26 +0000

    add check for the comments field
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@45 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/eeg_checkset.m
+++ functions/adminfunc/eeg_checkset.m
@@ -77,6 +77,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.2  2002/04/08 02:13:09  scott
+% improved wording of messages to user -sm
+%
 % Revision 1.1  2002/04/05 17:32:13  jorn
 % Initial revision
 %
@@ -299,6 +302,7 @@ end;
 
 if ~isfield(EEG, 'specdata') EEG.specdata = []; res = com; end;
 if ~isfield(EEG, 'specicaact') EEG.specicaact = []; res = com; end;
+if ~isfield(EEG, 'comments') EEG.comments = ''; res = com; end;
 
 % create fields if absent
 % -----------------------
