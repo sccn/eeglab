670af65c2f05f60d2f7f76e5afafe4cfa5f718a0
Arnaud Delorme <arno@ucsd.edu>
2002-04-10 19:31:12 +0000

    debugging (variable name error)
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@85 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/compvar.m
+++ functions/sigprocfunc/compvar.m
@@ -43,6 +43,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.1  2002/04/05 17:39:45  jorn
+% Initial revision
+%
 % 01-25-02 reformated help & license -ad 
 % 03-08-02 added the sphere and weight option -ad
 
@@ -56,7 +59,7 @@ end;
 sig = reshape(sig, size(sig,1), size(sig,2)*size(sig,3));
 squaresig  = sum(sum(abs(sig)));
 
-if iscell(icaact)
+if iscell(act)
     compproj   = winv(:,compos)*act(compos,:)-sig;
 else
     weight = act{2};
