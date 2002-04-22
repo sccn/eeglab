04b4b4bb3746cd77a3428484a21b26f1c48c5fbc
Arnaud Delorme <arno@ucsd.edu>
2002-04-22 23:36:53 +0000

    temporary modif
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@201 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_selectevent.m
+++ functions/popfunc/pop_selectevent.m
@@ -61,6 +61,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.5  2002/04/18 18:25:51  arno
+% typo can not
+%
 % Revision 1.4  2002/04/10 22:39:47  arno
 % removing debuging comment
 %
@@ -201,7 +204,7 @@ for index = 1:length(allfields)
    try, eval(['g.' allfields{index} ';']); catch, eval(['g.' allfields{index} '=[];' ]); end; 
    try, eval(['g.omit' allfields{index} ';']); catch, eval(['g.omit' allfields{index} '=[];' ]); end; 
 end;
-
+g
 
 % select the events to keep
 % -------------------------
