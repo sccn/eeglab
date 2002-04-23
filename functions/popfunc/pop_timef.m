ca572ff20846d45cce59da60183dc8e47b4227c7
Arnaud Delorme <arno@ucsd.edu>
2002-04-23 02:49:38 +0000

    changed graphic interface
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@209 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_timef.m
+++ functions/popfunc/pop_timef.m
@@ -3,7 +3,10 @@
 %           timelocked to a set of input events in one data channel. 
 %
 % Usage:
-%   > pop_timef(EEG, typeproc, num, tlimits,cycles,
+%   >> pop_timef(EEG, typeplot); % pop_up window
+%   >> pop_timef(EEG, typeplot, lastcom); % pop_up window
+%   >> pop_timef(EEG, typeplot, channel); % do not pop-up
+%   >> pop_timef(EEG, typeproc, num, tlimits,cycles,
 %                        'key1',value1,'key2',value2, ... );   
 %     
 % Inputs:            
@@ -44,6 +47,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.6  2002/04/08 20:27:29  arno
+% debuging command line calls
+%
 % Revision 1.5  2002/04/07 19:18:51  scott
 % rm'd int2str() attempt for topovec -sm
 %
@@ -75,40 +81,59 @@ if nargin < 2
 	help pop_timef;
 	return;
 end;	
+lastcom = [];
+if nargin < 3
+	popup = 1;
+else
+	popup = isstr(channel) | isempty(channel);
+	if isstr(channel)
+		lastcom = channel;
+	end;
+end;
 
 % pop up window
 % -------------
 if nargin < 3
-	promptstr    = { fastif(typeproc, 'Channel number:', 'Component number:') ...
-					 'Epoch time range [min max] (msec)' ...
-	                 'Wavelet cycles (0->FFT, see >> help timef)' ...
-	                 ['Compute linear inter-trial coherence (coher)' 10 'OR inter-trial phase coherence (phasecoher)'] ...
-	                 'Bootstrap significance level (Ex: 0.01 -> 1%)' ...
-	                 'Optional timef parameters (see >> help timef)' };
-	inistr       = {  '1' ...
-					  [ int2str(EEG.xmin*1000) ' ' int2str(EEG.xmax*1000) ] ...
-					  '0' ...
-					  'phasecoher' '' '''padratio'', 4'};
-	result       = inputdlg( promptstr, fastif(typeproc, 'Plot channel time frequency -- pop_timef()', ...
-	                'Plot component time frequency -- pop_timef()'), 1,  inistr );
+	[txt vars] = gethelpvar('timef.m');
+	
+	geometry = { [1 0.5 0.5] [1 0.5 0.5] [1 0.5 0.5] [0.92 0.1 0.78] [1 0.5 0.5] [1 0.9 0.1] [1] [1 1]};
+    uilist = { { 'Style', 'text', 'string', fastif(typeproc, 'Channel number', 'Component number'), 'fontweight', 'bold'  } ...
+			   { 'Style', 'edit', 'string', getdef(lastcom,3,[],'1') } {} ...
+			   { 'Style', 'text', 'string', 'Epoch time range [min max] (msec)', 'fontweight', 'bold', 'tooltipstring', 'Sub epoch time limits' } ...
+			   { 'Style', 'edit', 'string', getdef(lastcom,4,[],[ int2str(EEG.xmin*1000) ' ' int2str(EEG.xmax*1000) ]) }{} ...
+			   { 'Style', 'text', 'string', 'Wavelet cycles (0->FFT, see >> help timef)', 'fontweight', 'bold', 'tooltipstring', context('cycles',vars,txt) } ...
+			   { 'Style', 'edit', 'string', getdef(lastcom,4,[],'3') } {} ...
+			   { 'Style', 'text', 'string',  'Coherence (set)/Phase coherence(unset)', 'fontweight', 'bold', 'tooltipstring', ['Compute linear inter-trial coherence (coher)' 10 'OR inter-trial phase coherence (phasecoher)'] } ...
+			   { 'Style', 'checkbox', 'value', ~getdef(lastcom,'phasecoher','present',1) } { } ...
+			   { 'Style', 'text', 'string', 'Bootstrap significance level (Ex: 0.01 -> 1%)', 'fontweight', 'bold', 'tooltipstring', context('alpha',vars,txt) } ...
+			   { 'Style', 'edit', 'string', getdef(lastcom,'alpha') } {} ...
+			   { 'Style', 'text', 'string', 'Optional timef parameters (see help)', 'fontweight', 'bold', 'tooltipstring', 'See help' } ...
+			   { 'Style', 'edit', 'string', '''padratio'', 4' }, {} ...
+			   {} ...
+			   { 'Style', 'checkbox', 'value', ~getdef(lastcom,'plotersp','present',0), 'string', 'Plot Event Related Spectral Power', 'tooltipstring', 'On the upper pannel' } ...
+			   { 'Style', 'checkbox', 'value', ~getdef(lastcom,'plotitc','present',0), 'string', 'Plot Inter Trial Coherence', 'tooltipstring', 'On the lower pannel' } ...
+			 };
+
+	result = inputgui( geometry, uilist, 'pophelp(''pop_chanedit'');', fastif(typeproc, 'Plot channel time frequency -- pop_timef()', ...
+	                'Plot component time frequency -- pop_timef()'));
 	if length( result ) == 0 return; end;
 
 	num	     = eval( [ '[' result{1} ']' ] ); 
 	tlimits	 = eval( [ '[' result{2} ']' ] ); 
 	cycles	 = eval( [ '[' result{3} ']' ] );
-    switch lower(result{4})
-    	case 'coher',       options = [',''type'', ''coher''' ];
-    	case 'phasecoher',   options = [',''type'', ''phasecoher''' ];
-        otherwise, error('Invalid type of coherence');
+    if result{4}
+    	options = [',''type'', ''coher''' ];
+    else
+		options = [',''type'', ''phasecoher''' ];
     end;	
 	
     % add topoplot
     % ------------
     if ~isempty(EEG.chanlocs)
       if typeproc == 1
-	options = [options ', ''topovec'', ' int2str(num) ', ''elocs'', EEG.chanlocs' ];
+		  options = [options ', ''topovec'', ' int2str(num) ', ''elocs'', EEG.chanlocs' ];
       else
-	options = [options ', ''topovec'', EEG.icawinv(:,' int2str(num) ...
+		  options = [options ', ''topovec'', EEG.icawinv(:,' int2str(num) ...
 		   '), ''elocs'', EEG.chanlocs' ];
       end;
     end;
@@ -128,7 +153,12 @@ if nargin < 3
 	end;
 	if ~isempty( result{6} )
 		  options = [ options ',' result{6} ];
-	else 	
+	end;
+	if ~result{7}
+		options = [ options ', ''plotersp'', ''off''' ];
+	end;
+	if ~result{8}
+		options = [ options ', ''plotitc'', ''off''' ];
 	end;
 	figure;
 else
@@ -193,3 +223,71 @@ com = sprintf('%s timef( tmpsig(:, :), length(pointrange), [tlimits(1) tlimits(2
 eval(com)
 
 return;
+
+% get contextual help
+% -------------------
+function txt = context(var, allvars, alltext);
+	loc = strmatch( var, allvars);
+	if ~isempty(loc)
+		txt= alltext{loc(1)};
+	else
+		disp([ 'warning: variable ''' var ''' not found']);
+		txt = '';
+	end;
+
+% get default from command string
+% -------------------------------
+function txt = getdef(lastcom, var, mode, default)
+	if nargin < 4
+		default = '';
+	end;
+	if isempty(lastcom)
+		txt = default; return;
+	end;
+	if nargin < 3
+		mode = [];
+	end;
+	if isstr(mode) & strcmp('mode', 'present')
+		if ~isempty(findstr(var, lastcom))
+			txt = 1; return;
+		else
+			txt = 0; return;
+		end;
+	end;
+	if isnumeric(var)
+		comas = findstr(lastcom, ',');
+		if length(comas) >= vars
+			txt = lastcom(comas(var)+1:comas(var+1)-1);
+			txt = deblank(txt(end:-1:1));
+			txt = deblank(txt(end:-1:1));
+		else
+			txt = default;
+		end;
+		return;
+	else
+		comas  = findstr(lastcom, ',');
+		varloc = findstr(lastcom, var);
+		if ~isempty(varloc)
+			comas = comas(find(comas >varlocs));
+			txt = lastcom(comas(1)+1:comas(2)-1);
+			txt = deblank(txt(end:-1:1));
+			txt = deblank(txt(end:-1:1));
+			if strcmp(mode, 'full')
+				parent = findstr(lastcom, '}');
+				if ~isempty(parent)
+					comas = comas(find(comas >parent(1)));
+					txt = lastcom(comas(1)+1:comas(2)-1);
+				end;
+				txt = [ '''' var ''', ' txt ];	
+			elseif isnumeric(mode)
+				txt = str2num(txt);
+				if length(txt) >= max(mode)
+					txt = int2str(txt(mode));	
+				else 
+					txt = default;
+				end;
+			end;
+		else
+			txt = default;
+		end;
+	end;
