bdc26c9dc1866dc4b540fc422545c4e0a3da70f3
Arnaud Delorme <arno@ucsd.edu>
2002-04-23 21:45:48 +0000

    making the function standalone ,
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@230 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_saveset.m
+++ functions/popfunc/pop_saveset.m
@@ -1,15 +1,22 @@
 % pop_saveset() - save dataset(s) structure
 %
 % Usage:
-%   >> pop_saveset( EEG, filename, filepath);
-%   >> pop_saveset( indices, filename, filepath);
+%   >> pop_saveset( EEG ); % pop_up
+%   >> pop_saveset( ALLEEG ); % pop_up
+%   >> EEG = pop_saveset( EEG, filename, filepath);
+%   >> ALLEEG = pop_saveset( ALLEEG, indices, filename, filepath);
 %
 % Inputs:
-%   EEG        - data structure
+%   EEG        - dataset structure
+%   ALLEEG     - array of dataset structure
 %   indices    - indices of datasets to save from ALLEEG global
 %                structure
 %   filename   - name of the file to save (optional)
 %   filepath   - path of the file to save (optional)
+%
+% Outputs:
+%   EEG        - dataset after extended syntax check
+%   ALLEEG     - datasets after extended syntax check
 %
 % Note: individual dataset should be save with '.set' file extension
 %       and multiple datasets with '.sets' file extension
@@ -37,6 +44,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.1  2002/04/18 19:57:34  arno
+% Initial revision
+%
 % Revision 1.3  2002/04/11 03:35:48  arno
 % *** empty log message ***
 %
@@ -49,65 +59,70 @@
 
 % 01-25-02 reformated help & license -ad 
 
-function com = pop_saveset( inarg, curfilename, curfilepath);
-eeg_global;
+function [EEG, com] = pop_saveset( EEG, inarg, curfilename, curfilepath);
 
-if nargin > 1 & isnumeric(inarg)
-	indices = inarg;
+if nargin < 1
+	help pop_saveset;
+	return;
+end;
+
+if length(EEG) > 1
+	mode = 1; % multiple datasets
 else
-	indices = [];
-	for index = 1:length(ALLEEG)
-		if ~isempty(ALLEEG(index).data)
-			indices = [indices index];
-		end;
-	end;
+	mode = 0; % single datasets
 end;
 
 if nargin < 2
 	% ask user
-	if nargin < 1 | isnumeric(inarg)
+	if mode == 1 % multiple datasets
+		indices = [];
+		for index = 1:length(EEG)
+			if ~isempty(EEG(index).data)
+				indices = [indices index];
+			end;
+		end;
 		result = inputdlg( {'Indices of the datasets to save'}, 'Save several datasets -- pop_saveset()', 1, {int2str(indices)});
 		if length(result) == 0 return; end;
 		indices = eval( [ '[' result{1} ']' ] );
-	end;
-
-	if length(indices) == 1 | (nargin >= 1 & isstruct(inarg))
-		[curfilename, curfilepath] = uiputfile('*.set', 'Save dataset with .set extension -- pop_saveset()'); 	
-	else 
 		[curfilename, curfilepath] = uiputfile('*.sets', 'Save dataset with .sets extension -- pop_saveset()'); 	
+	else
+		[curfilename, curfilepath] = uiputfile('*.set', 'Save dataset with .set extension -- pop_saveset()'); 
 	end;
-	if curfilename == 0 return; end;
+	if curfilename == 0 return; end;	
+else 
+	if mode == 1
+		indices = inarg;
+	else
+		curfilepath = curfilename;
+		curfilename = inarg;
+	end;
 end;
 
-if (nargin >= 1 & isstruct(inarg)) | length(indices) == 1
-	if nargin >= 1 & isstruct(inarg)
-		EEG = inarg;
-	else
-		if indices < length(ALLEEG.data) & ~isempty(ALLEEG(indices).data)
-			EEG = eeg_retrieve(indices)	
-		else 
-			error('Pop_saveset: index out-of-bound');			
-		end;
-	end;
+if mode == 0  % single datasets
+	disp('Pop_saveset: extended datasets syntax check...');
+	EEG = eeg_checkset(EEG, 'eventconsistency');
 	EEG.filename    = curfilename;
 	EEG.filepath    = curfilepath;
+	tmpica = EEG.icaact;
 	EEG.icaact      = [];
 	disp('Saving dataset...');
 	command = sprintf('save -MAT %s EEG', [ curfilepath curfilename ]);
 	try, eval(command);
 	catch, error('Pop_saveset: saving error, check permission on file or directory');
 	end;
+	EEG.icaact = tmpica;
 	
-	EEG = eeg_checkset(EEG);
-	com = sprintf('pop_saveset( %s, ''%s'', ''%s'');', inputname(1), curfilename, curfilepath);
+	com = sprintf('EEG = pop_saveset( %s, ''%s'', ''%s'');', inputname(1), curfilename, curfilepath);
 else
+	ALLEEG = EEG; clear EEG;
+	
 	if max(indices) > length(ALLEEG)
 		error('Pop_saveset: index out-of-bound');
 	end;
-	
+
 	% checking datasets
 	% -----------------
-	disp('Pop_saveset: checking datasets syntax...');
+	disp('Pop_saveset: extended datasets syntax check...');
 	for index = 1:length(indices)
 		if ~isempty(ALLEEG(indices(index)).data)
 			try, ALLEEG(indices(index)) = eeg_checkset(ALLEEG(indices(index)), 'eventconsistency');
@@ -121,31 +136,26 @@ else
 					disp( ['Warning: dataset ' int2str(indices(index)) ' has non-consistent events. You must fix the problem.']);
 				end;
 			end;
-			ALLEEG(indices(index)).icaact = [];
 		else
 			disp(['Pop_saveset warning: dataset ' int2str(indices(index)) ' is empty']);
 		end;
 	end;
-
+	TMPALLEEG = ALLEEG;
+	for index = 1:length(indices)
+		ALLEEG(indices(index)).icaact = [];
+	end;
+	
 	% saving
 	% ------
 	disp('Pop_saveset: saving datasets...');
-	TMPSAVE = ALLEEG;
 	ALLEEG = ALLEEG(indices);
 	try, save([ curfilepath curfilename ], '-mat', 'ALLEEG');
 	catch, error('Pop_saveset: saving error, check permission on file or directory');
 	end;
-	ALLEEG = TMPSAVE;
-
-	% restoring
-	% ---------
-	for index = 1:length(indices)
-		if ~isempty(ALLEEG(indices(index)).data)
-			ALLEEG(indices(index)) = eeg_checkset(ALLEEG(indices(index)));
-		end;
-	end;
-
-	com = sprintf('pop_saveset( %s, ''%s'', ''%s'');', vararg2str(indices), curfilename, curfilepath);
+	ALLEEG = TMPALLEEG;
+	EEG = ALLEEG;
+	
+	com = sprintf('ALLEEG = pop_saveset( %s, %s, ''%s'', ''%s'');', inputname(1), vararg2str(indices), curfilename, curfilepath);
 end;
 return;
 
