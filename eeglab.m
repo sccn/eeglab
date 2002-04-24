3c566426bae60003d70fc57e163ad7cf8a02d055
Scott Makeig <smakeig@ucsd.edu>
2002-04-24 15:07:20 +0000

    trying background color change -sm
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@239 1e58238c-3a72-4c0f-aabe-36d030380f39

--- eeglab.m
+++ eeglab.m
@@ -176,6 +176,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.31  2002/04/23 23:49:09  arno
+% new embeded update
+%
 % Revision 1.30  2002/04/23 21:34:20  arno
 % updating pop_savesets.m
 %
@@ -293,6 +296,7 @@ else
 	end;
 end;
   
+set(gcf,'defaultcolor',[0.7 0.7 0.8]);
 % checking strings
 % ----------------
 e_try = 'try,';
