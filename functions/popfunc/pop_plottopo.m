d00ddf62dccc0bc0db77f0d0a85b0503129ac74c
Arnaud Delorme <arno@ucsd.edu>
2002-04-24 18:56:29 +0000

    adding vert and others
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@258 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_plottopo.m
+++ functions/popfunc/pop_plottopo.m
@@ -1,7 +1,11 @@
 % pop_plottopo() - plot concatenated multichannel data epochs in a
 %                  topographic array format using plottopo()
 % Usage:
+%   >> pop_plottopo( EEG ); % pop-up
+%   >> pop_plottopo( EEG, channels );
 %   >> pop_plottopo( EEG, channels, title, singletrials);
+%   >> pop_plottopo( EEG, channels, title, singletrials, axsize, ...
+%                         'color', ydir, vert);
 %
 % Inputs:
 %   EEG          - input dataset
@@ -9,6 +13,8 @@
 %   title        - plot title. Default is none.
 %   singletrials - [0|1], 0 plot average, 1 plot individual
 %                  single trials. Default is 0.
+%   others...    - plottopo additional arguments 'axsize', 'color', 'ydir'
+%                  'vert' } (see >> help plottopo)
 %
 % Author: Arnaud Delorme, CNL / Salk Institute, 10 March 2002
 %
@@ -33,6 +39,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.2  2002/04/18 15:45:11  scott
+% editted msgs -sm
+%
 % Revision 1.1  2002/04/05 17:32:13  jorn
 % Initial revision
 %
@@ -42,7 +51,7 @@
 % 03-18-02 added title -ad & sm
 % 03-30-02 added single trial capacities -ad
 
-function com = pop_plottopo( EEG, channels, plottitle, singletrials);
+function com = pop_plottopo( EEG, channels, plottitle, singletrials, varargin);
 
 com = '';
 if nargin < 1
@@ -56,17 +65,28 @@ if isempty(EEG.chanlocs)
 end;
 
 if nargin < 2
-	promptstr    = { 'Channels to plot:' 'Plot title:' 'Plot single trials instead of average (yes|no)' };
-	inistr       = { [ '1:' num2str( EEG.nbchan ) ] ['ERP in scalp order' fastif(isempty(EEG.setname), '',[' of ' EEG.setname])] 'no'};
-    help topoplot;
+	promptstr    = { 'Channels to plot:' ...
+					 'Plot title:' ...
+					 'Plot single trials instead of average (yes|no)' 'Vertical lines (ms)' };
+	inistr       = { [ '1:' num2str( EEG.nbchan ) ] ...
+					 ['ERP in scalp order' fastif(isempty(EEG.setname), '',[' of ' EEG.setname])] ...
+					 'no' ''};
 	result       = inputdlg( promptstr, 'Topographic ERP plot - pop_plottopo()', 1, inistr);
 	if size(result,1) == 0 return; end;
 	channels     = eval( [ '[' result{1} ']' ] );
 	plottitle    = result{2};
 	singletrials = strcmp( lower(result{3}), 'yes');
+	vert         = eval( [ '[' result{4} ']' ] );
     figure;
+	if ~isempty(vert)
+		options ={ EEG.chanlocs, EEG.pnts, [EEG.xmin EEG.xmax 0 0]*1000, plottitle, channels, [.07 .07],0,1,vert };
+	else 
+		options ={ EEG.chanlocs, EEG.pnts, [EEG.xmin EEG.xmax 0 0]*1000, plottitle, channels };
+	end;
+else 
+	options ={ EEG.chanlocs, EEG.pnts, [EEG.xmin EEG.xmax 0 0]*1000, plottitle, channels varargin{:}};
 end;
-
+	
 if exist('plottitle') ~= 1
     plottitle = '';
 end;    
@@ -74,14 +94,21 @@ if exist('singletrials') ~= 1
     singletrials = 0;
 end;    
 
-if singletrials  
-    plottopo( EEG.data, EEG.chanlocs, EEG.pnts, [EEG.xmin EEG.xmax 0 0]*1000, plottitle, channels );
+if singletrials 
+    plottopo( EEG.data, options{:} );
 else
-    plottopo( mean(EEG.data,3), EEG.chanlocs, EEG.pnts, [EEG.xmin EEG.xmax 0 0]*1000, plottitle, channels );
+    plottopo( mean(EEG.data,3), options{:} );
 end;
 set(gcf, 'name', 'ERP in scalp order -- pop_plottopo()');
 
-com = sprintf('figure; pop_plottopo(%s, [%s], ''%s'', %d);', inputname(1), int2str(channels), plottitle, singletrials);
-return;
+if ~isempty(options(6:end))
+	com = sprintf('figure; pop_plottopo(%s, [%s], ''%s'', %d %s);', ...
+				  inputname(1), int2str(channels), plottitle, singletrials, vararg2str(options(6:end)));
+else
+	com = sprintf('figure; pop_plottopo(%s, [%s], ''%s'', %d);', ...
+				  inputname(1), int2str(channels), plottitle, singletrials);
+end;
 
+return;
+	
 		
