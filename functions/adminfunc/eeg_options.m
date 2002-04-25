69bd51a6f8becdb82f53036624fb9a0020f26e83
Arnaud Delorme <arno@ucsd.edu>
2002-04-25 16:58:21 +0000

    updating header
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@284 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/eeg_options.m
+++ functions/adminfunc/eeg_options.m
@@ -1,4 +1,7 @@
-% eeg_options() - eeglab option script
+% eeg_options() - eeglab option script 
+%
+% Note: DO NOT EDIT, instead use pop_editoptions() or the menu
+%       /File/Maximize memory in EEGLAB gui
 
 option_computeica = 1 ;% Compute ICA activations automatically (requires memory)
 option_keepdataset = 1 ;% Don't overwrite parent dataset (requires memory) 
