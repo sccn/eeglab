b0d4ba45dfec078a0820bd27b216f0e1f979c7a9
Arnaud Delorme <arno@ucsd.edu>
2002-07-15 17:28:25 +0000

    editing header
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@615 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/miscfunc/shortread.m
+++ functions/miscfunc/shortread.m
@@ -1,21 +1,44 @@
-%shortread() - Read matrix from short file.
+% shortread() - Read matrix from short file.
 %
-%   >> A = shortread(filename,size,[format],[offset]) 
+% Usage:
+%  >> A = shortread(filename,size,'format',offset) 
 %
-%   Read matrix a from specified
-%   file while assuming four byte short integers. The vector
-%   SIZE determine the number of short elements to be read and the dimensions
-%   of the resulting matrix. If the last element of SIZE is INF the
-%   size of the last dimension is determined by the file length.
+% Inputs:
+%  filename - Read matrix a from specified file while assuming four byte  
+%             short integers. 
+%  size     - The vector SIZE determine the number of short elements to be 
+%             read and the dimensions of the resulting matrix. If the last 
+%             element of SIZE is INF the size of the last dimension is determined 
+%             by the file length.
 %
-%   The option FORMAT argument specifies the storage format as
-%   defined by fopen(). Default format ([]) is 'native'.
+% Optional inputs:
+% 'format'  - The option FORMAT argument specifies the storage format as
+%             defined by fopen(). Default format ([]) is 'native'.
+% offset    - The option OFFSET is offset in shorts from the beginning of file (=0)
+%             to start reading (2-byte shorts assumed).
+%             It uses fseek to read a portion of a large data file.
 %
-%   The option OFFSET is offset in shorts from the beginning of file (=0)
-%   to start reading (2-byte shorts assumed).
-%   It uses fseek to read a portion of a large data file.
+% Author: Sigurd Enghoff, CNL / Salk Institute, La Jolla, 7/1998 
 %
-%   See also floatread(), floatwrite(), fopen.
+% See also: floatread(), floatwrite(), fopen()
+
+% Copyright (C) Sigurd Enghoff, CNL / Salk Institute, La Jolla, 7/1998
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+% $Log: not supported by cvs2svn $
 
 % xx-07-98  written by Sigurd Enghoff, Salk Institute
 % 04-26-99  modified by Sigurd Enghoff to handle variable-sized and
