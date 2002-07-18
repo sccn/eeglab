39107611f12d737f992e838510fc33d009e926fc
Arnaud Delorme <arno@ucsd.edu>
2002-07-18 17:07:40 +0000

    no modif
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@626 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/supergui.m
+++ functions/adminfunc/supergui.m
@@ -59,6 +59,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.1  2002/04/05 17:39:45  jorn
+% Initial revision
+%
 
 function [handlers, outheight, allhandlers] = supergui( geometry, varargin);
 
