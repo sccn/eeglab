b00cdfe10800248c3446bb82b8ae0d24c088ecf2
Arnaud Delorme <arno@ucsd.edu>
2002-07-24 19:19:34 +0000

    debugging
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@676 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_erpimage.m
+++ functions/popfunc/pop_erpimage.m
@@ -78,6 +78,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.27  2002/07/23 21:32:58  arno
+% debugging not same size
+%
 % Revision 1.26  2002/05/01 23:43:05  arno
 % additional test
 %
@@ -522,6 +525,7 @@ com = sprintf('%s erpimage( %s, %s, linspace(EEG.xmin*1000, EEG.xmax*1000, EEG.p
 disp('Command executed by pop_erpimage:');
 disp(' '); disp(com); disp(' ');
 eval(com)
+popcom
 
 if ~popup
 	varargout{1} = [popcom 10 '% Call: ' com];
