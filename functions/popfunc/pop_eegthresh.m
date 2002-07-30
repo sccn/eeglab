8e88047dd6df416e6aa77b89b3aadf36ab6c82fd
Arnaud Delorme <arno@ucsd.edu>
2002-07-30 23:59:35 +0000

    debugging for ICA
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@801 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_eegthresh.m
+++ functions/popfunc/pop_eegthresh.m
@@ -56,6 +56,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.5  2002/07/30 23:26:09  arno
+% new rejection type
+%
 % Revision 1.4  2002/07/26 17:56:04  arno
 % still debugging
 %
@@ -157,7 +160,7 @@ else
         icaacttmp = (EEG.icaweights(elecrange,:)*EEG.icasphere)*reshape(EEG.data, EEG.nbchan, EEG.trials*EEG.pnts);
         icaacttmp = reshape( icaacttmp, length(elecrange), EEG.pnts, EEG.trials);
     end;
-	[Itmp Irej NS Erejtmp] = eegthresh( icaacttmp, 1:length(elecrange), negthresh, posthresh, [EEG.xmin EEG.xmax], starttime, endtime);
+	[Itmp Irej NS Erejtmp] = eegthresh( icaacttmp, EEG.pnts, elecrange, negthresh, posthresh, [EEG.xmin EEG.xmax], starttime, endtime);
     tmpelecIout = zeros(size(EEG.icaweights,1), EEG.trials);
     tmpelecIout(elecrange,Irej) = Erejtmp;
 end;
