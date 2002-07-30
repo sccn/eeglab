38a82f9164935ddf1316d5ffafe0192b3ef2effc
Arnaud Delorme <arno@ucsd.edu>
2002-07-30 00:43:08 +0000

    adding history for pop_editchan
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@770 1e58238c-3a72-4c0f-aabe-36d030380f39

--- eeglab.m
+++ eeglab.m
@@ -181,6 +181,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.96  2002/07/30 00:08:00  arno
+% adding history for crossf
+%
 % Revision 1.95  2002/07/29 23:26:47  arno
 % adding history to timef
 %
@@ -565,7 +568,7 @@ second_m = uimenu( W_MAIN, 'Label', 'Edit');
 	uimenu( second_m, 'Label', 'Event fields'     , 'CallBack', [ checkevent '[EEG LASTCOM] = pop_editeventfield(EEG);' e_store]);
 	uimenu( second_m, 'Label', 'Event values'     , 'CallBack', [ checkevent '[EEG LASTCOM] = pop_editeventvals(EEG);' e_store]);
 	uimenu( second_m, 'Label', 'About this dataset', 'CallBack', [ check      '[EEG.comments LASTCOM] =pop_comments(EEG.comments, ''About this dataset'');' e_store]);
-	uimenu( second_m, 'Label', 'Channel locations'   , 'CallBack', [ '[TMPCHAN LASTCOM] =pop_chanedit(EEG.chanlocs); if ~isempty(LASTCOM), EEG.chanlocs = TMPCHAN; clear TMPCHAN;' storecall 'end; eeglab(''redraw'');']);
+	uimenu( second_m, 'Label', 'Channel locations'   , 'CallBack', [ '[TMPCHAN LASTCOM] =pop_chanedit(EEG.chanlocs); if ~isempty(LASTCOM), EEG.chanlocs = TMPCHAN; clear TMPCHAN; h(LASTCOM);' storecall 'end; eeglab(''redraw'');']);
 	uimenu( second_m, 'Label', 'Select data'           , 'CallBack', [ check      '[EEG LASTCOM] = pop_select(EEG);' e_newset], 'Separator', 'on');
 	uimenu( second_m, 'Label', 'Select events'         , 'CallBack', [ checkevent '[EEG TMP LASTCOM] = pop_selectevent(EEG); clear TMP;' e_newset ]);
 	uimenu( second_m, 'Label', 'Copy current dataset'  , 'CallBack', [ check      '[ALLEEG LASTCOM] = pop_copyset; h(LASTCOM); eeglab(''redraw'');' e_hist], 'Separator', 'on');
