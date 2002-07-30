9f1bbd19ead32bea0edf199f525bce66bc335cb3
Arnaud Delorme <arno@ucsd.edu>
2002-07-30 00:21:26 +0000

    new gui and history
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@767 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_newcrossf.m
+++ functions/popfunc/pop_newcrossf.m
@@ -43,6 +43,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.12  2002/05/03 00:35:47  arno
+% further check
+%
 % Revision 1.11  2002/04/25 02:54:18  arno
 % debugging topovec
 %
@@ -89,63 +92,100 @@ if nargin < 2
 	help pop_crossf;
 	return;
 end;	
+lastcom = [];
+if nargin < 3
+	popup = 1;
+else
+	popup = isstr(num1) | isempty(num1);
+	if isstr(num1)
+		lastcom = num1;
+	end;
+end;
 
 % pop up window
 % -------------
-if nargin < 3
-	promptstr    = { fastif(typeproc, 'First channel number:', 'First component number:') ...
-					         fastif(typeproc, 'Second channel number:', 'Second component number:') ...
-					         'Epoch time range [min max] (msec)' ...
-	                 'Wavelet cycles (0->FFT, see >> help crossf)' ...
-	                 'Compute coherence (coher) or phase locking factor (phasecoher)' ...
-	                 'Compute bootstrap significance level (Ex: 0.01 -> 1%)' ...
-	                 'Optional crossf parameters (see >> help crossf)' };
-	inistr       = {  '1' '2' ...
-					          [ int2str(EEG.xmin*1000) ' ' int2str(EEG.xmax*1000) ] ...
-					          '0' ...
-					          'phasecoher' '' '''padratio'', 4' };
-    titlegui = fastif(typeproc, 'Plot channel cross-coherence -- pop_crossf()', ...
-                  'Plot component cross-coherence -- pop_crossf()');
-	result       = inputdlg( promptstr, titlegui, 1,  inistr );
+if popup
+	[txt vars] = gethelpvar('timef.m');
+	
+	geometry = { [1 0.5 0.5] [1 0.5 0.5] [1 0.5 0.5] [1 0.5 0.5] [0.92 0.1 0.78] [1 0.5 0.5] [1 0.8 0.2] [1] [1 1]};
+    uilist = { { 'Style', 'text', 'string', fastif(typeproc, 'First channel number', 'First component number'), 'fontweight', 'bold'  } ...
+			   { 'Style', 'edit', 'string', getkeyval(lastcom,3,[],'1') } {} ...
+			   { 'Style', 'text', 'string', fastif(typeproc, 'Second channel number', 'First component number'), 'fontweight', 'bold'  } ...
+			   { 'Style', 'edit', 'string', getkeyval(lastcom,4,[],'2') } {} ...
+			   { 'Style', 'text', 'string', 'Epoch time range [min max] (msec)', 'fontweight', 'bold', ...
+				 'tooltipstring', 'Sub epoch time limits' } ...
+			   { 'Style', 'edit', 'string', getkeyval(lastcom,5,[],[ int2str(EEG.xmin*1000) ' ' int2str(EEG.xmax*1000) ]) } {} ...
+			   { 'Style', 'text', 'string', 'Wavelet cycles (0->FFT, see >> help timef)', 'fontweight', 'bold', ...
+				 'tooltipstring', context('cycles',vars,txt) } ...
+			   { 'Style', 'edit', 'string', getkeyval(lastcom,6,[],'3') } {} ...
+			   { 'Style', 'text', 'string',  '[set]->Linear coher / [unset]->Phase coher', 'fontweight', 'bold', ...
+				 'tooltipstring', ['Compute linear inter-trial coherence (coher)' 10 ...
+					'OR Amplitude-normalized inter-trial phase coherence (phasecoher)'] } ...
+			   { 'Style', 'checkbox', 'value', ~getkeyval(lastcom,'phasecoher','present',1) } { } ...
+			   { 'Style', 'text', 'string', 'Bootstrap significance level (Ex: 0.01 -> 1%)', 'fontweight', 'bold', ...
+				 'tooltipstring', context('alpha',vars,txt) } ...
+			   { 'Style', 'edit', 'string', getkeyval(lastcom,'alpha') } {} ...
+			   { 'Style', 'text', 'string', 'Optional timef() arguments (see Help)', 'fontweight', 'bold', ...
+				 'tooltipstring', 'See crossf() help via the Help button on the right...' } ...
+			   { 'Style', 'edit', 'string', '''padratio'', 4' } ...
+			   { 'Style', 'pushbutton', 'string', 'Help', 'callback',  'pophelp(''crossf'');' } ...
+			   {} ...
+			   { 'Style', 'checkbox', 'value', ~getkeyval(lastcom,'plotersp','present',0), 'string', ...
+				 'Plot coherence amplitude', 'tooltipstring', ...
+				 'Plot coherence ampltitude image in the upper panel' } ...
+			   { 'Style', 'checkbox', 'value', ~getkeyval(lastcom,'plotitc','present',0), 'string', ...
+				 'Plot coherence phase', 'tooltipstring', ...
+				 'Plot coherence phase image in the lower panel' } ...
+			 };
+
+	result = inputgui( geometry, uilist, 'pophelp(''pop_crossf'');', ...
+					   fastif(typeproc, 'Plot channel cross-coherence -- pop_crossf()', ...
+							  'Plot component cross-coherence -- pop_crossf()'));
 	if length( result ) == 0 return; end;
 
-	num1	     = eval( [ '[' result{1} ']' ] ); 
-	num2	     = eval( [ '[' result{2} ']' ] ); 
-	tlimits	     = eval( [ '[' result{3} ']' ] ); 
-	cycles	     = eval( [ '[' result{4} ']' ] );
-    switch lower(result{5})
-    	case 'coher',       options = [',''type'', ''coher''' ];
-    	case 'phasecoher',   options = [',''type'', ''phasecoher''' ];
-        otherwise, error('Invalid type of coherence');
+	num1     = eval( [ '[' result{1} ']' ] ); 
+	num2     = eval( [ '[' result{2} ']' ] ); 
+	tlimits	 = eval( [ '[' result{3} ']' ] ); 
+	cycles	 = eval( [ '[' result{4} ']' ] );
+    if result{5}
+    	options = [',''type'', ''coher''' ];
+    else
+		options = [',''type'', ''phasecoher''' ];
     end;	
-
-   % add title
-   % ---------
-   if isempty( strmatch(  '''title''', result{7}))
-	   switch lower(result{5})
-		case 'coher', options = [options ', ''title'',' fastif(typeproc, '''Channel ', '''Component ') int2str(num1) '-' int2str(num2) ...
+	
+    % add topoplot
+    % ------------
+	if ~isempty(EEG.chanlocs)
+		if typeproc == 1
+			options = [options ', ''topovec'', [' int2str([num1 num2]) '], ''elocs'', EEG.chanlocs' ];
+		else % typeproc == 0
+			options = [options ', ''topovec'', EEG.icawinv(:, [' int2str([num1 num2]) '])'', ''elocs'', EEG.chanlocs' ];
+		end;
+	end;
+    
+    % add title
+    % ---------
+	if isempty( strmatch(  '''title''', result{7}))
+		switch lower(result{5})
+		 case 'coher', options = [options ', ''title'',' fastif(typeproc, '''Channel ', '''Component ') int2str(num1) '-' int2str(num2) ...
 					' Coherence' fastif(~isempty(EEG.setname), [' (' EEG.setname ')'''], '''')];
-		otherwise, options = [options ', ''title'',' fastif(typeproc, '''Channel ', '''Component ') int2str(num1) '-' int2str(num2) ...
+		 otherwise, options = [options ', ''title'',' fastif(typeproc, '''Channel ', '''Component ') int2str(num1) '-' int2str(num2) ...
 					' Phase Coherence' fastif(~isempty(EEG.setname), [' (' EEG.setname ')'''],'''') ];
-	   end;
-   end;
-   if ~isempty( result{6} )
-	   options      = [ options ', ''alpha'',' result{6} ];
-   end;
-   if ~isempty( result{7} )
-	   options = [ options ',' result{7} ];
-   end;
-   % add topoplot
-   % ------------
-   if ~isempty(EEG.chanlocs)
-	   if typeproc == 1
-		   options = [options ', ''topovec'', [' int2str([num1 num2]) '], ''elocs'', EEG.chanlocs' ];
-	   else % typeproc == 0
-		   options = [options ', ''topovec'', EEG.icawinv(:, [' int2str([num1 num2]) '])'', ''elocs'', EEG.chanlocs' ];
-	   end;
-   end;
-
-   figure;
+		end;
+	end;
+	if ~isempty( result{6} )
+		options      = [ options ', ''alpha'',' result{6} ];
+	end;
+	if ~isempty( result{7} )
+		  options = [ options ',' result{7} ];
+	end;
+	if ~result{8}
+		options = [ options ', ''plotersp'', ''off''' ];
+	end;
+	if ~result{9}
+		options = [ options ', ''plotitc'', ''off''' ];
+	end;
+	figure;
 else
 	options = [];
 	for i=1:length( varargin )
@@ -196,7 +236,7 @@ tmpsig2 = reshape( tmpsig2, 1, size(tmpsig2,2)*size(tmpsig2,3));
 % outputs
 % -------
 outstr = '';
-if nargin >= 3
+if ~popup
     for io = 1:nargout, outstr = [outstr 'varargout{' int2str(io) '},' ]; end;
     if ~isempty(outstr), outstr = [ '[' outstr(1:end-1) '] =' ]; end;
 end;
@@ -208,10 +248,19 @@ if length( options ) < 2
 end;
 varargout{1} = sprintf('figure; pop_crossf( %s, %d, %d, %d, [%s], %d %s);', ...
           inputname(1), typeproc, num1, num2, int2str(tlimits), cycles, options);
-com = sprintf(...
-    '%s crossf( tmpsig1, tmpsig2, length(pointrange), [tlimits(1) tlimits(2)], EEG.srate, cycles %s);',... 
+com = sprintf( '%s crossf( tmpsig1, tmpsig2, length(pointrange), [tlimits(1) tlimits(2)], EEG.srate, cycles %s);',... 
           outstr, options);
-com
 eval(com)
 
 return;
+
+% get contextual help
+% -------------------
+function txt = context(var, allvars, alltext);
+	loc = strmatch( var, allvars);
+	if ~isempty(loc)
+		txt= alltext{loc(1)};
+	else
+		disp([ 'warning: variable ''' var ''' not found']);
+		txt = '';
+	end;
