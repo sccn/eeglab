04103da3e4c72613f75a2850bd55cf71cd39ada2
Arnaud Delorme <arno@ucsd.edu>
2002-08-09 22:21:52 +0000

    implementing 0 zero phase
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@892 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/timefreqfunc/dftfilt.m
+++ functions/timefreqfunc/dftfilt.m
@@ -10,7 +10,8 @@
 %   k - oversampling
 %   q - [0;1] 0->fft, 1->c cycles
 %
-% Authors: Sigurd Enghoff & Scott Makeig, SCCN/INC/UCSD, La Jolla, 8/1/98
+% Authors: Sigurd Enghoff, Arnaud Delorme & Scott Makeig, 
+%          SCCN/INC/UCSD, La Jolla, 8/1/98
 
 % Copyright (C) 8/1/98 Sigurd Enghoff & Scott Makei, SCCN/INC/UCSD, scott@sccn.ucsd.edu
 %
@@ -29,18 +30,38 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.2  2002/08/09 18:59:45  arno
+% updating var anmes
+%
 % Revision 1.1  2002/04/05 17:36:45  jorn
 % Initial revision
 %
 
 % 01-25-02 reformated help & license -ad 
 
+% future developments
+% -------------------
+% input into dftfilt:
+% - lowfreq and maxfreq (of interest)
+% - lowcycle and maxcyle (ex: 3 cycles at low freq and 10 cycles at maxfreq)
+% - the delta in frequency: ex 0.5 Hz
+% The function should: compute the number of points (len) automatically
+% Warning with FFT compatibility
+% Still, something has to be done about the masking so that it would be comaptible
+
 function b = dftfilt(len,maxfreq,cycle,oversmp,wavfact)
 
-srate = 2*pi/len;						    % Angular increment.
-w = j * cycle * [0:srate:2*pi-srate/2]';	% Column.
-x = 1:1/oversmp:maxfreq*len/cycle;		    % Row.
-b = exp(-w*x);					            % Exponentiation of outer product.
+count = 1;
+for index = 1:1/oversmp:maxfreq*len/cycle % scan frequencies
+	w(:,count) = j * index * cycle * linspace(-pi+2*pi/len, pi-2*pi/len, len)'; % exp(-w) is a sinus curve
+	count = count+1; % -2*pi/len ensures that we really scan from -pi to pi without redundance (-pi=+pi) 
+end;
+b = exp(-w);
+
+%srate = 2*pi/len;						    % Angular increment.
+%w = j * cycle * [0:srate:2*pi-srate/2]';	% Column.
+%x = 1:1/oversmp:maxfreq*len/cycle;		    % Row.
+%b = exp(-w*x);					            % Exponentiation of outer product.
 
 for i = 1:size(b,2),
 	m  = round(wavfact*len*(i-1)/(i+oversmp-1));	% Number of elements to discard.
