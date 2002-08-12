f4fc77b79d1b9b6ec3c953d9811eeb84d8e2a575
Arnaud Delorme <arno@ucsd.edu>
2002-08-12 18:57:14 +0000

    Initial revision
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@990 1e58238c-3a72-4c0f-aabe-36d030380f39

---
+++ functions/adminfunc/warndlg2.m
@@ -0,0 +1,32 @@
+% warndlg2() - same as warndlg for eeglab()
+%
+% Author: Arnaud Delorme, CNL / Salk Institute, 12 August 2002
+%
+% See also: inpudlg2(), quesdlg2()
+
+%123456789012345678901234567890123456789012345678901234567890123456789012
+
+% Copyright (C) Arnaud Delorme, CNL / Salk Institute, arno@salk.edu
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+% $Log: not supported by cvs2svn $
+
+function warndlg2(Prompt, Title);
+
+if nargin <2
+	Title = 'Warning';
+end;
+questdlg2(Prompt, Title, 'OK', 'OK');
