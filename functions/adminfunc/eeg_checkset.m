37fb50d39fa37fcb2f1f4225f57b5d6e425910d6
Arnaud Delorme <arno@ucsd.edu>
2002-08-12 00:05:44 +0000

    changing manual color
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@923 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/eeg_checkset.m
+++ functions/adminfunc/eeg_checkset.m
@@ -91,6 +91,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.50  2002/08/09 00:58:38  arno
+% text
+%
 % Revision 1.49  2002/08/09 00:39:18  arno
 % debugging epoch
 %
@@ -558,7 +561,7 @@ if ~isfield(EEG.reject, 'rejglobalE')		EEG.reject.rejglobalE = []; res = com; en
 
 % default colors for rejection
 % ----------------------------
-if ~isfield(EEG.reject, 'rejmanualcol')		EEG.reject.rejmanualcol = [1.0000    0.9116    0.6464]; res = com; end;
+if ~isfield(EEG.reject, 'rejmanualcol')		EEG.reject.rejmanualcol = [1.0000    0.99      0.587]; res = com; end;
 if ~isfield(EEG.reject, 'rejthreshcol')		EEG.reject.rejthreshcol = [0.8487    1.0000    0.5008]; res = com; end;
 if ~isfield(EEG.reject, 'rejconstcol')		EEG.reject.rejconstcol  = [0.6940    1.0000    0.7008]; res = com; end;
 if ~isfield(EEG.reject, 'rejjpcol')		    EEG.reject.rejjpcol     = [1.0000    0.6991    0.7537]; res = com; end;
