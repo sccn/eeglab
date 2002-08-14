108ab8e2ecfd28d3da44a633a7f137571a24ad4c
Arnaud Delorme <arno@ucsd.edu>
2002-08-14 00:11:28 +0000

    update header
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@1078 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_saveset.m
+++ functions/popfunc/pop_saveset.m
@@ -9,7 +9,7 @@
 % Inputs:
 %   EEG        - dataset structure
 %   ALLEEG     - array of dataset structure
-%   indices    - indices of datasets to save from ALLEEG global
+%   indices    - indices of datasets to save from ALLEEG
 %                structure
 %   filename   - name of the file to save (optional)
 %   filepath   - path of the file to save (optional)
@@ -44,6 +44,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.5  2002/08/14 00:06:16  arno
+% empty command as default
+%
 % Revision 1.4  2002/08/12 18:34:13  arno
 % questdlg2
 %
