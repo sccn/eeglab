f2a2bd73b76994348bf732eb61d5742bd241c6d6
Arnaud Delorme <arno@ucsd.edu>
2002-08-14 21:21:02 +0000

    debug for windows
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@1106 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/miscfunc/textgui.m
+++ functions/miscfunc/textgui.m
@@ -45,6 +45,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.5  2002/08/14 21:20:22  arno
+% debug for windows
+%
 % Revision 1.4  2002/04/06 02:00:08  arno
 % correcting char size for times font
 %
@@ -83,11 +86,11 @@ if nargin >2
 else
     g = [];
 end;
-try, g.title;    catch g.title = ''; end;    
-try, g.fontname; catch g.fontname = 'courier'; end;    
-try, g.fontsize; catch g.fontsize = 12; end;    
-try, g.fontweight; catch g.fontweight = 'normal'; end;    
-try, g.linesperpage; catch g.linesperpage = 20; end;    
+try, g.title;    catch, g.title = ''; end;    
+try, g.fontname; catch, g.fontname = 'courier'; end;    
+try, g.fontsize; catch, g.fontsize = 12; end;    
+try, g.fontweight; catch, g.fontweight = 'normal'; end;    
+try, g.linesperpage; catch, g.linesperpage = 20; end;    
 
 if isempty( helparray )
 	helparray = cell(1,200);
