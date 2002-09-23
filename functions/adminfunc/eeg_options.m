1500fb883166f766222bfd09f80e7cd0b780d291
Arnaud Delorme <arno@ucsd.edu>
2002-09-23 16:10:12 +0000

    adding save as float option
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@1356 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/eeg_options.m
+++ functions/adminfunc/eeg_options.m
@@ -3,5 +3,6 @@
 % Note: DO NOT EDIT, instead use pop_editoptions() or the menu
 %       /File/Maximize memory in EEGLAB gui
 
-option_computeica = 1 ;% Precompute ICA activations (requires memory) 
-option_keepdataset = 1 ;% Don't overwrite parent dataset (requires memory) 
+option_computeica = 1 ;% Precompute ICA activations (requires mem
+option_keepdataset = 1 ;% Keep parent dataset (requires memory) 
+option_savefloat = 0 ;% Use a single Matlab file for saving datasets (requires disk space) 
