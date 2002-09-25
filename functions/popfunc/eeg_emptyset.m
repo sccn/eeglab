8e5c4817152dda44d49fc4d0f5ddbd1b764bfac7
Arnaud Delorme <arno@ucsd.edu>
2002-09-25 23:52:41 +0000

    average referece
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@1370 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/eeg_emptyset.m
+++ functions/popfunc/eeg_emptyset.m
@@ -29,6 +29,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.4  2002/08/08 18:08:57  arno
+% removing rt
+%
 % Revision 1.3  2002/06/25 14:37:26  arno
 % header problem fix
 %
@@ -61,6 +64,7 @@ EEG.event     = [];
 EEG.epoch  = [];
 EEG.chanlocs    = '';
 EEG.comments    = '';
+EEG.averef      = 'no';
 
 %EEG.reject.threshold  = [1 0.8 0.85];
 %EEG.reject.icareject  = [];
