1783e00f5f9c9cc752cf9a6a9c0ca2c89328a3f2
Scott Makeig <smakeig@ucsd.edu>
2002-09-26 13:53:34 +0000

    edited comments -sm
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@1372 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/eeg_options.m
+++ functions/adminfunc/eeg_options.m
@@ -3,6 +3,6 @@
 % Note: DO NOT EDIT, instead use pop_editoptions() or the menu
 %       /File/Maximize memory in EEGLAB gui
 
-option_computeica = 1 ;% Precompute ICA activations (requires mem
-option_keepdataset = 1 ;% Keep parent dataset (requires memory) 
-option_savematlab = 0 ;% Use a single Matlab file for saving datasets (requires disk space) 
+option_computeica = 1 ; % Precompute ICA activations (uses RAM)
+option_keepdataset = 1 ;% Retain parent datasets (uses RAM) 
+option_savematlab = 0 ; % Store data in the .mat file (uses disk space)
