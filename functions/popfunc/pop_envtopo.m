a79b42f5e7adf0b61b08287d25d980e709c49cfb
Arnaud Delorme <arno@ucsd.edu>
2002-10-09 22:00:53 +0000

    upodating for new envtopo
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@1419 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_envtopo.m
+++ functions/popfunc/pop_envtopo.m
@@ -2,15 +2,11 @@
 %                 of largest or specified components reference to their 
 %                 time point maximum amplitude. Calls envtopo().
 % Usage:
-%   >> pop_envtopo( EEG, timerange, compnums, title, 'key', 'val', ...);
+%   >> pop_envtopo( EEG, 'key', 'val', ...);
 %
 % Inputs:
 %   EEG        - input dataset
 %   timerange  - [min max] time range (in msec) to plot 
-%   compnums   - vector of component numbers to plot {default|0 -> all}
-%                ELSE n<0, plot the number of comp. maps with largest 
-%                projected variance in the plotted epoch. {default|[]-> 7}
-%   title      - plot title
 %
 % Optional inputs:
 %   'key','val' - optional spectopo() and topoplot() arguments 
@@ -42,6 +38,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.11  2002/10/08 15:55:04  arno
+% nothing
+%
 % Revision 1.10  2002/10/05 01:52:51  arno
 % adapt to new syntax (but still using old convension)
 %
@@ -77,7 +76,7 @@
 % 03-16-02 add all topoplot options -ad
 % 03-18-02 added title -ad & sm
 
-function varargout = pop_envtopo( EEG, timerange, compnums, envtitle, varargin);
+function varargout = pop_envtopo( EEG, timerange, varargin);
 
 varargout{1} = '';
 if nargin < 1
@@ -98,64 +97,59 @@ end;
 if nargin < 3
 	% which set to save
 	% -----------------
-	promptstr    = { 'Enter the time range (msec):', ...
+	promptstr    = { 'Enter plotting time range (msec):', ...
+					 'Enter time range for component contribution (msec):', ...
 					 'Plot this many largest components (1-7):', ...
 					 'ELSE plot these components only (<=7) (Ex: 2:4,7 9:11):', ...
+                     'Indices of component to subtract from data before plotting' ...
 					 'Plot title:' ...
-			         'Scalp map text options: See >> help topoplot' };
+			         'Additional spectopo() and topoplot() options:' };
 	inistr       = { [num2str( EEG.xmin*1000) ' ' num2str(EEG.xmax*1000)], ...
-	                 '7', ...
+	                 [num2str( EEG.xmin*1000) ' ' num2str(EEG.xmax*1000)], ...
+                     '7', ...
 	                 '', ...
+                     '', ...
 	                 ['Largest ERP components' fastif(isempty(EEG.setname), '',[' of ' EEG.setname])] ...
 	                 '''electrodes'',''off''' };
 	result       = inputdlg2( promptstr, 'Components and ERP envelope -- pop_envtopo()', 1, inistr, 'pop_envtopo');
-	size_result  = size( result );
-	if size_result(1) == 0 return; end;
-	timerange    = eval( [ '[' result{1} ']' ] );
-	compnums     = - eval( [ '[' result{2} ']' ] );
-	if ~isempty( result{3} ), compnums = eval( [ '[' result{3} ']' ] ); end;
-	envtitle     = result{4};
-	options      =  [ ',' result{5} ];
+	if length(result) == 0 return; end;
+
+    timerange    = eval( [ '[' result{1} ']' ] );
+    options = ',';
+	if ~isempty( result{2} ), options = [ options '''limcontrib'',  [' result{2} '],' ]; end;
+	if ~isempty( result{4} ), options = [ options '''compnums'',  [' result{4} '],' ]; 
+    else                      options = [ options '''compnums'',   -' result{3} ',' ];
+    end;
+    if ~isempty(result{5}),   options = [ options '''subcomps'',   [' result{5} '],' ]; end;
+    if ~isempty(result{6}),   options = [ options '''title'', ''' result{6} ''',' ]; end;
+	options      =  [ options result{7} ];
 	figure;
+    try, icadefs; set(gcf, 'color', BACKCOLOR); catch, end;
 else
-	options = [];
-	for i=1:length( varargin )
-		if isstr( varargin{ i } )
-			options = [ options ', ''' varargin{i} '''' ];
-		else
-			options = [ options ', [' num2str(varargin{i}) ']' ];
-		end;
-	end;	
+	options = [',' vararg2str( varargin ) ];
 end;
-try, icadefs; set(gcf, 'color', BACKCOLOR); catch, end;
+    
+sigtmp = reshape(EEG.data, EEG.nbchan, EEG.pnts, EEG.trials);
+posi = round( (timerange(1)/1000-EEG.xmin) * EEG.srate) + 1;
+posf = round( (timerange(2)/1000-EEG.xmin) * EEG.srate) + 1;
 
-if ~isempty(EEG.chanlocs)
-	sigtmp = reshape(EEG.data, EEG.nbchan, EEG.pnts, EEG.trials);
-	posi = round( (timerange(1)/1000-EEG.xmin) * EEG.srate) + 1;
-	posf = round( (timerange(2)/1000-EEG.xmin) * EEG.srate) + 1;
-
-	% outputs
-	% -------
-	outstr = '';
-	if nargin >= 4
-	    for io = 1:nargout, outstr = [outstr 'varargout{' int2str(io) '},' ]; end;
-	    if ~isempty(outstr), outstr = [ '[' outstr(1:end-1) '] =' ]; end;
-	end;
-
-	% plot the datas and generate output command
-	% --------------------------------------------
-	if length( options ) < 2
-	    options = '';
-	end;
-	varargout{1} = sprintf('figure; pop_envtopo(%s, [%s], [%s], ''%s'' %s);', inputname(1), num2str(timerange), num2str(compnums), envtitle, options);
-	popcom = sprintf('figure; pop_envtopo(%s, [%s], [%s], ''%s'' %s);', inputname(1), num2str(timerange), num2str(compnums), envtitle, options);
-	com =  sprintf('%s envtopo(mean(sigtmp(:,posi:posf,:),3), EEG.icaweights*EEG.icasphere, EEG.chanlocs, [timerange(1) timerange(2) 0 0], compnums, envtitle, [], [], [], 0, [] %s);', outstr, options);
-	eval(com)
-        varargout{1} = [ 10 popcom 10 '% ' com];
-else
-	fprintf('Cannot plot without knowing channel locations. Use Edit/Dataset info.\n');
-	return;
+% outputs
+% -------
+outstr = '';
+if nargin >= 4
+    for io = 1:nargout, outstr = [outstr 'varargout{' int2str(io) '},' ]; end;
+    if ~isempty(outstr), outstr = [ '[' outstr(1:end-1) '] =' ]; end;
 end;
+
+% plot the datas and generate output command
+% --------------------------------------------
+if length( options ) < 2, options = ''; end;
+varargout{1} = sprintf('figure; pop_envtopo(%s, [%s] %s);', inputname(1), num2str(timerange), ...
+                       options);
+com =  sprintf(['%s envtopo(mean(sigtmp(:,posi:posf,:),3), EEG.icaweights*EEG.icasphere, ' ...
+                '''chanlocs'', EEG.chanlocs, ''icawinv'', EEG.icawinv, ''limits'',' ...
+                '[timerange(1) timerange(2) 0 0] %s);' ] , outstr, options);
+eval(com);
 
 return;
 
