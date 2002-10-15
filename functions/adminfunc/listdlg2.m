bcb0380cdc31bd4544999cd868aa823b15990625
Arnaud Delorme <arno@ucsd.edu>
2002-10-15 17:24:13 +0000

    same
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@1497 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/listdlg2.m
+++ functions/adminfunc/listdlg2.m
@@ -26,6 +26,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.3  2002/10/15 16:56:45  arno
+% drawnow for windows
+%
 % Revision 1.2  2002/08/21 18:13:37  arno
 % debug for one element only
 %
@@ -93,7 +96,6 @@ okornot = 0;
 strval = '';
 vals = [];
 waitfor( fig, 'userdata');
-drawnow;
 try,
 	vals = get(h, 'value');
 	strval = '';
@@ -107,4 +109,5 @@ try,
 		okornot = 1;
 	end;
 	close(fig);
+    drawnow;
 end;
