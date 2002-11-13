d58436b32b62a232de81e17e867ce392342a5316
Arnaud Delorme <arno@ucsd.edu>
2002-11-13 17:57:03 +0000

    tutorial link update
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@1721 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/icadefs.m
+++ functions/sigprocfunc/icadefs.m
@@ -22,6 +22,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.14  2002/08/18 23:54:06  arno
+% changing ica executable
+%
 % Revision 1.13  2002/08/14 16:58:59  arno
 % update for new release
 %
@@ -72,7 +75,7 @@
 % -------------- EEGLAB DEFINITION (V 4.0) -------------
 % ------------------------------------------------------
 
-TUTORIAL_URL = 'http://sccn.ucsd.edu/eeglab/eeglab.html'; % online version
+TUTORIAL_URL = 'http://sccn.ucsd.edu/eeglab/eeglabdocs.html'; % online version
 
 ICABINARY = 'ica_linux2.4'; % <=INSERT name of ica executable for binica.m
 
