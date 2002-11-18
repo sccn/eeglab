b7c82cba369c2a39b8626f1b4c69e2ac8cff5635
Arnaud Delorme <arno@ucsd.edu>
2002-11-18 02:47:23 +0000

    adding continuous data baseline subtraction
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@1802 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_rmbase.m
+++ functions/popfunc/pop_rmbase.m
@@ -1,4 +1,5 @@
-% pop_rmbase() - remove baseline from an epoched dataset.
+% pop_rmbase() - remove baseline from an epoched or continuous
+%                eeg dataset.
 %
 % Usage:
 %   >> outeeg = pop_rmbase( eeg, timerange, pointrange);
@@ -6,11 +7,15 @@
 % Inputs:
 %   eeg        - Input dataset
 %   timerange  - Baseline time range [min_ms max_ms]. 
-%   pointrange - Baseline points range [min max]. 
+%   pointrange - Baseline points vector [min:max]. 
 %                (Overwritten by time range).
 %
 % Outputs:
 %   outeeg     - Output dataset
+%
+% Note: in the case of a continuous dataset, the baseline is removed
+%       seperatelly for the non-continuous regions (for instance if
+%       data was removed).
 %
 % Author: Arnaud Delorme, CNL / Salk Institute, 2001
 %
@@ -35,6 +40,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.5  2002/10/11 14:48:20  arno
+% recomputing ICA activations
+%
 % Revision 1.4  2002/08/17 20:26:22  scott
 % menu and help msg
 %
@@ -53,6 +61,10 @@
 function [EEG, com] = pop_rmbase( EEG, timerange, pointrange);
 
 com ='';
+if nargin < 1
+    help pop_rmbase;
+    return;
+end;
 if isempty(EEG.data)
     disp('pop_rmbase(): cannot remove baseline of an empty dataset'); return;
 end;    
@@ -60,11 +72,11 @@ if nargin < 1
 	help pop_rmbase;
 	return;
 end;	
-if nargin < 2
+if nargin < 2 & EEG.trials > 1
 	% popup window parameters
 	% -----------------------
-   promptstr    = {'Baseline time range (min_ms max_ms):',...
-         		   strvcat('Baseline points range (ex:1:56):', '(Overwritten by time limits).') };
+    promptstr    = {'Baseline time range (min_ms max_ms):',...
+         		   strvcat('Baseline points vector (ex:1:56):', '(Overwritten by time limits).') };
 	inistr       = { [num2str(EEG.xmin*1000) ' 0'], '' };
 	result       = inputdlg2( promptstr, 'Epoch baseline removal -- pop_rmbase()', 1,  inistr, 'pop_rmbase');
 	size_result  = size( result );
@@ -75,6 +87,14 @@ if nargin < 2
 	if isempty( result{1} ) & isempty( result{2} ), return; end;
     timerange = eval( [ '[' result{1} ']' ] );
     pointrange = eval( [ '[' result{2} ']' ] );
+elseif nargin < 2 & EEG.trials == 1
+	% popup window parameters
+	% -----------------------
+    resp = questdlg2(strvcat('Remove baseline for all data (separatelly for', ...
+                     'each portion of continuous data)'), 'pop_rmbase', 'Cancel', 'Ok', 'Ok');
+    if strcmpi(resp, 'Cancel'), return; end;
+    timerange = [];
+    pointrange = [1:EEG.pnts];
 end;
 
 if ~isempty(timerange)
@@ -89,7 +109,27 @@ if (min(pointrange) < 1) | (max( pointrange ) > EEG.pnts)
 end;
 
 fprintf('pop_rmbase(): Removing baseline...\n');
-EEG.data = rmbase( EEG.data(:,:), EEG.pnts, pointrange );
+% add boundaries if continuous data
+% ----------------------------------
+if EEG.trials == 1 & ~isempty(EEG.event) & isfield(EEG.event, 'type') & isstr(EEG.event(1).type)
+	boundaries = strmatch('boundary', {EEG.event.type});
+	if ~isempty(boundaries)
+        fprintf('Pop_rmbase: finding continuous data discontinuities\n');
+        boundaries = cell2mat({EEG.event(boundaries).latency})-0.5-pointrange(1)+1;
+        boundaries(find(boundaries>=pointrange(end)-pointrange(1))) = [];
+        boundaries(find(boundaries<1)) = [];
+        boundaries = [0 boundaries pointrange(end)-pointrange(1)];
+        for index=1:length(boundaries)-1
+            tmprange = [boundaries(index)+1:boundaries(index+1)];
+            EEG.data(:,tmprange) = rmbase( EEG.data(:,tmprange), length(tmprange), [1:length(tmprange)]);
+        end;
+    else
+        EEG.data = rmbase( EEG.data(:,:), EEG.pnts, pointrange );    
+    end;		
+else 
+    EEG.data = rmbase( EEG.data(:,:), EEG.pnts, pointrange );
+end;
+
 EEG.data = reshape( EEG.data, EEG.nbchan, EEG.pnts, EEG.trials);
 EEG.icaact = [];
 
@@ -97,8 +137,8 @@ if ~isempty(timerange)
 	com = sprintf('%s = pop_rmbase( %s, [%s]);', inputname(1), inputname(1), ...
 			num2str(timerange));
 else
-	com = sprintf('%s = pop_rmbase( %s, [], [%s]);', inputname(1), inputname(1), ...
-			num2str(pointrange));
+	com = sprintf('%s = pop_rmbase( %s, [], %s);', inputname(1), inputname(1), ...
+			vararg2str({pointrange}));
 end;			
 
 return;
