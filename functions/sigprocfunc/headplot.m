56df9b3f9a28c79c449541839427b840906ad3e8
Arnaud Delorme <arno@ucsd.edu>
2002-11-19 19:29:59 +0000

    mhead.mat -> mhead for windows compatibility
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@1816 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/headplot.m
+++ functions/sigprocfunc/headplot.m
@@ -68,6 +68,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.10  2002/10/23 19:21:24  arno
+% fixing division error
+%
 % Revision 1.9  2002/10/23 19:01:51  arno
 % nothing
 %
@@ -118,7 +121,7 @@ end
 icadefs   % load definitions
 set(gca,'Color',BACKCOLOR);
 % mesh_file  = [ICADIR '/newupper.mat']; % whole head model file (183K)
-mesh_file  = ['mhead.mat'];      % upper head model file (987K)
+mesh_file  = ['mhead'];      % upper head model file (987K)
 
 Lighting   = 'on';
 Maplimits  = 'absmax';
