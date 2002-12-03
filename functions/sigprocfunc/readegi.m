ac74f3e4f532b97dd8f62bb4c8318531a6d8362e
Cooper Roddey <jc.roddey42@gmail.com>
2002-12-03 21:50:16 +0000

    readegi now compatible with EGI Simple Binary datafile versions 2,3,4,5,6, & 7.
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@1869 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/readegi.m
+++ functions/sigprocfunc/readegi.m
@@ -1,4 +1,4 @@
-% readegi() - read EGI Simple Binary (version 2,3,or 5) data file
+% readegi() - read EGI Simple Binary datafile (versions 2,3,4,5,6,7).
 %	      Return header info, EEG data, and any event data.
 %
 % Usage:
@@ -38,6 +38,11 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.9  2002/11/26 20:16:00  cooper
+% Added support for version 5 RAW datafiles.
+% Added a check that the total # of data points
+% read is equal to the # specified in the header.
+%
 % Revision 1.8  2002/11/14 19:00:13  scott
 % help msg
 %
@@ -80,37 +85,60 @@ end
 fprintf('Importing binary EGI data file ...\n');
 head = readegihdr(fid);
 
+% do we have segmented data?
+segmented = 0;
+switch(head.version),
+    case {2,4,6}
+       segmented = 0;
+    case {3,5,7}
+       segmented = 1;
+end
+
 % each type of event has a dedicated "channel"
 FrameVals = head.nchan + head.eventtypes;
-TrialData = zeros(FrameVals,head.segsamps*head.segments);
+
+if (segmented)
+    TrialData = zeros(FrameVals,head.segsamps*head.segments);
+    readexpected = FrameVals*head.segsamps*head.segments; 
+else
+    TrialData = zeros(FrameVals,head.samples);
+    readexpected = FrameVals*head.samples;
+end
 
 % get datatype from version number
 switch(head.version)
-   case {2, 3}
+   case {2,3}
       datatype = 'integer*2';
-   case 5,
+   case {4,5} 
       datatype = 'float32';
+   case {6,7}
+      datatype = 'float64';
    otherwise,
       error('Unknown data format');
 end
 
-% read in epoch data 
-totcount = 0;
-for i=1:head.segments,
-	if (head.version > 2),
-		SegmentCatIndex(i) = fread(fid,1,'integer*2');
-		SegmentStartTime(i) = fread(fid,1,'integer*4');
-	end
+% read in epoch data
+readtotal = 0;
+if (segmented),
+
+    for i=1:head.segments,
+	SegmentCatIndex(i) = fread(fid,1,'integer*2');
+	SegmentStartTime(i) = fread(fid,1,'integer*4');
 
 	[TrialData(:,[1+(i-1)*head.segsamps:i*head.segsamps]), count] = ...
 	   fread(fid,[FrameVals,head.segsamps],datatype);
 
-        totcount = totcount + count;
+        readtotal = readtotal + count;
+    end;
+
+else
+    % read unsegmented data
+    [TrialData, readtotal] = fread(fid, [FrameVals,head.samples],datatype); 
 end
 
 fclose(fid);
 
-if ~isequal(totcount,FrameVals*head.segsamps*head.segments)
+if ~isequal(readtotal, readexpected)
      error('Number of values read not equal to number given in header.');
 end 
 
