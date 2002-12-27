ce8069c915a51e059d662268c5620e17209242da
Scott Makeig <smakeig@ucsd.edu>
2002-12-27 20:37:48 +0000

    minor header edit -sm
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@1922 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_readlocs.m
+++ functions/popfunc/pop_readlocs.m
@@ -1,12 +1,12 @@
-% pop_readlocs() - load a EGI EEG file (pop out window if no arguments).
+% pop_readlocs() - load a EGI-format EEG file (pop out window if no arguments).
 %
 % Usage:
 %   >> EEG = pop_readlocs;             % a window pops up
-%   >> EEG = pop_readlocs( filename, 'key', val, ...);
+%   >> EEG = pop_readlocs( filename, 'key', val, ...); % no window
 %
 % Inputs:
 %   filename       - Electrode location file name
-%   'key',val      - same as readlocs()
+%   'key',val      - Same options as readlocs()
 % 
 % Outputs: same as readlocs()
 %
@@ -33,6 +33,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.4  2002/12/27 18:50:14  arno
+% debugging
+%
 % Revision 1.3  2002/12/27 18:26:01  arno
 % making it compatible with besa formats
 %
