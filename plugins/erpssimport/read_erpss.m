3ac62da87b430be4edf1a127e4d6d8d4aed03066
Arnaud Delorme <arno@ucsd.edu>
2003-01-24 00:53:30 +0000

    updating header
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@2036 1e58238c-3a72-4c0f-aabe-36d030380f39

--- plugins/erpssimport/read_erpss.m
+++ plugins/erpssimport/read_erpss.m
@@ -9,14 +9,13 @@
 % Outputs:
 %   data     - Data array [nchans samples]
 %   events   - Event information structure:
-%              events.sample_offset[] - Event offsets in samples 
-%                                       from the first sample (0)
-%              events.event_code[]    - Event codes (integers: 1-128)
+%              events.sample_offset[]: Event offsets in samples 
+%                                      from the first sample (0)
+%              events.event_code[]     and Event codes (integers: 1-128)
 %   datasize - Data size information structure:
-%              datasize.nchans        - Number of channels
-%              datasize.nframes       - Number of data frames 
-%                                       (samples, timepoints)
-
+%              datasize.nchans   (Number of channels)
+%              datasize.nframes  (Number of data frames (samples, timepoints))
+%
 % Notes: ERPSS was developed by Jonathan Hansen at the Hillyard ERP lab 
 %        of UCSD (http://sdepl.ucsd.edu/erpss/).
 %
@@ -25,54 +24,54 @@
 
 function [eeg,ev,header] = read_erpss(filename)
   
-  eeg = [];
-  ev = [];
-  header = [];
-  
-  fp = fopen(filename,'rb','ieee-le');
-  if fp == -1,
-    disp('read_erpss(): Cannot open data file...!');
-    return;
-  else
-    disp('File opened:');
-  end
-  
-  fseek(fp,6,-1);
-  header.nchans = fread(fp,1,'uint16');
-  
-  cnt = 0;
-  ev_cnt = 0;
-  while(~feof(fp)),
-    tag = fread(fp,1,'uint32');
-    if length(tag) == 0,
-      break;
+    eeg = [];
+    ev = [];
+    header = [];
+    
+    fp = fopen(filename,'rb','ieee-le');
+    if fp == -1,
+        disp('read_erpss(): Cannot open data file...!');
+        return;
+    else
+        disp('File opened:');
     end
-    if tag == hex2dec('f0aa55'),
-      cnt = cnt + 1;
-      disp(['  block ' num2str(cnt) ' found']);
+    
+    fseek(fp,6,-1);
+    header.nchans = fread(fp,1,'uint16');
+    
+    cnt = 0;
+    ev_cnt = 0;
+    while(~feof(fp)),
+        tag = fread(fp,1,'uint32');
+        if length(tag) == 0,
+            break;
+        end
+        if tag == hex2dec('f0aa55'),
+            cnt = cnt + 1;
+            disp(['  block ' num2str(cnt) ' found']);
 
-      % Read nchans and block length
-      fseek(fp,2,0);
-      nchans = fread(fp,1,'uint16');
-      block_size = power(2,fread(fp,1,'uint16'));
+            % Read nchans and block length
+            fseek(fp,2,0);
+            nchans = fread(fp,1,'uint16');
+            block_size = power(2,fread(fp,1,'uint16'));
 
-      % Read events
-      fseek(fp,62,0);
-      for i=1:110,
-	samp_off = fread(fp,1,'char');
-	cond_code = fread(fp,1,'char');
-	ev_code = fread(fp,1,'uint16');
-	if samp_off ~= 0,
-	  ev_cnt = ev_cnt + 1;
-	  ev(ev_cnt).sample_offset = samp_off + (cnt-1)*128;
-	  ev(ev_cnt).event_code = ev_code;
-	end
-      end
-      data = fread(fp,nchans*block_size,'int16');
-      data = reshape(data,nchans,block_size);
-      eeg = [eeg data]; % concatenate data blocks
+            % Read events
+            fseek(fp,62,0);
+            for i=1:110,
+                samp_off = fread(fp,1,'char');
+                cond_code = fread(fp,1,'char');
+                ev_code = fread(fp,1,'uint16');
+                if samp_off ~= 0,
+                    ev_cnt = ev_cnt + 1;
+                    ev(ev_cnt).sample_offset = samp_off + (cnt-1)*128;
+                    ev(ev_cnt).event_code = ev_code;
+                end
+            end
+            data = fread(fp,nchans*block_size,'int16');
+            data = reshape(data,nchans,block_size);
+            eeg = [eeg data]; % concatenate data blocks
+        end
     end
-  end
-  
-  fclose(fp);
-  header.nframes = size(eeg,2);
+    
+    fclose(fp);
+    header.nframes = size(eeg,2);
