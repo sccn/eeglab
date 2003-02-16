9a8507609e08a8272e8f1be269f1d3632dff7eec
Arnaud Delorme <arno@ucsd.edu>
2003-02-16 23:53:12 +0000

    typo last
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@2091 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_rmbase.m
+++ functions/popfunc/pop_rmbase.m
@@ -8,7 +8,7 @@
 % Graphical interface:
 %    1st edit box: Baseline time range ([min_ms max_ms]). Same as
 %                  the 'timerange' command line input.
-%    1st edit box: Baseline points vector (ex:1:56). Overwritten by 
+%    2nd edit box: Baseline points vector (ex:1:56). Overwritten by 
 %                  the time limits option above. Same as the 
 %                  'pointrange' command line input.
 %
@@ -48,6 +48,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.7  2003/02/16 23:52:49  arno
+% adding gui info
+%
 % Revision 1.6  2002/11/18 02:47:23  arno
 % adding continuous data baseline subtraction
 %
