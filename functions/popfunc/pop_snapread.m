eca2acde1b36490fdb1726838169d54d3f6ed694
Arnaud Delorme <arno@ucsd.edu>
2003-03-02 22:51:56 +0000

    gain
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@2186 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_snapread.m
+++ functions/popfunc/pop_snapread.m
@@ -1,10 +1,29 @@
 % pop_snapread() - load an EEG SnapMaster file (pop out window if no arguments).
 %
 % Usage:
-%   >> [dat] = pop_snapread( filename);
+%   >> [dat] = pop_snapread( filename, gain);
+%
+% Graphic interface:
+%   "Relative gain" - [edit box] to compute the relative gain, vizualise
+%                   the text header of the snapmater file with a text editor. 
+%                   Find the recording unit, usually in volts (UNITS field)).  
+%                   Then, find the voltage range in the "CHANNEL.RANGE" [cmin cmax]
+%                   field. Finally, look at the gain of the amplifier (directly
+%                   on the machine, not in the header file).
+%                   Knowing that the recording precision is 12 bits. The folowing
+%                   formula 
+%                                    1/2^12*[cmax-cmin]*1e6/gain 
+%                   returns the relative gain
+%                   you have to enter in the edit box. For a gain of 50 and
+%                   a channel range of of [-2.5 2.5], the relative gain is 400. 
+%                   (note that if the voltage range is not the same for all channels
+%                   or if the CONVERSION.POLY field in the file header
+%                   is not "0 + 1x" for all channels,  you will have to load the data 
+%                   using snapread() and scale manually all channels).
 %
 % Inputs:
 %   filename       - SnapMaster file name
+%   gain           - relative gain. See graphic interface help.
 % 
 % Outputs:
 %   dat            - EEGLAB data structure
@@ -32,15 +51,30 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.1  2002/04/05 17:32:13  jorn
+% Initial revision
+%
 
-function [EEG, command] = pop_snapread(filename); 
+function [EEG, command] = pop_snapread(filename, gain); 
 command = '';
+EEG = [];
 
 if nargin < 1 
 	% ask user
 	[filename, filepath] = uigetfile('*.SMA', 'Choose a SnapMaster file -- pop_snapread()'); 
 	if filename == 0 return; end;
 	filename = [filepath filename];
+     
+    promptstr    = { 'Relative gain (see help)' };
+    inistr       = { '400' };
+    result       = inputdlg2( promptstr, 'Import SnapMaster file -- pop_snapread()', 1,  inistr, 'pop_snapread');
+    if length(result) == 0 return; end;
+    gain   = eval( result{1} );
+
+end;
+
+if exist('gain') ~= 1
+    gain = 1;
 end;
 
 % load datas
@@ -48,6 +82,7 @@ end;
 EEG = eeg_emptyset;
 [EEG.data,params,events, head] = snapread(filename);  
 
+EEG.data            = EEG.data*gain;
 EEG.filename        = filename;
 EEG.filepath        = '';
 EEG.setname 		= 'SnapMaster file';
@@ -63,6 +98,6 @@ if ~isempty(A)
 end;
 
 EEG = eeg_checkset(EEG);
-command = sprintf('EEG = pop_snapread(''%s'');', filename); 
+command = sprintf('EEG = pop_snapread(''%s'', %f);', filename, gain); 
 
 return;
