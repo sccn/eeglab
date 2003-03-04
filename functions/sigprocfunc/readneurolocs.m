bef4b749d57d644ec91cc4fb802bd5f6aa4e59ea
Arnaud Delorme <arno@ucsd.edu>
2003-03-04 20:04:17 +0000

    debuging
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@2225 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/readneurolocs.m
+++ functions/sigprocfunc/readneurolocs.m
@@ -2,18 +2,24 @@
 %
 % Usage:
 %   >> CHANLOCS = readneurolocs( filename );
-%   >> CHANLOCS = readneurolocs( filename, plot, czindex, fzindex );
+%   >> CHANLOCS = readneurolocs( filename, plot, czindex, fzindex, c3index );
 %
 % Inputs:
 %   filename       - file name
 %
 % Optional inputs:
-%   plot           - [0|1] if 1, plot the electrode locations
+%   plot           - [0|1] if 1, plot the electrode locations. Default 0.
 %   czindex        - index of electrode Cz if the label is not defined 
-%                    in the file
+%                    in the file. Default is [] (find electrode label
+%                    automatically).
 %   fzindex        - index of electrode Fz if the label is not defined 
 %                    in the file. (for 3D accurate conversion of electrode 
-%                    positions)
+%                    positions). Set it to [] so that the data will not
+%                    be rescaled along the y axis.
+%   c3index        - index of electrode c3 if the label is not defined 
+%                    in the file. (for 3D accurate conversion of electrode 
+%                    positions). Set it to [] so that the data will not
+%                    be rescaled along the x axis.
 %
 % Outputs:
 %   CHANLOCS       - EEGLAB channel location data structure. See
@@ -42,8 +48,11 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.1  2003/03/04 19:18:35  arno
+% Initial revision
+%
 
-function readneurolocs( filename, plottag, indexcz, indexfz)
+function chanlocs = readneurolocs( filename, plottag, indexcz, indexfz)
     
     if nargin < 1
         help readneurolocs;
@@ -55,7 +64,13 @@ function readneurolocs( filename, plottag, indexcz, indexfz)
     
     % read location file
     % ------------------
-    locs = loadtxt( filename );
+    locs  = loadtxt( filename );
+    
+    % remove trailing control channels
+    % --------------------------------
+    while isnumeric( locs{end,1} ) & locs{end,1} ~= 0
+        locs  = locs(1:end-1,:);
+    end;    
     
     % find first numerical index
     % --------------------------
@@ -67,16 +82,27 @@ function readneurolocs( filename, plottag, indexcz, indexfz)
     % extract location array
     % ----------------------   
     nchans = size( locs, 1 ) - index +1;
-    chans  = locs(end-nchans+1:end, 1:5);
+    chans  = cell2mat(locs(end-nchans+1:end, 1:5));
     names  = locs(end-nchans*2+1: end-nchans, 2);
+    for index = 1:length(names)
+        if ~isstr(names{index})
+            names{index} = int2str(names{index});
+        end;
+    end;
     
     % find Cz and Fz
     % --------------
-    if exist('czindex') == 1
+    if exist('indexcz') ~= 1
         indexcz = strmatch( 'cz', lower(names') );
+        if isempty(indexcz), 
+            error('Can not find Cz electrode, define the electrode index manually in readneurolocs()');
+        end;
     end;
-    if exist('czindex') == 1
+    if exist('indexfz') ~= 1
         indexfz = strmatch( 'fz', lower(names') );
+    end;
+    if exist('indexc3') ~= 1
+        indexc3 = strmatch( 'c3', lower(names') );
     end;
     
     % plot all channels
@@ -92,21 +118,30 @@ function readneurolocs( filename, plottag, indexcz, indexfz)
         end;
     end;
     
+    x = - x + x(indexcz);
+    y = - y + y(indexcz);
+
+    % shrink coordinates
+    % ------------------
+    if ~isempty(indexfz)
+        y = y/y(indexfz)*0.25556;
+        disp('Readneurolocs: electrode location scaled along the y axis for standard Fz location');
+        if isempty(indexc3), indexc3 = indexfz; end;
+    end;
+    if ~isempty(indexc3)
+        x = x/x(indexc3)*0.25556;
+        disp('Readneurolocs: electrode location scaled along the x axis for standard C3 location');
+    end;
+
     % convert to polar
     % ----------------
-    x = x - x(indexcz);
-    y = - y + y(indexfz);    
-    [theta r] = cart2pol(x, y);
-    
-    % skink radius 
-    % ------------
-    r = r/r(indexfz)*0.25556
+    [theta r] = cart2pol(x, y);    
     
     % create structure
     % ----------------
     for index = 1:length(names)
         chanlocs(index).labels = names{index};
-        chanlocs(index).theta  = theta(index)/pi*180+90;
+        chanlocs(index).theta  = theta(index)/pi*180-90;
         chanlocs(index).radius = r(index);
     end;
         
