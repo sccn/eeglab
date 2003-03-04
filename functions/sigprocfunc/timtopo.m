dd38c8621b9281363b6e3f9269811773a9b42a7c
Scott Makeig <smakeig@ucsd.edu>
2003-03-04 18:13:58 +0000

    debug last -sm
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@2208 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/timtopo.m
+++ functions/sigprocfunc/timtopo.m
@@ -38,6 +38,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.18  2003/03/04 18:11:36  scott
+% debug last -sm
+%
 % Revision 1.17  2003/03/04 18:09:45  scott
 % debug last -sm
 %
@@ -367,7 +370,6 @@ for t=1:ntopos % draw oblique lines through to the topoplots
 %   >> axis off % finally, hide large axes
 
   maxdata = max(matsel(data,frames,plotframes(t))); % max data value at plotframe
-  from = changeunits([plotframes(t),maxdata],axdata,axall);
 
   head_sep = 1.2;
   axtp = axes('Units','Normalized','Position',...
@@ -378,8 +380,9 @@ for t=1:ntopos % draw oblique lines through to the topoplots
   postp = axis(axtp);
   axis([-1 1 -1 1]);
 
+  from = changeunits([plotframes(t),maxdata],axdata,axall);
   to   = changeunits([0,-1],axtp,axall);
-  cla
+  delete(axtp);
 
   axes(axall);
   l1 = plot([from(1) from(2)],[to(1) to(2)]);
