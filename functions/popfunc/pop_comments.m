1dd9f9329c5b824c49c32a199c27c7db47e2894f
Arnaud Delorme <arno@ucsd.edu>
2003-04-09 23:28:12 +0000

    debuging command line call
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@2382 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_comments.m
+++ functions/popfunc/pop_comments.m
@@ -44,6 +44,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.9  2002/08/17 01:03:34  scott
+% font change, OK -> SAVE
+%
 % Revision 1.8  2002/08/12 01:23:16  arno
 % update colors
 %
@@ -78,7 +81,9 @@ com = '';
 if exist('comments') ~=1, comments = '';
 elseif iscell(comments), comments = strvcat(comments{:}); 
 end;
-newcomments = comments;
+if nargin < 3
+    newcomments = comments;
+end;
 if nargin < 3
 	try, icadefs;
 	catch,
