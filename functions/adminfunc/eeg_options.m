5484fcd48068f63e411da78ace9f467ed0061583
Arnaud Delorme <arno@ucsd.edu>
2003-04-10 17:28:16 +0000

    adding space char for correct text parsing
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@2384 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/eeg_options.m
+++ functions/adminfunc/eeg_options.m
@@ -4,5 +4,5 @@
 %       /File/Maximize memory in EEGLAB gui
 
 option_computeica = 1 ; % Precompute ICA activations (uses RAM)
-option_keepdataset = 1 ;% Retain parent datasets (uses RAM) 
+option_keepdataset = 1 ; % Retain parent datasets (uses RAM) 
 option_savematlab = 0 ; % Store data in the .mat file (uses disk space)
