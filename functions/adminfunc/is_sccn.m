8ea5f5fc204ad9082d5fe8666ba5b29a289deb77
Arnaud Delorme <arno@ucsd.edu>
2003-04-22 21:30:50 +0000

    Initial revision
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@2437 1e58238c-3a72-4c0f-aabe-36d030380f39

---
+++ functions/adminfunc/is_sccn.m
@@ -0,0 +1,12 @@
+% returns 1 if computer is located at SCCN (Swartz Center
+% for computational Neuroscience and 0 otherwise
+
+function bool = is_sccn;
+    
+    domnane = '';
+    if isunix, [tmp domname] = unix('hostname -d');
+    else return;
+    end;
+    bool = strcmpi(domname(1:end-1), 'sccn.ucsd.edu');
+    return
+    
