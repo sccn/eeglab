0701cb1ecb0973ef2dbafdaa5f75c5a47c68647a
Arnaud Delorme <arno@ucsd.edu>
2003-04-29 18:27:44 +0000

    Initial revision
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@2504 1e58238c-3a72-4c0f-aabe-36d030380f39

---
+++ functions/miscfunc/imagesclogy.m
@@ -0,0 +1,71 @@
+% imagesclogy() - make an imagesc(0) plot with log y-axis values (ala semilogy())
+%
+% Usage:  >> imagesclogy(times,freqs,data);
+%
+% Input:
+%   times = vector of x-axis values
+%   freqs = vector of y-axis values (LOG spaced)
+%   data  = matrix of size (freqs,times)
+%
+% Author: Arnaud Delorme, SCCN/INC/UCSD, La Jolla, 4/2003 
+
+% Copyright (C) 4/2003 Arnaud Delorme, SCCN/INC/UCSD, scott@sccn.ucsd.edu
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+% $Log: not supported by cvs2svn $
+
+function imagesclogy(times,freqs,data)
+
+  if size(data,1) ~= length(freqs)
+      fprintf('logfreq(): data matrix must have %d rows!\n',length(freqs));
+      return
+  end
+  if size(data,2) ~= length(times)
+      fprintf('logfreq(): data matrix must have %d columns!\n',length(times));
+      return
+  end
+  if min(freqs)<= 0
+      fprintf('logfreq(): frequencies must be > 0!\n');
+      return
+  end
+  
+  % problem with log images in Matlab: border are automatically added
+  % to account for half of the width of a line: but they are added as
+  % if the data was linear. The commands below compensate for this effect
+  
+  steplog = log(freqs(2))-log(freqs(1)); % same for all points
+  realborders = [exp(log(freqs(1))-steplog/2) exp(log(freqs(end))+steplog/2)];
+  newfreqs    = linspace(realborders(1), realborders(2), length(freqs));
+  
+  % regressing 3 times
+  border  = mean(newfreqs(2:end)-newfreqs(1:end-1))/2; % automatically added to the borders in imagesc
+  newfreqs = linspace(realborders(1)+border, realborders(2)-border, length(freqs));
+  border  = mean(newfreqs(2:end)-newfreqs(1:end-1))/2; % automatically added to the borders in imagesc
+  newfreqs = linspace(realborders(1)+border, realborders(2)-border, length(freqs));
+  border  = mean(newfreqs(2:end)-newfreqs(1:end-1))/2; % automatically added to the borders in imagesc
+  newfreqs = linspace(realborders(1)+border, realborders(2)-border, length(freqs));
+  
+  imagesc(times,newfreqs,data);
+  set(gca, 'yscale', 'log');
+  
+  % puting labels
+  % -------------
+  divs = linspace(log(freqs(1)), log(freqs(dispf(end))), 10);
+  set(gca, 'xtickmode', 'manual');
+  divs = ceil(exp(divs)); divs = unique(divs); % ceil is critical here, round might misalign
+                                               % out-of border label with within border ticks
+  set(gca, 'xtick', divs);
+  
