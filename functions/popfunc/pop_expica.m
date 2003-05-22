d3c97a5021199b2e1fec0538b737ebb145109262
Arnaud Delorme <arno@ucsd.edu>
2003-05-22 15:04:30 +0000

    header typo
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@2639 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_expica.m
+++ functions/popfunc/pop_expica.m
@@ -13,7 +13,7 @@
 % 
 % Author: Arnaud Delorme, CNL / Salk Institute, Mai 14, 2003
 %
-% See also: eeglab(), readegi(), readegihdr()
+% See also: pop_export()
 
 %123456789012345678901234567890123456789012345678901234567890123456789012
 
@@ -34,6 +34,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.2  2003/05/14 15:28:19  arno
+% sphere matrixc
+%
 % Revision 1.1  2003/05/14 15:10:20  arno
 % Initial revision
 %
