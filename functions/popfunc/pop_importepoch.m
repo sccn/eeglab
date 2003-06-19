cd20abe5fdbd1bdbfada23885a6983d2f55ecaab
Arnaud Delorme <arno@ucsd.edu>
2003-06-19 16:16:17 +0000

    nothing
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@2712 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_importepoch.m
+++ functions/popfunc/pop_importepoch.m
@@ -105,6 +105,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.25  2003/06/19 16:07:11  arno
+% make ur
+%
 % Revision 1.24  2003/04/10 17:31:29  arno
 % header edit
 %
