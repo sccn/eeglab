f78ec01ca9ae30fe41f2ea59e4ee01601dd1a869
Arnaud Delorme <arno@ucsd.edu>
2003-06-19 21:12:32 +0000

    nothing
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@2713 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_snapread.m
+++ functions/popfunc/pop_snapread.m
@@ -51,6 +51,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.4  2003/06/19 16:14:17  arno
+% make ur
+%
 % Revision 1.3  2003/04/10 17:36:28  arno
 % header edit
 %
