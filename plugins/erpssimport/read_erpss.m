5b6cecbe66edc32fbd33b1c0b5420f82857ffb3f
Arnaud Delorme <arno@ucsd.edu>
2003-06-20 00:10:00 +0000

    ?\027[A?\027[Aallowing to read compressed data
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@2714 1e58238c-3a72-4c0f-aabe-36d030380f39

--- plugins/erpssimport/read_erpss.m
+++ plugins/erpssimport/read_erpss.m
@@ -1,4 +1,5 @@
-% read_erpss() - read an uncompressed ERPSS-format file (.RAW or .RDF) 
+% read_erpss() - read an compressed and uncompressed ERPSS file formats 
+%                (.RAW or .RDF) 
 %                
 % Usage: 
 %         >> [data,events,datasize] = read_erpss(filename);
@@ -18,7 +19,8 @@
 % Notes: ERPSS format was developed by Jonathan Hansen at the Hillyard lab 
 %              at UCSD (http://sdepl.ucsd.edu/erpss/).
 %
-% Authors: Jeng-Ren Duann, Arnaud Delorme, CNL/Salk & INC/UCSD, 2002-12-12
+% Authors: Jeng-Ren Duann, CNL/Salk & INC/UCSD, 2002-12-12
+%          Arnaud Delorme, debug, spped-up and read compressed data 2003-06
 %          with help from Andrey Vankov
 
 function [eeg,ev,header] = read_erpss(filename)
@@ -26,6 +28,7 @@ function [eeg,ev,header] = read_erpss(filename)
     eeg = [];
     ev = [];
     header = [];
+    tmptag = hex2dec('f0aa55');
     
     fp = fopen(filename,'rb','ieee-le');
     if fp == -1,
@@ -34,6 +37,18 @@ function [eeg,ev,header] = read_erpss(filename)
         disp('File opened:');
     end
     
+    fseek(fp,4,-1);
+    compressed = fread(fp,1,'uint16');
+    if compressed, 
+        disp('Reading compressed data'); 
+        complendian = 0;
+        switch computer
+         case {'MAC2','SUN4','SOL2','SGI','SGI64'}, complendian = 1;
+         case {'PCWIN','LNX86','GLNX86'}, complendian = 0;
+         otherwise 
+          disp('Disp: endian of computer not known, set to PC (windows/linux) by default');
+        end;
+    end;
     fseek(fp,6,-1);
     header.nchans = fread(fp,1,'uint16');
     
@@ -42,24 +57,33 @@ function [eeg,ev,header] = read_erpss(filename)
     
     % first pass, scan data
     totalsize = 0;
+    totblocks = 0;
     disp('finding total number of blocks ...');
     while(~feof(fp)),
         tag = fread(fp,1,'uint32');
         if length(tag) == 0,
             break;
         end
-        if tag == hex2dec('f0aa55'),
+        if tag == tmptag,
             cnt = cnt + 1;
 
             % Read nchans and block length
             fseek(fp,2,0);
             nchans = fread(fp,1,'uint16');
-            block_size = power(2,fread(fp,1,'uint16'));
+            fread(fp,1,'uint16');
+            block_size = fread(fp,1,'uint16');
 
             % Read events
-            fseek(fp,62+110*4+nchans*block_size*2,0);
+            if compressed
+                fseek(fp,10,0);
+                block_size_compress = fread(fp,1,'uint16');
+                fseek(fp,48+110*4+block_size_compress*2,0);
+            else
+                fseek(fp,60+110*4+nchans*block_size*2,0);
+            end;
             totalsize = totalsize + block_size;
         end
+        totblocks = totblocks+1;
     end
     eeg = zeros(header.nchans, totalsize);
     
@@ -80,7 +104,7 @@ function [eeg,ev,header] = read_erpss(filename)
         if length(tag) == 0,
             break;
         end
-        if tag == hex2dec('f0aa55'),
+        if tag == tmptag,
             cnt = cnt + 1;
             if ~mod(cnt,100)
                 fprintf('%d ', cnt);
@@ -99,9 +123,10 @@ function [eeg,ev,header] = read_erpss(filename)
             err_detect = fread(fp,1,'uint16');
             nlost = fread(fp,1,'uint16');
             nevents = fread(fp,1,'uint16');
+            block_size_compress = fread(fp,1,'uint16');
 	    
             % Read events
-            fseek(fp,50,0);
+            fseek(fp,48,0);
             for i=1:nevents,
                 samp_off = fread(fp,1,'uint8');
                 cond_code = fread(fp,1,'uint8');
@@ -111,7 +136,12 @@ function [eeg,ev,header] = read_erpss(filename)
                 ev(ev_cnt).event_code = ev_code;
             end
             fseek(fp,4*(110-nevents),0);
-            data = fread(fp,nchans*block_size,'int16');
+            if compressed
+                data = uint8(fread(fp,2*block_size_compress,'uchar'));
+                data = decompresserpss(data, nchans, block_size, complendian);
+            else 
+                data = fread(fp,nchans*block_size,'int16');
+            end;
             try, 
                 eeg(:,totalsize+1:totalsize+length(data)/nchans) = reshape(data,nchans,length(data)/nchans); % concatenate data blocks
                 totalsize = totalsize + length(data)/nchans;
