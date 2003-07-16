bcfc0d78ea8e2c47c65381e38baeef3f4dc8b4db
Scott Makeig <smakeig@ucsd.edu>
2003-07-16 01:38:07 +0000

    help topoplot help topoplot
    return
    
    
    
    
    whos
    
    
    exit
    
    
    exit
    reutnr
    return
    k
    debug off
    :vi
    
    
    
    
    quit
    stop
    
    
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@2817 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_chanedit.m
+++ functions/popfunc/pop_chanedit.m
@@ -77,6 +77,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.51  2003/07/10 18:30:03  arno
+% debuging rotate axis cancelation
+%
 % Revision 1.50  2003/05/13 23:49:13  arno
 % [Aallowing to write channel location file
 %
