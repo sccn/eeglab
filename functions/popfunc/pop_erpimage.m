24224de4f95dd85f91c419af556e2515bda2f0d9
Scott Makeig <smakeig@ucsd.edu>
2003-07-26 17:29:44 +0000

    test
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@2908 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_erpimage.m
+++ functions/popfunc/pop_erpimage.m
@@ -171,6 +171,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.114  2003/05/10 19:06:21  arno
+% debug last
+%
 % Revision 1.113  2003/05/10 19:02:42  arno
 % debug last
 %
@@ -883,6 +886,7 @@ if length( options ) < 2
 end;
 
 % varargout{1} = sprintf('figure; pop_erpimage(%s,%d,%d,''%s'',%d,%d,{%s},[%s],''%s'',''%s''%s);', inputname(1), typeplot, channel, titleplot, smooth, decimate, typetxt, int2str(sortingwin), sortingeventfield, renorm, options);
+
 popcom = sprintf('figure; pop_erpimage(%s,%d, [%s],[%s],''%s'',%d,%d,{%s},[%s],''%s'' %s);', inputname(1), typeplot, int2str(channel), int2str(projchan), titleplot, smooth, decimate, typetxt, int2str(sortingwin), sortingeventfield, options);
 
 com = sprintf('%s erpimage( %s, %s, linspace(EEG.xmin*1000, EEG.xmax*1000, EEG.pnts), ''%s'', %d, %d %s);', outstr, tmpsig, events, titleplot, smooth, decimate, options);
@@ -891,7 +895,7 @@ disp(' '); disp(com); disp(' ');
 eval(com)
 
 if popup
-	varargout{1} = popcom; % [10 '% Call: ' com];
+	varargout{1} = [popcom 10 '% Call: ' com];
 end;
 
 return;
