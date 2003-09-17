4904ed6f027edfa3909667d93e1fbc1742729627
Arnaud Delorme <arno@ucsd.edu>
2003-09-17 02:11:55 +0000

    nothing
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3065 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/miscfunc/testica.m
+++ functions/miscfunc/testica.m
@@ -35,6 +35,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.1  2002/04/05 17:36:45  jorn
+% Initial revision
+%
 
 % 2-28-97 added source, shape and exppow parameters, kurtosis -sm
 % 4-03-97 shortened name to testica() -sm
@@ -180,7 +183,7 @@ if nargout == 0
 end
 
  fprintf('Decomposing the resulting simulated data using runica() ...\n');
- [weights,sphere,activations,bias,sign,lrates] ...
+ [weights,sphere,activations] ...
               = runica(data, ...
                      'block',block, ...
                      'lrate',lrate, ...
