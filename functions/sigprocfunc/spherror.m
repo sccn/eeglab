c305a005e780c174fe7bb7a985d9b047b832acc7
Arnaud Delorme <arno@ucsd.edu>
2003-10-09 19:08:36 +0000

    typo
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3103 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/spherror.m
+++ functions/sigprocfunc/spherror.m
@@ -1,5 +1,5 @@
 % spherror() - cart2topo() sub function to compute minimum distance
-%               of carthesian coordinates to a sphere
+%               of Cartesian coordinates to a sphere
 %
 % Author: Scott Makeig, CNL / Salk Institute, 2000
 
@@ -22,6 +22,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.1  2002/04/05 17:36:45  jorn
+% Initial revision
+%
 
 function wobble = spherror(center,x,y,z)
   
