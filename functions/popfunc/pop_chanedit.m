3f7049cb4945e213fe0b5d2356858ca9a52c7239
Arnaud Delorme <arno@ucsd.edu>
2003-10-16 23:39:45 +0000

    eeglabsources ?\027OA eeglabsources
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3116 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_chanedit.m
+++ functions/popfunc/pop_chanedit.m
@@ -74,6 +74,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.58  2003/08/08 17:00:37  arno
+% fix header
+%
 % Revision 1.57  2003/08/05 18:30:46  arno
 % nothing
 %
