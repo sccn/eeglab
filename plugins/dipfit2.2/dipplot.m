7475126a42205a84c4a96c6d490f892d45da7a93
Arnaud Delorme <arno@ucsd.edu>
2003-11-05 20:30:51 +0000

    nothing
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3187 1e58238c-3a72-4c0f-aabe-36d030380f39

--- plugins/dipfit2.2/dipplot.m
+++ plugins/dipfit2.2/dipplot.m
@@ -132,6 +132,9 @@
 % - Gca 'userdata' stores imqge names and position
 
 %$Log: not supported by cvs2svn $
+%Revision 1.63  2003/11/05 18:50:28  arno
+%nothing
+%
 %Revision 1.62  2003/10/31 23:29:29  arno
 %dicreases dipole size for summary mode
 %
