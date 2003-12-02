e9a31ef3e267c104830ed4cff5b41d18e0003d28
Arnaud Delorme <arno@ucsd.edu>
2003-12-02 18:02:06 +0000

    chancenter history
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3300 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_chanedit.m
+++ functions/popfunc/pop_chanedit.m
@@ -77,6 +77,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.64  2003/12/02 17:55:37  arno
+% verbose on for convertlocs
+%
 % Revision 1.63  2003/12/02 17:54:34  arno
 % conversion of coordinates
 %
@@ -457,6 +460,9 @@ if nargin < 2
 				chans = pop_chanedit(chans, tmpcom{:}); % apply modification to channel structure
                 if iscell(tmpcom{2}) & (length(tmpcom{2}) == 2) & isstr(tmpcom{2}{2}) & strcmpi(tmpcom{2}{2}, 'gui'),
                     tmpcom = { tmpcom{1} tmpcom{2}{1} };
+                end;
+                if iscell(tmpcom{2}) & isstr(tmpcom{2}{1}) & strcmpi(tmpcom{2}{1}, 'chancenter')
+                    tmpcom = { tmpcom{1} { tmpcom{2}{1} tmpcom{2}{2} 0 } };
                 end;
                 totaluserdat = { totaluserdat{:} tmpcom{:} };
 			catch
