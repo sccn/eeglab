fdc3d402e1032dcf28bf5a6e5be47dc6fdf6fd63
Arnaud Delorme <arno@ucsd.edu>
2003-12-03 02:11:04 +0000

    Initial revision
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3310 1e58238c-3a72-4c0f-aabe-36d030380f39

---
+++ functions/sigprocfunc/spec.m
@@ -0,0 +1,95 @@
+% spec() - power spectrum. This function replaces psd() function if the signal
+%          processing toolbox is not present. It uses the timef() function.
+%
+% Usage:
+%   >> [power freqs] = spec(X);
+%   >> [power freqs] = spec(X, nfft, fs, win, overlap);
+%
+% Inputs:
+%   X       - data
+%   nfft    - zero padding to length nfft
+%   fs      - sampling frequency
+%   win     - window size
+%   overlap - window overlap
+%              
+% Outputs:
+%   power  - spectral estimate in dB
+%   freqs  - frequency array
+%
+%
+% Note: this function is just an approximation of the psd method. We
+%       strongly recommend to use the psd function if you have access
+%       to it.
+%
+% Author: Arnaud Delorme, SCCN, Dec 2, 2003
+
+%123456789012345678901234567890123456789012345678901234567890123456789012
+
+% Copyright (C) 2003 Arnaud Delorme, Salk Institute, arno@salk.edu
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+% $Log: not supported by cvs2svn $
+
+function [power, freqs] = spec(X, nfft, fs, win, overlap);
+
+if nargin < 1
+    help spec;
+    return;
+end;
+
+% default parameters
+% ------------------
+if nargin < 2
+    nfft = 256;
+end;
+nfft = min(length(X), nfft);
+if nargin < 3
+    fs = 2;
+end;
+if nargin < 4
+    win = nfft;
+end;
+if nargin < 5
+    overlap = 0;
+end;
+
+% compute corresponding parameters for timef
+% ------------------------------------------
+padratio = pow2(nextpow2(nfft/win));
+timesout = floor(length(X)/(win-overlap));
+
+[ersp itc mbase times freqs] = timef(X(:)', length(X), [0 length(X)]/fs, fs, ...
+                                        0, 'padratio', padratio, 'timesout', timesout, 'winsize', win, 'maxfreq', fs/2, ...
+                                        'plotersp', 'off', 'plotitc', 'off', 'baseline', NaN);
+
+ersp = 10.^(ersp/10); % back to amplitude
+spec = mean(ersp,2)*2.7/win; % this formula is a best approximation (I couldn't find the actual one)
+                             % in practice the difference with psd is less than 0.1 dB
+spec = 10*log10(spec);
+hold on;
+h = plot(freqs, spec);
+set(h, 'linewidth', 2);
+return;
+
+%figure;
+%stdv = std(ersp, [], 2);
+
+h = plot(freqs, spec+stdv, 'r:');
+set(h, 'linewidth', 2);
+h = plot(freqs, spec-stdv, 'r:');
+set(h, 'linewidth', 2);
+xlabel('Frequency (Hz)');
+ylabel('Power (log)');
