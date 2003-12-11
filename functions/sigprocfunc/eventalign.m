8d850ac944f222e951724d9fce9d3b18e2bec7dc
Arnaud Delorme <arno@ucsd.edu>
2003-12-11 02:26:22 +0000

    Initial revision
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3411 1e58238c-3a72-4c0f-aabe-36d030380f39

---
+++ functions/sigprocfunc/eventalign.m
@@ -0,0 +1,31 @@
+% eventalign - function called by pop_importevent() to find the best 
+%              sampling rate ratio to align 2 arrays of data events.
+%
+% Author: Arnaud Delorme, SCCN/INC/UCSD, Dec 2003
+
+%123456789012345678901234567890123456789012345678901234567890123456789012
+
+% Copyright (C) Arnaud Delorme, CNL / Salk Institute, 9 Feb 2002, arno@salk.edu
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+% $Log: not supported by cvs2svn $
+
+function mindiff = alignevent(factor, a, b)
+    
+       diffarray = abs(factor*a-b)';
+       [allmins poss] = min(diffarray);
+       mindiff = mean(allmins);
+       
