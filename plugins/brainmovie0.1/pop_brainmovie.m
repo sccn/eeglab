e595e6150b3121cc154affa6f7e7748ecdd6a487
Arnaud Delorme <arno@ucsd.edu>
2003-12-20 01:52:29 +0000

    final size
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3471 1e58238c-3a72-4c0f-aabe-36d030380f39

--- plugins/brainmovie0.1/pop_brainmovie.m
+++ plugins/brainmovie0.1/pop_brainmovie.m
@@ -113,6 +113,9 @@
 % See also: brainmovie(), timecrossf()
 
 % $Log: not supported by cvs2svn $
+% Revision 1.67  2003/12/20 01:43:31  arno
+% same
+%
 % Revision 1.66  2003/12/20 01:42:33  arno
 % same
 %
@@ -639,7 +642,7 @@ else %%%%%%%%%%%%% 3D MOVIE PARAMS %%%%%%%%%%%%%%%%
     coordinates(:,2) =  tmp;
     coordinates(:,3) =  -coordinates(:,3); % restore dipplot coordinates
     brainmovieoptions = { 'condtitle' alltitles 'coordinates', coordinates, ...
-                        'circfactor', g.circfactor, 'size', [698 698] };
+                        'circfactor', g.circfactor, 'size', [505 505] };
     if iscell(g.movparams), brainmovieoptions = { brainmovieoptions{:} g.movparams{:}}; 
     elseif strcmpi(g.movparams, 'mrirear'), brainmovieoptions = { brainmovieoptions{:} 'view', [0 -1 0] };
     elseif strcmpi(g.movparams, 'mriside'), brainmovieoptions = { brainmovieoptions{:} 'view', [1 0 0] };
