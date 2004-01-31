b199636e2496927abb835acb8ef11879f9e0a442
Arnaud Delorme <arno@ucsd.edu>
2004-01-31 01:23:24 +0000

    nothing
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3573 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_importevent.m
+++ functions/popfunc/pop_importevent.m
@@ -114,6 +114,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.29  2004/01/31 01:16:53  arno
+% message change
+%
 % Revision 1.28  2003/12/12 00:26:55  arno
 % debuging oldevents and align
 %
