be5daa45a2246d351e85e9e84d153b1a5106f1c1
Arnaud Delorme <arno@ucsd.edu>
2004-02-12 19:44:10 +0000

    reading 3D coordinates
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3602 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/readneurolocs.m
+++ functions/sigprocfunc/readneurolocs.m
@@ -40,6 +40,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.11  2003/12/17 23:14:49  arno
+% do not auto-rotate
+%
 % Revision 1.10  2003/12/02 19:15:48  arno
 % pass on paramters to adjustlocs
 %
@@ -81,66 +84,78 @@ function chanlocs = readneurolocs( filename, varargin)
         plottag = 0;
     end;
     
-    % read location file
+   % read location file
     % ------------------
-    if isstr( filename )
-        locs  = loadtxt( filename );
-    
-        if locs{1,1}(1) == ';'
-            % remove trailing control channels
-            % --------------------------------
-            while isnumeric( locs{end,1} ) & locs{end,1} ~= 0
-                locs  = locs(1:end-1,:);
-            end;    
-            
-            % find first numerical index
-            % --------------------------
-            index = 1;
-            while isstr( locs{index,1} )
-                index = index + 1;
-            end;
-            
-            % extract location array
-            % ----------------------   
-            nchans = size( locs, 1 ) - index +1;
-            chans  = cell2mat(locs(end-nchans+1:end, 1:5));
-            names  = locs(end-nchans*2+1: end-nchans, 2);
-            for index = 1:length(names)
-                if ~isstr(names{index})
-                    names{index} = int2str(names{index});
-                end;
-            end;
-            x = chans(:,3);
-            y = -chans(:,4);
+   if isstr(filename)
+       locs  = loadtxt( filename );
+   end;
+    if ~isstr(filename) | locs{1,1}(1) == ';' | size(locs,2) < 5
+        if ~isstr(filename)
+            names = filename{1};
+            x     = filename{2};
+            y     = filename{3};
         else
-            [tmp2 tmpind] = sort(cell2mat(locs(:,1))');
-            locs = locs(tmpind,:);
-            y      = cell2mat(locs(:,end));
-            x      = cell2mat(locs(:,end-1));
-            x      = x/513.1617*44;
-            y      = y/513.1617*44;
-            names = locs(:,end-2);
+            if locs{1,1}(1) == ';'
+                % remove trailing control channels
+                % --------------------------------
+                while isnumeric( locs{end,1} ) & locs{end,1} ~= 0
+                    locs  = locs(1:end-1,:);
+                end;    
+                
+                % find first numerical index
+                % --------------------------
+                index = 1;
+                while isstr( locs{index,1} )
+                    index = index + 1;
+                end;
+                
+                % extract location array
+                % ----------------------   
+                nchans = size( locs, 1 ) - index +1;
+                chans  = cell2mat(locs(end-nchans+1:end, 1:5));
+                names  = locs(end-nchans*2+1: end-nchans, 2);
+                for index = 1:length(names)
+                    if ~isstr(names{index})
+                        names{index} = int2str(names{index});
+                    end;
+                end;
+                x = chans(:,3);
+                y = -chans(:,4);
+            else
+                [tmp2 tmpind] = sort(cell2mat(locs(:,1))');
+                locs = locs(tmpind,:);
+                y      = cell2mat(locs(:,end));
+                x      = cell2mat(locs(:,end-1));
+                x      = x/513.1617*44;
+                y      = y/513.1617*44;
+                names = locs(:,end-2);
+            end;
         end;
-    else
-        names = filename{1};
-        x     = filename{2};
-        y     = filename{3};
+        
+        % second solution using angle
+        % ---------------------------
+        [phi,theta] = cart2pol(x, y);
+        phi = phi/pi*180;
+        
+        % convert to other types of coordinates
+        % -------------------------------------
+        labels = names';
+        chanlocs = struct('labels', labels, 'sph_theta_besa', mat2cell(theta)', 'sph_phi_besa', mat2cell(phi)');
+        chanlocs = convertlocs( chanlocs, 'sphbesa2all');
+        
+        for index = 1:length(chanlocs)
+            chanlocs(index).labels = num2str(chanlocs(index).labels);
+        end;
+        
+        % re-calibration
+        % --------------
+        chanlocs = adjustlocs(chanlocs, 'autoscale', 'on', 'autorotate', 'off', varargin{:});
+        
+    else % 5 rows, xyz positions
+        for index = 1:size(locs,1)
+            locs{index,3} = - locs{index,3};
+        end;
+        chanlocs = struct('labels', locs(:,1), 'type', locs(:,2), 'X', locs(:,4), 'Y', locs(:,3), 'Z', locs(:,5));
+        chanlocs = convertlocs( chanlocs, 'cart2all');
     end;
     
-    % second solution using angle
-    % ---------------------------
-    [phi,theta] = cart2pol(x, y);
-    phi = phi/pi*180;
-    
-    % convert to other types of coordinates
-    % -------------------------------------
-    labels = names';
-    chanlocs = struct('labels', labels, 'sph_theta_besa', mat2cell(theta)', 'sph_phi_besa', mat2cell(phi)');
-    chanlocs = convertlocs( chanlocs, 'sphbesa2all');
-    for index = 1:length(chanlocs)
-        chanlocs(index).labels = num2str(chanlocs(index).labels);
-    end;
-    
-    % re-calibration
-    % --------------
-    chanlocs = adjustlocs(chanlocs, 'autoscale', 'on', 'autorotate', 'off', varargin{:});
