a96db653637d23f4a3ad14702b2101e40899fce4
Arnaud Delorme <arno@ucsd.edu>
2004-02-13 19:13:56 +0000

    nothing
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3611 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_chanedit.m
+++ functions/popfunc/pop_chanedit.m
@@ -132,6 +132,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.88  2004/02/13 02:36:21  arno
+% updating location after chancenter
+%
 % Revision 1.87  2004/02/13 00:10:31  arno
 % implementing new history for channel recentering
 %
