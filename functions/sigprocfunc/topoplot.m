7d387b88ae513aeda2c48500f5303dbc5bd61219
Scott Makeig <smakeig@ucsd.edu>
2004-02-15 17:05:34 +0000

    same
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3631 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/topoplot.m
+++ functions/sigprocfunc/topoplot.m
@@ -95,6 +95,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.89  2004/02/15 17:04:42  scott
+% same
+%
 % Revision 1.88  2004/02/15 17:04:12  scott
 % same
 %
@@ -725,7 +728,7 @@ if ~strcmpi(STYLE,'blank') % if draw scalp map
   % fprintf('Current axes size %g,%g\n',pos(3),pos(4));
 
   if strcmp(STYLE,'contour')
-    [cls chs] = contour(Xi,Yi,Zi,CONTOURNUM,'LineSpec',{CCOLOR});
+    [cls chs] = contour(Xi,Yi,Zi,CONTOURNUM,{'LineSpec',CCOLOR});
     set(chs(1))
       for h=chs
           set(h,'color',CCOLOR);
