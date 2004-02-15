9d639d939531be4586181955bfb968a71dc29d61
Scott Makeig <smakeig@ucsd.edu>
2004-02-15 20:18:03 +0000

    same
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3656 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/topoplot.m
+++ functions/sigprocfunc/topoplot.m
@@ -93,6 +93,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.113  2004/02/15 20:16:35  scott
+% same
+%
 % Revision 1.112  2004/02/15 20:15:13  scott
 % same
 %
@@ -1048,9 +1051,9 @@ function [newTh] = skirt_Th(Th,Rd)
    q2 = find(Th>=pi/2 & Th<pi);
    if ~isempty(q2)
      Th(q2) = Th(q2)-pi/2; % rotate to q1
-     dr = Rd(q1)-0.5;
+     dr = Rd(q2)-0.5;
      x = asin(sin(3/8*pi).*dr/(0.25+dr.^2-dr.*cos(3/8*pi)));
-     Th(q2) = x+(pi/2)*Th(q1)/(pi/2-2*x);
+     Th(q2) = x+(pi/2)*Th(q2)/(pi/2-2*x);
      Th(q2) = Th(q2)+pi/2; % rotate back
    end
   fprintf('rotated %d q2 angles\n',length(q2)); % DEBUG
@@ -1058,9 +1061,9 @@ function [newTh] = skirt_Th(Th,Rd)
    q3 = find(Th<-pi/2 & Th>=-pi);
    if ~isempty(q3)
      Th(q3) = Th(q3)+pi; % rotate to q1
-     dr = Rd(q1)-0.5;
+     dr = Rd(q3)-0.5;
      x = asin(sin(3/8*pi).*dr/(0.25+dr.^2-dr.*cos(3/8*pi)));
-     Th(q3) = x+(pi/2)*Th(q1)/(pi/2-2*x);
+     Th(q3) = x+(pi/2)*Th(q3)/(pi/2-2*x);
      Th(q3) = Th(q3)-pi; % rotate back
    end
   fprintf('rotated %d q3 angles\n',length(q3)); % DEBUG
@@ -1068,11 +1071,10 @@ function [newTh] = skirt_Th(Th,Rd)
    q4 = find(Th<0 & Th>=-pi/2);
    if ~isempty(q4)
      Th(q4) = Th(q4)+pi/2; % rotate to q1
-     dr = Rd(q1)-0.5;
+     dr = Rd(q4)-0.5;
      x = asin(sin(3/8*pi).*dr/(0.25+dr.^2-dr.*cos(3/8*pi)));
-     Th(q4) = x+(pi/2)*Th(q1)/(pi/2-2*x);
+     Th(q4) = x+(pi/2)*Th(q4)/(pi/2-2*x);
      Th(q4) = Th(q4)-pi/2; % rotate back
    end
   fprintf('rotated %d q4 angles\n',length(q4)); % DEBUG
-  fprintf('rotated %d angles\n',length([q1 q2 q3 q4]);
 return
