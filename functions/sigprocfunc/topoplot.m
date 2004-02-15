e2155525b0774b7910011d87d1557e555ed59899
Scott Makeig <smakeig@ucsd.edu>
2004-02-15 20:15:13 +0000

    same
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3654 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/topoplot.m
+++ functions/sigprocfunc/topoplot.m
@@ -93,6 +93,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.111  2004/02/15 20:05:56  scott
+% same
+%
 % Revision 1.110  2004/02/15 20:04:20  scott
 % same
 %
@@ -704,7 +707,6 @@ else
 end
 if (isstr('shrinkfactor') & strcmp(lower(shrinkfactor),'skirt')) | ~isstr('shrinkfactor')
    fprintf('max Th: %g\n',max(Th)); % DEBUG
-   [Th' Rd'] % DEBUG
    Th = skirt_Th(Th,Rd);  % rotate the angles of the electrodes in the 'skirt'
    fprintf('max Th: %g\n',max(Th)); % DEBUG
 end
@@ -1032,7 +1034,6 @@ axis square; % keep head round!
 %%%%%%%%%%%%%%%%%%%%%%%%% Warp electrode angles in the 'skirt' %%%%%%%%%%%
 %
 function [newTh] = skirt_Th(Th,Rd)
-   [Th' Rd'] % DEBUG
    q1 = find(Th>=0 & Th<pi/2);
    if ~isempty(q1)
      dr = Rd(q1)-0.5;
@@ -1040,37 +1041,35 @@ function [newTh] = skirt_Th(Th,Rd)
      Th(q1) = x+(pi/2)*Th(q1)/(pi/2-2*x);
    end
   fprintf('rotated %d q1 angles\n',length(q1)); % DEBUG
+
    q2 = find(Th>=pi/2 & Th<pi);
    if ~isempty(q2)
      Th(q2) = Th(q2)-pi/2; % rotate to q1
      dr = Rd(q1)-0.5;
      x = asin(sin(3/8*pi).*dr/(0.25+dr.^2-dr.*cos(3/8*pi)));
-     Th(q1) = x+(pi/2)*Th(q1)/(pi/2-2*x);
+     Th(q2) = x+(pi/2)*Th(q1)/(pi/2-2*x);
      Th(q2) = Th(q2)+pi/2; % rotate back
    end
   fprintf('rotated %d q2 angles\n',length(q2)); % DEBUG
+
    q3 = find(Th<-pi/2 & Th>=-pi);
    if ~isempty(q3)
      Th(q3) = Th(q3)+pi; % rotate to q1
      dr = Rd(q1)-0.5;
      x = asin(sin(3/8*pi).*dr/(0.25+dr.^2-dr.*cos(3/8*pi)));
-     Th(q1) = x+(pi/2)*Th(q1)/(pi/2-2*x);
+     Th(q3) = x+(pi/2)*Th(q1)/(pi/2-2*x);
      Th(q3) = Th(q3)-pi; % rotate back
    end
   fprintf('rotated %d q3 angles\n',length(q3)); % DEBUG
+
    q4 = find(Th<0 & Th>=-pi/2);
    if ~isempty(q4)
      Th(q4) = Th(q4)+pi/2; % rotate to q1
      dr = Rd(q1)-0.5;
      x = asin(sin(3/8*pi).*dr/(0.25+dr.^2-dr.*cos(3/8*pi)));
-     Th(q1) = x+(pi/2)*Th(q1)/(pi/2-2*x);
+     Th(q4) = x+(pi/2)*Th(q1)/(pi/2-2*x);
      Th(q4) = Th(q4)-pi/2; % rotate back
    end
   fprintf('rotated %d q4 angles\n',length(q4)); % DEBUG
-return
-
-function [newTh] = rot_Th(Th,Rd,q)
-     dr = Rd(q)-0.5;
-     x = asin(sin(3/8*pi).*dr/(0.25+dr.^2-dr.*cos(3/8*pi)));
-     Th(q) = x+(pi/2)*Th(q)/(pi/2-2*x);
+  fprintf('rotated %d angles\n',length([q1(:);q2(:);q3(:);q4(:)]);
 return
