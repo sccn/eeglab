2dc03512d8d65e7991a9e0a1b73fbdabd9869f4e
Scott Makeig <smakeig@ucsd.edu>
2004-02-15 19:45:27 +0000

    same
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3641 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/topoplot.m
+++ functions/sigprocfunc/topoplot.m
@@ -93,6 +93,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.98  2004/02/15 19:44:44  scott
+% same
+%
 % Revision 1.97  2004/02/15 19:42:31  scott
 % same
 %
@@ -989,28 +992,28 @@ function [newTh] = skirt_Th(Th,Rd)
    if ~isempty(q1)
      Th = rot_Th(Th,Rd,q1);
    end
-  fprintf('rotated %d q1 angles\n,length(q1));
+  fprintf('rotated %d q1 angles\n',length(q1));
    q2 = find(Th>=pi/2 & Th<pi);
    if ~isempty(q2)
      Th(q2) = Th(q2)-pi/2; % rotate to q1
      Th = rot_Th(Th,Rd,q1);
      Th(q2) = Th(q2)+pi/2; % rotate back
    end
-  fprintf('rotated %d q2 angles\n,length(q2));
+  fprintf('rotated %d q2 angles\n',length(q2));
    q3 = find(Th<-pi/2 & Th>=-pi);
    if ~isempty(q3)
      Th(q3) = Th(q3)+pi; % rotate to q1
      Th = rot_Th(Th,Rd,q1);
      Th(q3) = Th(q3)-pi; % rotate back
    end
-  fprintf('rotated %d q3 angles\n,length(q3));
+  fprintf('rotated %d q3 angles\n',length(q3));
    q4 = find(Th<0 & Th>=-pi/2);
    if ~isempty(q4)
      Th(q4) = Th(q4)+pi/2; % rotate to q1
      Th = rot_Th(Th,Rd,q1);
      Th(q4) = Th(q4)-pi/2; % rotate back
    end
-  fprintf('rotated %d q4 angles\n,length(q4));
+  fprintf('rotated %d q4 angles\n',length(q4));
 
 function [newTh] = rot_Th(Th,Rd,q)
      dr = Rd(q)-0.5;
