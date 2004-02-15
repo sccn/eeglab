c087cd45e2c055f6c5fcffad35f2001056e5ffcd
Scott Makeig <smakeig@ucsd.edu>
2004-02-15 20:43:47 +0000

    same
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3668 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/topoplot.m
+++ functions/sigprocfunc/topoplot.m
@@ -93,6 +93,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.125  2004/02/15 20:39:39  scott
+% same
+%
 % Revision 1.124  2004/02/15 20:35:56  scott
 % same
 %
@@ -739,7 +742,7 @@ Rd = Rd(enum);
 labels = labels(enum,:);
 
 if (isstr('shrinkfactor') & strcmp(lower(shrinkfactor),'skirt')) | ~isstr('shrinkfactor')
-   Th = skirt_Th(Th,Rd);  % rotate the angles of the electrodes in the 'skirt'
+   Th = skirt_Th(Th,Rd,rmax*squeezefactor);  % rotate the angles of the electrodes in the 'skirt'
 end
 
 [x,y] = pol2cart(Th,Rd);      % transform from polar to cartesian coordinates
@@ -1069,16 +1072,19 @@ axis square; % keep head round!
 %
 %%%%%%%%%%%%%%%%%%%%%%%%% Warp electrode angles in the 'skirt' %%%%%%%%%%%
 %
-function [newTh] = skirt_Th(Th,Rd)
-   q1 = find(Th>=0 & Th<pi/2);
-   q2 = find(Th>=pi/2 & Th<pi);
-   q3 = find(Th<-pi/2 & Th>=-pi);
-   q4 = find(Th<0 & Th>=-pi/2);
+function [newTh] = skirt_Th(Th,Rd,maxr)
+   q1 = find(Th>=0 & Th<pi/2) & Rd>maxr);
+   q2 = find(Th>=pi/2 & Th<pi & Rd>maxr);
+   q3 = find(Th<-pi/2 & Th>=-pi & Rd>maxr);
+   q4 = find(Th<0 & Th>=-pi/2 & Rd>maxr);
 
    if ~isempty(q1)
      dr = Rd(q1)-0.5;
      x = asin(sin(3/8*pi).*dr/(0.25+dr.^2-dr.*cos(3/8*pi)));
+x % DEBUG
+oldTh = Th; % DEBUG
      Th(q1) = x+(pi/2)*Th(q1)/(pi/2-2*x);
+[oldTh Th] % DEBUG
    end
 %  fprintf('rotated %d q1 angles\n',length(q1)); % DEBUG
 
@@ -1107,6 +1113,6 @@ function [newTh] = skirt_Th(Th,Rd)
      Th(q4) = x+(pi/2)*Th(q4)/(pi/2-2*x);
      Th(q4) = Th(q4)-pi/2; % rotate back
    end
-  fprintf('rotated %d q4 angles\n',length(q4)); % DEBUG
+%  fprintf('rotated %d q4 angles\n',length(q4)); % DEBUG
   newTh = Th;
 return
