45e6c8025d37ac70d724ebcfec783dcc6571aa16
Scott Makeig <smakeig@ucsd.edu>
2004-02-15 20:01:34 +0000

    same
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3649 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/topoplot.m
+++ functions/sigprocfunc/topoplot.m
@@ -93,6 +93,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.106  2004/02/15 20:00:13  scott
+% same
+%
 % Revision 1.105  2004/02/15 19:59:04  scott
 % same
 %
@@ -1017,6 +1020,7 @@ axis square; % keep head round!
 %%%%%%%%%%%%%%%%%%%%%%%%% Warp electrode angles in the 'skirt' %%%%%%%%%%%
 %
 function [newTh] = skirt_Th(Th,Rd)
+   [Th Rd]
    q1 = find(Th>=0 & Th<pi/2);
    if ~isempty(q1)
      dr = Rd(q1)-0.5;
