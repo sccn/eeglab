85bf361209a174e3e9d70e76d93c4badd65d76ef
Scott Makeig <smakeig@ucsd.edu>
2004-02-23 15:30:18 +0000

    added 'shrink','skirt' to topoplot call
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3699 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_selectcomps.m
+++ functions/popfunc/pop_selectcomps.m
@@ -38,6 +38,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.22  2004/02/23 15:28:08  scott
+% turned 'electrodes','off' in topoplots
+%
 % Revision 1.21  2003/07/24 23:17:27  arno
 % removing extra plot
 %
@@ -205,7 +208,8 @@ for ri = compnum
 			return;
 		end;
 		ha = axes('Units','Normalized', 'Position',[X Y sizewx sizewy].*s+q);
-		topoplot( EEG.icawinv(:,ri), EEG.chanlocs, 'verbose', 'off', 'style' , 'fill','electrodes','off');
+		topoplot( EEG.icawinv(:,ri), EEG.chanlocs, 'verbose', ...
+                            'off', 'style' , 'fill','electrodes','off','shrink','skirt');
 		axis square;
 
 		% plot the button
