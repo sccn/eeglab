3f593dc96a975a128ee0f30405013e68472cbb1f
Scott Makeig <smakeig@ucsd.edu>
2004-03-02 04:54:24 +0000

    debugged and fully commented
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3734 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/miscfunc/eeg_time2prev.m
+++ functions/miscfunc/eeg_time2prev.m
@@ -1,9 +1,9 @@
-% eeg_time2prev() - returns a vector giving, for each event of a specified ("target") type, 
-%                   the delay (in ms) since the past preceding event (if any) of an event of 
-%                   a specified ("previous") type. Requires the EEG.urevent structure, plus 
+% eeg_time2prev() - returns a vector giving, for each event of specified ("target") type(s), 
+%                   the delay (in ms) since the preceding event (if any) of specified 
+%                   ("previous") type(s). Requires the EEG.urevent structure, plus 
 %                   EEG.event().urevent pointers to it. 
 % Usage:
-%      >> [delays,targets,urtargs,urprevs] = eeg_time2prev(EEG,{target},{previous});
+%           >> [delays,targets,urtargs,urprevs] = eeg_time2prev(EEG,{target},{previous});
 % Inputs:
 %  EEG      - structure containing an EEGLAB dataset
 % {target}  - cell array of strings naming event type(s) of the specified target events 
@@ -28,102 +28,116 @@
 
 function [delays,targets,urtargets,urprevs] = eeg_time2prev(EEG,target,previous);
 
-verbose = 1; % 1=on|0=off
-nevents   = length(EEG.urevent);
+verbose = 1; % FLAG (1=on|0=off)
+nevents = length(EEG.event);
 
+%
+%%%%%%%%%%%%%%%%%%%% Test input arguments %%%%%%%%%%%%%%%%%%%%%%%%%
+%
 if ~iscell(target)
-  error('2nd arg "target" must be a cell array.\n');
+  error('2nd argument "target" must be a {cell array} of event types.');
   return
 end
-
 if ~iscell(previous)
-  error('3rd arg "previous" must be a cell array.\n');
+  error('3rd argument "previous" must be a {cell array} of event types.');
   return
 end
-
 for k=1:length(target) % make all target types strings
   if ~ischar(target{k})
     target{k} = num2str(target{k});
   end
 end
-
 for k=1:length(previous) % make all previous types strings
   if ~ischar(previous{k})
     previous{k} = num2str(previous{k});
   end
 end
-
 if ~isfield(EEG,'urevent')
-  error('requires the urevent structure be present.\n');
+  error('requires the urevent structure be present.');
+  return
+end
+if length(EEG.urevent) < nevents
+  error('WARNING: In dataset, number of urevents < number of events!?');
   return
 end
 
-if length(EEG.urevent) < length(EEG.event)
-  error('WARNING: In dataset, number of urevents < number of events!!\n');
-  return
-end
-
+% 
+%%%%%%%%%%%%%%%%%%%% Initialize output arrays %%%%%%%%%%%%%%%%%%%%%%
+%
 delays    = zeros(1,nevents); % holds output times in ms
 targets   = zeros(1,nevents); % holds indxes of targets
 urtargets = zeros(1,nevents); % holds indxes of targets
 urprevs   = zeros(1,nevents); % holds indxes of prevs
-targetcount = 0;
 
+targetcount = 0; % index of current target
+% Below:
+%   idx   = current event index
+%   uridx = current urevent index
+%   tidx  = target type index
+%   uidx  = previous urevent index
+%   pidx  = previous type index
 %
 %%%%%%%%%%%for each event in the dataset %%%%%%%%%%%%%%%%%%%%
 %
-for idx = 1:length(EEG.event)                      % for each event in the dataset
-
+for idx = 1:nevents                                % for each event in the dataset
  %
  %%%%%%%%%%%%%%%%%%%%%%%% find target events %%%%%%%%%%%%%%%%%
  %
- uridx = EEG.event(idx).urevent;
- istarget = 0; tidx = 1; % target index
- while istarget==0 & tidx<=length(target)          % for each potential target type
+ uridx = EEG.event(idx).urevent;                   % find its urevent index
+ istarget = 0;                                     % initialize target flag
+ tidx = 1;                                         % initialize target type index
+ while ~istarget & tidx<=length(target)            % for each potential target type
     if strcmpi(num2str(EEG.urevent(uridx).type),target(tidx))
-      istarget=1;
-      targetcount = targetcount+1;
-      targets(targetcount) = idx;
-      urtargets(targetcount) = uridx;
+      istarget=1;                                  % flag event as target
+      targetcount = targetcount+1;                 % increment target count
+      targets(targetcount) = idx;                  % save event index
+      urtargets(targetcount) = uridx;              % save urevent index
+      break                                        % stop target type checking
+    else
+      tidx = tidx+1;                               % else try next target type
     end
-    tidx = tidx+1;                                 % try next target type
  end 
 
- %
- %%% Compute delay from current target to latest prev event %%%
- %
  if istarget  % if current event is a target type
-  if urprevs(targetcount) > 0                      % if a 'previous' type event was already found
-                                                   % save the latency difference in ms
+  %
+  %%%%%%%%%%%%%%%%%%% Find a 'previous' event %%%%%%%%%%%%%%%%%%
+  %
+  isprevious = 0;                                  % initialize previous flag
+  uidx = uridx-1;                                  % begin with the previous urevent
+  while uridx > 0
+   pidx = 1;                                       % initialize previous type index
+   while ~isprevious & pidx<=length(previous)      % for each previous event type
+     if strcmpi(num2str(EEG.urevent(uidx).type),previous(pidx)) 
+       isprevious=1;                               % flag 'previous' event
+       urprevs(targetcount) = uidx;                % mark event as previous
+       break                                       % stop previous type checking
+     else
+       pidx = pidx+1;                              % try next 'previous' event type
+     end
+   end  % pidx
+   if isprevious
+     break                                         % stop previous event checking
+   else
+     uidx = uidx-1;                                % keep checking for a 'previous' type event
+   end % isprevious
+  end % uidx
+  %
+  %%% Compute delay from current target to previous event %%%%%
+  %
+  if isprevious                                    % if type 'previous' event found
     delays(targetcount) = 1000/EEG.srate * ...
      (-1)*(EEG.urevent(uridx).latency - EEG.urevent(urprevs(targetcount)).latency);
-
-  elseif targetcount>1 & urprevs(targetcount-1)>0  % if there was an earlier 'previous' event
-    urprevs(targetcount) = urprevs(targetcount-1); % duplicate it here 
-                                                   % and compute its latency difference
-    delays(targetcount) = 1000/EEG.srate * ...
-     (-1)*(EEG.urevent(uridx).latency - EEG.urevent(urprevs(targetcount)).latency);
-
+  else
+    delays(targetcount) = 0;                        % mark no previous event with 0
   end
   if verbose
-    fprintf('%4d. target event %4d - previous event %4d = %4.1f ms\n',...
-               targetcount,idx,urprevs(targetcount),delays(targetcount));
-  end
+    fprintf('%4d. (targ %s) %4d - (prev %s) %4d = %4.1f ms\n',...
+                targetcount, targets(tidx),idx, ...
+                             previous(pidx),urprevs(targetcount),...
+                                          delays(targetcount));
+  end % verbose
  end % istarget
-
- %
- %%% Determine whether this is  a potential 'previous' event %%%
- %
- isprevious = 0; pidx = 1; % previous index
- while ~isprevious & pidx<=length(previous)        % for each previous event type
-    if strcmpi(num2str(EEG.urevent(uridx).type),previous(pidx)) 
-      isprevious=1;                                % find a potential next 'previous' event
-      urprevs(targetcount+1) = uridx;              % mark event as potential next 'previous' 
-    end
-    pidx = pidx+1;                                 % try next 'previous' event type
- end 
-
-end % main event loop
+end % event loop
 
 %
 %%%%%%%%%%%%%% Truncate the output arrays %%%%%%%%%%%%%%%%%%%%%%
@@ -134,7 +148,9 @@ if targetcount > 0
    urprevs   = urprevs(1:targetcount);
    delays    = delays(1:targetcount);
 else
-   fprintf('eeg_time2prev(): No target type events found.\n')
+   if verbose
+     fprintf('eeg_time2prev(): No target type events found.\n')
+   end
    targets   = [];
    urtargets = [];
    urprevs   = [];
