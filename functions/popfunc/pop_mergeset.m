a378383f51eb451814e6607ddc994c4118f644b8
Arnaud Delorme <arno@ucsd.edu>
2004-05-14 22:10:18 +0000

    new eeg_insertbound call
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@3979 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_mergeset.m
+++ functions/popfunc/pop_mergeset.m
@@ -40,6 +40,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.25  2004/05/06 21:58:27  arno
+% setfields of new events
+%
 % Revision 1.24  2004/05/06 17:47:22  arno
 % debug add boundary event
 %
@@ -264,7 +267,7 @@ else
         % add discontinuity event if continuous
         % -------------------------------------
         if INEEG1trials  == 1 & INEEG2trials == 1
-            INEEG1 = eeg_insertbound(INEEG1, INEEG1pnts+0.5, INEEG1pnts+0.5, 0, 0);
+            INEEG1.event = eeg_insertbound(INEEG1.event, INEEG.pnts, INEEG1pnts+0.5, INEEG1pnts+0.5, 0, 0);
         end;
  
 	end;
