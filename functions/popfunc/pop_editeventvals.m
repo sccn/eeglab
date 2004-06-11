5af298dc4e82908ffcbc061bd99c1e62405e4ebb
Arnaud Delorme <arno@ucsd.edu>
2004-06-11 22:52:07 +0000

    remove debug msg
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@4097 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_editeventvals.m
+++ functions/popfunc/pop_editeventvals.m
@@ -48,6 +48,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.25  2004/05/27 02:01:42  arno
+% update setfield for urevent
+%
 % Revision 1.24  2004/05/26 23:24:29  arno
 % same
 %
@@ -298,7 +301,6 @@ if nargin >=1 & isstr(EEG) % interpreting command from GUI
       % ---------------
       if isfield(EEG, 'urevent') & isfield(eventtmp, 'urevent')
           urvalnum  = eventtmp(valnum).urevent;
-          disp('Urevent structure updated');
           
           % latency case
           % ------------
