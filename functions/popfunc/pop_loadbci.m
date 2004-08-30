a0b23849c2124488ee5b369af3f0015decec681f
Arnaud Delorme <arno@ucsd.edu>
2004-08-30 22:37:24 +0000

    cdnew revision
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@4228 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_loadbci.m
+++ functions/popfunc/pop_loadbci.m
@@ -33,6 +33,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.15  2003/12/16 20:02:02  arno
+% remove folder separator
+%
 % Revision 1.14  2003/09/23 20:34:03  arno
 % DEBUGGED VERSION 1.9
 %
@@ -69,121 +72,294 @@
 %
 
 function [EEG, command] = pop_loadbci(filename, srate); 
-EEG = [];
-command = '';
 
-if nargin < 1
-	% ask user
-	[filename, filepath] = uigetfile('*.*', 'Choose a BCI file -- pop_loadbci'); 
-    drawnow;
-	if filename == 0 return; end;
-	filename = [filepath filename];
-	promptstr    = { 'Sampling rate' };
-	inistr       = { '256' };
-	result       = inputdlg2( promptstr, 'Import BCI2000 data -- pop_loadbci()', 1,  inistr, 'pop_loadbci');
-	if length(result) == 0 return; end;
-	srate   = eval( result{1} );
-end;
-
-% try to read as matlab
-EEG = eeg_emptyset;
-fprintf('Pop_loadbci: importing BCI file...\n');
-try
-	load( filename, '-mat');
-catch
-	
-	fid = fopen(filename, 'r');
-	allcollumns = fgetl(fid);
-	colnames = {};
-	while ~isempty(deblank(allcollumns))
-		[colnames{end+1} allcollumns] = strtok(allcollumns);
-	end;
-	tmpdata = fscanf(fid, '%f', Inf);
-	tmpdata = reshape(tmpdata, length(colnames), length(tmpdata)/length(colnames));
-	%tmpdata = tmpdata';
-	
-	% find electrode indices
-	% ----------------------
-	indices = [];
-	for index = 1:length(colnames)
-		if strcmp( colnames{index}(1:2), 'ch')
-			indices = [indices index ];
-		end;
-	end;
+    EEG = [];
+    command = '';
     
-    EEG.data = tmpdata(indices,:);
-    EEG.nbchan = size(EEG.data, 1);
-    EEG.srate  = srate;
-    try
-        eventindices = setdiff(1:length(colnames), indices);
-        ISIind = eventindices(3 + 9);
-        eventindices(3 + [ 1 2 3 4 7 8 9 10 11 12]) = [];
-        eventindices(1:3) = []; % suppress these event 
-        
-        % add the trial number
-        % --------------------
-        tmptrial = find( diff(tmpdata(ISIind, :)) ~= 0);
-        tmptrial = tmptrial+1;
-        
-        % process events
-        % --------------
-        fprintf('Pop_loadbci: importing events...\n');
-        counte = 1; % event counter
-        events(10000).latency = 0;
-        for index = eventindices
-            counttrial = 1;
-            tmpevent = find( diff(tmpdata(index, :)) ~= 0);
-            tmpevent = tmpevent+1;
-            for tmpi = tmpevent
-                if tmpdata(index, tmpi)
-                    events(counte).type    = [ colnames{index} int2str(tmpdata(index, tmpi)) ];
-                    events(counte).latency = tmpi;
-                    %events(counte).value   = tmpdata(index, tmpi);
-                    %while tmpi > tmptrial(counttrial) & counttrial < length(tmptrial)
-                    %	counttrial = counttrial+1;
-                    %end;
-                    %events(counte).trial = counttrial;				
-                    counte = counte+1;
-                    %if mod(counte, 100) == 0, fprintf('%d ', counte); end;
-                end;
-            end;
-        end;
-	
-        % add up or down events
-        % ---------------------
-        EEG = eeg_checkset(EEG);
-        EEG.event = events(1:counte-1);	
-        EEG = pop_editeventvals( EEG, 'sort', { 'latency', [0] } );
-        for index=1:length(EEG.event)
-            if strcmp(EEG.event(index).type(1:6), 'Target')
-                targetcode = str2num(EEG.event(index).type(end));
-                if targetcode == 1
-                    EEG.event(index).type = 'toptarget';
-                else
-                    EEG.event(index).type = 'bottomtarget';
-                end;
-            else 
-                if strcmp(EEG.event(index).type(1:6), 'Result')
-                    resultcode = str2num(EEG.event(index).type(end));
-                    if resultcode == 1
-                        EEG.event(index).type = 'topresp';
-                    else
-                        EEG.event(index).type = 'bottomresp';
-                    end;
-                    EEG.event(end+1).latency = EEG.event(index).latency;
-                    if (resultcode == targetcode) 
-                        EEG.event(end).type = 'correct';
-                    else
-                        EEG.event(end).type = 'miss';
-                    end;
-                end;
-            end;
-        end;
-        EEG = pop_editeventvals( EEG, 'sort', { 'latency', [0] } );
-        %EEG.data = tmpdata([72 73 75],:);
-    catch, disp('Failed to import data events');
+    if nargin < 1
+        % ask user
+        [filename, filepath] = uigetfile('*.*', 'Choose a BCI file -- pop_loadbci'); 
+        drawnow;
+        if filename == 0 return; end;
+        filename = [filepath filename];
+        promptstr    = { 'Sampling rate' };
+        inistr       = { '256' };
+        result       = inputdlg2( promptstr, 'Import BCI2000 data -- pop_loadbci()', 1,  inistr, 'pop_loadbci');
+        if length(result) == 0 return; end;
+        srate   = eval( result{1} );
     end;
-end;
+    
+    % import data
+    % -----------
+    EEG = eeg_emptyset;
+    fprintf('Pop_loadbci: importing BCI file...\n');
+    try
+        % try to read as matlab
+        % ---------------------
+        bci = load( filename, '-mat');
+    catch
+        % get file names
+        % --------------
+        fields = loadtxt(filename, 'nlines', 1, 'verbose', 'off');
+        if length(fields) > 300
+            error('Not a BCI ASCII file');
+        end;
+        
+        % read data
+        % ---------
+        fid = fopen(filename, 'r');
+        allcollumns = fgetl(fid);
+        tmpdata = fscanf(fid, '%d', Inf);
+        tmpdata = reshape(tmpdata, length(fields), length(tmpdata)/length(fields));
+        
+        % data channel range
+        % ------------------
+        indices = strmatch('ch', fields);
+        
+        bci = [];
+        for index = setdiff(1:length(fields), indices)
+            bci = setfield(bci, fields{index}, tmpdata(index,:));
+        end;
+        bci.signal = tmpdata(indices,:);
+    end;
 
-command = sprintf('EEG = pop_loadbci(''%s'', %f);',filename, srate); 
-return;
+    % ask for which event to import
+    % -----------------------------
+    geom = {[0.7 0.7 0.7]};
+    uilist = { { 'style' 'text' 'string' 'State name' 'fontweight' 'bold' } ...
+               { 'style' 'text' 'string' '    Import' 'fontweight' 'bold'  } ...
+               { 'style' 'text' 'string' 'Type of'  'fontweight' 'bold' } };
+    allfields = setdiff(fieldnames(bci), 'signal');
+    latencyfields = { '-----' };
+    for index = 1:length(allfields)
+        if ~isempty(findstr( lower(allfields{index}), 'time')) 
+            latencyfields{end+1} = allfields{index};
+        end;
+    end;
+    for index = 1:length(allfields)
+        geom   = { geom{:} [1.3 0.3 0.3 0.3 1] };
+        uilist = { uilist{:} { 'style' 'text' 'string' allfields{index} } };
+        if ~isempty(findstr( lower(allfields{index}), 'time'))
+            uilist = { uilist{:} { 'style' 'checkbox' 'value' 0 } { } { } { } };
+        else
+            uilist = { uilist{:} { 'style' 'checkbox' } { } { } ...
+                       { 'style' 'listbox' 'string' strvcat(latencyfields) } };
+        end;
+    end;
+    geom   = { geom{:} [1] [0.08 1] };
+    uilist = { uilist{:} { } { 'style' 'checkbox' 'value' 0 } ...
+               { 'style' 'text' 'string' 'Attempt to adjust event latencies using sourcetime?' } };
+    
+    result = inputgui( geom, uilist, 'pophelp(''pop_loadbci'')', 'Import BCI2000 data files - pop_loadbci()');
+    if isempty(result), return; end;
+    
+    % convert results to command line input
+    % -------------------------------------
+    listimport = {};
+    count = 1;
+    for index = 1:length(allfields)
+        if ~isempty(findstr( lower(allfields{index}), 'time')) 
+            if result{count}, listimport =  { listimport{:} 'event' { allfields{index} } }; end;
+            count = count+1;
+        else 
+            if result{count}
+                if result{count+1} ~= 1
+                    listimport =  { listimport{:} 'event' { allfields{index}  allfields{result{count+1}-1} } };
+                else
+                    listimport =  { listimport{:} 'event' { allfields{index} } };
+                end;
+            end;
+            count = count+2;                
+        end;
+    end;
+    if result{end}, adjust = 1; else adjust = 0; end;
+    
+    % decode command line input 
+    % -------------------------
+    count = 1;
+    for index = 2:2:length(listimport)
+        tmpindmatch = strmatch(listimport{index}{1}, allfields);
+        if ~isempty(tmpindmatch), indeximport(count) = tmpindmatch; 
+        else error(['State ''' listimport{index}{1} ''' not found']); 
+        end;
+        if length( listimport{index} ) > 1
+            tmpindmatch = strmatch(listimport{index}{2}, allfields);
+            if ~isempty(tmpindmatch), corresp(count) = tmpindmatch; 
+            else error(['State ''' listimport{index}{2} ''' not found']); 
+            end;
+        else
+            corresp(count) = 0;
+        end;
+        count = count+1;
+    end;
+        
+    % find block size
+    % ---------------
+    tmpevent = find( diff(getfield(bci, 'SourceTime')) ~= 0);
+    diffevent = tmpevent(2:end)-tmpevent(1:end-1);
+    blocksize = unique(diffevent);
+    if length(blocksize) > 1, error('Error in determining block size'); 
+    else                      fprintf('Blocksize: %d\n', blocksize); 
+    end;
+    
+    % find types
+    % ----------
+    tmpcorresp      = find(corresp);
+    indexcorresp    = corresp(tmpcorresp);
+    indexcorrespval = indeximport(tmpcorresp);
+    if length(tmpcorresp) ~= length(intersect(corresp, indeximport))
+        disp('Warning: correspondance problem, some information will be lost');
+    end;
+    
+    % remove type from latency array
+    % ------------------------------
+    indeximport(tmpcorresp) = [];
+    if adjust
+        fprintf('Latency of event will be adjusted\n');
+    else fprintf('WARNING: Latency of event will not be adjusted (latency uncertainty %2.1f ms)\n', ...
+                 blocksize/srate*1000);
+    end;
+    
+	% process events
+	% --------------
+	fprintf('Pop_loadbci: importing events...\n');
+	counte = 1; % event counter
+	events(10000).latency = 0;
+    indexsource =  strmatch('sourcetime', lower( allfields ) );
+    sourcetime  = getfield(bci, allfields{ indexsource });
+	for index = 1:length(indeximport)
+        tmpdata  = getfield(bci, allfields{indeximport(index)});
+		tmpevent = find( diff(tmpdata) ~= 0);
+		tmpevent = tmpevent+1;
+        tmpcorresp = find(indexcorresp == indeximport(index));
+        
+		for tmpi = tmpevent'
+            if ~isempty(tmpcorresp)
+                events(counte).type = allfields{ indexcorrespval(tmpcorresp) };
+            else 
+                events(counte).type = allfields{ indeximport(index) };
+            end;
+            if adjust
+                baselatency = sourcetime(tmpi); % note that this is the first bin a block
+                realtmpi    = tmpi+blocksize;   % jump to the end of the block+1
+                if curlatency < baselatency, curlatency = curlatency+65536; end; % in ms
+                events(counte).latency = realtmpi+(curlatency-baselatency)/1000*srate;
+                % (curlatency-baselatency)/1000*srate
+                % there is still a potentially large error between baselatency <-> realtmpi
+            else
+                events(counte).latency = tmpi+(blocksize-1)/2;
+            end;
+            counte = counte+1;
+		end;
+	end; 
+           
+	EEG.data = bci.signal';
+	EEG.nbchan = size(EEG.data, 1);
+	EEG.pnts   = size(EEG.data, 2);
+	EEG.trials = 1;
+	EEG.srate  = srate;
+	EEG = eeg_checkset(EEG);
+	EEG.event = events(1:counte-1);	
+	EEG = eeg_checkset( EEG, 'eventconsistency' );
+	EEG = pop_editeventvals( EEG, 'sort', { 'latency', [0] } );
+    
+% $$$ 
+% $$$ 
+% $$$ 
+% $$$ 
+% $$$ 
+% $$$ 
+% $$$ 
+% $$$ 
+% $$$ 
+% $$$ 
+% $$$ 
+% $$$ 
+% $$$ 
+% $$$ 
+% $$$ dsffd
+% $$$         
+% $$$ 	% find electrode indices
+% $$$ 	% ----------------------
+% $$$ 	indices = [];
+% $$$ 	for index = 1:length(colnames)
+% $$$ 		if strcmp( colnames{index}(1:2), 'ch')
+% $$$ 			indices = [indices index ];
+% $$$ 		end;
+% $$$ 	end;
+% $$$     
+% $$$     EEG.data = tmpdata(indices,:);
+% $$$     EEG.nbchan = size(EEG.data, 1);
+% $$$     EEG.srate  = srate;
+% $$$     try
+% $$$         eventindices = setdiff(1:length(colnames), indices);
+% $$$         ISIind = eventindices(3 + 9);
+% $$$         eventindices(3 + [ 1 2 3 4 7 8 9 10 11 12]) = [];
+% $$$         eventindices(1:3) = []; % suppress these event 
+% $$$         
+% $$$         % add the trial number
+% $$$         % --------------------
+% $$$         tmptrial = find( diff(tmpdata(ISIind, :)) ~= 0);
+% $$$         tmptrial = tmptrial+1;
+% $$$         
+% $$$         % process events
+% $$$         % --------------
+% $$$         fprintf('Pop_loadbci: importing events...\n');
+% $$$         counte = 1; % event counter
+% $$$         events(10000).latency = 0;
+% $$$         for index = eventindices
+% $$$             counttrial = 1;
+% $$$             tmpevent = find( diff(tmpdata(index, :)) ~= 0);
+% $$$             tmpevent = tmpevent+1;
+% $$$             for tmpi = tmpevent
+% $$$                 if tmpdata(index, tmpi)
+% $$$                     events(counte).type    = [ colnames{index} int2str(tmpdata(index, tmpi)) ];
+% $$$                     events(counte).latency = tmpi;
+% $$$                     %events(counte).value   = tmpdata(index, tmpi);
+% $$$                     %while tmpi > tmptrial(counttrial) & counttrial < length(tmptrial)
+% $$$                     %	counttrial = counttrial+1;
+% $$$                     %end;
+% $$$                     %events(counte).trial = counttrial;				
+% $$$                     counte = counte+1;
+% $$$                     %if mod(counte, 100) == 0, fprintf('%d ', counte); end;
+% $$$                 end;
+% $$$             end;
+% $$$         end;
+% $$$ 	
+% $$$         % add up or down events
+% $$$         % ---------------------
+% $$$         EEG = eeg_checkset(EEG);
+% $$$         EEG.event = events(1:counte-1);	
+% $$$         EEG = pop_editeventvals( EEG, 'sort', { 'latency', [0] } );
+% $$$         for index=1:length(EEG.event)
+% $$$             if strcmp(EEG.event(index).type(1:6), 'Target')
+% $$$                 targetcode = str2num(EEG.event(index).type(end));
+% $$$                 if targetcode == 1
+% $$$                     EEG.event(index).type = 'toptarget';
+% $$$                 else
+% $$$                     EEG.event(index).type = 'bottomtarget';
+% $$$                 end;
+% $$$             else 
+% $$$                 if strcmp(EEG.event(index).type(1:6), 'Result')
+% $$$                     resultcode = str2num(EEG.event(index).type(end));
+% $$$                     if resultcode == 1
+% $$$                         EEG.event(index).type = 'topresp';
+% $$$                     else
+% $$$                         EEG.event(index).type = 'bottomresp';
+% $$$                     end;
+% $$$                     EEG.event(end+1).latency = EEG.event(index).latency;
+% $$$                     if (resultcode == targetcode) 
+% $$$                         EEG.event(end).type = 'correct';
+% $$$                     else
+% $$$                         EEG.event(end).type = 'miss';
+% $$$                     end;
+% $$$                 end;
+% $$$             end;
+% $$$         end;
+% $$$         EEG = pop_editeventvals( EEG, 'sort', { 'latency', [0] } );
+% $$$         %EEG.data = tmpdata([72 73 75],:);
+% $$$     catch, disp('Failed to import data events');
+% $$$     end;
+% $$$ 
+% $$$ command = sprintf('EEG = pop_loadbci(''%s'', %f);',filename, srate); 
+% $$$ return;
