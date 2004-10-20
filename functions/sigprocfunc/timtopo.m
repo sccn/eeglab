dc9d692717be75154ef102db562192a27e830049
Scott Makeig <smakeig@ucsd.edu>
2004-10-20 15:20:27 +0000

    still bug at line 551 -sm
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@4340 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/timtopo.m
+++ functions/sigprocfunc/timtopo.m
@@ -40,6 +40,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.69  2004/10/20 15:19:03  scott
+% nothing
+%
 % Revision 1.68  2004/04/25 20:07:28  scott
 % typo
 %
@@ -544,11 +547,8 @@ for t=1:ntopos
   axes(axtp)                             % topoplot axes
   cla
 
-  topoargs = ' ';
   if ~isempty(varargin)
-  for v = varargin{:}
-    topoargs = [ topoargs ' ' cell2mat(v)];
-  end
+    topoargs = varargin{:};
   else
     topoargs = [];
   end
