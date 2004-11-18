b47793ac538bd5c99e36987ed1239060a27e01ea
Arnaud Delorme <arno@ucsd.edu>
2004-11-18 23:20:25 +0000

    fixed urevent boundary latency
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@4449 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_mergeset.m
+++ functions/popfunc/pop_mergeset.m
@@ -40,6 +40,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.30  2004/11/15 22:36:52  arno
+% latency of urevent boundary
+%
 % Revision 1.29  2004/11/09 23:51:00  arno
 % boundary in urevent
 %
@@ -257,7 +260,7 @@ else
             % insert boundary event
             % ---------------------
             INEEG1.urevent(end+1).type    = 'boundary';
-            INEEG1.urevent(end  ).latency = max(INEEG1.pnts, INEEG1.urevent(end-1).latency)+0.5;
+            INEEG1.urevent(end  ).latency = max(INEEG1pnts, INEEG1.urevent(end-1).latency)+0.5;
             
             % update urevent indices for second dataset
             % -----------------------------------------
