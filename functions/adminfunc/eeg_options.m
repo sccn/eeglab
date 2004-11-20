387d8677e0e052277aa811b18899bf3d06a65b8a
Arnaud Delorme <arno@ucsd.edu>
2004-11-20 01:23:33 +0000

    change text
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@4453 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/eeg_options.m
+++ functions/adminfunc/eeg_options.m
@@ -5,4 +5,4 @@
 
 option_computeica = 1 ; % Precompute ICA activations (uses RAM) 
 option_keepdataset = 1 ; % Retain parent datasets (uses RAM)
-option_savematlab = 1 ; % Store data in the .set file (inactivate for clustering)
+option_savematlab = 1 ; % Store data in the .set file (recommended for future clustering option)
