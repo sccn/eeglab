21fb1b0b62103f85d1bd2af3f704da8ce43650ee
Hilit Serby <hilits@gmail.com>
2004-11-29 23:12:43 +0000

    verifing that everyone have perimissions to eeglab.cfg
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@4478 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/uigetfile2.m
+++ functions/sigprocfunc/uigetfile2.m
@@ -24,6 +24,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.2  2004/11/23 19:27:14  scott
+% edit help
+%
 % Revision 1.1  2004/09/09 22:56:44  arno
 % Initial revision
 %
@@ -50,7 +53,12 @@ function varargout = uigetfile2(varargin);
     [varargout{1} varargout{2}] = uigetfile(varargin{:});
     if varargout{1} ~= 0
         Path = varargout{2};
+        cd(olddir);
         save(fullfile(getenv('TEMP'),'eeglab.cfg'),'Path','-mat');
+        if isunix
+            system('chmod 777 eeglab.cfg');
+        end
+    else
+       cd(olddir) 
     end;
-    cd(olddir)
-    
+     
