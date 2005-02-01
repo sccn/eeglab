5f1f8907f4ebd0b5681c13c732daff9b9c16cb0b
Arnaud Delorme <arno@ucsd.edu>
2005-02-01 23:21:21 +0000

    update by Tom
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@4576 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/eeg_amplitudearea.m
+++ functions/popfunc/eeg_amplitudearea.m
@@ -2,9 +2,9 @@
 %                       at a new sample rate (resrate) in Hz to get the exact limits 
 %                       of the window of integration. Finely samples the window 
 %                       and adds together very narrow rectangles capped by 
-%                       right-angled triangles under the window. Output is in uVms. 
-%                       trade-off between speed and number of resamples and 
-%                       ??? channel selected occurs.
+%                       right-angled triangles under the window. Output is in uV. 
+%                       Trade-off between speed and number of resamples and number of 
+%                        channels selected occurs.
 % Usage:
 %      >> [channels, amplitude] = eeg_amplitudearea(EEG,channels, resrate, wstart, wend);
 % Inputs:
@@ -25,14 +25,16 @@
 %         Engineering Centre, Helsinki University Central Hospital Helsinki Brain 
 %         Research Centre (tom.campbell@helsinki.fi) Spartam nanctus es: Hanc exorna. 
 %         Combined with amplitudearea_msuV() by Darren Weber, UCSF 28/1/05
+%         Retested and debugged Tom Campbell 2/2/05
+
 
 function [channels,overall_amplitude] = eeg_amplitudearea(EEG, channels, resrate, wstart, wend)
 
 if wstart > wend
     error ('ERROR: wstart must be greater than wend')
 else
-    [channels, overall_amplitude1] = amplitudearea_msuV (EEG,channels, resrate, wstart, wend)
-    overall_amplitude = overall_amplitude1/(wend -wstart)
+    [channels, overall_amplitude] = amplitudearea_msuV (EEG,channels, resrate, wstart, wend)
+    overall_amplitude = overall_amplitude/(wend - wstart)
 end
 
 return
@@ -40,21 +42,23 @@ return
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [channels, overall_area] = amplitudearea_msuV (EEG, channels, resrate, wstart, wend)
 
-if ndim(EEG.data) ~= 3
-  error('EEG.data must be 3-D data epochs');
-end
+%if ndim(EEG.data) ~= 3
+%  error('EEG.data must be 3-D data epochs');
+%end
 erp = mean(EEG.data,3);
-restep = (100/resrate)
+
 [tmp ind1] =min( abs( EEG.times - wstart ) ); % closest time index to wstart
 [tmp ind2] =min( abs( EEG.times - wend ) ); % closest time index to wend
+restep = 1000/resrate
 
-if EEG.times(ind1) > wstart
+
+if EEG.times(ind1) > wstart 
     ind1= ind1 -1;
 end
 
-if EEG.times(ind2) < wend
+if EEG.times(ind2) < wend 
     ind2= ind2 +1;
-end
+end    
 
 for x= ind1:ind2
     t = (x -ind1)+1;
@@ -70,80 +74,73 @@ end
 
 for x = 1:size(channels,2)
     channel = channels(x)
-    % resamples
+    %resamples
     rerp(x, 1:tr) = spline(tim(:),erp(channel, ind1:ind2), timr(1:tr))
     for y = 1:(tr -1)
-          % identify which sample is the height of the rectangle under the
-          % curve and which sample is the height of the triangle capping the
-          % rectangle
+        %identify which sample is the height of the rectangle under the
+        %curve and which sample is the height of the triangle capping the
+        %rectangle
         if (abs(rerp(x,y)) <= abs(rerp(x,y+1)))
             rhtsamp = y
             thtsamp = y + 1
-        else
+        else 
             rhtsamp = y + 1
             thtsamp = y
         end
-        if y < (tr-1)
+        if y < (tr-1) | ((y == (tr-1)) & (wend - timr(tr - 1)) == restep) 
             if ((rerp(x,y) > 0)& (rerp(x,y+1) < 0))|((rerp(x,y) < 0)& (rerp(x,y+1) > 0))
-              % handles samples containing a zerocrossing with two
-              % triangles and trigonometry
+                %handles samples containing a zerocrossing with two
+                %triangles and trigonometry
                 opposite = abs(rerp(x,y))+abs(rerp(x,y+1))
                 adjacent = restep
                 tantheta = opposite/adjacent
-                zerocross = abs(rerp(x,y))/tantheta
-                remainder = adjacent - zerocross
-                area(x,y) = ((zerocross/2)* (rerp(x,y)))+ ((remainder/2)* (rerp(x,y+1)))
-            else
-                rectanglearea = rerp(x,(rhtsamp))*restep
-                trianglearea = (rerp(x,(thtsamp))-rerp(x,(rhtsamp)))*restep/2
+                zerocross(x,y) = abs(rerp(x,y))/tantheta
+                remainder(x,y) = adjacent - zerocross(x,y)
+                area(x,y) = ((zerocross(x,y)/2)* (rerp(x,y)))+ ((remainder(x,y)/2)* (rerp(x,y+1)))
+            else       
+                rectanglearea = rerp(x,(rhtsamp))*restep 
+                trianglearea = (rerp(x,(thtsamp))-rerp(x,(rhtsamp)))*restep/2 
                 area(x,y) =  rectanglearea  + trianglearea
-            end
-        elseif y == (tr-1)
-              % special conditions for dealing with last and possibly curtailed
-              % sample in window of integration
-              % first calculate preconditions to see if zerocrossing occurs
-              % prior to end of window of integration in the sample
-            endstep =  wend - timr(tr-1)
+            end 
+        elseif (y == (tr-1)) & ((wend - timr(tr - 1)) < restep)
+            endstep = wend - timr(tr - 1)
             opposite = abs(rerp(x,y))+abs(rerp(x,y+1))
             adjacent = restep
             tantheta = opposite/adjacent
             zerocross = abs(rerp(x,y))/tantheta
-            if zerocross < endstep
+             if (((rerp(x,y) > 0)& (rerp(x,y+1) < 0))|((rerp(x,y) < 0)& (rerp(x,y+1) > 0)))&(zerocross < endstep)
                 %handles samples containing a zerocrossing with two
                 %triangles and trigonometry
                 remainder = endstep - zerocross
                 opposite2 = tantheta * remainder * sign(rerp(x,y)) * -1
                 area(x,y) = ((zerocross/2)* (rerp(x,y)))+ ((remainder/2)* opposite2)
-            elseif rhtsamp > thtsamp
-              % tests for amplitude decrease from left to right and fits area with
-              % two rectangles capped by a triangle with trigonometry
+            elseif thtsamp > rhtsamp
+                % test for amplitude increase from left to right and fits
+                % area with a rectangle capped by a triangle
                 opposite = abs(rerp(x,(thtsamp))) - abs(rerp(x,(rhtsamp)))
                 adjacent = restep
                 tantheta = opposite/adjacent
-                endstep = wend - timr(tr- 1)
+                triht = tantheta * endstep * sign(rerp(x,(thtsamp)))
+                trianglearea = (endstep/2)*triht 
+                rectanglearea = rerp(x,(rhtsamp))*endstep
+                area(x,y) =  rectanglearea + trianglearea
+            elseif rhtsamp > thtsamp 
+                 % tests for amplitude decrease from left to right and fits area with
+                % two rectangles capped by a triangle with trigonometry
+                opposite = abs(rerp(x,(thtsamp))) - abs(rerp(x,(rhtsamp))) 
+                adjacent = restep
+                tantheta = opposite/adjacent
                 excessstep = restep - endstep
-                extrarectangleht = tantheta* excessstep
+                extrarectangleht = tantheta* excessstep * sign(rerp(x,(thtsamp)))
                 extrarectanglearea = extrarectangleht * endstep *sign(rerp(x,(thtsamp)))
-                triht= opposite - extrarectangleht
+                triht = (opposite - extrarectangleht) * sign(rerp(x,(thtsamp)))
                 trianglearea = (endstep/2)*triht *sign(rerp(x,(thtsamp)))
                 rectanglearea = rerp(x,(rhtsamp))*endstep
                 area(x,y) =  rectanglearea + extrarectanglearea + trianglearea
-            elseif thtsamp > rhtsamp
-              % test for amplitude increase from left to right and fits
-              % area with a rectangle capped by a triangle
-                opposite = abs(rerp(x,(rhtsamp))) - abs(rerp(x,(rhtsamp)))
-                adjacent = restep
-                tantheta = opposite/adjacent
-                endstep = timr(tr) - wend
-                triht = rerp(x,thtsamp)- rerp(x,rhtsamp)
-                trianglearea = (endstep/2)*triht
-                rectanglearea = rerp(x,(rhtsamp))*endstep
-                area(x,y) =  rectanglearea + trianglearea
             end
         end
     end
 end
-
 
 for x = 1:(size(channels,2))
     overall_area(x) = sum(area(x,:))
