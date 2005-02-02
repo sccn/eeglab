bb11fd73b2acedbdee09cfed6354fec3568dec21
Arnaud Delorme <arno@ucsd.edu>
2005-02-02 20:08:06 +0000

    empty SegCatIndex if not segmented data
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@4582 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/readegi.m
+++ functions/sigprocfunc/readegi.m
@@ -45,6 +45,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.15  2005/02/02 20:06:37  arno
+% error msg
+%
 % Revision 1.14  2005/02/02 20:05:15  arno
 % returning segment category indices
 %
@@ -169,6 +172,7 @@ end
 % read in epoch data
 readtotal = 0;
 j=0;
+SegmentCatIndex = [];
 if (segmented),
 
     for i=1:head.segments,
