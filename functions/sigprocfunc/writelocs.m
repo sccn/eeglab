56e07000d5a7406d1d229dea774ebb2cca4befaa
Arnaud Delorme <arno@ucsd.edu>
2005-03-09 18:49:57 +0000

    new format
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@4664 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/writelocs.m
+++ functions/sigprocfunc/writelocs.m
@@ -90,6 +90,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.10  2003/05/13 23:18:51  arno
+% default unicoord on
+%
 % Revision 1.9  2003/05/13 23:13:23  arno
 % verbose off for convertlocs
 %
@@ -128,7 +131,18 @@ end;
 
 % get infos from readlocs
 % -----------------------
-[listtype formatinfo listcolformat formatskip] = readlocs('getinfos');
+%[listtype formatinfo listcolformat formatskip] = readlocs('getinfos');
+[chanformat listcolformat] = readlocs('getinfos');
+indformat  = [];
+for index = 1:length(chanformat), 
+    if ~isstr(chanformat(index).importformat)
+        indformat = [ indformat index ];
+    end;
+    if isempty(chanformat(index).skipline), chanformat(index).skipline = 0; end;
+end;
+listtype   = { chanformat(indformat).type };
+formatinfo = { chanformat(indformat).importformat };
+formatskip = [ chanformat(indformat).skipline ];
 
 g = finputcheck( varargin, ...
                  { 'filetype'	  'string'	 listtype 			'loc';
