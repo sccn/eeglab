b12d63b1b59f2882a07b0f2d5550185883bc3e85
Arnaud Delorme <arno@ucsd.edu>
2005-03-10 19:58:23 +0000

    adding header
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@4695 1e58238c-3a72-4c0f-aabe-36d030380f39

--- plugins/dipfit2.2/sph2spm.m
+++ plugins/dipfit2.2/sph2spm.m
@@ -1,5 +1,37 @@
-% compute homogenous transformation matrix from BESA spherical coordinates 
-% to SPM 3-D coordinates
+% sph2spm() - compute homogenous transformation matrix from 
+%             BESA spherical coordinates  to SPM 3-D coordinate
+%
+% Usage:
+%  >> trans = sph2spm;
+%
+% Outputs:
+%   trans - homogenous transformation matrix
+%
+% Note: head radius for spherical model is assumed to be 85 mm.
+%
+% Author: Robert Oostenveld, SMI/FCDC, Nijmegen 2005
+%         Arnaud Delorme, SCCN, La Jolla 2005
+
+% SMI, University Aalborg, Denmark http://www.smi.auc.dk/
+% FC Donders Centre, University Nijmegen, the Netherlands http://www.fcdonders.kun.nl/
+
+% Copyright (C) 2003 Robert Oostenveld, SMI/FCDC roberto@smi.auc.dk
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+% $Log: not supported by cvs2svn $
 
 function besa2SPM_result = besa2SPM;
 
