dc362b54a93bfc8d33e47abe629bb0cf3ab66594
Arnaud Delorme <arno@ucsd.edu>
2005-03-17 18:07:28 +0000

    beginning to remove BESA
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@4749 1e58238c-3a72-4c0f-aabe-36d030380f39

--- plugins/dipfit2.2/pop_dipplot.m
+++ plugins/dipfit2.2/pop_dipplot.m
@@ -19,8 +19,6 @@
 %
 % Inputs:
 %   EEG   - Input dataset
-%   type  - ['DIPFIT'|'BESA'] use either 'DIPFIT' dipoles or
-%           'BESA' dipoles.
 %   comps - [integer array] plot component indices. If empty
 %           all the localized components are plotted.
 %
@@ -53,6 +51,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.25  2005/03/17 16:39:12  arno
+% using coordformat instead of sph2spm
+%
 % Revision 1.24  2005/03/14 20:09:59  arno
 % fix spherical
 %
@@ -132,7 +133,7 @@ if nargin < 1
     return;
 end;
 
-if nargin < 3
+if nargin < 2
 	% popup window parameters
 	% -----------------------
     varstr = '';;
@@ -192,7 +193,13 @@ if nargin < 3
     if result{ind+7} == 1, options = { options{:} 'normlen'   'on' }; end;
     if ~isempty( result{ind+8} ), tmpopt = eval( [ '{' result{ind+8} '}' ] ); options = { options{:} tmpopt{:} }; end;
 else 
-    options = varargin;
+    if isstr(comp)
+        options = { comp varargin{:} };
+        comp = typedip;
+        typedip = 'dipfit';
+    else
+        options = varargin;
+    end;
 end;
 
 if strcmpi(typedip, 'besa')
