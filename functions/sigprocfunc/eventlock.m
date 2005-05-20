e22df6a4da4cfd2cab04e75872d534aa91a1dadf
Arnaud Delorme <arno@ucsd.edu>
2005-05-20 22:08:02 +0000

    type
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@4861 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/eventlock.m
+++ functions/sigprocfunc/eventlock.m
@@ -51,6 +51,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.2  2002/08/17 00:17:24  arno
+% header
+%
 % Revision 1.1  2002/04/05 17:36:45  jorn
 % Initial revision
 %
@@ -105,7 +108,7 @@ else
 end
 
 if MULTICHANNEL
-  if ceil(frames*ntrials ~= size(data,2))
+  if ceil(double(frames*ntrials) ~= size(data,2))
    fprintf(...
  'Input data length (%d) is not a multiple of the eventframes length (%d).\n',...
                    size(data,2),                                    frames);
