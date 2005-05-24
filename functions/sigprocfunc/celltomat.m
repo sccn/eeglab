64ebd7d8c9e09f9506d9eae68a93114d5464427a
Arnaud Delorme <arno@ucsd.edu>
2005-05-24 17:52:29 +0000

    cell2mat -> celltomat
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@4878 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/celltomat.m
+++ functions/sigprocfunc/celltomat.m
@@ -1,10 +1,10 @@
-% cell2mat() - convert cell array to matrix
+% celltomat() - convert cell array to matrix
 %
-% Usage: >> M = cell2mat( C );
+% Usage: >> M = celltomat( C );
 %
 % Author: Arnaud Delorme, CNL / Salk Institute, Jan 25 2002
 %
-% Note: This function overloads the neuralnet toolbox function CELL2MAT,
+% Note: This function overloads the neuralnet toolbox function CELLTOMAT,
 % but does not have all its capacities. Delete this version if you have 
 % the toolbox.
 
@@ -27,14 +27,17 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.2  2003/01/01 17:55:53  scott
+% header edits
+%
 % Revision 1.1  2002/04/05 17:39:45  jorn
 % Initial revision
 %
 
-function M = cell2mat( C, varargin );
+function M = celltomat( C, varargin );
 
 if nargin < 1
-	help cell2mat;
+	help celltomat;
 	return;
 end;
 
