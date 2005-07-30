cd00be27dc688dd43bd1566b55ff315752b344e5
Arnaud Delorme <arno@ucsd.edu>
2005-07-30 01:52:32 +0000

    typo
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@4940 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_loadset.m
+++ functions/popfunc/pop_loadset.m
@@ -40,6 +40,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.34  2005/07/30 01:49:59  arno
+% load data
+%
 % Revision 1.33  2005/04/12 21:43:59  arno
 % same
 %
@@ -237,7 +240,7 @@ else
     if isstr(VAROUT.data), VAROUT.filepath = inputpath; end;
 end;
 if strcmpi(mode, 'all')
-    VAROUT = eeg_checkset(VAROUT, 'dataload');
+    VAROUT = eeg_checkset(VAROUT, 'loaddata');
 end;
 command = sprintf('EEG = pop_loadset( ''%s'', ''%s'');', inputname, inputpath);
 return;
