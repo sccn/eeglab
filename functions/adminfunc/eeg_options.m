aa7047aa73106455cd98057c2258803bd256ac22
Arnaud Delorme <arno@ucsd.edu>
2005-08-04 15:40:12 +0000

    new version
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@4988 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/eeg_options.m
+++ functions/adminfunc/eeg_options.m
@@ -3,8 +3,9 @@
 % Note: DO NOT EDIT, instead use pop_editoptions() or the menu
 %       /File/Maximize memory in EEGLAB gui
 
-option_computeica = 1 ;  % [Set|Unset] -> [Do|Do not] Precompute ICA activations (+RAM) 
-option_keepdataset = 1 ; % [Set|Unset] -> [Do|Do not] Retain parent datasets (+RAM)
-option_savematlab = 1 ;  % [Set|Unset] -> [Do|Do not] write all dataset in a single file
-option_rememberfolder = 1 ;  % [Set|Unset] -> [Do|Do not] remember old folder when reading dataset
-option_storedisk = 0 ;   % [Set|Unset] -> [Do|Do not] write on disk non-current dataset
+% Memory options 
+option_storedisk = 0 ;   % If set, keep at most one dataset in memory (this allow processing dozens of datasets at a time)
+option_savematlab = 1 ;  % If set, write data in same file as dataset (unset - 2 files - allow reading one channel at a time from disk)
+option_computeica = 1 ;  % If set, precompute ICA activations (requires RAM, but faster plotting of component activation) 
+% Folder options
+option_rememberfolder = 1 ;  % If set, remember old folder when reading dataset
