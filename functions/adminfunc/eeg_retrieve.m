7ca55e2854e472a1f56a3b419399451cbdec8f0a
Arnaud Delorme <arno@ucsd.edu>
2005-09-08 16:54:29 +0000

    preserve saved field
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@5072 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/eeg_retrieve.m
+++ functions/adminfunc/eeg_retrieve.m
@@ -37,6 +37,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.11  2005/09/08 16:41:04  arno
+% fixing storing problem for saved datasets
+%
 % Revision 1.10  2005/08/08 18:03:55  arno
 % fix dissimilar structure problem
 %
@@ -82,14 +85,14 @@ end;
 try
     eeg_optionsbackup;
     eeg_options;
+    tmpsaved = ALLEEG(CURRENTSET).saved;
     if length(CURRENTSET) > 1 & option_storedisk
         [ EEG tmpcom ] = eeg_checkset(ALLEEG(CURRENTSET)); % do not load data if several datasets
     else
         [ EEG tmpcom ] = eeg_checkset(ALLEEG(CURRENTSET), 'loaddata');
     end;
-    if ~isempty(tmpcom)
-        [ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG, CURRENTSET);
-    end;
+    [ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG, CURRENTSET);
+    EEG.saved = tmpsaved;
 catch
 	fprintf('Warning: cannot retrieve dataset with index %d\n', CURRENTSET); 
 	return;
