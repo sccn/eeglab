ff17c62864df4817f2c271e4a51eee76d5f8e118
Scott Makeig <smakeig@ucsd.edu>
2005-09-29 14:56:41 +0000

    nothing
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@5113 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/topoplot.m
+++ functions/sigprocfunc/topoplot.m
@@ -158,6 +158,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.257  2005/09/27 21:59:49  arno
+% fix plotrad issue call from pop_chanedit
+%
 % Revision 1.256  2005/09/05 15:58:14  scott
 % nothing - spacing while looking through code -sm
 %
