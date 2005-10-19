0cd229e269bfe3d195a3add747e4c72bd397c0d6
Hilit Serby <hilits@gmail.com>
2005-10-19 19:24:37 +0000

    help message edits
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@5169 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/std_findoutlierclust.m
+++ functions/studyfunc/std_findoutlierclust.m
@@ -1,9 +1,9 @@
-% cls_findoutlierclust() - find if an outlier cluster already exist for a 
-%       specific cluster. If so return the outlier cluster index, if not 
-%       return zero.
+% cls_findoutlierclust() - determine whether an outlier cluster already exist 
+%       for a specified cluster. If so return the outlier cluster index;  
+%       if not return zero.
 %
-%       This is a helper function called from pop_clustedit(), 
-%       cls_moveoutlier() & cls_renameclust(). 
+%       This helper function is called by  pop_clustedit(), 
+%       cls_moveoutlier(), & cls_renameclust(). 
 
 function outlier_clust = cls_findoutlierclust(STUDY,clust)
 outlier_clust = 0;
