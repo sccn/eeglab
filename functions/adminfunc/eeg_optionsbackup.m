07cf18195c07a21c24ad31740c15e813996cbdbc
Arnaud Delorme <arno@ucsd.edu>
2005-10-25 21:52:22 +0000

    new option
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@5175 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/eeg_optionsbackup.m
+++ functions/adminfunc/eeg_optionsbackup.m
@@ -8,5 +8,6 @@ option_storedisk = 0 ;   % If set, keep at most one dataset in memory. Allows pr
 option_warningstore = 1 ;   % If set, warn user before overwriting a dataset file on disk - relevant only if the option above is set.
 option_savematlab = 1 ;  % If set, write the data and dataset structure into the same file; if unset, write two files per dataset (.dat .set).
 option_computeica = 1 ;  % If set, precompute ICA activations. This requires more RAM but allows faster plotting of component activations. 
+option_splinefile = 0 ;  % If set, save 3-D spline transformation in a seperate file; if unset, store the information localy in the dataset. 
 % Folder options
 option_rememberfolder = 1 ;  % If set, when reading dataset assume the folder/directory of previous dataset.
