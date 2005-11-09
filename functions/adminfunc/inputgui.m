ef9bd24b55fb0395bed33c7a6787d53dde0b2f09
Arnaud Delorme <arno@ucsd.edu>
2005-11-09 23:07:01 +0000

    nothing
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@5265 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/inputgui.m
+++ functions/adminfunc/inputgui.m
@@ -73,6 +73,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.28  2005/11/09 22:44:56  arno
+% fixing calling format and header
+%
 % Revision 1.27  2005/11/09 22:30:32  arno
 % new calling format
 %
