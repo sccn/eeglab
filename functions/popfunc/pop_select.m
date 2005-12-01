ca6c37be3b7b06ca2f60995c9e9ebe2f085ba139
Arnaud Delorme <arno@ucsd.edu>
2005-12-01 18:19:47 +0000

    nothing
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@5346 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_select.m
+++ functions/popfunc/pop_select.m
@@ -94,6 +94,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.51  2005/12/01 03:36:47  toby
+% Corrected channel remove bug
+%
 % Revision 1.50  2005/12/01 03:32:39  toby
 % *** empty log message ***
 %
