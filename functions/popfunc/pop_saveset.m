7e86ad6614ecc47c5c86ac51bb7bad06bb139fe7
Arnaud Delorme <arno@ucsd.edu>
2005-12-03 00:36:20 +0000

    nothing
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@5354 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_saveset.m
+++ functions/popfunc/pop_saveset.m
@@ -50,6 +50,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.64  2005/12/03 00:34:18  arno
+% processing multiple datasets
+%
 % Revision 1.63  2005/12/03 00:32:22  arno
 % remove last edits
 %
