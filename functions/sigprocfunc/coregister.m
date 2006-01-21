d3d2b8b027f3affba0ecdf6b45f0ec5cfe2c1a38
Scott Makeig <smakeig@ucsd.edu>
2006-01-21 02:55:56 +0000

    nothing
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@5492 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/coregister.m
+++ functions/sigprocfunc/coregister.m
@@ -1,34 +1,33 @@
-% coregister() -  Coregister electrode location file with template or mesh.
+% coregister() -  Coregister electrode location file with head template or mesh.
 %
 % Usage:
 %        >> coregister( chan1, chan2, 'key', 'val' )
 %
 % Inputs:
-%    chan1    - eglab channel location structure to align.
-%    chan2    - reference channel location file or eeglab channel location 
+%    chan1    - EEG.chanlocs channel location structure to align.
+%    chan2    - reference channel location file or EEG channel locations
 %               structure
 %
 % Optional input:
-%    'alignfid'  - [cell array of string] name of fiducials for alignment.
-%    'warp'      - [cell array of string] name of electrode for warping.
-%    'transform' - [real array] homogenous transformation matrix or 1x9
-%                  matrix containing 9 parameter traditional "Talairach-model" 
-%                  transformation (see traditional()).
-%    'mesh'      - [cell array|string] head mesh. Can contain 
-%                  { points triangles } or { point triangle normals } see the 
-%                  function plotmesh() for details. Can also contain the name
-%                  of a file containing head mesh information (several format
-%                  recognized).
-%    'chaninfo1' - [struct] channel information structure for first eeglab
-%                  channel location structure (might contain fiducials).
-%    'chaninfo2' - [struct] channel information structure for second eeglab
-%                  channel location structure (might contain fiducials).
+%    'alignfid'  - [cell array of strings] names of fiducials for alignment.
+%    'warp'      - [cell array of string] names of electrode for warping.
+%    'transform' - [real array] homogenous transformation matrix or a 
+%                  1x9 matrix containing traditional 9-parameter "Talairach 
+%                  model" transformation (see traditional()).
+%    'mesh'      - [cell array|string] head mesh. Can contain {points triangles} 
+%                  or {points triangle normals}. See the function plotmesh() 
+%                  for details. May also contain the name of a file containing 
+%                  head mesh information (several formats recognized).
+%    'chaninfo1' - [struct] EEG.chanlocs.chaninfo channel information structure 
+%                  for first EEG.chanlocs structure (may contain fiducials).
+%    'chaninfo2' - [struct] EEG.chanlocs.chaninfo channel information structure 
+%                  for second EEG.chanlocs structure (might contain fiducials).
 %    'autoscale' - ['on'|'off'] autoscale electrode radius when aligning 
-%                  fiducials default is 'on'.
+%                  fiducials. {default: 'on'}
 %    'helpmsg'   - ['on'|'off'] pop-up help message when calling function.
-%                  Default is 'off'.
+%                  {default: 'off'}
 % Output:
-%    chan1       - transformed channel location structure
+%    chan1       - transformed EEG.chanlocs channel location structure
 %    transform   - transformation matrix. Use function traditional() to 
 %                  convert to homogenous transformation matrix and input
 %                  it into functions like headplot().
@@ -54,6 +53,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.22  2006/01/21 00:31:24  arno
+% update help text message
+%
 % Revision 1.21  2006/01/21 00:02:41  arno
 % adding fiducials
 %
