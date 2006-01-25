e3ab40a71928f8ef59c95085890a7c0d5ffc7c36
Arnaud Delorme <arno@ucsd.edu>
2006-01-25 21:27:30 +0000

    nothing
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@5510 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_subcomp.m
+++ functions/popfunc/pop_subcomp.m
@@ -47,6 +47,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.15  2006/01/25 21:26:15  arno
+% add icachansind
+%
 % Revision 1.14  2006/01/25 21:24:16  arno
 % fixing remove components
 %
