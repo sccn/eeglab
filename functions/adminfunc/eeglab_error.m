909567bf271ab2593be570d24379e7cc120ea62c
Arnaud Delorme <arno@ucsd.edu>
2006-01-31 19:59:42 +0000

    fixing matlab 5 etc ...
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@5546 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/eeglab_error.m
+++ functions/adminfunc/eeglab_error.m
@@ -27,6 +27,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.1  2006/01/31 00:11:31  arno
+% Initial revision
+%
 % Revision 1.1  2006/01/26 21:25:24  arno
 % Initial revision
 %
@@ -35,12 +38,12 @@ function eeglab_error;
 
     % handling errords
     % ----------------
-    v = version;
-    if v(1) > 5
+    tmplasterr = lasterr;
+    try
         tmp = lasterror;
         tmperr = [ 'EEGLAB error in function ' tmp.stack(1).name '() at line ' int2str(tmp.stack(1).line) ':' 10 10 lasterr ];
-    else
-        tmperr = [ 'EEGLAB error:' 10 10 lasterr ];
+    catch % Matlab 5 and when the stack is empty
+        tmperr = [ 'EEGLAB error:' 10 10 tmplasterr ];
     end;
     tmperr = [ tmperr 10 10 'If you think it is a bug, send a detailed' 10 ...
                'description of how to reproduce the problem' 10 ...
