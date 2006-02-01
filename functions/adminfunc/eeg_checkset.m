c96b08feaa099957a2bb15b15ddd74e35dca15ca
Arnaud Delorme <arno@ucsd.edu>
2006-02-01 18:08:34 +0000

    nothing
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@5571 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/eeg_checkset.m
+++ functions/adminfunc/eeg_checkset.m
@@ -121,6 +121,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.181  2006/01/31 20:52:16  arno
+% eeglab options
+%
 % Revision 1.180  2006/01/26 00:23:32  arno
 % nothing
 %
