2b5d1a1ae71f9e1b5bb4b266dd5dcce7f485ad2c
Arnaud Delorme <arno@ucsd.edu>
2006-02-03 21:52:07 +0000

    nothing
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@5602 1e58238c-3a72-4c0f-aabe-36d030380f39

--- eeglab.m
+++ eeglab.m
@@ -187,6 +187,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.448  2006/02/03 00:47:34  arno
+% call to study
+%
 % Revision 1.447  2006/02/02 22:50:29  arno
 % fixing extracting epochs etc...
 %
@@ -1771,7 +1774,7 @@ catchstrs.new_non_empty          = e_newset;
 	cb_study1      = [ nocheck '[STUDYTMP ALLEEGTMP LASTCOM] = pop_study([], ALLEEG         , ''gui'', ''on'');'          e_load_study]; 
 	cb_study2      = [ nocheck '[STUDYTMP ALLEEGTMP LASTCOM] = pop_study([], isempty(ALLEEG), ''gui'', ''on'');'          e_load_study]; 
 	cb_loadstudy   = [ nocheck '[STUDYTMP ALLEEGTMP LASTCOM] = pop_loadstudy;'                                            e_load_study]; 
-	cb_savestudy1  = [ check   '[STUDYTMP ALLEEGTMP LASTCOM] = pop_savestudy(STUDY, EEG, ''savemode'', ''resave'');' e_load_study ];
+	cb_savestudy1  = [ check   '[STUDYTMP ALLEEGTMP LASTCOM] = pop_savestudy(STUDY, EEG, ''savemode'', ''resave'');'      e_load_study ];
 	cb_savestudy2  = [ check   '[STUDYTMP ALLEEGTMP LASTCOM] = pop_savestudy(STUDY, EEG);' e_hist_nh ];
 	cb_clearstudy  =           'LASTCOM = ''STUDY = []; CURRENTSTUDY = 0;''; eval(LASTCOM); eegh( LASTCOM );';
 	cb_editoptions = [ nocheck 'if isfield(ALLEEG, ''nbchan''), LASTCOM = pop_editoptions(length([ ALLEEG.nbchan ]) >1);' ...
