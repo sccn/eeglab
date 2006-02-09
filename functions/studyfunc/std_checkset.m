97cbf8e1392103bc4a593738d8e3024c9d25a968
Arnaud Delorme <arno@ucsd.edu>
2006-02-09 20:05:48 +0000

    same
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@5648 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/std_checkset.m
+++ functions/studyfunc/std_checkset.m
@@ -138,7 +138,7 @@ if isempty(STUDY.cluster)
     for k = 1:size(STUDY.setind,2)
         ind_nonnan = find(~isnan(STUDY.setind(:,k)));
         ncomps = size(ALLEEG(STUDY.datasetinfo(ind_nonnan(1)).index).icaweights,1);
-        STUDY.cluster(1).sets =  [STUDY.cluster(1).sets k*ones(1,ncomps];
+        STUDY.cluster(1).sets =  [STUDY.cluster(1).sets k*ones(1,ncomps)];
         STUDY.cluster(1).comps = [STUDY.cluster(1).comps      [1:ncomps]];
     end
     if length(STUDY.condition) > 1
