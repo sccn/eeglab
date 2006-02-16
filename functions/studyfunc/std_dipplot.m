9efb4fa462b7081503fc850ff478ead41e1bf1c9
Arnaud Delorme <arno@ucsd.edu>
2006-02-16 21:57:19 +0000

    making mode more compatible
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@5731 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/std_dipplot.m
+++ functions/studyfunc/std_dipplot.m
@@ -86,6 +86,9 @@ for k = 3:2:nargin
             return;
         case 'mode' % Plotting mode 'apart' / 'joined'
             mode = varargin{k-1};
+            if strcmpi(mode, 'centroid') | strcmpi(mode, 'comps')
+                if length(cls) == 1, mode = 'apart'; else mode = 'joined'; end;
+            end;
          case 'figure'
             if strcmpi(varargin{k-1},'off') 
                 opt_dipplot{end + 1} = 'gui';
