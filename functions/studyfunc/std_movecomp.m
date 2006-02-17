95bdb4891e2288f95ee0fe9189f34818422da056
Arnaud Delorme <arno@ucsd.edu>
2006-02-17 00:36:22 +0000

    moving cluster to and from Noclust
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@5742 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/std_movecomp.m
+++ functions/studyfunc/std_movecomp.m
@@ -42,11 +42,6 @@
 
 function STUDY = cls_movecomp(STUDY, ALLEEG, old_clus, new_clus, comps)
 icadefs;
-% Cannot move components if clusters are 'Notclust' clusters
-if strncmpi('Notclust',STUDY.cluster(old_clus).name,8) | strncmpi('Notclust',STUDY.cluster(new_clus).name,8)
-    warndlg2('cls_movecomp: cannot move components to or from Notclust cluster');
-    return;
-end
 
 % Cannot move components if clusters have children clusters
 if ~isempty(STUDY.cluster(old_clus).child)  | ~isempty(STUDY.cluster(new_clus).child)  
@@ -61,50 +56,63 @@ end
 % clusters (didn'y come from the same level of clustering), 
 % unless the cluster, components are moved to, is an empty new cluster. 
 if (length(STUDY.cluster(old_clus).parent) ~= length(STUDY.cluster(new_clus).parent)) & ~strcmp(STUDY.cluster(new_clus).parent, 'manual')
-    warndlg2('Cannot move components if clusters have different parent clusters!' , 'Aborting move components');
+    warndlg2(strvcat('Cannot move components if clusters have different parent clusters!', ...
+                     'This limitation will be fixed in the future'), 'Aborting move components');
     return;
 end
 % Check if all parents are the same
 if (~strcmp(STUDY.cluster(new_clus).parent, 'manual')) 
     if ~(sum(strcmp(STUDY.cluster(old_clus).parent, STUDY.cluster(new_clus).parent)) == length(STUDY.cluster(new_clus).parent))% different parent
-        warndlg2('Cannot move components if clusters have different parent clusters!' , 'Aborting move components');
+        warndlg2(strvcat('Cannot move components if clusters have different parent clusters!', ...
+                     'This limitation will be fixed in the future') , 'Aborting move components');
         return;
     end
 end
-try 
-    h_wait = waitbar(0,['Moving components ...'], 'Color', BACKEEGLABCOLOR,'position', [300, 200, 300, 48]);
-catch % for Matlab 5.3
-    h_wait = waitbar(0,['Moving components ...'],'position', [300, 200, 300, 48]);
-end        
+disp('Moving components...');
 for  ci = 1:length(comps)
-    comp =  STUDY.cluster(old_clus).comps(comps(ci));
+    comp = STUDY.cluster(old_clus).comps(comps(ci));
     sets = STUDY.cluster(old_clus).sets(:,comps(ci));
+    
     %update new cluster
-    STUDY.cluster(new_clus).comps(end+1) = comp;%with comp index
-    STUDY.cluster(new_clus).sets(:,end+1) = sets; %with set indices 
-    STUDY.cluster(new_clus).preclust.preclustdata(end+1,:) = STUDY.cluster(old_clus).preclust.preclustdata(comps(ci),:); %with preclustdata
+    indcomp = length(STUDY.cluster(new_clus));
+    STUDY.cluster(new_clus).comps(indcomp) = comp;%with comp index
+    STUDY.cluster(new_clus).sets(:,indcomp) = sets; %with set indices 
     if strcmpi(STUDY.cluster(new_clus).parent, 'manual')
         STUDY.cluster(new_clus).preclust.preclustparams = STUDY.cluster(old_clus).preclust.preclustparams;
         STUDY.cluster(new_clus).parent = STUDY.cluster(old_clus).parent;
         STUDY.cluster(find(strcmp({STUDY.cluster.name},STUDY.cluster(new_clus).parent))).child{end+1} = STUDY.cluster(new_clus).name;
     end
-    %sort by sets
+    
+    % update preclustering array
+    % --------------------------
+    if strncmpi('Notclust',STUDY.cluster(old_clus).name,8)
+        STUDY.cluster(new_clus).preclust.preclustparams = [];
+        STUDY.cluster(new_clus).preclust.preclustdata   = [];
+        STUDY.cluster(new_clus).preclust.preclustcomp   = [];
+        disp('Important warning: pre-clustering information removed for target cluster');
+        disp('(this is because the component moved had no pre-clustering data associated to it)');
+    elseif ~strncmpi('Notclust',STUDY.cluster(new_clus).name,8)
+        STUDY.cluster(new_clus).preclust.preclustdata(indcomp,:) = STUDY.cluster(old_clus).preclust.preclustdata(comps(ci),:); %with preclustdata
+    end;
+
+    % sort by sets
+    % ------------
     [tmp,sind] = sort(STUDY.cluster(new_clus).sets(1,:));
-    waitbar(ci/((length(comps)+1)*3),h_wait)
     STUDY.cluster(new_clus).sets = STUDY.cluster(new_clus).sets(:,sind);
     STUDY.cluster(new_clus).comps = STUDY.cluster(new_clus).comps(sind);
-    STUDY.cluster(new_clus).preclust.preclustdata = STUDY.cluster(new_clus).preclust.preclustdata(sind,:);
+    if ~isempty(STUDY.cluster(new_clus).preclust.preclustdata)
+        STUDY.cluster(new_clus).preclust.preclustdata(sind,:) = STUDY.cluster(new_clus).preclust.preclustdata(:,:);
+    end;
 end
 STUDY = cls_centroid(STUDY, ALLEEG, new_clus);%new centroid
-waitbar((3*(ci-1)+2)/((length(comps)+1)*3),h_wait)
 % Remove data from old cluster
 % left_comps - are all the components of the cluster after the
 % components that were moved to the new cluster were removed. 
 left_comps = find(~ismember([1:length(STUDY.cluster(old_clus).comps)],comps));
 STUDY.cluster(old_clus).comps = STUDY.cluster(old_clus).comps(left_comps);
 STUDY.cluster(old_clus).sets = STUDY.cluster(old_clus).sets(:,left_comps);
-STUDY.cluster(old_clus).preclust.preclustdata = STUDY.cluster(old_clus).preclust.preclustdata(left_comps,:);
+if ~isempty(STUDY.cluster(old_clus).preclust.preclustdata)
+    STUDY.cluster(old_clus).preclust.preclustdata = STUDY.cluster(old_clus).preclust.preclustdata(left_comps,:);
+end;
 STUDY = cls_centroid(STUDY, ALLEEG, old_clus);%new centroid
-waitbar((3*(ci-1)+3)/((length(comps)+1)*3),h_wait)
-waitbar(1,h_wait)
-delete(h_wait)
+disp('Done.');
