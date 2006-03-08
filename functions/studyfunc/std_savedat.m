fc0e363c0cd4fb9a843af781f8ac22a7531dfb2d
Arnaud Delorme <arno@ucsd.edu>
2006-03-08 23:38:36 +0000

    Initial revision
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@5955 1e58238c-3a72-4c0f-aabe-36d030380f39

---
+++ functions/studyfunc/std_savedat.m
@@ -0,0 +1,10 @@
+% save computed data
+
+function std_savedat( tmpfile, structure)
+    
+    v = version;
+    if v(1) < 7
+        save('-mat', tmpfile, '-struct', structure);
+    else
+        save('-v6' , tmpfile, '-struct', structure);
+    end;
