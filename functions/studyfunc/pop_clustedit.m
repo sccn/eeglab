720a2b08887edee6185a7ad4901a7e3852e429a9
Arnaud Delorme <arno@ucsd.edu>
2006-03-10 00:33:08 +0000

    remove reference to etc field
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@6010 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/pop_clustedit.m
+++ functions/studyfunc/pop_clustedit.m
@@ -305,10 +305,11 @@ if ~isstr(varargin{1})
     
     % enable buttons
     % --------------
-    if isfield(ALLEEG(1).etc, 'icaspec') , spec_enable  = 'on'; else spec_enable  = 'off'; end;
-    if isfield(ALLEEG(1).etc, 'icaerp')  , erp_enable   = 'on'; else erp_enable   = 'off'; end;
-    if isfield(ALLEEG(1).etc, 'icascalp'), scalp_enable = 'on'; else scalp_enable = 'off'; end;
-    if isfield(ALLEEG(1).etc, 'icaersp') , ersp_enable  = 'on'; else ersp_enable  = 'off'; end;
+    filename = fullfile( ALLEEG(1).filepath, ALLEEG(1).filename(1:end-3));
+    if exist([filename 'icaspec']) , spec_enable = 'on'; else spec_enable  = 'off'; end;
+    if exist([filename 'icaerp'] )  , erp_enable = 'on'; else erp_enable   = 'off'; end;
+    if exist([filename 'icatopo']), scalp_enable = 'on'; else scalp_enable = 'off'; end;
+    if exist([filename 'icaersp']) , ersp_enable = 'on'; else ersp_enable  = 'off'; end;
     
     if isfield(ALLEEG(1).dipfit, 'model'), dip_enable   = 'on'; else dip_enable   = 'off'; end;
     
