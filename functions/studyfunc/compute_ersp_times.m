a0c41f72ddc02a78e359ec240335a53a2427754a
Arnaud Delorme <arno@ucsd.edu>
2006-03-11 00:24:25 +0000

    add header
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@6096 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/compute_ersp_times.m
+++ functions/studyfunc/compute_ersp_times.m
@@ -3,7 +3,27 @@
 %        frequency range, wavelet parameters, sampling rate and frequency 
 %        resolution that are used by timef(). 
 %        This helper function is called by pop_preclust() & std_ersp(). 
+%
+% Authors: Hilit Serby & Arnaud Delorme, SCCN, INC, UCSD, Feb 03, 2005
 
+%123456789012345678901234567890123456789012345678901234567890123456789012
+
+% Copyright (C) Hilit Serby, SCCN, INC, UCSD, Feb 03, 2005, hilit@sccn.ucsd.edu
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+-
 function [time_range, winsize] = compute_ERSP_times(cycles, srate, epoch_lim, lowfreq, padratio) 
 
 if cycles == 0 %FFT option
