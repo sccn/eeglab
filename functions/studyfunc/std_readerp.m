6ad444abb2b2d2814ed7a6358755ca95b9a12141
Arnaud Delorme <arno@ucsd.edu>
2006-03-11 07:18:43 +0000

    header information
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@6115 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/std_readerp.m
+++ functions/studyfunc/std_readerp.m
@@ -9,10 +9,9 @@
 %
 % Usage:    
 %   >> [erp, t] = std_readerp(ALLEEG, setind, component);  
-%   This functions returns the ERP of an ICA component. 
-%   The information is loaded from a float file, which a pointer 
-%   to is saved in the EEG dataset. The float file was created
-%   by the pre - clustering function std_erp. 
+%   % This functions returns the ERP of an ICA component. 
+%   % The information is loaded from a Matlab file, created
+%   % by the pre-clustering function std_erp(). 
 %
 % Inputs:
 %   ALLEEG     - an EEGLAB data structure, which holds EEG sets (can also be one EEG set). 
@@ -51,6 +50,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.9  2006/03/10 16:08:19  arno
+% select time range
+%
 % Revision 1.8  2006/03/10 00:36:34  arno
 % error msg
 %
