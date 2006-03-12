03f651f0aeecccf399255e0689100dfc0747a6fd
Arnaud Delorme <arno@ucsd.edu>
2006-03-12 03:26:04 +0000

    matlab version saving
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@6138 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/std_savedat.m
+++ functions/studyfunc/std_savedat.m
@@ -27,7 +27,7 @@ function std_savedat( tmpfile, structure)
     delims = find( tmpfile == '.');
     structure.datafile = [ tmpfile(1:delims(end)-1) '.set' ];
     v = version;
-    if v(1) < 7
+    if v(1) > '6'
         save('-mat', tmpfile, '-struct', 'structure');
     else
         save('-v6' , tmpfile, '-struct', 'structure');
