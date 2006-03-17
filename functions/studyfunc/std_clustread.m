5c45c5d0f197d18336c860355ad953f784dd8a83
Scott Makeig <smakeig@ucsd.edu>
2006-03-17 18:26:17 +0000

    help msg
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@6220 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/std_clustread.m
+++ functions/studyfunc/std_clustread.m
@@ -1,17 +1,54 @@
 % std_clustread() - load a requested measure (i.e. ['erp'|'spec'|'ersp'|'itc'|...
 %                   'dipole'|'scalp']), for all components of a specified cluster.  
-%                   Called by cluster plotting functions: std_envtopo(), std_erpplot(),
-%                   std_erspplot(), ...
+%                   Called by cluster plotting functions: std_envtopo(), 
+%                   std_erpplot(), std_erspplot(), ...
+% Usage:
+%         >> clusinfo = std_clustread(STUDY,ALLEEG, cluster, infotype, cond);
+%
+% Inputs:
+%         STUDY - studyset structure containing some or all files in ALLEEG
+%        ALLEEG - vector of loaded EEG datasets
+%       cluster - cluster number in STUDY
+%      infotype - ['erp'|'spec'|'ersp'|'itc'|'dipole'|'scalp'] type of stored
+%                 cluster information to read
+%          cond - STUDY condition number to read {default: 1}
+%
+% Output:
+%      clusinfo - structure containing relevant information fields:
+%                 clusinfo.cluster
+%                 clusinfo.components
+%                 clusinfo.condition
+%                 clusinfo.erp
+%                   clusinfo.t     % vector of erp latencies
+%                 clusinfo.spec
+%                   clusinfo.f     % vector of spec frequencies
+%                 clusinfo.ersp
+%                 clusinfo.itc
+%                   clusinfo.times % vector of ersp/itc latencies
+%                   clusinfo.logf  % vector of ersp/itc frequencies
+%                 clusinfo.grid    % scalp topomaps
+%                   clusinfo.xi
+%                   clusinfo.yi
+%      Note: When dipole information is returned, clusinfo is EEG.dipfit.model
+%            for the cluster components.
+%
+%
+% Author: Hilit Serby, SCCN/INC/UCSD, 2005
 
 function clusinfo = std_clustread(STUDY,ALLEEG, cluster, infotype, cond);
 
-% infotype - ['erp'|'spec'|'ersp'|'itc'|'dipole'|'scalp']
+if nargin < 5
+  cond = 1; % default
+end
 
 if nargin < 4
     help std_clustread;
     return;
 end
 clusinfo = [];
+clusinfo.cluster = cluster;
+clusinfo.components = STUDY.cluster(cluster).comps;
+clusinfo.condition = cond;
 
 len = length(STUDY.cluster(cluster).comps);
 for k = 1:len
