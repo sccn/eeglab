ae713910d19611fdb911710695adaa7b62448de0
Arnaud Delorme <arno@ucsd.edu>
2006-03-20 17:35:34 +0000

    new output format, allow to process multiple infotype
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@6239 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/std_clustread.m
+++ functions/studyfunc/std_clustread.m
@@ -3,39 +3,50 @@
 %                   Called by cluster plotting functions: std_envtopo(), 
 %                   std_erpplot(), std_erspplot(), ...
 % Usage:
-%         >> clusinfo = std_clustread(STUDY,ALLEEG, cluster, infotype, cond);
+%         >> clustinfo = std_clustread(STUDY,ALLEEG, cluster, infotype, cond);
 %
 % Inputs:
 %         STUDY - studyset structure containing some or all files in ALLEEG
 %        ALLEEG - vector of loaded EEG datasets
 %       cluster - cluster number in STUDY
 %      infotype - ['erp'|'spec'|'ersp'|'itc'|'dipole'|'scalp'] type of stored
-%                 cluster information to read
+%                 cluster information to read. May also be a cell array of
+%                 these type, for instance { 'erp' 'scalp' 'dipole' }
 %          cond - STUDY condition number to read {default: 1}
 %
 % Output:
-%      clusinfo - structure containing relevant information fields:
-%                 clusinfo.cluster
-%                 clusinfo.components
-%                 clusinfo.condition
-%                 clusinfo.erp
-%                   clusinfo.t     % vector of erp latencies
-%                 clusinfo.spec
-%                   clusinfo.f     % vector of spec frequencies
-%                 clusinfo.ersp
-%                 clusinfo.itc
-%                   clusinfo.times % vector of ersp/itc latencies
-%                   clusinfo.logf  % vector of ersp/itc frequencies
-%                 clusinfo.grid    % scalp topomaps
-%                   clusinfo.xi
-%                   clusinfo.yi
-%      Note: When dipole information is returned, clusinfo is EEG.dipfit.model
-%            for the cluster components.
-%
-%
-% Author: Hilit Serby, SCCN/INC/UCSD, 2005
+%      clustinfo - structure containing relevant information fields:
+%                 clustinfo.name         % cluster name
+%                 clustinfo.clusternum   % cluster index
+%                 clustinfo.comps        % component indices of the component
+%                 clustinfo.sets         % dataset indices (one row per condition)
+%                 clustinfo.condition    % condition read
+%                 clustinfo.erp          % vector of erp value for cluster
+%                 ->clustinfo.erp_times  % vector of erp latencies
+%                 clustinfo.spec         % vector of spectral values for cluster
+%                 ->clustinfo.spec_freqs % vector of spec frequencies
+%                 clustinfo.ersp         % 2-D ERSP array for cluster
+%                 ->clustinfo.ersp_times % vector of ersp latencies
+%                 ->clustinfo.ersp_freqs % vector of ersp frequencies
+%                 clustinfo.itc          % 2-D ITC array for cluster
+%                 ->clustinfo.itc_times  % vector of itc latencies
+%                 ->clustinfo.itc_freqs  % vector of itc frequencies
+%                 clustinfo.scalp        % scalp topomap array for cluster centroid
+%                 ->clustinfo.xi         % abscicia of columns in array above
+%                 ->clustinfo.yi         % ordinate of rows in array above
+%                 clustinfo.dipole       % when dipole information is returned, 
+%                                         clustinfo.dipole is has the same format 
+%                                         as EEG.dipfit.model
+%                                         for the cluster components.
+% Example:
+%   % assuming some cluster have been computed
+%   % to plot the ERP of all the member of the cluster
+%   clustinfo = std_clustread(STUDY, ALLEEG, 3, 'erp');
+%   figure; plot(clustinfo.erp_times, clustinfo.erp);
+% 
+% Author: Hilit Serby & Arnaud Delorme, SCCN/INC/UCSD, 2005
 
-function clusinfo = std_clustread(STUDY,ALLEEG, cluster, infotype, cond);
+function clustinfo = std_clustread(STUDY,ALLEEG, cluster, infotype, cond);
 
 if nargin < 5
   cond = 1; % default
@@ -45,10 +56,12 @@ if nargin < 4
     help std_clustread;
     return;
 end
-clusinfo = [];
-clusinfo.cluster = cluster;
-clusinfo.components = STUDY.cluster(cluster).comps;
-clusinfo.condition = cond;
+if ~iscell(infotype), infotype = { infotype }; end;
+clustinfo = [];
+clustinfo.name       = STUDY.cluster(cluster).name;
+clustinfo.clusternum = cluster;
+clustinfo.comps      = STUDY.cluster(cluster).comps;
+clustinfo.condition  = cond;
 
 len = length(STUDY.cluster(cluster).comps);
 for k = 1:len
@@ -59,59 +72,49 @@ for k = 1:len
     end
     comp = STUDY.cluster(cluster).comps(k);
     
-	switch infotype        
-        case 'erp'
-            if nargin < 5
-                error('Reading cluster ERP requires condition number');
-            end
-            [erp, t] = std_readerp(ALLEEG, abset, comp, STUDY.preclust.erpclusttimes);
-            if  k == 1
-                clusinfo.erp = zeros(len,length(erp));
-                clusinfo.t = t;
-            end
-            clusinfo.erp(k,:) = erp;
-            
-        case 'spec'
-            if nargin < 5
-                error('Reading cluster spectrum requires condition number');
-            end
-            [spec, f] = std_readspec(ALLEEG, abset, comp, STUDY.preclust.specclustfreqs);
-            if  k == 1
-                clusinfo.spec = zeros(len,length(spec));
-                clusinfo.f = f;
-            end
-            clusinfo.spec(k,:) = spec;
-            
-        case 'ersp'
-            if nargin < 5
-                error('Reading cluster ERSP requires condition number');
-            end
-            [ersp, logfreqs, timevals] = std_readersp(ALLEEG, abset, comp, ...
-                      STUDY.preclust.erspclusttimes,  STUDY.preclust.erspclustfreqs);
-            clusinfo.ersp{k}  = ersp;
-            clusinfo.logf{k}  = logfreqs;
-            clusinfo.times{k} = timevals;
-            
-        case 'itc'
-            if nargin < 5
-                error('Reading cluster ITC requires condition number');
-            end
-            [itc, logfreqs, timevals] = std_readitc(ALLEEG, abset, comp, STUDY.preclust.erspclusttimes, ...
-                                                                    STUDY.preclust.erspclustfreqs );
-            clusinfo.itc{k}   = itc;
-            clusinfo.logf{k}  = logfreqs;
-            clusinfo.times{k} = timevals;
-            
-        case 'dipole'
-            clusinfo(k) = ALLEEG(abset).dipfit.model(comp);            
-            
-        case { 'scalp' 'topo' }
-            [grid, yi, xi] = std_readtopo(ALLEEG, abset, comp); 
-            clusinfo.grid{k} = grid;
-            clusinfo.yi{k} = yi;
-            clusinfo.xi{k} = xi;
-    end
-            
+    for index = 1:length(infotype)
+        switch infotype{index}        
+            case 'erp'
+                [erp, t] = std_readerp(ALLEEG, abset, comp, STUDY.preclust.erpclusttimes);
+                if  k == 1
+                    clustinfo.erp       = zeros(len,length(erp));
+                    clustinfo.erp_times = t;
+                end
+                clustinfo.erp(k,:) = erp;
+
+            case 'spec'
+                [spec, f] = std_readspec(ALLEEG, abset, comp, STUDY.preclust.specclustfreqs);
+                if  k == 1
+                    clustinfo.spec       = zeros(len,length(spec));
+                    clustinfo.spec_freqs = f;
+                end
+                clustinfo.spec(k,:) = spec;
+
+            case 'ersp'
+                [ersp, logfreqs, timevals] = std_readersp(ALLEEG, abset, comp, ...
+                          STUDY.preclust.erspclusttimes,  STUDY.preclust.erspclustfreqs);
+                clustinfo.ersp{k}  = ersp;
+                clustinfo.ersp_freqs{k}  = logfreqs;
+                clustinfo.ersp_times{k} = timevals;
+
+            case 'itc'
+                [itc, logfreqs, timevals] = std_readitc(ALLEEG, abset, comp, STUDY.preclust.erspclusttimes, ...
+                                                                        STUDY.preclust.erspclustfreqs );
+                clustinfo.itc{k}   = itc;
+                clustinfo.itc_freqs{k}  = logfreqs;
+                clustinfo.itc_times{k} = timevals;
+
+            case 'dipole'
+                clustinfo.dipole(k) = ALLEEG(abset).dipfit.model(comp);            
+
+            case { 'scalp' 'topo' }
+                [grid, yi, xi] = std_readtopo(ALLEEG, abset, comp); 
+                clustinfo.scalp{k} = grid;
+                clustinfo.yi{k} = yi;
+                clustinfo.xi{k} = xi;
+            otherwise, error('Unrecognized ''infotype'' entry');
+        end
+    end;     
 end
 
         
