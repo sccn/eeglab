f57126c52ca427a4602c4c17b75d136601105087
Arnaud Delorme <arno@ucsd.edu>
2006-03-21 15:46:58 +0000

    new .sets format; remove cluster component creation from function
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@6254 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/std_createclust.m
+++ functions/studyfunc/std_createclust.m
@@ -1,20 +1,20 @@
-%  std_createclust()  - create a new empty cluster.  After creation, components 
-%                       may be (re)assigned to this cluster using std_movecomp().
+%  std_createclust()  - dreate a new empty cluster.  After creation, components 
+%                       may be (re)assigned to it using std_movecomp().
 % Usage:
-%            >> [STUDY] = std_createclust(STUDY, ALLEEG, name);
+%                    >> [STUDY] = std_createclust(STUDY, ALLEEG, name);
 % Inputs:
-%   STUDY    - STUDY structure comprising some or all of the EEG datasets in ALLEEG.
-%   ALLEEG   - vector of EEG dataset structures included in the STUDY, typically 
-%              created using load_ALLEEG().
+%   STUDY    - STUDY set comprising some or all of the EEG datasets in ALLEEG.
+%   ALLEEG   - vector of EEG datasets included in the STUDY, typically created 
+%              using load_ALLEEG().
 % Optional inputs:
 %   name     - ['string'] name of the new cluster {default: 'Cls #', where 
 %              '#' is the next available cluster number}
 % Outputs:
-%   STUDY    - the input STUDY structure modified with the new cluster.
+%   STUDY    - the input STUDY set structure modified with the new cluster.
 %
 % Example: 
-%               % Create a new cluster named 'eye_movements'.
 %            >> [STUDY] = std_createclust(STUDY, ALLEEG, 'eye_movements');
+%            % Create a new cluster named 'eye_movements'.
 %
 %  See also  pop_clustedit(), std_movecomp()
 %
@@ -22,7 +22,7 @@
 
 %123456789012345678901234567890123456789012345678901234567890123456789012
 
-% Copyright (C) Arnaud Delorme & Scott Makeig, SCCN/INC/UCSD, 06/07/2005, smakeig@ucsd.edu
+% Copyright (C) Hilit Serby, SCCN, INC, UCSD, June 07, 2005, hilit@sccn.ucsd.edu
 %
 % This program is free software; you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
@@ -40,18 +40,17 @@
 
 function [STUDY, clusters] = std_createclust(STUDY, ALLEEG, varargin)
 
-    if length(varargin) > 1
-        [STUDY, clusters] = std_createclust2(STUDY, ALLEEG, varargin{:});
-        return;
-    end;
-    
- if isempty(varargin) | strcmpi(varargin,'')
+if nargin< 2
+    help std_createclust;
+    return;
+end;
+
+if isempty(varargin) | strcmpi(varargin,'')
     name = 'Cls';
 else
     name = varargin{1};
 end
-
-% Find the highst cluster id number (in cluster name), to find
+% Find out the highst cluster id number (in cluster name), to find
 % next available cluster index
 max_id = 0;
 for k = 1:length(STUDY.cluster)
@@ -62,7 +61,6 @@ end
 max_id = max_id + 1;
 name = sprintf('%s %d', name, max_id);
 STUDY.cluster(end+1).name = name;
-
 % Initialize the new cluster fields.
 STUDY.cluster(end).parent{1} = 'manual'; % update parent cluster if exists.
 STUDY.cluster(end).child = [];
@@ -74,74 +72,4 @@ STUDY.cluster(end).preclust.preclustparams = [];
 STUDY.cluster(end).preclust.preclustdata = [];
 
 
-function [STUDY, clusters] = std_createclust2(STUDY,IDX,C, algorithm)
 
-clusters = [];
-sets = [];
-comp = [];
-Ncond = length(STUDY.condition);
-if Ncond == 0
-    Ncond = 1;
-end
-nsets = length(STUDY.etc.preclust.preclustcomps); 
-for k = 1: nsets
-    sets = [sets k*ones(1,length(STUDY.etc.preclust.preclustcomps{k}))];
-    comp = [comp STUDY.etc.preclust.preclustcomps{k}];
-end
-if Ncond > 1
-    tmp = ones(Ncond, length(sets));
-    for l = 1:Ncond
-        tmp(l,:) = sets + (l-1)*nsets;
-    end
-    sets = tmp;
-    clear tmp
-end
-
-cls = size(C,1);
-
-% Find the next available cluster index
-nc = 0; 
-for k =  1:length(STUDY.cluster)
-    ti = strfind(STUDY.cluster(k).name, ' ');
-    tmp = STUDY.cluster(k).name(ti(end) + 1:end);
-    nc = max(nc,str2num(tmp));
-    % check if there is a cluster of Notclust components
-    if strcmp(STUDY.cluster(k).parent,STUDY.cluster(STUDY.etc.preclust.clustlevel).name) 
-        STUDY.cluster(k).preclust.preclustparams = STUDY.etc.preclust.preclustparams;
-        clusters = [clusters k];
-    end
-end
-len = length(STUDY.cluster);
-
-if ~isempty(find(IDX==0)) %outliers exist
-    l = 0;
-    nc = nc + 1;
-    len = len +1;
-else
-    l = 1;
-end
-
-for k = l:cls 
-    tmp = find(IDX==k);
-    STUDY.cluster(k+len).sets = sets(:,tmp);
-    STUDY.cluster(k+len).comps = comp(tmp);
-    if k == 0
-        STUDY.cluster(len).name = ['outlier ' num2str(k+nc)];
-    else
-        STUDY.cluster(k+len).name = [ 'Cls ' num2str(k+nc)];
-    end
-    STUDY.cluster(k+len).algorithm = algorithm;
-    if STUDY.etc.preclust.clustlevel == 0
-        STUDY.cluster(k+len).parent = [];
-    else
-        STUDY.cluster(k+len).parent{end+1} = STUDY.cluster(STUDY.etc.preclust.clustlevel).name;
-        %update parents clusters with cluster child indices
-        STUDY.cluster(STUDY.etc.preclust.clustlevel).child{end+1} = STUDY.cluster(k+nc).name;
-    end
-    STUDY.cluster(k+len).child = [];
-    STUDY.cluster(k+len).preclust.preclustdata = STUDY.etc.preclust.preclustdata(tmp,:);
-    STUDY.cluster(k+len).preclust.preclustparams = STUDY.etc.preclust.preclustparams;
-    STUDY.cluster(k+len).preclust.preclustcomps = STUDY.etc.preclust.preclustcomps;
-end
-
-clusters = [ clusters l+len:cls+len];%the new created clusters indices.
