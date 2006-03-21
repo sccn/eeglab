45e1d06e446454f31c56ee9406dabed771ae1b5e
Arnaud Delorme <arno@ucsd.edu>
2006-03-21 15:45:36 +0000

    new .sets format; embeding component creation
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@6253 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/pop_clust.m
+++ functions/studyfunc/pop_clust.m
@@ -1,9 +1,9 @@
-% pop_clust() - select and run a clustering algorithm on components from a STUDY 
-%               structure of EEG datasets. Clustering data should be prepared beforehand 
-%               using pop_preclust() and/or eeg_preclust(). The number of clusters must 
-%               be specified in advance. If called in gui mode, the pop_clustedit() 
-%               window appears when the clustering is complete to display clustering 
-%               results and allow the user to review and edit them.
+% pop_clust() - select and run a clustering algorithm on components from an EEGLAB STUDY 
+%               structure of EEG datasets. Clustering data should be prepared beforehand using 
+%               pop_preclust() and/or eeg_preclust(). The number of clusters must be
+%               specified in advance. If called in gui mode, the pop_clustedit() window
+%               appears when the clustering is complete to display clustering results
+%               and allow the user to review and edit them.
 % Usage: 
 %               >> STUDY = pop_clust( STUDY, ALLEEG); % pop up a graphic interface
 %               >> STUDY = pop_clust( STUDY, ALLEEG, 'key1', 'val1', ...); % no pop-up
@@ -38,7 +38,7 @@
 
 %123456789012345678901234567890123456789012345678901234567890123456789012
 
-% Copyright (C) Arnaud Delorme & Scott Makeig, SCCN/INC/UCSD, 2005, smakeig@ucsd.edu
+% Copyright (C) Hilit Serby, SCCN, INC, UCSD, October 11, 2004, hilit@sccn.ucsd.edu
 %
 % This program is free software; you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
@@ -200,17 +200,17 @@ if isempty(varargin) %GUI call
                 if outliers_on
                     command = sprintf('%s %s %s %s', command, '''outliers'', ', stdval, ',');
                     [IDX,C,sumd,D,outliers] = robust_kmeans(clustdata,clus_num,str2num(stdval),5);
-                    [STUDY, clusters] = std_createclust(STUDY,IDX,C,  {'robust_kmeans', clus_num});
+                    [STUDY, clusters] = std_createclust2(STUDY,IDX,C,  {'robust_kmeans', clus_num});
                 else
                     [IDX,C,sumd,D] = kmeans(clustdata,clus_num,'replicates',10,'emptyaction','drop');
-                    [STUDY, clusters] = std_createclust(STUDY,IDX,C,  {'Kmeans', clus_num});
+                    [STUDY, clusters] = std_createclust2(STUDY,IDX,C,  {'Kmeans', clus_num});
                 end    
          case 'Hierarchical tree'
              %[IDX,C] = hierarchical_tree(clustdata,clus_num);
-             %[STUDY, clusters] = std_createclust(STUDY,IDX,C,  {'Neural Network', clus_num});
+             %[STUDY, clusters] = std_createclust2(STUDY,IDX,C,  {'Neural Network', clus_num});
          case 'Neural Network'
              [IDX,C] = neural_net(clustdata,clus_num);
-             [STUDY, clusters] = std_createclust(STUDY,IDX,C,  {'Neural Network', clus_num});
+             [STUDY, clusters] = std_createclust2(STUDY,IDX,C,  {'Neural Network', clus_num});
              command = sprintf('%s %s %d %s', command, '''algorithm'', ''Neural Network'',''clus_num'', ', clus_num, ',');
         end
         disp('Done.');
@@ -278,14 +278,14 @@ else %command line call
         case 'kmeans'                
             if outliers == Inf
                 [IDX,C,sumd,D] = kmeans(clustdata,clus_num,'replicates',10,'emptyaction','drop');
-                [STUDY, clusters] = std_createclust(STUDY,IDX,C,  {'Kmeans', clus_num});
+                [STUDY, clusters] = std_createclust2(STUDY,IDX,C,  {'Kmeans', clus_num});
             else
                 [IDX,C,sumd,D,outliers] = robust_kmeans(clustdata,clus_num,outliers,5);
-                [STUDY, clusters] = std_createclust(STUDY,IDX,C,  {'robust_kmeans', clus_num});
+                [STUDY, clusters] = std_createclust2(STUDY,IDX,C,  {'robust_kmeans', clus_num});
             end
         case 'Neural Network'
             [IDX,C] = neural_net(clustdata,clus_num);
-            [STUDY, clusters] = std_createclust(STUDY,IDX,C,  {'Neural Network', clus_num});
+            [STUDY, clusters] = std_createclust2(STUDY,IDX,C,  {'Neural Network', clus_num});
         otherwise
             disp('pop_clust: unknown algorithm return');
             return
@@ -300,3 +300,73 @@ else %command line call
    end       
 end
 STUDY.saved = 'no';
+
+
+% IDX - index of cluster for each component. Ex: 63 components and 2
+% clusters: IDX will be a 61x1 vector of 1 and 2 (and 0=outlisers)
+% C - centroid for clusters. If 2 clusters, size will be 2 x
+%     width of the preclustering matrix
+function [STUDY, clusters] = std_createclust2(STUDY,IDX,C, algorithm)
+
+clusters = [];
+sets = [];
+comp = [];
+nsets = length(STUDY.etc.preclust.preclustcomps); 
+for k = 1: nsets
+    sets = [sets k*ones(1,length(STUDY.etc.preclust.preclustcomps{k}))];
+    comp = [comp STUDY.etc.preclust.preclustcomps{k}];
+end
+
+% Find the next available cluster index
+% -------------------------------------
+cls = size(C,1); % number of cluster = number of row of centroid matrix
+nc  = 0; % index of last cluster 
+for k =  1:length(STUDY.cluster)
+    ti = strfind(STUDY.cluster(k).name, ' ');
+    tmp = STUDY.cluster(k).name(ti(end) + 1:end);
+    nc = max(nc,str2num(tmp));
+    % check if there is a cluster of Notclust components
+    if strcmp(STUDY.cluster(k).parent,STUDY.cluster(STUDY.etc.preclust.clustlevel).name) 
+        STUDY.cluster(k).preclust.preclustparams = STUDY.etc.preclust.preclustparams;
+        clusters = [clusters k];
+    end
+end
+len = length(STUDY.cluster);
+
+if ~isempty(find(IDX==0)) %outliers exist
+    firstind = 0;
+    nc  = nc + 1;
+    len = len + 1;
+else
+    firstind = 1;
+end
+
+% create all clusters
+% -------------------
+for k = firstind:cls 
+    
+    % cluster name
+    % ------------
+    if k == 0
+         STUDY.cluster(len).name   = [ 'outlier ' num2str(k+nc)];
+    else STUDY.cluster(k+len).name = [ 'Cls ' num2str(k+nc)];
+    end
+
+    % find indices
+    % ------------
+    tmp = find(IDX==k);
+    STUDY.cluster(k+len).sets  = STUDY.cluster(STUDY.etc.preclust.clustlevel).sets(:,tmp);
+    STUDY.cluster(k+len).comps = STUDY.cluster(STUDY.etc.preclust.clustlevel).comps(tmp);
+    STUDY.cluster(k+len).algorithm = algorithm;
+    STUDY.cluster(k+len).parent{end+1} = STUDY.cluster(STUDY.etc.preclust.clustlevel).name;
+    STUDY.cluster(k+len).child = [];
+    STUDY.cluster(k+len).preclust.preclustdata = STUDY.etc.preclust.preclustdata(tmp,:);
+    STUDY.cluster(k+len).preclust.preclustparams = STUDY.etc.preclust.preclustparams;
+    STUDY.cluster(k+len).preclust.preclustcomps = STUDY.etc.preclust.preclustcomps;
+
+    %update parents clusters with cluster child indices
+    % -------------------------------------------------
+    STUDY.cluster(STUDY.etc.preclust.clustlevel).child{end+1} = STUDY.cluster(k+nc).name;
+end
+
+clusters = [ clusters firstind+len:cls+len];%the new created clusters indices.
