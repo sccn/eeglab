46d51b5e06684c2350d40e6ba5f5779aa5303382
Arnaud Delorme <arno@ucsd.edu>
2006-09-12 19:22:01 +0000

    bug fix (commented inside) by Hans-Peter
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@6547 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/readneurolocs.m
+++ functions/sigprocfunc/readneurolocs.m
@@ -40,6 +40,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.14  2006/01/10 23:25:27  arno
+% fix orientation problem for some files
+%
 % Revision 1.13  2005/05/24 17:10:59  arno
 % cell2mat -> celltomat
 %
@@ -81,87 +84,89 @@
 %
 
 function chanlocs = readneurolocs( filename, varargin)
-    
-    if nargin < 1
-        help readneurolocs;
-        return;
-    end;
-    if nargin < 2
-        plottag = 0;
-    end;
-    
-   % read location file
-    % ------------------
-   if isstr(filename)
-       locs  = loadtxt( filename );
-   end;
-    if ~isstr(filename) | locs{1,1}(1) == ';' | size(locs,2) < 5
-        if ~isstr(filename)
-            names = filename{1};
-            x     = filename{2};
-            y     = filename{3};
-        else
-            if locs{1,1}(1) == ';'
-                % remove trailing control channels
-                % --------------------------------
-                while isnumeric( locs{end,1} ) & locs{end,1} ~= 0
-                    locs  = locs(1:end-1,:);
-                end;    
-                
-                % find first numerical index
-                % --------------------------
-                index = 1;
-                while isstr( locs{index,1} )
-                    index = index + 1;
-                end;
-                
-                % extract location array
-                % ----------------------   
-                nchans = size( locs, 1 ) - index +1;
-                chans  = [ locs{end-nchans+1:end, 1:5} ];
-                names  = locs(end-nchans*2+1: end-nchans, 2);
-                for index = 1:length(names)
-                    if ~isstr(names{index})
-                        names{index} = int2str(names{index});
-                    end;
-                end;
-                x = chans(:,3);
-                y = -chans(:,4);
-            else
-                [tmp2 tmpind] = sort( [ locs{:,1} ]);
-                locs = locs(tmpind,:);
-                y      = [ locs{:,end} ];
-                x      = [ locs{:,end-1} ];
-                x      = x/513.1617*44;
-                y      = y/513.1617*44;
-                names = locs(:,end-2);
+
+if nargin < 1
+    help readneurolocs;
+    return;
+end;
+if nargin < 2
+    plottag = 0;
+end;
+
+% read location file
+% ------------------
+if isstr(filename)
+    locs  = loadtxt( filename );
+end;
+
+if ~isstr(filename) | locs{1,1}(1) == ';' | size(locs,2) < 5
+    if ~isstr(filename)
+        names = filename{1};
+        x     = filename{2};
+        y     = filename{3};
+    else
+        if locs{1,1}(1) == ';'
+            % remove trailing control channels
+            % --------------------------------
+            while isnumeric( locs{end,1} ) & locs{end,1} ~= 0
+                locs  = locs(1:end-1,:);
             end;
+
+            % find first numerical index
+            % --------------------------
+            index = 1;
+            while isstr( locs{index,1} )
+                index = index + 1;
+            end;
+
+            % extract location array
+            % ----------------------
+            nchans = size( locs, 1 ) - index +1;
+            chans  = [locs{end-nchans+1:end, 1:5}];
+            chans  = reshape(chans,nchans,5);               %% Added this line in order to get x = chans(:,3)
+            names  = locs(end-nchans*2+1: end-nchans, 2);
+            for index = 1:length(names)
+                if ~isstr(names{index})
+                    names{index} = int2str(names{index});
+                end;
+            end;
+            x = chans(:,3);
+            y = -chans(:,4);
+        else
+            [tmp2 tmpind] = sort( [ locs{:,1} ]);
+            locs = locs(tmpind,:);
+            y      = [ locs{:,end} ];
+            x      = [ locs{:,end-1} ];
+            x      = x/513.1617*44;
+            y      = y/513.1617*44;
+            names = locs(:,end-2);
         end;
-        
-        % second solution using angle
-        % ---------------------------
-        [phi,theta] = cart2pol(x, y);
-        phi = phi/pi*180;
-        
-        % convert to other types of coordinates
-        % -------------------------------------
-        labels = names';
-        chanlocs = struct('labels', labels(:), 'sph_theta_besa', mattocell(theta)', 'sph_phi_besa', mattocell(phi)');
-        chanlocs = convertlocs( chanlocs, 'sphbesa2all');
-        
-        for index = 1:length(chanlocs)
-            chanlocs(index).labels = num2str(chanlocs(index).labels);
-        end;
-        
-        % re-calibration
-        % --------------
-        chanlocs = adjustlocs(chanlocs, 'autoscale', 'on', 'autorotate', 'off', varargin{:});
-        
-    else % 5 rows, xyz positions
-        for index = 1:size(locs,1)
-            locs{index,3} = - locs{index,3};
-        end;
-        chanlocs = struct('labels', locs(:,1), 'type', locs(:,2), 'X', locs(:,4), 'Y', locs(:,3), 'Z', locs(:,5));
-        chanlocs = convertlocs( chanlocs, 'cart2all');
     end;
+
+    % second solution using angle
+    % ---------------------------
+    [phi,theta] = cart2pol(x, y);
+    phi = phi/pi*180;
+
+    % convert to other types of coordinates
+    % -------------------------------------
+    labels = names';
+    chanlocs = struct('labels', labels, 'sph_theta_besa', mattocell(theta)', 'sph_phi_besa', mattocell(phi)');      %% labels instead of labels(:) 
+    chanlocs = convertlocs( chanlocs, 'sphbesa2all');
+
+    for index = 1:length(chanlocs)
+        chanlocs(index).labels = num2str(chanlocs(index).labels);
+    end;
+
+    % re-calibration
+    % --------------
+    chanlocs = adjustlocs(chanlocs, 'autoscale', 'on', 'autorotate', 'off', varargin{:});
+
+else % 5 rows, xyz positions
+    for index = 1:size(locs,1)
+        locs{index,3} = - locs{index,3};
+    end;
+    chanlocs = struct('labels', locs(:,1), 'type', locs(:,2), 'X', locs(:,4), 'Y', locs(:,3), 'Z', locs(:,5));
+    chanlocs = convertlocs( chanlocs, 'cart2all');
+end;
     
