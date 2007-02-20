c87acf4c10689163b3a44760dcbd31edbddccfe1
Arnaud Delorme <arno@ucsd.edu>
2007-02-20 14:18:21 +0000

    now can process several datasets
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@6981 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_rmbase.m
+++ functions/popfunc/pop_rmbase.m
@@ -47,6 +47,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.20  2005/08/05 16:44:42  arno
+% fixed time range
+%
 % Revision 1.19  2005/05/24 17:26:23  arno
 % remove cell2mat
 %
@@ -118,20 +121,20 @@ if nargin < 1
     help pop_rmbase;
     return;
 end;
-if isempty(EEG.data)
+if isempty(EEG(1).data)
     disp('pop_rmbase(): cannot remove baseline of an empty dataset'); return;
 end;    
 if nargin < 1
 	help pop_rmbase;
 	return;
 end;	
-if nargin < 2 & EEG.trials > 1
+if nargin < 2 & EEG(1).trials > 1
 	% popup window parameters
 	% -----------------------
     promptstr    = {'Baseline latency range (min_ms max_ms) ([] = whole epoch):',...
          		   strvcat('Else, baseline points vector (ex:1:56) ([] = whole epoch):', ...
                            '(overwritten by latency range above).') };
-	inistr     = { [num2str(EEG.xmin*1000) ' 0'], '' }; % latency range in ms
+	inistr     = { [num2str(EEG(1).xmin*1000) ' 0'], '' }; % latency range in ms
 	result       = inputdlg2( promptstr, 'Epoch baseline removal -- pop_rmbase()', ...
                                   1,  inistr, 'pop_rmbase');
 	size_result  = size( result );
@@ -141,7 +144,7 @@ if nargin < 2 & EEG.trials > 1
 	% -----------------
 	if numel(result) < 2 | ((isempty(result{1}) | strcmp(result{1},'[]') ) ...
                            & (isempty(result{2}) | strcmp(result{2},'[]')))
-       timerange = [num2str(EEG.xmin*1000) num2str(EEG.xmax*1000)]; % whole epoch latency range
+       timerange = [num2str(EEG(1).xmin*1000) num2str(EEG(1).xmax*1000)]; % whole epoch latency range
        fprintf('pop_rmbase(): using whole epoch as baseline.\n');
 
        % fprintf('pop_rmbase(): baseline limits must be specified.\n');
@@ -150,14 +153,22 @@ if nargin < 2 & EEG.trials > 1
       timerange = eval( [ '[' result{1} ']' ] );
       pointrange = eval( [ '[' result{2} ']' ] );
     end
-elseif nargin < 2 & EEG.trials == 1
+elseif nargin < 2 & EEG(1).trials == 1
 	% popup window parameters
 	% -----------------------
     resp = questdlg2(strvcat('Remove baseline of each channel (in', ...
              'each unbroken portion of continuous data)'), 'pop_rmbase', 'Cancel', 'Ok', 'Ok');
     if strcmpi(resp, 'Cancel'), return; end;
     timerange = [];
-    pointrange = [1:EEG.pnts];
+    pointrange = [1:EEG(1).pnts];
+end;
+
+% process multiple datasets
+% -------------------------
+if length(EEG) > 1
+    [ EEG com ] = eeg_eval( 'pop_rmbase', EEG, 'warning', 'on', 'params', ...
+                            { timerange pointrange } );
+    return;
 end;
 
 if exist('pointrange') ~= 1 & ~isempty(timerange)
