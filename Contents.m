15259542b9fd6ae22d48c28559133f6e0f4e3828
Toby Fernsler <toby.fernsler@gmail.com>
2007-03-09 21:14:22 +0000

    Initial revision
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@7071 1e58238c-3a72-4c0f-aabe-36d030380f39

---
+++ Contents.m
@@ -0,0 +1,25 @@
+% EEGLab - Toolbox to process EEG data
+%
+% eeglab() - Matlab graphic user interface environment for 
+%   electrophysiological data analysis incorporating the ICA/EEG toolbox 
+%   (Makeig et al.) developed at CNL / The Salk Institute, 1997-2001. 
+%   Released 11/2002- as EEGLAB (Delorme, Makeig, et al.) at the Swartz Center 
+%   for Computational Neuroscience, Institute for Neural Computation, 
+%   University of California San Diego (http://sccn.ucsd.edu/). 
+%   User feedback welcome: email eeglab@sccn.ucsd.edu
+%
+% Authors: Arnaud Delorme, Scott Makeig, with substantial contributions
+%   from Colin Humphries, Sigurd Enghoff, Tzyy-Ping Jung, plus contributions 
+%   from Tony Bell, Te-Won Lee, Luca Finelli and other contributors. 
+%
+% Description:
+%   EEGLAB is Matlab software for processing continuous or event-related 
+%   EEG or other physiological data. It is designed for use by both novice and 
+%   expert Matlab users. In normal use, the EEGLAB graphic interface calls 
+%   graphic functions via pop-up function windows. The EEGLAB history mechanism 
+%   can save the resulting Matlab calls to disk for later incorporation into 
+%   Matlab scripts.  A single data structure ('EEG') containing all dataset 
+%   parameters may be accessed and modified directly from the Matlab commandline. 
+%   EEGLAB now recognizes "plugins," sets of EEGLAB functions linked to the EEGLAB
+%   main menu through an "eegplugin_[name].m" function (Ex. >> help eeplugin_besa.m). 
+%
