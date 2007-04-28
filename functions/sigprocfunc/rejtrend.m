c118c194b99efdbb79c8ba54260f7679c85c483a
Toby Fernsler <toby.fernsler@gmail.com>
2007-04-28 04:20:30 +0000

    Modified to catch very flat line data, added waitbar, clarified documentation
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@7227 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/rejtrend.m
+++ functions/sigprocfunc/rejtrend.m
@@ -2,16 +2,20 @@
 %                  epoched trials based on the accuracy of the linear
 %                  fit.
 % Usage:
-%   >> [rej rejE] = rejtrend( signal, winsize, minslope, minR, step);
+%   >> [rej rejE] = rejtrend( signal, winsize, maxslope, minR, step);
 %
 % Inputs:
 %   signal     - 3 dimensional signal (channels x frames x trials)
 %   winsize    - integer determining the number of consecutive points
 %                for the detection of linear patterns
-%   minslope   - minimal absolute slope of the linear trend of the 
-%                activity for rejection
+%   maxslope   - maximum acceptable absolute slope of the linear trend. If the slope
+%                of the line fitted to a data epoch is greater than or equal to 
+%                maxslope, that epoch is rejected (assuming a sufficient R^2 value, 
+%                see minR below).
 %   minR       - minimal R^2 (coefficient of determination between
-%                0 and 1)
+%                0 and 1). The R^2 value reflects how well the data epoch is
+%                approximated by a line. An epoch is not rejected unless its R^2
+%                value is greater than minR.
 %   step       - step for the window. Default is 1 point (2 points 
 %                will divide by two the computation time)  
 %
@@ -46,9 +50,15 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.1  2002/04/05 17:39:45  jorn
+% Initial revision
+%
 % 01-25-02 reformated help & license -ad 
 
-function [rej, rejE] = rejtrend( signal, pointrange, minslope, minstd, step);
+function [rej, rejE] = rejtrend( signal, pointrange, maxslope, minstd, step);
+
+% This is to avoid divide-by-zero and machine errors.
+SST_TOLERANCE = 1000*pointrange*eps('single');
 
 if nargin < 3
 	help rejtrend;
@@ -68,29 +78,35 @@ rejE   = zeros( chans, trials);
 
 %fprintf('finding stable low variability regions of %d consecutive time points...\n', pointrange);	
 x = linspace( 1/pointrange, 1, pointrange );
+waitbarhandle = waitbar(0,'rejtrend.m Please wait...');
 for c = 1:chans
 	%fprintf('%d ', c);
 	for t = 1:trials 
 		for w = 1:step:(pnts-pointrange+1)
 			y = signal(c, [w:w+pointrange-1], t);
 		   	coef = polyfit(x,y,1);   		
-			if abs(coef(1)) > minslope
+			if abs(coef(1)) >= maxslope
 			   	ypred = polyval(coef,x);   % predictions
 			   	dev = y - mean(y);          % deviations - measure of spread
 			   	SST = sum(dev.^2);          % total variation to be accounted for
+                if SST < SST_TOLERANCE      % make sure SST is not too close to zero
+                    SST = SST_TOLERANCE;
+                end
 			   	resid = y - ypred;          % residuals - measure of mismatch
 			   	SSE = sum(resid.^2);        % variation NOT accounted for
 			   	Rsq = 1 - SSE/SST;          % percent of error explained
 				if Rsq > minstd
 					rejE( c, t ) = 1;
-				end;
+                end
 				% see the page 	http://www.facstaff.bucknell.edu/maneval/help211/fitting.html
-			end;			
+            end			
 			%rejE( c, t ) = rejE( c, t ) | all( abs(detrend(signal(c, [w:w+pointrange-1], t))) < minstd);
 			%rejE( c, t ) = rejE( c, t ) | all( abs(signal(c, [w:w+pointrange-1], t)) < minstd);
-		end;
-	end;
-end;
+        end
+    end
+    waitbar(c/chans);
+end
+close(waitbarhandle);
 %fprintf('\n');					
 rej = max( rejE, [], 1);
 
