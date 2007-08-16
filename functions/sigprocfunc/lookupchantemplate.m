ed24b129b62bafe0cd6f2a246b8aeee93d7cbc62
Arnaud Delorme <arno@ucsd.edu>
2007-08-16 19:21:52 +0000

    Initial revision
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@7611 1e58238c-3a72-4c0f-aabe-36d030380f39

---
+++ functions/sigprocfunc/lookupchantemplate.m
@@ -0,0 +1,35 @@
+% lookupchantemplate - look up channel template.
+%
+% Usage:
+%  [ found transform ] = lookupchantemplate( filename, template_struct);
+%
+% Inputs:
+%   filename        - channel location file name
+%   template_struct - template strcuture as defined in dipfitdefs
+%
+% Outputs:
+%   found     - [0|1] 1 if a transformation was found for this template
+%   transform - [array] tranditional tailairach transformation
+%
+% Author: Arnaud Delorme, SCCN, INC, 2007
+
+[allkeywordstrue, transform] = lookupchantemplate(chanfile, tmpl);
+
+if nargin < 2
+    help lookupchantemplate;
+    return;
+end;
+
+transform = [];
+for ind = 1:length(tmpl)
+    allkeywordstrue = 1;
+    if isempty(tmpl(ind).keywords), allkeywordstrue = 0; end;
+    for k = 1:length(tmpl(ind).keywords)
+        if isempty(findstr(chanfile, tmpl(ind).keywords{k})), allkeywordstrue = 0; end;
+    end;
+    if allkeywordstrue,
+        transform = tmpl(ind).transform;
+    end;
+end;
+
+
