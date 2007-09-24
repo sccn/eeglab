a8d694e917864e56c7fb834bc9164614a515e667
Nima Bigdely Shamlo <nimabg@gmail.com>
2007-09-24 23:40:40 +0000

    Initial revision
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@7713 1e58238c-3a72-4c0f-aabe-36d030380f39

---
+++ functions/popfunc/eeg_dipselect.m
@@ -0,0 +1,55 @@
+function brainComponents = eeg_dipselect(EEG, rvThreshold, selectionType, depthThreshold);
+% eeg_dipselect()  - select componet dipoles of EEG dataset with
+%                    reisdual variance (rv) less than a threshold
+%                    and location inside brain volume.
+% Usage:  
+%  >> selctedComponents = eeg_dipselect(EEG, rvThreshold, selectionType, depthThreshold)
+%
+% Inputs:
+%       EEG            - EEGLAB dataset structure
+%
+% Optional Inputs
+%       rvThreshold    - residual variance threshold. Dipoles will residual variance 
+%                        less than this value will be selected. {default = 0.15}
+%       selectionType  - criteria for selecting dipoles: 
+%                         'rv'  = only by residual variance, 
+%                         'inbrain' = inside brain volume and residual variance. 
+%                         {default = 'inbrain'}
+%
+%       depthThreshold - maximum accepted distance outside brain volume {default = 1}
+%
+% Outputs:
+%   selctedComponents          - vector of selected components
+%
+% Example:
+%  >> selctedComponents = eeg_dipselect(EEG)             % select in-brain dipoles with rv less than 0.15 (default value)
+%  >> selctedComponents = eeg_dipselect(EEG, 0.2,'rv')   % select dipoles with rv less than 0.2
+%
+% Author: Nima Bigdely Shamlo, Copyright (C) September 2007 
+% based on an script from Julie Onton and sourcedepth() function
+% provided by Robert Oostenveld.
+%
+% See also: sourcedepth()
+
+if nargin<2
+    rvThreshold =  0.15;
+    fprintf('Maximum residual variance for selected dipoles set to %f (default).',rvThreshold);
+end;
+
+if nargin<4
+    depthThreshold = 1;
+end;
+
+% find components with low residual variance
+
+for ic = 1:length(EEG.dipfit.model)
+   residualvariance(1,ic) =EEG.dipfit.model(ic).rv;
+end;
+compLowResidualvariance = find(residualvariance <rvThreshold);
+
+load(EEG.dipfit.hdmfile);
+
+posxyz = cell2mat({EEG.dipfit.model(compLowResidualvariance).posxyz}');% select positions for components with low residual variance
+depth = sourcedepth(posxyz, vol);
+
+brainComponents = compLowResidualvariance(find(depth<=depthThreshold));
---
+++ functions/sigprocfunc/voltype.m
@@ -0,0 +1,35 @@
+function [type] = voltype(vol, desired)
+
+% VOLTYPE determines the type of volume conduction model
+%
+% Use as
+%   [type] = voltype(vol)
+% to get a string describing the type, or
+%   [flag] = voltype(vol, desired)
+% to get a boolean value.
+%
+% See also COMPUTE_LEADFIELD
+
+% Copyright (C) 2007, Robert Oostenveld
+%
+% $Log: not supported by cvs2svn $
+% Revision 1.1  2007/07/25 08:31:12  roboos
+% implemented new helper function
+%
+
+if isfield(vol, 'type')
+  type = vol.type;
+elseif isfield(vol, 'r') && prod(size(vol.r))==1
+  type = 'singlesphere';
+elseif isfield(vol, 'r') && isfield(vol, 'o') && all(size(vol.r)==size(vol.o))
+  type = 'multisphere';
+elseif isfield(vol, 'r')
+  type = 'concentric';
+elseif isfield(vol, 'bnd')
+  type = 'bem';
+end
+
+if nargin>1
+  type = strcmp(type, desired);
+end
+
