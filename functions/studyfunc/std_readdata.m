73ec3de2f8f82c98efa8cf4c7cf52be32e2bd3e7
Arnaud Delorme <arno@ucsd.edu>
2008-02-15 16:50:45 +0000

    Refix the typo, just in case
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@7855 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/std_readdata.m
+++ functions/studyfunc/std_readdata.m
@@ -4,7 +4,7 @@
 %                  Called by cluster plotting functions 
 %                  std_envtopo(), std_erpplot(), std_erspplot(), ...
 % Usage:
-%         >> [STUDY, clustinfo, finalinds] = std_readdata(STUDY, ALLEEG); 
+%         >> [STUDY, clustinfo, finalinds] = std_readdata(STUDY, ALLEEG);
 %                                              % return all measures
 %         >> [STUDY, clustinfo, finalinds] = std_readdata(STUDY, ALLEEG, ...
 %                                              cluster, infotype,varargin);
@@ -99,9 +99,6 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
-% Revision 1.41  2008/02/12 19:07:20  nima
-% _
-%
 % Revision 1.40  2008/02/08 20:21:42  arno
 % *** empty log message ***
 %
