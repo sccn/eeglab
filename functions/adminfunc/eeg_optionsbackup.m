e30509908d41b4f6a43254efc16fb456096cc438
Arnaud Delorme <arno@ucsd.edu>
2008-04-16 17:44:10 +0000

    added new options for memory and for STUDYes
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@7889 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/eeg_optionsbackup.m
+++ functions/adminfunc/eeg_optionsbackup.m
@@ -3,11 +3,14 @@
 % Note: DO NOT EDIT, instead use pop_editoptions() or the menu
 %       /File/Maximize memory in EEGLAB gui
 
+% STUDY options (set these checkboxes if you intend to work with studies) 
+option_storedisk     = 0 ;  % If set, keep at most one dataset in memory. This allows processing hundreds of datasets within studies.
+option_savetwofiles  = 1 ;  % If set, save not one but two files for each dataset (header and data). This allows faster data loading in studies.
+option_saveica       = 0 ;  % If set, write ICA activations to disk. This speeds up loading ICA components when dealing with studies.
 % Memory options 
-option_storedisk = 0 ;   % If set, keep at most one dataset in memory. Allows processing many datasets at a time.
-option_savematlab = 1 ;  % If set, write the data and dataset structure into the same file; if unset, write two files per dataset (.dat .set).
-option_computeica = 1 ;  % If set, precompute ICA activations. This requires more RAM but allows faster plotting of component activations. 
+option_single        = 1 ;  % If set, use single precision under Matlab 7.x. This saves RAM but can lead to rare numerical imprecisions. 
+% ICA options 
+option_computeica    = 1 ;  % If set, precompute ICA activations. This requires more RAM but allows faster plotting of component activations. 
+option_scaleicarms   = 1 ;  % If set, scale ICA component activities to RMS (Root Mean Square) in microvolt (recommended).
 % Folder options
-option_rememberfolder = 1 ;  % If set, when reading dataset assume the folder/directory of previous dataset.
-% ICA options
-option_scaleicarms = 0 ;  % If set, scale ICA component activities to RMS (Root Mean Square) in microvolt
+option_rememberfolder = 1 ;  % If set, when browsing to open a new dataset assume the folder/directory of previous dataset.
