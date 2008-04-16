7613967b5b9e8b251a3927607ef69336e669b1d8
Nima Bigdely Shamlo <nimabg@gmail.com>
2008-04-16 00:55:25 +0000

    _
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@7881 1e58238c-3a72-4c0f-aabe-36d030380f39

---
+++ functions/miscfunc/uniqe_cell_string.m
@@ -0,0 +1,28 @@
+function uniqueStrings = uniqe_cell_string(c)
+% uniqe string from a cell-array containing only strings, ignores all
+% non-strings.
+
+nonStringCells = [];
+for i=1:length(c) % remove non-string cells
+    if ~strcmp(class(c{i}),'char')
+        nonStringCells = [nonStringCells i];
+    end;
+end;
+c(nonStringCells) = [];
+
+uniqueStrings = {};
+for i=1:length(c) % remove non-string cells
+    if ~isAlreadyEncountered(c{i}, uniqueStrings);
+        uniqueStrings{end+1} = c{i};
+    end;
+end;
+
+function result = isAlreadyEncountered(s, u) 
+result = false;
+for i=1:length(u)
+    if strcmp(u{i}, s)
+        result = true;
+    end;
+end;
+        
+    
