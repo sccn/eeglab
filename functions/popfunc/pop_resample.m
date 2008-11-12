ac972515bfcd66f371da21f6b417d205239ade54
Arnaud Delorme <arno@ucsd.edu>
2008-11-12 22:43:54 +0000

    nothing
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@8010 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_resample.m
+++ functions/popfunc/pop_resample.m
@@ -43,6 +43,9 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+% Revision 1.24  2007/02/20 14:18:37  arno
+%  now can process several datasets
+%
 % Revision 1.23  2006/11/17 22:07:40  arno
 % help message
 %
