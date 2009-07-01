1524effde72db07fef49f7606a259779f235002d
Arnaud Delorme <arno@ucsd.edu>
2009-07-01 23:14:44 +0000

    removing pop_readbdf.m
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@8227 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_readbdf.m
+++
@@ -1,5 +0,0 @@
-% pop_readbdf - obsolete function, use the function pop_biosig() instead
-
-function varargout = pop_readbdf(varargin)
-    
-error('The function pop_readbdf() is obsolete, use the function pop_biosig instead');
