b8f102e64ac7d2da18bb26c364b9feb53ae3094f
Arnaud Delorme <arno@ucsd.edu>
2009-07-10 02:01:37 +0000

    *** empty log message ***
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@8282 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/icadefs.m
+++ functions/sigprocfunc/icadefs.m
@@ -45,7 +45,7 @@ HZDIR = 'up';               % ascending freqs = 'up'; descending = 'down'
 % ----------------------------------------------------------------------
 
 % EEGLAB version s=stable, b=beta, a=alpha (SCCN only)
-EEGLAB_VERSION = '7.0.1.5b';
+EEGLAB_VERSION = '7.0.2.5b';
 
 TUTORIAL_URL = 'http://sccn.ucsd.edu/wiki/EEGLAB'; % online version
 DEFAULT_SRATE = 256.0175;      % default local sampling rate (rarely used)
