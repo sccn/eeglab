fc1cd0973d216303420b4f748b6239cc74e8a00a
Arnaud Delorme <arno@ucsd.edu>
2009-08-05 03:21:23 +0000

    New header and new options
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@8330 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/statcondfieldtrip.m
+++ functions/sigprocfunc/statcondfieldtrip.m
@@ -1,34 +1,11 @@
-% statcond()  - compare two or more data conditions statistically using 
-%               standard parametric or nonparametric permutation-based ANOVA 
-%               (1-way or 2-way) or t-test methods. Parametric testing uses 
-%               fcdf() from the Matlab Statistical Toolbox. Use of up to 
-%               4-D data matrices speeds processing.
+% statcondfiledtrip()  - same as statcond except that it uses the fieldtrip
+%                        statistical functions. This is useful to perform
+%                        a wider variety of corrections for multiple 
+%                        comparisons for instance.
 % Usage:
 %          >> [stats, df, pvals, surrog] = statcond( data, 'key','val'... );
 % Inputs:
-%   data       = one-or two-dimensional cell array of data matrices. 
-%                   For nonparametric, permutation-based testing, the 
-%                last dimension of the data arrays (which may be of up to 
-%                4 dimensions) is permuted across conditions, either in
-%                a 'paired' fashion (not changing the, e.g., subject or
-%                trial order in the last dimension) or in an umpaired 
-%                fashion (not respecting this order). If the number of 
-%                elements in the last dimension is not the same across 
-%                conditions, the 'paired' option is turned 'off'.  Note: 
-%                All other dimensions MUST be constant across conditions. 
-%                   For example, consider a (1,3) cell array of matrices 
-%                of size (100,20,x) each holding a (100,20) time/frequency 
-%                transform from each of x subjects. Only the last dimension 
-%                (here x, the number of subjects) may differ across the 
-%                three conditions. 
-%                   The test used depends on the size of the data array input: 
-%                When the data cell array has 2 columns and the data are 
-%                paired, a paired t-test is performed; when the data are 
-%                unpaired, an unpaired t-test is performed. If 'data' 
-%                has only one row (paired or unpaired) and more than 2 
-%                columns, a one-way ANOVA is performed. If the data cell 
-%                array contains several rows and columns (of paired or 
-%                unpaired data), a two-way ANOVA is performed.
+%   data       = same as for statcond()
 %
 % Optional inputs:
 %   'paired'   = ['on'|'off'] pair the data array {default: 'on' unless 
@@ -37,8 +14,16 @@
 %                 'param' = parametric testing (standard ANOVA or t-test); 
 %                 'perm' = non-parametric testing using surrogate data 
 %                  made by permuting the input data {default: 'perm'}
-%   'naccu'    = [integer] Number of surrogate data copies to use in 'perm' 
-%                 mode estimation (see above) {default: 200}.
+%   'naccu'    = this input is passed on as 'numrandomization' to Fieldtrip
+%   'chanlocs' = EEGLAB channel location structure to perfom statistics
+%                and cluster correction for multiple comparisons accross 
+%                channels (native fieldtrip entries may also be used).
+%
+% Fieldtrip options:
+%   Any option to the freqanalysis, the statistics_montecarlo, the
+%   statistics_analysis, statistics_stat, statistics_glm may be used
+%   using 'key', val argument pairs.
+%
 % Outputs:
 %   stats      = F- or T-value array of the same size as input data without 
 %                the last dimension. A T value is returned only when the data 
@@ -49,56 +34,11 @@
 %   surrog     = surrogate data array (same size as input data with the last 
 %                dimension filled with a number ('naccu') of surrogate data sets.
 %
-% Important note: When a two-way ANOVA is performed, outputs are cell arrays
-%                 with three elements: output(1) = column effects; 
-%                 output(2) = row effects; output(3) = interactions
-%                 between rows and columns.
-% Examples:
-%      >> a = { rand(1,10) rand(1,10)+0.5 }; % pseudo 'paired' data vectors
-%         [t df pvals] = statcond(a);        % perform paired t-test
-%           pvals =                  
-%              5.2807e-04 % standard t-test probability value
-%         % Note: for different rand() outputs, results will differ.
-%
-%         [t df pvals surog] = statcond(a, 'mode', 'perm', 'naccu', 2000); 
-%           pvals =
-%              0.0065 % nonparametric t-test using 2000 permuted data sets
-%
-%         a = { rand(2,11) rand(2,10) rand(2,12)+0.5 }; % pseudo 'unpaired' 
-%         [F df pvals] = statcond(a); % perform an unpaired ANOVA 
-%           pvals =
-%              0.00025 % p-values for difference between columns 
-%              0.00002 % for each data row
-%
-%         a = { rand(3,4,10) rand(3,4,10) rand(3,4,10); ...
-%               rand(3,4,10) rand(3,4,10) rand(3,4,10)+0.5 }; 
-%         % pseudo (2,3)-condition data array, each entry containing 
-%         %                                    ten (3,4) data matrices
-%         [F df pvals] = statcond(a);  % perform a paired 2-way ANOVA 
-%         % Output:
-%           pvals{1} % a (3,4) matrix of p-values; effects across columns
-%           pvals{2} % a (3,4) matrix of p-values; effects across rows 
-%           pvals{3} % a (3,4) matrix of p-values; interaction effects
-%                                      % across rows and columns
-%
 % Author: Arnaud Delorme, SCCN/INC/UCSD, La Jolla, 2005-
-%         With rhanks to Robert Oostenveld for fruitful discussions 
+%         With thanks to Robert Oostenveld for fruitful discussions 
 %         and advice on this function.
 %
-% See also: anova1_cell(), anova2_cell(), fcdf()
-
-% perform a paired t-test
-% -----------------------
-% a = { rand(2,10) rand(2,10) };
-% [t df pval] = statcond(a); pval
-% [h p t stat] = ttest( a{1}(1,:), a{2}(1,:)); p
-% [h p t stat] = ttest( a{1}(2,:), a{2}(2,:)); p
-%
-% compare significance levels
-% --------------------------
-% a = { rand(1,10) rand(1,10) }; 
-% [F df pval] = statcond(a, 'mode', 'perm', 'naccu', 200); pval
-% [h p t stat] = ttest( a{1}(1,:), a{2}(1,:)); p
+% See also: freqanalysis(), statistics_montecarlol()
 
 %123456789012345678901234567890123456789012345678901234567890123456789012
 
@@ -119,94 +59,51 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
-% Revision 1.19  2007/05/04 23:19:02  arno
+% Revision 1.1  2009/08/04 23:19:22  arno
 % *** empty log message ***
-%
-% Revision 1.18  2007/05/04 23:18:18  arno
-% same
-%
-% Revision 1.17  2007/05/04 23:17:02  arno
-% compute correct pval for unpaired t-test
-%
-% Revision 1.16  2007/04/27 22:06:34  arno
-% unpaired condition shuffling
-%
-% Revision 1.15  2007/04/26 22:53:17  arno
-% stat for t-test
-%
-% Revision 1.14  2007/04/06 19:34:42  arno
-% Matlab 6.5 compatibiltiy
-%
-% Revision 1.13  2006/12/30 01:16:55  scott
-% help msg
-% quit
-%
-% Revision 1.12  2006/11/22 19:03:28  arno
-% number of dimensions fix
-%
-% Revision 1.11  2006/11/22 18:46:15  arno
-% fix typo
-%
-% Revision 1.10  2006/11/07 23:11:16  arno
-% implement umpaired t-test, add documentation
-%
-% Revision 1.9  2006/10/03 22:00:58  scott
-% minor help msg edits -sm
-%
-% Revision 1.8  2006/05/11 15:48:07  arno
-% now better detect unpaired data
-%
-% Revision 1.5  2005/12/11 03:06:40  scott
-% help msg editing -sm
-%
-% Revision 1.4  2005/12/09 19:05:13  arno
-% editing header
-%
-% Revision 1.3  2005/12/09 18:43:39  arno
-% scott's edit
-%
-% Revision 1.2  2005/12/07 23:31:23  arno
-% chaging help message - allowing 4-D permutation
-%
-% Revision 1.1  2005/12/07 17:26:09  arno
-% Initial revision
-%
 
-function [ ori_vals, df, pvals, surrogval ] = statcond( data, varargin );
+
+function [ ori_vals, df, pvals, surrogval ] = statcondfieldtrip( data, varargin );
     
     if nargin < 1
-        help statcond;
+        help statcondfieldtrip;
         return;
     end;
     
-    g = finputcheck( varargin, { 'naccu'   'integer'   [1 Inf]             200;
-                                 'mode'    'string'    { 'param' 'perm' }  'param';
-                                 'paired'  'string'    { 'on' 'off' }      'on' }, 'statcond');
-    if isstr(g), error(g); end;
+    [g cfgparams] = finputcheck( varargin, { 'naccu'    'integer'   [1 Inf]             200;
+                                             'mode'     'string'    { 'param' 'perm' 'bootstrap' }  'param';
+                                             'chanlocs' 'struct'    { }   struct([]);
+                                             'method'   'string'    { 'montecarlo' 'analytic' 'stat' 'glm' } 'analytic';
+                                             'paired'   'string'    { 'on' 'off' }      'on' }, 'statcond', 'ignore');
+    if isstr(g), error(g); end;    
     
-    if strcmp(g.mode, 'param' ) & exist('fcdf') ~= 2
-      fprintf(['statcond(): parametric testing requires fcdf() \n' ...
-               '            from the Matlab StatsticaL Toolbox.\n' ...
-               '            Running nonparametric permutation tests\n.']);
-      g.mode = 'perm';
-    end
     if size(data,2) == 1, data  = transpose(data); end; % cell array transpose
     
     tmpsize   = size(data{1});
-    if ~strcmpi(g.mode, 'param')
-         surrogval = zeros([ tmpsize(1:end-1) g.naccu ]);
-    else surrogval = [];
-    end;
     
     % cfg configuration for Fieldtrip
     % -------------------------------
-    cfg.method      = 'montecarlo';
-    cfg.correctm    = 'no';
-    cfg.neighbours  = {};
+    cfg = struct(cfgparams{:});
+    cfg.method      = g.method;
+    if ~strcmpi(g.mode, 'param'), cfg.method = 'montecarlo'; end;
+    if ~isempty(g.chanlocs)
+        EEG = eeg_emptyset;
+        EEG.chanlocs = g.chanlocs;
+        EEG.nbchan   = length(g.chanlocs);
+        EEG.data     = zeros(EEG.nbchan,100,1);
+        EEG = eeg_checkset(EEG);
+        tmpcfg = eeglab2fieldtrip(EEG, 'preprocessing', 'none');
+        lay = prepare_layout(tmpcfg, tmpcfg);
+        tmpcfg.layout        = lay;
+        tmpcfg.neighbourdist = 1000; %mean(abs([ EEG.chanlocs.X ]));
+        cfg.neighbours    = neighbourselection(tmpcfg, []);
+    else
+        cfg.neighbours  = {};
+    end;
     cfg.feedback    = 'no';
     cfg.ivar        = 1;
     cfg.numrandomization = g.naccu;
-            
+    
     % test if data can be paired
     % --------------------------
     if length(unique(cellfun('size', data, ndims(data{1}) ))) > 1
@@ -236,8 +133,15 @@ function [ ori_vals, df, pvals, surrogval ] = statcond( data, varargin );
             % -------------
             cfg.statistic   = 'depsamplesT';
             [newdata design1 design2 design3] = makefieldtripdata(data, 0);
+            for index = 1:length(newdata)
+                newdata{index}.powspctrm = squeeze(newdata{index}.powspctrm);
+                newdata{index}.label     = { g.chanlocs.labels };
+                newdata{index}.freq      = 1;
+            end;
             cfg.design      = [ design1; design3 ];
             cfg.uvar        = 2;
+            cfg
+            newdata{1}
             stat            = freqstatistics(cfg, newdata{:});
             ori_vals        = stat.stat;
             df              = [];
@@ -372,7 +276,6 @@ function [newdata, design1, design2, design3] = makefieldtripdata(data, chanflag
     design1 = [];
     design2 = [];
     design3 = [];
-    data
     for i = 1:size(data,2)
         for j = 1:size(data,1)
             ij = j+(i-1)*size(data,1);
