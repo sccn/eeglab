77a5778deb8ba16f6e6e04b1633bd6d31f5fbee8
Devapratim Sarma <devapratim.sarma@gmail.com>
2009-10-20 22:19:56 +0000

    added log to file
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@8446 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/miscfunc/arrow.m
+++ functions/miscfunc/arrow.m
@@ -61,6 +61,9 @@
 % He has permission to distribute ARROW with MATDRAW.
 
 
+% $Log: not supported by cvs2svn $
+
+
 function [h,yy,zz] = arrow(arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8, ...
                            arg9,arg10,arg11,arg12,arg13,arg14,arg15,arg16, ...
                            arg17,arg18,arg19,arg20,arg21,arg22,arg23,arg24)
