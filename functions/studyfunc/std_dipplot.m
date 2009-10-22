f6f895a2e4b24490a303607e0b5efe0c2e1762c1
Arnaud Delorme <arno@ucsd.edu>
2009-10-22 00:50:35 +0000

    revert previous version
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@8461 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/std_dipplot.m
+++ functions/studyfunc/std_dipplot.m
@@ -459,43 +459,39 @@ function STUDY = std_centroid(STUDY,ALLEEG, clsind, tmp);
             fprintf('.');
             comp  = STUDY.cluster(clsind(clust)).comps(k);
             abset = STUDY.cluster(clsind(clust)).sets(1,k);
-            if ~isnan(abset) 
-                if ~isfield(ALLEEG(abset), 'dipfit')
-                    warndlg2(['No dipole information available in dataset ' num2str(abset) ], 'Aborting compute centroid dipole');
-                    return;
-                end
-                if ~isempty(ALLEEG(abset).dipfit.model(comp).posxyz)
-                    ndip   = ndip +1;
-                    posxyz = ALLEEG(abset).dipfit.model(comp).posxyz;
-                    momxyz = ALLEEG(abset).dipfit.model(comp).momxyz;
-                    if size(posxyz,1) == 2
-                        if all(posxyz(2,:) == [ 0 0 0 ])
-                            posxyz(2,:) = [];
-                            momxyz(2,:) = [];
-                        end;
+            if ~isfield(ALLEEG(abset), 'dipfit')
+               warndlg2(['No dipole information available in dataset ' num2str(abset) ], 'Aborting compute centroid dipole');
+               return;
+            end
+            if ~isempty(ALLEEG(abset).dipfit.model(comp).posxyz)
+                ndip   = ndip +1;
+                posxyz = ALLEEG(abset).dipfit.model(comp).posxyz;
+                momxyz = ALLEEG(abset).dipfit.model(comp).momxyz;
+                if size(posxyz,1) == 2
+                    if all(posxyz(2,:) == [ 0 0 0 ])
+                        posxyz(2,:) = [];
+                        momxyz(2,:) = [];
                     end;
-                    tmppos = tmppos + mean(posxyz,1);
-                    tmpmom = tmpmom + mean(momxyz,1);
-                    tmprv = tmprv + ALLEEG(abset).dipfit.model(comp).rv;
-                    if strcmpi(ALLEEG(abset).dipfit.coordformat, 'spherical')
-                       if isfield(ALLEEG(abset).dipfit, 'hdmfile') %dipfit 2 spherical model
-                           load('-mat', ALLEEG(abset).dipfit.hdmfile);
-                           max_r = max(max_r, max(vol.r));
-                       else % old version of dipfit
-                           max_r = max(max_r,max(ALLEEG(abset).dipfit.vol.r));
-                       end
-                    end
                 end;
+                tmppos = tmppos + mean(posxyz,1);
+                tmpmom = tmpmom + mean(momxyz,1);
+                tmprv = tmprv + ALLEEG(abset).dipfit.model(comp).rv;
+                if strcmpi(ALLEEG(abset).dipfit.coordformat, 'spherical')
+                   if isfield(ALLEEG(abset).dipfit, 'hdmfile') %dipfit 2 spherical model
+                       load('-mat', ALLEEG(abset).dipfit.hdmfile);
+                       max_r = max(max_r, max(vol.r));
+                   else % old version of dipfit
+                       max_r = max(max_r,max(ALLEEG(abset).dipfit.vol.r));
+                   end
+               end
             end
         end
         centroid{clust}.dipole.posxyz =  tmppos/ndip;
         centroid{clust}.dipole.momxyz =  tmpmom/ndip;
         centroid{clust}.dipole.rv =  tmprv/ndip;
-        if ~isnan(abset)
-            if strcmpi(ALLEEG(abset).dipfit.coordformat, 'spherical') & (~isfield(ALLEEG(abset).dipfit, 'hdmfile')) %old dipfit
-                centroid{clust}.dipole.maxr = max_r;
-            end
-        end;
+        if strcmpi(ALLEEG(abset).dipfit.coordformat, 'spherical') & (~isfield(ALLEEG(abset).dipfit, 'hdmfile')) %old dipfit
+            centroid{clust}.dipole.maxr = max_r;
+        end
         STUDY.cluster(clsind(clust)).dipole = centroid{clust}.dipole;
     end
     fprintf('\n');
