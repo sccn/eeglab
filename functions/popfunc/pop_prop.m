2029204569cf553620d4ad1a4ce57af0de5247a4
Arnaud Delorme <arno@ucsd.edu>
2010-01-25 20:28:32 +0000

    David Groppe's fix for erpimage
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@8563 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_prop.m
+++ functions/popfunc/pop_prop.m
@@ -43,6 +43,15 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 % $Log: not supported by cvs2svn $
+%
+% 'erp_vltg_ticks' option added to erpimage calls for epoched data.  This
+% makes ERP/ERA plots under ERPimages more readable by limiting the number
+% of y-ticks to 2. Minor 'ei_smooth == 1' bug changed to 'ei_smooth = 1'
+% and some mistaken typecomp comments fixed. -DG
+%
+% Revision 1.43  2008/02/29 16:09:42  arno
+% fixing (again) the buggy erpimage plot for continuous data
+%
 % Revision 1.42  2007/11/29 22:05:45  arno
 % change text in gui
 %
@@ -315,27 +324,32 @@ if EEG.trials > 1
     else
       ei_smooth = 3;
     end
-    if typecomp == 1 % plot component
+    if typecomp == 1 % plot channel
          offset = nan_mean(EEG.data(chanorcomp,:));
+         erp=nan_mean(squeeze(EEG.data(chanorcomp,:,:))')-offset;
+         erp_limits=get_era_limits(erp);
          erpimage( EEG.data(chanorcomp,:)-offset, ones(1,EEG.trials)*10000, EEG.times , ...
-                       '', ei_smooth, 1, 'caxis', 2/3, 'cbar','erp');   
-    else % plot channel
+                       '', ei_smooth, 1, 'caxis', 2/3, 'cbar','erp','erp_vltg_ticks',erp_limits);   
+    else % plot component
           if option_computeica  
                   offset = nan_mean(EEG.icaact(chanorcomp,:));
+                  era=nan_mean(squeeze(EEG.icaact(chanorcomp,:,:))')-offset;
+                  era_limits=get_era_limits(era);
                   erpimage( EEG.icaact(chanorcomp,:)-offset, ones(1,EEG.trials)*10000, EEG.times , ...
-                       '', ei_smooth, 1, 'caxis', 2/3, 'cbar','erp', 'yerplabel', '');   
+                       '', ei_smooth, 1, 'caxis', 2/3, 'cbar','erp', 'yerplabel', '','erp_vltg_ticks',era_limits);   
           else
                   icaacttmp = (EEG.icaweights(chanorcomp,:) * EEG.icasphere) ...
                                    * reshape(EEG.data(EEG.icachansind,:,:), length(EEG.icachansind), EEG.trials*EEG.pnts);
                   offset = nan_mean(icaacttmp);
+                  era=nan_mean(reshape(icaacttmp,EEG.pnts,EEG.trials)')-offset;
+                  era_limits=get_era_limits(era);
                   erpimage( icaacttmp-offset, ones(1,EEG.trials)*10000, EEG.times, ...
-                       '', ei_smooth, 1, 'caxis', 2/3, 'cbar','erp', 'yerplabel', '');   
+                       '', ei_smooth, 1, 'caxis', 2/3, 'cbar','erp', 'yerplabel', '','erp_vltg_ticks',era_limits);  
           end;
     end;
     axes(hhh);
     title(sprintf('%s activity \\fontsize{10}(global offset %3.3f)', basename, offset), 'fontsize', 14);
 else
-
     % put title at top of erpimage
     EI_TITLE = 'Continous data';
     axis off
@@ -345,31 +359,35 @@ else
        ERPIMAGELINES = round(0.9 * ERPIMAGELINES);
     end
     if ERPIMAGELINES > 2   % give up if data too small
-      if ERPIMAGELINES < 10
-         ei_smooth == 1;
-      else
-        ei_smooth = 3;
-      end
+        if ERPIMAGELINES < 10
+            ei_smooth = 1;
+        else
+            ei_smooth = 3;
+        end
       erpimageframes = floor(size(EEG.data,2)/ERPIMAGELINES);
       erpimageframestot = erpimageframes*ERPIMAGELINES;
       eegtimes = linspace(0, erpimageframes-1, EEG.srate/1000);
-      if typecomp == 1 % plot component
+      if typecomp == 1 % plot channel
            offset = nan_mean(EEG.data(chanorcomp,:));
+           % Note: we don't need to worry about ERP limits, since ERPs
+           % aren't visualized for continuous data
            erpimage( reshape(EEG.data(chanorcomp,1:erpimageframestot),erpimageframes,ERPIMAGELINES)-offset, ones(1,ERPIMAGELINES)*10000, eegtimes , ...
-                         EI_TITLE, ei_smooth, 1, 'caxis', 2/3, 'cbar');   
-      else % plot channel
+                         EI_TITLE, ei_smooth, 1, 'caxis', 2/3, 'cbar');  
+      else % plot component
             if option_computeica  
-                    offset = nan_mean(EEG.icaact(chanorcomp,:));
-                    erpimage( ...
-              reshape(EEG.icaact(chanorcomp,1:erpimageframestot),erpimageframes,ERPIMAGELINES)-offset, ...
-                   ones(1,ERPIMAGELINES)*10000, eegtimes , ...
-                         EI_TITLE, ei_smooth, 1, 'caxis', 2/3, 'cbar','yerplabel', '');   
+                offset = nan_mean(EEG.icaact(chanorcomp,:));
+                % Note: we don't need to worry about ERA limits, since ERAs
+                % aren't visualized for continuous data
+                erpimage(reshape(EEG.icaact(chanorcomp,1:erpimageframestot),erpimageframes,ERPIMAGELINES)-offset,ones(1,ERPIMAGELINES)*10000, eegtimes , ...
+                    EI_TITLE, ei_smooth, 1, 'caxis', 2/3, 'cbar','yerplabel', '');
             else
-                    icaacttmp = EEG.icaweights(chanorcomp,:) * EEG.icasphere ...
-                                     * EEG.data(EEG.icachansind,1:erpimageframestot);
-                    offset = nan_mean(icaacttmp);
-                    erpimage( icaacttmp-offset, ones(1,ERPIMAGELINES)*10000, eegtimes, ...
-                         EI_TITLE, ei_smooth, 1, 'caxis', 2/3, 'cbar', 'yerplabel', '');   
+                icaacttmp = EEG.icaweights(chanorcomp,:) * EEG.icasphere ...
+                    * EEG.data(EEG.icachansind,1:erpimageframestot);
+                offset = nan_mean(icaacttmp);
+                % Note: we don't need to worry about ERA limits, since ERAs
+                % aren't visualized for continuous data
+                erpimage( icaacttmp-offset, ones(1,ERPIMAGELINES)*10000, eegtimes, ...
+                    EI_TITLE, ei_smooth, 1, 'caxis', 2/3, 'cbar', 'yerplabel', '');
             end;
       end
     else
@@ -520,4 +538,53 @@ function out = nan_mean(in)
     out = sum(in)./nonnans;
     out(nononnans) = NaN;
 
+    
+function era_limits=get_era_limits(era)
+%function era_limits=get_era_limits(era)
+%
+% Returns the minimum and maximum value of an event-related
+% activation/potential waveform (after rounding according to the order of
+% magnitude of the ERA/ERP)
+%
+% Inputs:
+% era - [vector] Event related activation or potential
+%
+% Output:
+% era_limits - [min max] minimum and maximum value of an event-related
+% activation/potential waveform (after rounding according to the order of
+% magnitude of the ERA/ERP)
+
+mn=min(era);
+mx=max(era);
+mn=orderofmag(mn)*round(mn/orderofmag(mn));
+mx=orderofmag(mx)*round(mx/orderofmag(mx));
+era_limits=[mn mx];
+
+
+function ord=orderofmag(val)
+%function ord=orderofmag(val)
+%
+% Returns the order of magnitude of the value of 'val' in multiples of 10
+% (e.g., 10^-1, 10^0, 10^1, 10^2, etc ...)
+% used for computing erpimage trial axis tick labels as an alternative for
+% plotting sorting variable
+
+val=abs(val);
+if val>=1
+    ord=1;
+    val=floor(val/10);
+    while val>=1,
+        ord=ord*10;
+        val=floor(val/10);
+    end
+    return;
+else
+    ord=1/10;
+    val=val*10;
+    while val<1,
+        ord=ord/10;
+        val=val*10;
+    end
+    return;
+end
 
