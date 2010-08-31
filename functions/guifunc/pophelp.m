8de5b703d50006587a824ccb11a7b91096e002a6
Arnaud Delorme <arno@ucsd.edu>
2010-08-31 01:57:43 +0000

    Handle txt license file
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@8979 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/guifunc/pophelp.m
+++ functions/guifunc/pophelp.m
@@ -38,19 +38,24 @@ if nargin <2
 	nonmatlab = 0;
 end;
 
-text1 = help2html(funct);
-if length(funct) > 4 & strcmpi(funct(1:4), 'pop_')
-	try,
-		text2 = help2html(funct(5:end))
-		text1 = [text1 '<br><pre>___________________________________________________________________' 10 ...
-					   ' ' 10 ...
-                       ' The ''pop'' function above calls the eponymous Matlab function below' 10 ...
-                       ' and could use some of its optional parameters' 10 ...
-					   '___________________________________________________________________</pre><br><br>' text2 ];
-	catch, end;
+if length(funct) > 3 && strcmpi(funct(end-3:end), '.txt')
+    web(funct);
+else
+    text1 = help2html(funct);
+    if length(funct) > 4 & strcmpi(funct(1:4), 'pop_')
+        try,
+            text2 = help2html(funct(5:end))
+            text1 = [text1 '<br><pre>___________________________________________________________________' 10 ...
+                           ' ' 10 ...
+                           ' The ''pop'' function above calls the eponymous Matlab function below' 10 ...
+                           ' and could use some of its optional parameters' 10 ...
+                           '___________________________________________________________________</pre><br><br>' text2 ];
+        catch, end;
+    end;
+
+    web([ 'text://' text1 ]);
 end;
 
-web([ 'text://' text1 ]);
 return;
 
 doc1 = readfunc(funct, nonmatlab);
