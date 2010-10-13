52353fcf50120a3a0c5eadd6242fff642235fc37
Arnaud Delorme <arno@ucsd.edu>
2010-10-13 01:06:19 +0000

    At revision 1919.
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@9047 1e58238c-3a72-4c0f-aabe-36d030380f39

--- external/fieldtrip-partial/fieldtripdefs.m
+++ external/fieldtrip-partial/fieldtripdefs.m
@@ -24,7 +24,7 @@ function fieldtripdefs
 %    You should have received a copy of the GNU General Public License
 %    along with FieldTrip. If not, see <http://www.gnu.org/licenses/>.
 %
-% $Id: fieldtripdefs.m 948 2010-04-21 18:02:21Z roboos $
+% $Id: fieldtripdefs.m 1419 2010-07-16 07:04:14Z roboos $
 
 % set the global defaults, the checkconfig function will copy these into the local configurations
 global ft_default
@@ -37,9 +37,15 @@ if ~isfield(ft_default, 'checksize'),   ft_default.checksize   = 1e5;      end %
 persistent versionpath
 persistent signalpath
 
-% don't use path caching with the persistent variable, this makes it slower
-% but ensures that during the transition the subdirectories are added smoothly
-clear hastoolbox
+% some people mess up their path settings with addpath(genpath(...))which
+% results in different versions of SPM or other other toolboxes on the path
+list = which('spm', '-all');
+if length(list)>1
+  warning('multiple versions of SPM on your path will confuse FieldTrip');
+  for i=1:length(list)
+    warning('spm is found here: %s', list{i});
+  end
+end
 
 if isempty(which('hastoolbox'))
   % the fieldtrip/public directory contains the hastoolbox function
@@ -49,7 +55,7 @@ end
 
 try
   % this directory contains the backward compatibility wrappers for the ft_xxx function name change
-  hastoolbox('compat', 1, 1);
+  hastoolbox('compat', 3, 1); % not required
 end
 
 try
@@ -70,19 +76,19 @@ end
 try
   % this contains the low-level reading functions
   hastoolbox('fileio', 1, 1);
-  hastoolbox('fileio/compat', 1, 1);
+  hastoolbox('fileio/compat', 3, 1); % not required
 end
 
 try
   % this is for filtering time-series data
   hastoolbox('preproc', 1, 1);
-  hastoolbox('preproc/compat', 1, 1);
+  hastoolbox('preproc/compat', 3, 1); % not required
 end
 
 try
   % this contains forward models for the EEG and MEG volume conduction problem
   hastoolbox('forward', 1, 1);
-  hastoolbox('forward/compat', 1, 1);
+  hastoolbox('forward/compat', 3, 1); % not required
 end
 
 try
@@ -98,86 +104,16 @@ end
 try
   % this contains intermediate-level plotting functions, e.g. multiplots and 3-d objects
   hastoolbox('plotting', 1, 1);
+  hastoolbox('plotting/compat', 1, 1);
+end
+
+try 
+  % this contains the functions to compute connecitivy metrics
+  hastoolbox('connectivity', 1,1);
 end
 
 try
   % this contains specific code and examples for realtime processing
-  hastoolbox('realtime', 1, 1);
+  hastoolbox('realtime', 3, 1);             % not required
+  hastoolbox('realtime/datasource', 3, 1);  % not required
 end
-
-if isempty(versionpath)
-  % fieldtrip/compat contains version specific subdirectories to facilitate in backward compatibility support
-  switch version('-release')
-    case '13'
-      % Version 6.5.1.199709 Release 13 (Service Pack 1)
-      versionpath = fullfile(fileparts(which('fieldtripdefs')), 'compat', 'R13');
-    case '14'
-      % Version 7.0.4.352 (R14) Service Pack 2
-      % Version 7.1.0.183 (R14) Service Pack 3
-      versionpath = fullfile(fileparts(which('fieldtripdefs')), 'compat', 'R14');
-    case '2006a'
-      % Version 7.2.0.283 (R2006a)
-      versionpath = fullfile(fileparts(which('fieldtripdefs')), 'compat', '2006a');
-    case '2006b'
-      % Version 7.3
-      versionpath = fullfile(fileparts(which('fieldtripdefs')), 'compat', '2006b');
-    case '2007a'
-      % Version 7.4
-      versionpath = fullfile(fileparts(which('fieldtripdefs')), 'compat', '2007a');
-    case '2007b'
-      % Version 7.5
-      versionpath = fullfile(fileparts(which('fieldtripdefs')), 'compat', '2007b');
-    case '2008a'
-      % Version 7.6
-      versionpath = fullfile(fileparts(which('fieldtripdefs')), 'compat', '2008a');
-    case '2008b'
-      % Version 7.7
-      versionpath = fullfile(fileparts(which('fieldtripdefs')), 'compat', '2008b');
-    case '2009a'
-      % Version 7.8
-      versionpath = fullfile(fileparts(which('fieldtripdefs')), 'compat', '2009a');
-    case '2009b'
-      % Version 7.9
-      versionpath = fullfile(fileparts(which('fieldtripdefs')), 'compat', '2009b');
-    otherwise
-      % unknown release number, do nothing
-      versionpath = 'unknown';
-  end % switch
-  if ~strcmp(versionpath, 'unknown') && isdir(versionpath) && isempty(strfind(path, versionpath))
-    % only add the directory if it exists and was not added to the path before
-    addpath(versionpath);
-  end
-end % if isempty(versionpath)
-
-% test whether compat directories have been added corresponding to another matlab version
-p = path;
-r = version('-release');
-v = {
-  'R13'
-  'R14'
-  '2006a'
-  '2006b'
-  '2007a'
-  '2007b'
-  '2008a'
-  '2008b'
-  '2009a'
-  '2009b'
-  };
-for i=1:length(v)
-  versionpath = fullfile(fileparts(which('fieldtripdefs')), 'compat', v{i});
-  if ~isempty(strfind(p, versionpath)) && ~strcmp(r, v{i})
-    warning('The directory %s was found on your path, whereas you are running Matlab version %s.', versionpath, r)
-    pause(0.5); % ensure that the user has time to read it
-    warning('You should NOT add all subdirectories of fieldtrip to your path using addpath(genpath(...)).');
-    pause(0.2); % ensure that the user has time to read it
-    warning('You should ONLY add the fieldtrip main directory to your path and then call ''fieldtripdefs''.');
-    pause(1.7); % ensure that the user has time to read it
-    fprintf('Trying to remove incompatible directory from path...\n');
-    pause(2.3); % ensure that the user has time to read it
-    t = strfind(p, versionpath);
-    p(t:t+length(versionpath)) = []; % don't use rmpath, because it may be broken due to the incorrect "fieldtrip/compat/release" version
-    path(p);
-  end
-end
-
