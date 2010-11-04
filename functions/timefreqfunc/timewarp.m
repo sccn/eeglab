dc0c440da32a60ac3bf4536fbb9d203b2983413e
Arnaud Delorme <arno@ucsd.edu>
2010-11-04 01:53:53 +0000

    copy version from sigprocfunc folder
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@9068 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/timefreqfunc/timewarp.m
+++ functions/timefreqfunc/timewarp.m
@@ -1,36 +1,65 @@
-% timeWarp()   - Given two event marker vectors, timeWarp computes a matrix
-%                that can be used to warp a timeserie so that its
-%                evLatencies match newLatencies. Values of the warped
+% timewarp()   - Given two event marker vectors, computes a matrix
+%                that can be used to warp a time series so that its
+%                evlatencies match newlatencies. Values of the warped
 %                timeserie that falls between two frames in the original
 %                timeserie will be linear interpolated.
-%
 % Usage:
-%   >> warpMat = timeWarp(evLatency, newLatency)
+%   >> warpmat = timewarp(evlatency, newlatency)
 %
 % Necessary inputs:
-%   evLatency  - [vector] time markers on the original time-series, in
-%                frames unit. Markers have to be ordered by increasing
-%                frame latency. If you want to warp the entire
-%                time-series, make sure frame 1 and the last frame are in
-%                that vector.
-%   newLatency - [vector] wished time marker latencies. The original
-%                time-serie will be warped so that its time markers (see
-%                evLatency) match the ones in newLatency. newLatency
-%                frames have to be sorted by ascending latencies in frame
-%                unit. Both vectors have to be the same length.
+%   evlatency  - [vector] event markers in the original time series, in frames
+%                Markers must be ordered by increasing frame latency. 
+%                If you want to warp the entire time-series, make sure 
+%                the first (1) and last frames are in the vector.
+%   newlatency - [vector] desired warped event time latencies. The original
+%                time series will be warped so that the frame numbers of its 
+%                events (see evlatency above) match the frame numbers in 
+%                newlatency. newlatency frames must be sorted by ascending 
+%                latency. Both vectors must be the same length.
 %   
 % Optional outputs:
-%      warpMat - [matrix] Multiplying this matrix with the original
-%                time-serie (column) vector performs the warping.
+%      warpmat - [matrix] Multiplying this matrix with the original
+%                time series (column) vectors performs the warping.
 %
 % Example:
-%   >> warpMat = timeWarp([1 5 10], [1 6 9])
-%   >> warpMat*[1:10]'
+%      % In 10-frame vectors, warp frames 3 and 5 to frames 4 and 8,
+%      % respectively. Generate a matrix to warp data epochs in 
+%      % variable 'data' of size (10,k)
+%      >> warpmat = timewarp([1 3 5  10], [1 4 8 10])
+%      >> warped_data = warpmat*data;
 %
 % Authors: Jean Hausser, SCCN/INC/UCSD, 2006
 %
-% See also: angTimeWarp(), phasecoher(), erpimage(), newtimef()
-
+% See also: angtimewarp(), phasecoher(), erpimage()
+%
+function M=timewarp(evLatency, newLatency)
+  M = [0];
+  if min(sort(evLatency) == evLatency) == 0
+    error('evLatency should be in ascending order');
+    return;
+  end
+  if min(sort(newLatency) == newLatency) == 0
+    error('newLatency should be in ascending order');
+    return;
+  end
+  if length(evLatency) ~= length(newLatency)
+    error('evLatency and newLatency must have the same length.');
+    return;
+  end
+  if length(evLatency) < 2 | length(newLatency) < 2
+    error(['There should be at least two events in evlatency and ' ...
+          'newlatency (e.g., "begin" and "end")'] );
+    return;
+  end
+  if evLatency(1) ~= 1
+    disp(['Assuming old and new time series beginnings are synchronized.']);
+    disp(['Make sure you have defined an ending event in both the old and new time series!']);
+    evLatency(end+1)=1;
+    newLatency(end+1)=1;
+    evLatency = sort(evLatency);
+    newLatency = sort(newLatency);
+  end
+    
   t = 1:max(evLatency);
   
   for k=1:length(evLatency)-1
@@ -43,8 +72,7 @@
   end
   
   
-  %Check what's going on at tp(max(newLatency)), should equal t(max(evLatency))
-% $$$   keyboard;
+  % Check what's going on at tp(max(newLatency)), should equal t(max(evLatency))
   tp(max(evLatency)) = max(newLatency);
   ts = tp-min(newLatency)+1;
   
@@ -58,11 +86,9 @@
     end
 % $$$     k = k-1;
     
-% $$$     keyboard;
     if k == 0
       % Check wether i == ts(1) and i == 1
       % In that case, M(1,1) = 1
-% $$$       keyboard;
       M(1,1) = 1;
     else
       M(i,k) = 1 - (i-ts(k))/(ts(k+1)-ts(k));
