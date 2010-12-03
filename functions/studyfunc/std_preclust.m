019775d6574939ec889fd7f86efe9aa73eade8a5
Arnaud Delorme <arno@ucsd.edu>
2010-12-03 01:04:17 +0000

    Fixed checking for a "complete" STUDY when doing pre-clustering
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@9100 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/std_preclust.m
+++ functions/studyfunc/std_preclust.m
@@ -252,7 +252,7 @@ function [ STUDY, ALLEEG ] = std_preclust(STUDY, ALLEEG, cluster_ind, varargin)
         % check that all datasets are in preclustering for current design
         % ---------------------------------------------------------------
         tmpstruct = std_setcomps2cell(STUDY, STUDY.cluster(cluster_ind).sets, STUDY.cluster(cluster_ind).comps);
-        alldatasets = unique( [ tmpstruct.setinds{:} ]);
+        alldatasets = unique(STUDY.cluster(cluster_ind).sets(:));
         
         if length(alldatasets) < length(STUDY.datasetinfo)
             error( [ 'Some datasets not included in preclustering' 10 ... 
