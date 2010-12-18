aaee22654660079850a49a5226a2c9bfbb41f257
Arnaud Delorme <arno@ucsd.edu>
2010-12-18 01:33:48 +0000

    backward compatibility with Matlab 7.4
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@9115 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/guifunc/pophelp.m
+++ functions/guifunc/pophelp.m
@@ -38,54 +38,53 @@ if nargin <2
 	nonmatlab = 0;
 end;
 
-if length(funct) > 3 && strcmpi(funct(end-3:end), '.txt')
-    web(funct);
+if exist('help22html')
+    if length(funct) > 3 && strcmpi(funct(end-3:end), '.txt')
+        web(funct);
+    else
+        text1 = help2html(funct);
+        if length(funct) > 4 & strcmpi(funct(1:4), 'pop_')
+            try,
+                text2 = help2html(funct(5:end));
+                text1 = [text1 '<br><pre>___________________________________________________________________' 10 ...
+                               ' ' 10 ...
+                               ' The ''pop'' function above calls the eponymous Matlab function below' 10 ...
+                               ' and could use some of its optional parameters' 10 ...
+                               '___________________________________________________________________</pre><br><br>' text2 ];
+            catch, end;
+        end;
+
+        web([ 'text://' text1 ]);
+    end;
 else
-    text1 = help2html(funct);
+    doc1 = readfunc(funct, nonmatlab);
     if length(funct) > 4 & strcmpi(funct(1:4), 'pop_')
         try,
-            text2 = help2html(funct(5:end));
-            text1 = [text1 '<br><pre>___________________________________________________________________' 10 ...
-                           ' ' 10 ...
-                           ' The ''pop'' function above calls the eponymous Matlab function below' 10 ...
-                           ' and could use some of its optional parameters' 10 ...
-                           '___________________________________________________________________</pre><br><br>' text2 ];
+            doc2 = readfunc(funct(5:end), nonmatlab);
+            doc1 = { doc1{:} ' _________________________________________________________________ ' ...
+                           ' ' ...
+                           ' The ''pop'' function above calls the eponymous Matlab function below, ' ...
+                           ' which may contain more information for some parameters. '...
+                           ' ' ...
+                           ' _________________________________________________________________ ' ...
+                           ' ' ...
+                    doc2{:} };
         catch, end;
     end;
 
-    web([ 'text://' text1 ]);
+    textgui(doc1);1000
+    h = findobj('parent', gcf, 'style', 'slider');
+    try, icadefs; catch, 
+        GUIBUTTONCOLOR = [0.8 0.8 0.8]; 
+        GUITEXTCOLOR   = 'k'; 
+    end;
+    set(h, 'backgroundcolor', GUIBUTTONCOLOR);
+    h = findobj('parent', gcf, 'style', 'pushbutton');
+    set(h, 'backgroundcolor', GUIBUTTONCOLOR);
+    h = findobj('parent', gca);
+    set(h, 'color', GUITEXTCOLOR);
+    set(gcf, 'color', BACKCOLOR);
 end;
-
-return;
-
-doc1 = readfunc(funct, nonmatlab);
-if length(funct) > 4 & strcmpi(funct(1:4), 'pop_')
-	try,
-		doc2 = readfunc(funct(5:end), nonmatlab);
-		doc1 = { doc1{:} ' _________________________________________________________________ ' ...
-					   ' ' ...
-                       ' The ''pop'' function above calls the eponymous Matlab function below, ' ...
-                       ' which may contain more information for some parameters. '...
-					   ' ' ...
-					   ' _________________________________________________________________ ' ...
-                       ' ' ...
-				doc2{:} };
-	catch, end;
-end;
-
-textgui(doc1);1000
-h = findobj('parent', gcf, 'style', 'slider');
-try, icadefs; catch, 
-	GUIBUTTONCOLOR = [0.8 0.8 0.8]; 
-	GUITEXTCOLOR   = 'k'; 
-end;
-set(h, 'backgroundcolor', GUIBUTTONCOLOR);
-h = findobj('parent', gcf, 'style', 'pushbutton');
-set(h, 'backgroundcolor', GUIBUTTONCOLOR);
-h = findobj('parent', gca);
-set(h, 'color', GUITEXTCOLOR);
-set(gcf, 'color', BACKCOLOR);
-
 return;
 
 function [doc] = readfunc(funct, nonmatlab)
