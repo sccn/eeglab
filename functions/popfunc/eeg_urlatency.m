0681038a7442e79cc8518bf9a2922f2ac458a72d
Arnaud Delorme <arno@ucsd.edu>
2011-03-19 03:34:25 +0000

    fix header and allow computing latencies on input arrays in addition to single values
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@9174 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/eeg_urlatency.m
+++ functions/popfunc/eeg_urlatency.m
@@ -1,5 +1,5 @@
-% eeg_urlatency() - find the real latency of an event in the continuous 
-%                   data.
+% eeg_urlatency() - find the original (ur) latency of a time point in 
+%                   the original continuous data.
 %
 % Usage:
 %   >> lat_out = eeg_urlatency( event, lat_in );
@@ -8,14 +8,17 @@
 %   event      - event structure. If this structure contain boundary
 %                events, the length of these events is added to restore
 %                the original latency from the relative latency in 'lat_in'
-%   lat_in     - relative latency.
+%   lat_in     - relative latency in sample point
 %
 % Outputs:
 %   lat_out     - output latency
 %
+% Note: the function that finds the latency in the current dataset using (ur)
+%       original latencies as input is eeg_latencyur()
+% 
 % Author: Arnaud Delorme, SCCN, INC, UCSD, April, 15, 2004
 %
-% See also: eeg_eegrej()
+% See also: eeg_latencyur()
 
 % Copyright (C) 2004 Arnaud Delorme, SCCN, INC, UCSD, arno@salk.edu
 %
@@ -47,9 +50,8 @@ function latout = eeg_urlatency( events, latin );
         
         if isfield(events, 'duration') & ~isempty(indbound)
             for index = indbound'
-                if events(index).latency < latin
-                    latout = latout + events(index).duration;
-                end;
+                tmpInds = find(events(index).latency < latin); % the find handles several input latencies
+                latout(tmpInds) = latout(tmpInds) + events(index).duration;
             end;
         elseif ~isempty(indbound) % boundaries but with no associated duration
             latout = NaN;
