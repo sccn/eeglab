b26ba322495d09f0d3259727bdcf74fb7e775948
Arnaud Delorme <arno@ucsd.edu>
2011-04-26 03:24:29 +0000

    added option to visualize rejected portions of data and reuse precomputed data
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@9201 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/pop_rejcont.m
+++ functions/popfunc/pop_rejcont.m
@@ -51,7 +51,7 @@
 % along with this program; if not, write to the Free Software
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-function [EEG selectedregions com ] = pop_rejcont(EEG, varargin);
+function [EEG selectedregions precompstruct com ] = pop_rejcont(EEG, varargin);
 
 com = '';
 if nargin < 1
@@ -97,43 +97,76 @@ else
     options = varargin;
 end;
 
-opt = finputcheck(options, { 'threshold'   'real'   []    10;
-                             'elecrange'   'real'   []    [1:EEG.nbchan];
-                             'freqlimit'   'real'   []    [20 40];
-                             'contiguous'  'real'   []    4;
-                             'addlength'   'real'   []    0.25;
-                             'eegplot'     'string' { 'on' 'off' } 'off';
-                             'epochlength' 'real'   []    0.5 }, 'pop_rejcont');
-     
+opt = finputcheck(options, { 'threshold'     { 'real' 'cell' }  []    10;
+                             'freqlimit'     { 'real' 'cell' }  []    [35 128];
+                             'elecrange'     'real'   []    [1:EEG.nbchan];
+                             'rejectori'     'real'   []    [];
+                             'contiguous'    'real'   []    4;
+                             'addlength'     'real'   []    0.25;
+                             'precompstruct' 'struct' []    struct([]);
+                             'eegplot'       'string' { 'on' 'off' } 'off';
+                             'limitsonly'    'string' { 'on' 'off' } 'off';
+                             'verbose'       'string' { 'on' 'off' } 'on';
+                             'overlap'       'real'   []    0.25;
+                             'epochlength'   'real'   []    0.5 }, 'pop_rejcont');
+if isstr(opt), error(opt); end;
+if ~iscell(opt.threshold) && length(opt.threshold) == 2 && ...
+    iscell(opt.freqlimit) && length(opt.freqlimit) == 2
+    opt.threshold = { opt.threshold(1) opt.threshold(2) };
+end;
+if ~iscell(opt.threshold), opt.threshold = { opt.threshold }; end;
+if ~iscell(opt.freqlimit), opt.freqlimit = { opt.freqlimit }; end;
+
 %EEG.event = [];
 grouplen  = opt.contiguous/2*opt.epochlength*EEG.srate+1; % maximum number of points for grouping regions
 color     = [ 0 0.9 0];             % color of rejection window
 
-% compute power spectrum
-% ----------------------
 NEWEEG = EEG;
-% average reference 
-% NEWEEG.data(opt.elecrange,:) = NEWEEG.data(opt.elecrange,:)-repmat(mean(NEWEEG.data(opt.elecrange,:),1), [length(opt.elecrange) 1]);
+if isempty(opt.precompstruct)
+    % compute power spectrum
+    % ----------------------
+    % average reference 
+    % NEWEEG.data(opt.elecrange,:) = NEWEEG.data(opt.elecrange,:)-repmat(mean(NEWEEG.data(opt.elecrange,:),1), [length(opt.elecrange) 1]);
 
-[TMPNEWEEG]= eeg_regepochs(NEWEEG, 0.25, [0 opt.epochlength], NaN);
-%[TMPNEWEEG indices] = pop_rejspec(TMPNEWEEG, 1, [1:64], -100, 15, 30, 45, 0, 0);
-%rejepoch = find(indices);
-tmp   = fft(TMPNEWEEG.data, [], 2);
-freqs = linspace(0, TMPNEWEEG.srate/2, size(tmp,2)/2);
-freqs = freqs(2:end); % remove DC (match the output of PSD)
-tmp   = tmp(:,2:size(tmp,2)/2,:);
-warning('off', 'MATLAB:log:logOfZero');
-tmpspec  = 10*log10(abs(tmp).^2);  
-warning('on', 'MATLAB:log:logOfZero');
-tmpspec  = tmpspec - repmat( mean(tmpspec,3), [1 1 TMPNEWEEG.trials]);
-specdata = tmpspec;
+    [TMPNEWEEG]= eeg_regepochs(NEWEEG, opt.overlap, [0 opt.epochlength], NaN);
+    %[TMPNEWEEG indices] = pop_rejspec(TMPNEWEEG, 1, [1:64], -100, 15, 30, 45, 0, 0);
+    %rejepoch = find(indices);
+    tmp   = fft(TMPNEWEEG.data, [], 2);
+    freqs = linspace(0, TMPNEWEEG.srate/2, size(tmp,2)/2);
+    freqspectrum = freqs(2:end); % remove DC (match the output of PSD)
+    tmp   = tmp(:,2:size(tmp,2)/2,:);
+    warning('off', 'MATLAB:log:logOfZero');
+    tmpspec  = 10*log10(abs(tmp).^2);  
+    warning('on', 'MATLAB:log:logOfZero');
+    tmpspec  = tmpspec - repmat( mean(tmpspec,3), [1 1 TMPNEWEEG.trials]);
+    specdata = tmpspec;
+
+    % compute mean spectrum
+    % ---------------------
+    meanspectrum = nan_mean(specdata(opt.elecrange, :, :), 1);
+    precompstruct.spec  = meanspectrum;
+    precompstruct.freqs = freqspectrum;
+    precompstruct.EEG   = TMPNEWEEG;
+else
+    meanspectrum  = opt.precompstruct.spec;
+    freqspectrum  = opt.precompstruct.freqs;
+    TMPNEWEEG     = opt.precompstruct.EEG;
+    precompstruct = opt.precompstruct;
+end;
 
 % apply threshold to average of all electrodes
 % --------------------------------------------
-%[I1 Irej NS Erej] = eegthresh( mean(specdata(opt.elecrange, :, :), 1), size(specdata,2), 1:length(opt.elecrange), -100, 15, [freqs(1) freqs(end)], 30, 45);
-if length(opt.threshold) == 1, opt.threshold = [ -100 opt.threshold ]; end;
-[I1 rejepoch NS Erej] = eegthresh( mean(specdata(opt.elecrange, :, :), 1), size(specdata,2), 1, opt.threshold(1), opt.threshold(2), [freqs(1) freqs(end)], opt.freqlimit(1), opt.freqlimit(2));
-fprintf('%d regions selected for rejection\n', length(rejepoch));
+rejepoch = [];
+for iReject = 1:length(opt.threshold)
+    threshold = opt.threshold{iReject};
+    freqLim   = opt.freqlimit{iReject};
+    if length(threshold) == 1, threshold = [ -100 threshold ]; end;
+    [I1 tmpRejEpoch NS Erej] = eegthresh( meanspectrum, size(meanspectrum,2), 1, threshold(1), threshold(2), [freqspectrum(1) freqspectrum(end)], freqLim(1), freqLim(2));
+    rejepoch = union(rejepoch, tmpRejEpoch);
+    if strcmpi(opt.verbose, 'on')
+        fprintf('%d regions selected for rejection, threshold %3.2f-%3.2f dB, frequency limits %3.1f-%3.1f\n', length(tmpRejEpoch), threshold(1), threshold(2), freqLim(1), freqLim(2));
+    end;
+end;
 
 % build the winrej array for eegplot
 % ----------------------------------
@@ -143,18 +176,19 @@ if ~isempty(find(cellfun(@isempty, { TMPNEWEEG.event.epoch }) == 1))
 end;
 tmpevent = TMPNEWEEG.event;
 allepoch = [ tmpevent.epoch ];
-for index = 1:length(rejepoch)
-    eventepoch = find( rejepoch(index) == allepoch );
-    if strcmpi(TMPNEWEEG.event(eventepoch(1)).type, 'X')
-        urevent = TMPNEWEEG.event(eventepoch(1)).urevent;
-        lat     = TMPNEWEEG.urevent(urevent).latency;
-        %if length(
-        winrej  = [ winrej; lat lat+opt.epochlength*NEWEEG.srate-1 color ]; %Erej(:,index)'];
-    else
-        error('Wrong type for epoch');
+if ~isempty(rejepoch)
+    for index = 1:length(rejepoch)
+        eventepoch = find( rejepoch(index) == allepoch );
+        if strcmpi(TMPNEWEEG.event(eventepoch(1)).type, 'X')
+            urevent = TMPNEWEEG.event(eventepoch(1)).urevent;
+            lat     = TMPNEWEEG.urevent(urevent).latency;
+            winrej  = [ winrej; lat lat+opt.epochlength*TMPNEWEEG.srate-1 color ]; %Erej(:,index)'];
+        else
+            error('Wrong type for epoch');
+        end;
     end;
+    winrej(:,6:6+length(opt.elecrange)-1) = 0;
 end;
-winrej(:,6:6+length(opt.elecrange)-1) = 0;
 
 % remove isolated regions and merge others
 % ----------------------------------------
@@ -174,8 +208,10 @@ for index = size(winrej,1):-1:1
         merged = merged + 1;
     end;
 end;
-fprintf('%d regions merged\n', merged);
-fprintf('%d regions removed\n', isolated);
+if strcmpi(opt.verbose, 'on')
+    fprintf('%d regions merged\n', merged);
+    fprintf('%d regions removed\n', isolated);
+end;
 
 % add time before and after each region
 % -------------------------------------
@@ -186,12 +222,38 @@ end;
 
 % plot result
 % -----------
-selectedregions = winrej(:,1:2);
-command = 'EEG = pop_select(EEG, ''nopoint'', TMPREJ(:,1:2)); [ALLEEG EEG CURRENTSET LASTCOM] = pop_newset(ALLEEG, EEG, CURRENTSET, ''study'', ~isempty(STUDY)+0); eeglab redraw';
-if nargin < 2 || strcmpi(opt.eegplot, 'on')
-    eegplot(NEWEEG.data(opt.elecrange,:), 'srate', NEWEEG.srate, 'winrej', winrej, 'command', command, 'events', EEG.event);
+if ~isempty(winrej) 
+    selectedregions = winrej(:,1:2);
+    if strcmpi(opt.limitsonly, 'off')
+        % merge with initial regions
+        if ~isempty(opt.rejectori)
+            winrej(:,3) = 1; % color
+            for iRow = 1:size(opt.rejectori,1)
+                winrej(end+1,1:2) = opt.rejectori(iRow,:);
+                winrej(end  ,4)   = 1; % color
+                winrej(end  ,5)   = 1; % color
+            end;
+        end;
+        
+        command = 'EEG = pop_select(EEG, ''nopoint'', TMPREJ(:,1:2)); [ALLEEG EEG CURRENTSET LASTCOM] = pop_newset(ALLEEG, EEG, CURRENTSET, ''study'', ~isempty(STUDY)+0); eeglab redraw';
+        if nargin < 2 || strcmpi(opt.eegplot, 'on')
+            eegplot(NEWEEG.data(opt.elecrange,:), 'srate', NEWEEG.srate, 'winrej', winrej, 'command', command, 'events', EEG.event, 'winlength', 50);
+            disp('Green is overlap');
+            disp('Light blue is ORIGINAL rejection');
+            disp('Yellow is AUTOMATIC rejection');
+        else
+            NEWEEG = pop_select(EEG, 'nopoint', round(selectedregions));
+        end;
+        EEG = NEWEEG;
+    else
+        EEG = [];
+    end;
 else
-    NEWEEG = pop_select(EEG, 'nopoint', round(selectedregions));
+    selectedregions = [];
+    if strcmpi(opt.verbose, 'on')
+        disp('No region removed');
+    end;
 end;
-EEG = NEWEEG;
-com = sprintf('EEG = pop_rejcont(EEG, %s);', vararg2str(options));
+if nargout > 3
+    com = sprintf('EEG = pop_rejcont(EEG, %s);', vararg2str(options));
+end;
