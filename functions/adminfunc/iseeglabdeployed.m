6608b423e2e05537025d302625e172d6dce1bb70
Arnaud Delorme <arno@ucsd.edu>
2011-04-26 17:34:00 +0000

    add try, catch clause
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@9207 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/iseeglabdeployed.m
+++ functions/adminfunc/iseeglabdeployed.m
@@ -1,8 +1,7 @@
 % iseeglabdeployed - true for EEGLAB compile version and false otherwise
-
-function val = iseeglabdeployed;
-%val = 1; return;
-if exist('isdeployed')
+function val = iseeglabdeployed
+try
      val = isdeployed;
-else val = 0;
-end;
+catch
+    val = 0;
+end
