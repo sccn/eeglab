23f015f4822ea1694d68db864e58570807eec1cd
Arnaud Delorme <arno@ucsd.edu>
2011-05-08 20:20:31 +0000

    adding java chat functions
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@9223 1e58238c-3a72-4c0f-aabe-36d030380f39

---
+++ functions/javachatfunc/startpane.m
@@ -0,0 +1,12 @@
+import client.EEGLABChat;
+import client.VisualToolbar;
+import java.awt.*;
+import javax.swing.*;
+
+tb = VisualToolbar();
+F = gcf;
+tb.setPreferredSize(Dimension(0, 75));
+
+javacomponent(tb,'South',F);
+
+refresh(F);
