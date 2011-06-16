b28804f1cc4d3c6bd9570c62800a75866f2aaa1b
Arnaud Delorme <arno@ucsd.edu>
2011-06-16 07:33:11 +0000

    try-catch clause to call the prctile function
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@9280 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/erpimage.m
+++ functions/sigprocfunc/erpimage.m
@@ -1808,8 +1808,11 @@ else
         100*n_ties/length(sortvar),37,n_ties,length(sortvar));
     fprintf('Distribution of number ties per unique value of sortvar:\n');
     if exist('prctile')
-        fprintf('Min: %d, 25th ptile: %d, Median: %d, 75th ptile: %d, Max: %d\n',min(tie_dist),round(prctile(tie_dist,25)), ...
-            round(median(tie_dist)),round(prctile(tie_dist,75)),max(tie_dist));
+        try
+            fprintf('Min: %d, 25th ptile: %d, Median: %d, 75th ptile: %d, Max: %d\n',min(tie_dist),round(prctile(tie_dist,25)), ...
+                round(median(tie_dist)),round(prctile(tie_dist,75)),max(tie_dist));
+        catch
+        end;
     end;
     if replace_ties==YES,
         fprintf('Trials with tied sorting values will be replaced by their mean.\n');
