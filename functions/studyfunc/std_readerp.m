76ddb271e9955b894b714fa5d5a5c6107cfbd7a2
Arnaud Delorme <arno@ucsd.edu>
2011-08-04 18:13:36 +0000

    fix bug when retrieving non-interpolated channels
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@9356 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/std_readerp.m
+++ functions/studyfunc/std_readerp.m
@@ -178,7 +178,7 @@ for ind = 1:length(finalinds) % scan channels or components
                         else inds = 1:length(allinds{c,g}); end;
                     end;
                     if ~isempty(inds)
-                        if ~isempty(opt.channels), alldata{c, g} = std_readfile( setinfo(setinds{c,g}(:)), 'measure', dtype, opts{:}, 'channels'  , allChangrp(allinds{c,g}(:)));
+                        if ~isempty(opt.channels), alldata{c, g} = std_readfile( setinfo(setinds{c,g}(:)), 'measure', dtype, opts{:}, 'channels'  , opt.channels);
                         else                       alldata{c, g} = std_readfile( setinfo(setinds{c,g}(:)), 'measure', dtype, opts{:}, 'components', allinds{c,g}(:));
                         end;
                     end;
@@ -188,7 +188,7 @@ for ind = 1:length(finalinds) % scan channels or components
             for c = 1:nc
                 for g = 1:ng
                     if ~isempty(setinds{c,g})
-                        if ~isempty(opt.channels), alldata{c, g} = std_readfile( setinfo(setinds{c,g}(:)), 'measure', dtype, opts{:}, 'channels'  , allChangrp(allinds{c,g}(:)));
+                        if ~isempty(opt.channels), alldata{c, g} = std_readfile( setinfo(setinds{c,g}(:)), 'measure', dtype, opts{:}, 'channels'  , opt.channels);
                         else                       alldata{c, g} = std_readfile( setinfo(setinds{c,g}(:)), 'measure', dtype, opts{:}, 'components', allinds{c,g}(:));
                         end;
                     end;
