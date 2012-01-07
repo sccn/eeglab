cd9c2171f5fa367154734553d346f6ce2478997e
Arnaud Delorme <arno@ucsd.edu>
2012-01-07 06:19:13 +0000

    adding interpolation and removing components
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@9484 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/eeg_getdatact.m
+++ functions/adminfunc/eeg_getdatact.m
@@ -48,78 +48,124 @@
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 function data = eeg_getdatact( EEG, varargin);
+
+data = [];
+if nargin < 1
+    help eeg_getdatact;
+    return;
+end;
+
+% reading data from several datasets and concatening it
+% -----------------------------------------------------
+if iscell(EEG) || (~isstr(EEG) && length(EEG) > 1)
+    % decode some arguments
+    % ---------------------
+    trials  = cell(1,length(EEG));
+    rmcomps = cell(1,length(EEG));
+    for iArg = length(varargin)-1:-2:1
+        if strcmpi(varargin{iArg}, 'trialindices')
+            trials = varargin{iArg+1};
+            varargin(iArg:iArg+1) = [];
+        elseif strcmpi(varargin{iArg}, 'rmcomps')
+            rmcomps = varargin{iArg+1};
+            varargin(iArg:iArg+1) = [];
+        end;
+    end;
     
+    % concatenate datasets
+    % --------------------
     data = [];
-    if nargin < 1
-        help eeg_getdatact;
-        return;
-    end;
-    
-    opt = finputcheck(varargin, { ...
-        'channel'   'integer' {} [1:EEG.nbchan];
-        'verbose'   'string'  { 'on','off' } 'on';
-        'reshape'   'string'  { '2d','3d' }  '3d';
-        'projchan'  {'integer' 'cell' } { {} {} } [];        
-        'component' 'integer' {} [];        
-        'samples'   'integer' {} [];        
-        'trialindices' 'integer' {} [1:EEG.trials];        
-        'rmcomps'   'integer' {} [] }, 'eeg_getdatact');
-    
-    if isstr(opt), error(opt); end;
-    if (~isempty(opt.rmcomps) | ~isempty(opt.component)) & isempty(EEG.icaweights)
-        error('No ICA weight in dataset');
-    end;
-    
-    if strcmpi(EEG.data, 'in set file')
-        EEG = pop_loadset('filename', EEG.filename, 'filepath', EEG.filepath);
-    end;
-    
-    % getting channel or component activation
-    % ---------------------------------------
-    filename = fullfile(EEG.filepath, [ EEG.filename(1:end-4) '.icaact' ] );
-    if ~isempty(opt.component) & ~isempty(EEG.icaact)
-
-        data = EEG.icaact(opt.component,:,:);
-
-    elseif ~isempty(opt.component) & exist(filename)
-
-        % reading ICA file
-        % ----------------
-        data = repmat(single(0), [ length(opt.component) EEG.pnts EEG.trials ]);
-        fid = fopen( filename, 'r', 'ieee-le'); %little endian (see also pop_saveset)
-        if fid == -1, error( ['file ' filename ' could not be open' ]); end;
-        for ind = 1:length(opt.component)
-            fseek(fid, (opt.component(ind)-1)*EEG.pnts*EEG.trials*4, -1);
-            data(ind,:) = fread(fid, [EEG.trials*EEG.pnts 1], 'float32')';
+    for dat = 1:length(EEG)
+        if iscell(EEG)
+             tmpdata = eeg_getdatact(EEG{dat}, 'trialindices', trials{dat}, 'rmcomps', rmcomps{dat}, varargin{:} );
+        else tmpdata = eeg_getdatact(EEG(dat), 'trialindices', trials{dat}, 'rmcomps', rmcomps{dat}, varargin{:} );
         end;
-        fclose(fid);
-
-    elseif ~isempty(opt.component)
-
-        if isempty(EEG.icaact)
-            data = eeg_getdatact( EEG );
-            data = (EEG.icaweights(opt.component,:)*EEG.icasphere)*data(EEG.icachansind,:);
+        if isempty(data), data = tmpdata;
         else
-            data = EEG.icaact(opt.component,:,:);
+            if size(data,1) ~= size(tmpdata,1), error('Datasets to be concatenated do not have the same number of channels'); end;
+            if size(data,2) ~= size(tmpdata,2), error('Datasets to be concatenated do not have the same number of time points'); end;
+            data(:,:,end+1:end+size(tmpdata,3)) = tmpdata; % concatenating trials
         end;
+    end;
+    return;
+end;
 
-    elseif isnumeric(EEG.data) % channel
+% if string load dataset
+% ----------------------
+if isstr(EEG)
+    EEG = pop_loadset('filename', EEG, 'loadmode', 'info');
+end;
 
-        data = EEG.data(opt.channel,:,:);
+opt = finputcheck(varargin, { ...
+    'channel'   'integer' {} [1:EEG.nbchan];
+    'verbose'   'string'  { 'on','off' } 'on';
+    'reshape'   'string'  { '2d','3d' }  '3d';
+    'projchan'  {'integer' 'cell' } { {} {} } [];
+    'component' 'integer' {} [];
+    'samples'   'integer' {} [];
+    'interp'    'struct'  { }        struct([]);
+    'trialindices' {'integer' 'cell'} { {} {} } [];
+    'rmcomps'      {'integer' 'cell'} { {} {} } [] }, 'eeg_getdatact');
 
+if isstr(opt), error(opt); end;
+if isempty(opt.trialindices), opt.trialindices = [1:EEG.trials]; end;
+if iscell( opt.trialindices), opt.trialindices = opt.trialindices{1}; end;
+if iscell(opt.rmcomps     ), opt.rmcomps      = opt.rmcomps{1};      end;
+if (~isempty(opt.rmcomps) | ~isempty(opt.component)) & isempty(EEG.icaweights)
+    error('No ICA weight in dataset');
+end;
+
+if strcmpi(EEG.data, 'in set file')
+    EEG = pop_loadset('filename', EEG.filename, 'filepath', EEG.filepath);
+end;
+
+% getting channel or component activation
+% ---------------------------------------
+filename = fullfile(EEG.filepath, [ EEG.filename(1:end-4) '.icaact' ] );
+if ~isempty(opt.component) & ~isempty(EEG.icaact)
+    
+    data = EEG.icaact(opt.component,:,:);
+    
+elseif ~isempty(opt.component) & exist(filename)
+    
+    % reading ICA file
+    % ----------------
+    data = repmat(single(0), [ length(opt.component) EEG.pnts EEG.trials ]);
+    fid = fopen( filename, 'r', 'ieee-le'); %little endian (see also pop_saveset)
+    if fid == -1, error( ['file ' filename ' could not be open' ]); end;
+    for ind = 1:length(opt.component)
+        fseek(fid, (opt.component(ind)-1)*EEG.pnts*EEG.trials*4, -1);
+        data(ind,:) = fread(fid, [EEG.trials*EEG.pnts 1], 'float32')';
+    end;
+    fclose(fid);
+    
+elseif ~isempty(opt.component)
+    
+    if isempty(EEG.icaact)
+        data = eeg_getdatact( EEG );
+        data = (EEG.icaweights(opt.component,:)*EEG.icasphere)*data(EEG.icachansind,:);
+    else
+        data = EEG.icaact(opt.component,:,:);
+    end;
+    
+else
+    if isnumeric(EEG.data) % channel
+    
+        data = EEG.data;
+    
     else % channel but no data loaded
 
         filename = fullfile(EEG.filepath, EEG.data);
         fid = fopen( filename, 'r', 'ieee-le'); %little endian (see also pop_saveset)
         if fid == -1
             error( ['file ' filename ' not found. If you have renamed/moved' 10 ...
-                    'the .set file, you must also rename/move the associated data file.' ]);
-        else 
+                'the .set file, you must also rename/move the associated data file.' ]);
+        else
             if strcmpi(opt.verbose, 'on')
                 fprintf('Reading float file ''%s''...\n', filename);
             end;
         end;
-        
+
         % old format = .fdt; new format = .dat (transposed)
         % -------------------------------------------------
         datformat = 0;
@@ -138,7 +184,7 @@ function data = eeg_getdatact( EEG, varargin);
             data = memmapdata(filename, [EEG.nbchan EEG.pnts EEG.trials]);
         else
             if datformat
-                if length(opt.channel) == EEG.nbchan
+                if length(opt.channel) == EEG.nbchan || ~isempty(opt.interp)
                     data = fread(fid, [EEG.trials*EEG.pnts EEG.nbchan], 'float32')';
                 else
                     data = repmat(single(0), [ length(opt.channel) EEG.pnts EEG.trials ]);
@@ -146,23 +192,23 @@ function data = eeg_getdatact( EEG, varargin);
                         fseek(fid, (opt.channel(ind)-1)*EEG.pnts*EEG.trials*4, -1);
                         data(ind,:) = fread(fid, [EEG.trials*EEG.pnts 1], 'float32')';
                     end;
+                    opt.channel = [1:size(data,1)];
                 end;
             else
                 data = fread(fid, [EEG.nbchan Inf], 'float32');
-                data = data(opt.channel,:,:);
             end;
             fclose(fid);
         end;
-
+        
     end;
- 
+    
     % subracting components from data
     % -------------------------------
     if ~isempty(opt.rmcomps)
         if strcmpi(opt.verbose, 'on')
             fprintf('Removing %d artifactual components\n', length(opt.rmcomps));
         end;
-        rmcomps = eeg_getdatact( EEG, 'component', opt.rmcomps); % loaded from file        
+        rmcomps = eeg_getdatact( EEG, 'component', opt.rmcomps); % loaded from file
         rmchan    = [];
         rmchanica = [];
         for index = 1:length(opt.channel)
@@ -173,7 +219,7 @@ function data = eeg_getdatact( EEG, varargin);
             end;
         end;
         data(rmchan,:) = data(rmchan,:) - EEG.icawinv(rmchanica,opt.rmcomps)*rmcomps(:,:);
-
+        
         %EEG = eeg_checkset(EEG, 'loaddata');
         %EEG = pop_subcomp(EEG, opt.rmcomps);
         %data = EEG.data(opt.channel,:,:);
@@ -183,38 +229,49 @@ function data = eeg_getdatact( EEG, varargin);
         %end;
     end;
     
-    % projecting components on data channels
-    % --------------------------------------
-    if ~isempty(opt.projchan)
-        if iscell(opt.projchan)
-            opt.projchan = std_chaninds(EEG, opt.projchan);
-        end;
-        
-        finalChanInds = [];
-        for iChan = 1:length(opt.projchan)
-            tmpInd = find(EEG.icachansind == opt.projchan(iChan));
-            if isempty(tmpInd)
-                error(sprintf('Warning: can not backproject component on channel %d (not used for ICA)\n', opt.projchan(iChan)));
-            end;
-            finalChanInds = [ finalChanInds tmpInd ];
-        end;
-        
-        data = EEG.icawinv(finalChanInds, opt.component)*data(:,:);
+    if ~isempty(opt.interp)
+        EEG.data        = data;
+        EEG.event  = [];
+        EEG.epoch  = [];
+        EEG = eeg_interp(EEG, opt.interp, 'spherical');
+        data = EEG.data;
+    end;
+
+    data = data(opt.channel,:,:);
+end;
+
+
+% projecting components on data channels
+% --------------------------------------
+if ~isempty(opt.projchan)
+    if iscell(opt.projchan)
+        opt.projchan = std_chaninds(EEG, opt.projchan);
     end;
     
-    try,
-        if  strcmpi(opt.reshape, '3d')
-             data = reshape(data, size(data,1), EEG.pnts, EEG.trials);
-        else data = reshape(data, size(data,1), EEG.pnts*EEG.trials);
+    finalChanInds = [];
+    for iChan = 1:length(opt.projchan)
+        tmpInd = find(EEG.icachansind == opt.projchan(iChan));
+        if isempty(tmpInd)
+            error(sprintf('Warning: can not backproject component on channel %d (not used for ICA)\n', opt.projchan(iChan)));
         end;
-    catch
-        error('The file size on disk does not correspond to the dataset information.');
+        finalChanInds = [ finalChanInds tmpInd ];
     end;
     
-    if length(opt.trialindices) ~= EEG.trials
-        data = data(:,:,opt.trialindices);
+    data = EEG.icawinv(finalChanInds, opt.component)*data(:,:);
+end;
+
+try,
+    if  strcmpi(opt.reshape, '3d')
+        data = reshape(data, size(data,1), EEG.pnts, EEG.trials);
+    else data = reshape(data, size(data,1), EEG.pnts*EEG.trials);
     end;
-    if ~isempty(opt.samples)
-        data = data(:,opt.samples,:);
-    end;
-    
+catch
+    error('The file size on disk does not correspond to the dataset information.');
+end;
+
+if length(opt.trialindices) ~= EEG.trials
+    data = data(:,:,opt.trialindices);
+end;
+if ~isempty(opt.samples)
+    data = data(:,opt.samples,:);
+end;
