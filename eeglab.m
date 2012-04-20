4b9e14d7f6fa0d2c33e3a2ce9cd48339b93e4cec
Arnaud Delorme <arno@ucsd.edu>
2012-04-20 21:01:26 +0000

    fix updater
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@9561 1e58238c-3a72-4c0f-aabe-36d030380f39

--- eeglab.m
+++ eeglab.m
@@ -915,7 +915,7 @@ end;
 %% automatic updater
 try
     [dummy eeglabVersionNumber currentReleaseDateString] = eeg_getversion;
-    eeglabUpdater = up.updater(eeglabVersionNumber, 'http://sccn.ucsd.edu/~nima/toolbox/eeglab/latest_version.php', 'EEGLAB', currentReleaseDateString);
+    eeglabUpdater = up.updater(eeglabVersionNumber, 'http://sccn.ucsd.edu/eeglab/updater/latest_version.php', 'EEGLAB', currentReleaseDateString);
     
     % create a new GUI item (e.g. under Help)
     %newerVersionMenu = uimenu(help_m, 'Label', 'Upgrade to the Latest Version', 'visible', 'off', 'userdata', 'startup:on;study:on');
