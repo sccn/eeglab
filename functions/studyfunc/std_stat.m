d5c3acbade33c97eae0c8d2c6b9ddbd9c02e4af4
Arnaud Delorme <arno@ucsd.edu>
2012-07-04 04:20:20 +0000

    adding support for Fieldtrip. Adding Holms and Bonferoni corrections for multiple comparisons
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@9651 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/std_stat.m
+++ functions/studyfunc/std_stat.m
@@ -1,38 +1,58 @@
 % std_stat() - compute statistics for ERP/spectral traces or ERSP/ITC images
-%              of a component or channel cluster in a STUDY. 
+%              of a component or channel cluster in a STUDY.
 % Usage:
 %          >> std_stat( data, 'key', 'val', ...)
 % Inputs:
 %  data  -  [cell array] mean data for each subject group and/or data
-%           condition. For example, to compute mean ERPs statistics from a  
-%           STUDY for epochs of 800 frames in two conditions from three  
+%           condition. For example, to compute mean ERPs statistics from a
+%           STUDY for epochs of 800 frames in two conditions from three
 %           groups of 12 subjects:
 %
 %           >> data = { [800x12] [800x12] [800x12];... % 3 groups, cond 1
 %                       [800x12] [800x12] [800x12] };  % 3 groups, cond 2
 %           >> pcond = std_stat(data, 'condstats', 'on');
 %
-%           By default, parametric statistics are computed across subjects 
-%           in the three groups. See below and >> help statcond 
+%           By default, parametric statistics are computed across subjects
+%           in the three groups. See below and >> help statcond
 %           for more information about the statistical computations.
 %
-% Statistics options:
+% Statistics options (EEGLAB):
 %  'groupstats'  - ['on'|'off'] Compute (or not) statistics across groups.
 %                  {default: 'off'}
 %  'condstats'   - ['on'|'off'] Compute (or not) statistics across groups.
 %                  {default: 'off'}
-%  'statistics'  - ['param'|'perm'] Type of statistics to use: 'param' for
-%                  parametric; 'perm' for permutations {default: 'param'}
-%  'naccu'       - [integer] Number of surrogate averges fo accumulate when 
+%  'method'      - ['parametric'|'permutation'] Type of statistics to use
+%                  default is 'parametric'. 'perm' and 'param' legacy
+%                  abreviations are still functional.
+%  'naccu'       - [integer] Number of surrogate averages fo accumulate when
 %                  computing permutation-based statistics. For example, to
-%                  test p<0.01 use naccu>=200; for p<0.001, use naccu>=2000. 
-%                  If a non-NaN 'threshold' is set (see below) and 'naccu' 
-%                  is too low, it will be automatically increased. This 
+%                  test p<0.01 use naccu>=200; for p<0.001, use naccu>=2000.
+%                  If a non-NaN 'threshold' is set (see below) and 'naccu'
+%                  is too low, it will be automatically increased. This
 %                  keyword available only from the command line {default:500}
-%  'threshold'   - [NaN|p-value] threshold for computing p-value. In this 
+%  'alpha'       - [NaN|p-value] threshold for computing p-value. In this
 %                  function, it is only used to compute naccu above. NaN
-%                  means that no threshold has been set. 
+%                  means that no threshold has been set.
 %  'mcorrect'    - ['none'|'fdr'] apply correcting for multiple comparisons.
+%  'mode'        - ['eeglab'|'fieldtrip'] statistical framework to use. 
+%                  'eeglab' uses EEGLAB statistical functions and 'fieldtrip'
+%                  uses Fieldtrip statistical funcitons. Default is 'eeglab'.
+%
+% Fieldtrip statistics options:
+%  'fieldtripnaccu'   - 'numrandomization' Fieldtrip parameter
+%  'fieldtripalpha'   - 'alpha' Fieldtrip parameter. Default is 0.05.
+%  'fieldtripmethod'  - 'method' Fieldtrip parameter. Default is 'analytic'
+%  'fieldtripmcorrect' - 'mcorrect' Fieldtrip parameter. Default is 'no'.
+%  'fieldtripclusterparam' - string or cell array for optional parameters
+%                            for cluster correction method, see function
+%                            ft_statistics_montecarlo for more information.
+%  'fieldtripchannelneighbor' - Fieldtrip channel neighbour structure for 
+%                               cluster correction method, see function
+%                               std_prepare_neighbors for more information.
+
+% Legacy parameters:
+%   'threshold'  - now 'alpha'
+%   'statistics' - now 'method'  
 %
 % Outputs:
 %  pcond        - [cell] condition pvalues or mask (0 or 1). One element per group.
@@ -41,18 +61,24 @@
 %                 group pvalues (condition pooled) and interaction pvalues.
 %
 % Author: Arnaud Delorme, CERCO, CNRS, 2006-
-% 
+%
 % See also: statcond()
 
-%  'statmode'    - ['subjects'|'trials'] standard statistics are 
-%                  'subjects' where the statistics is performed accross
-%                  the mean ERSP (or ITC) of single subjects. For 'trials'
-%                  statistics, the single-trial data epochs of all subjects
-%                  are pooled together. This requires that they were
-%                  saved on disk using option 'savetrials', 'on' at the time
-%                  of computation. Note that these single-trial data
-%                  may use several GB of disk space and that computation 
-%                  of 'trials' statistics requires a lot of RAM.
+% Copyright (C) Arnaud Delorme
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 function [pcond, pgroup, pinter] = std_stat(data, varargin)
 
@@ -66,32 +92,19 @@ end;
 
 % decode inputs
 % -------------
-opt = {};
-if nargin > 1
-    if isstruct(varargin{1})
-        g = struct(varargin{2:end});
-        if isfield(g, 'condstats'),  varargin{1} = rmfield(varargin{1}, 'condstats'); end;
-        if isfield(g, 'groupstats'), varargin{1} = rmfield(varargin{1}, 'groupstats'); end;
-        tmpparams      = fieldnames(varargin{1}); tmpparams = tmpparams';
-        tmpparams(2,:) = struct2cell(varargin{1});
-        varargin       = { tmpparams{:} varargin{2:end} };
-    end;
+if isstruct(varargin{1})
+    opt   = varargin{1};
+else
+    statstruct.etc = STUDY.etc;
+    statstruct     = pop_statparams(statstruct, varargin{:});
+    opt            = statstruct.etc.statistics;
 end;
-opt = finputcheck( varargin, { 'threshold'   'real'    []               NaN;
-                               'mcorrect'    'string'  { 'none','fdr' } 'none';
-                               'naccu'       'integer' []               [];
-                               'groupstats'  'string'  { 'on','off' }   'off';
-                               'paired'      'cell'    { 'on','off' }   { 'on','on' };
-                               'condstats'   'string'  { 'on','off' }   'off';
-                               'statistics'  'string'  { 'param','perm','bootstrap' }       'param' }, ...
-                               'std_stat', 'ignore');
 
-if isstr(opt), error(opt); end;
-if ~isnan(opt.threshold(1)) && isempty(opt.naccu), opt.naccu = 1/opt.threshold(end)*2; end;
+if ~isnan(opt.eeglab.alpha(1)) && isempty(opt.eeglab.naccu), opt.eeglab.naccu = 1/opt.eeglab.alpha(end)*2; end;
 if any(any(cellfun('size', data, 2)==1)), opt.groupstats = 'off'; opt.condstats = 'off'; end;
-if strcmpi(opt.mcorrect, 'fdr'), opt.naccu = opt.naccu*20; end;
-if isempty(opt.naccu), opt.naccu = 2000; end;
-if strcmpi(opt.statistics, 'param') && ~isreal(data{1})
+if strcmpi(opt.eeglab.mcorrect, 'fdr'), opt.eeglab.naccu = opt.eeglab.naccu*20; end;
+if isempty(opt.eeglab.naccu), opt.eeglab.naccu = 2000; end;
+if strcmpi(opt.eeglab.method(1:3), 'par') && ~isreal(data{1})
     fprintf('*** Cannot use parametric statistics for single-trial ITC significance ***\n');
     return;
 end;
@@ -99,57 +112,112 @@ nc = size(data,1);
 ng = size(data,2);
 
 % compute significance mask
-% --------------------------
-if strcmpi(opt.condstats, 'on') && nc > 1
-    for g = 1:ng
-        [F df pval] = statcond(data(:,g), 'mode', opt.statistics, 'naccu', opt.naccu, 'paired', opt.paired{1}); 
-        pcond{g} = squeeze(pval);
-    end;
-else
-    pcond = {};
-end;
-if strcmpi(opt.groupstats, 'on') && ng > 1
-    for c = 1:nc
-        [F df pval] = statcond(data(c,:), 'mode', opt.statistics, 'naccu', opt.naccu, 'paired', opt.paired{2}); 
-        pgroup{c} = squeeze(pval);
-    end;
-else
-    pgroup = {};
-end;
-if ( strcmpi(opt.groupstats, 'on') && strcmpi(opt.condstats, 'on') ) & ng > 1 & nc > 1
-    opt.paired = sort(opt.paired); % put 'off' first if present
-    [F df pval] = statcond(data, 'mode', opt.statistics, 'naccu', opt.naccu, 'paired', opt.paired{1});
-    for index = 1:length(pval)
-        pinter{index} = squeeze(pval{index});
-    end;
-else
-    pinter = {};
-end;
-
-if ~isempty(opt.groupstats) || ~isempty(opt.condstats)   
-    if strcmpi(opt.mcorrect, 'fdr'), 
-        disp('Applying FDR correction for multiple comparisons');
-        for ind = 1:length(pcond),  pcond{ind} = fdr( pcond{ind} ); end;
-        for ind = 1:length(pgroup), pgroup{ind} = fdr( pgroup{ind} ); end;
-        if ~isempty(pinter), 
-            pinter{1} = fdr(pinter{1}); 
-            pinter{2} = fdr(pinter{2}); 
-            pinter{3} = fdr(pinter{3}); 
+% -------------------------
+if strcmpi(opt.mode, 'eeglab')
+    % EEGLAB statistics
+    % -----------------
+    if strcmpi(opt.condstats, 'on') && nc > 1
+        for g = 1:ng
+            [F df pval] = statcond(data(:,g), 'method', opt.eeglab.method, 'naccu', opt.eeglab.naccu, 'paired', opt.paired{1});
+            pcond{g} = squeeze(pval);
         end;
+    else
+        pcond = {};
     end;
-    if ~isnan(opt.threshold)
-        for ind = 1:length(pcond),  pcond{ind}  = applythreshold(pcond{ind},  opt.threshold); end;
-        for ind = 1:length(pgroup), pgroup{ind} = applythreshold(pgroup{ind}, opt.threshold); end;
-        for ind = 1:length(pinter), pinter{ind} = applythreshold(pinter{ind}, opt.threshold); end;
+    if strcmpi(opt.groupstats, 'on') && ng > 1
+        for c = 1:nc
+            [F df pval] = statcond(data(c,:), 'method', opt.eeglab.method, 'naccu', opt.eeglab.naccu, 'paired', opt.paired{2});
+            pgroup{c} = squeeze(pval);
+        end;
+    else
+        pgroup = {};
     end;
-end;
-
-function newdata = applythreshold(data, threshold)
-    threshold = sort(threshold);
-    newdata = zeros(size(data));
-    for index = 1:length(threshold)
-        inds = data < threshold(index);
-        data(inds)    = 1;
-        newdata(inds) = length(threshold)-index+1;
+    if ( strcmpi(opt.groupstats, 'on') && strcmpi(opt.condstats, 'on') ) & ng > 1 & nc > 1
+        opt.paired = sort(opt.paired); % put 'off' first if present
+        [F df pval] = statcond(data, 'method', opt.eeglab.method, 'naccu', opt.eeglab.naccu, 'paired', opt.paired{1});
+        for index = 1:length(pval)
+            pinter{index} = squeeze(pval{index});
+        end;
+    else
+        pinter = {};
     end;
     
+    if ~isempty(opt.groupstats) || ~isempty(opt.condstats)
+        if strcmpi(opt.eeglab.mcorrect, 'fdr'),
+            disp('Applying FDR correction for multiple comparisons');
+            for ind = 1:length(pcond),  pcond{ind}  = mcorrect( pcond{ind} , opt.eeglab.mcorrect ); end;
+            for ind = 1:length(pgroup), pgroup{ind} = mcorrect( pgroup{ind}, opt.eeglab.mcorrect ); end;
+            if ~isempty(pinter),
+                pinter{1} = mcorrect(pinter{1}, opt.eeglab.mcorrect);
+                pinter{2} = mcorrect(pinter{2}, opt.eeglab.mcorrect);
+                pinter{3} = mcorrect(pinter{3}, opt.eeglab.mcorrect);
+            end;
+        end;
+        if ~isnan(opt.eeglab.alpha)
+            for ind = 1:length(pcond),  pcond{ind}  = applythreshold(pcond{ind},  opt.eeglab.alpha); end;
+            for ind = 1:length(pgroup), pgroup{ind} = applythreshold(pgroup{ind}, opt.eeglab.alpha); end;
+            for ind = 1:length(pinter), pinter{ind} = applythreshold(pinter{ind}, opt.eeglab.alpha); end;
+        end;
+    end;
+else
+    % Fieldtrip statistics
+    % --------------------
+    params = {};
+    if strcmpi(opt.fieldtrip.mcorrect, 'cluster')
+        params = eval( [ '{' opt.fieldtrip.clusterparam '}' ]);
+        if isempty(opt.fieldtrip.channelneighbor), opt.fieldtrip.channelneighbor = struct([]); end;
+        params = { params{:} 'neighbours' opt.fieldtrip.channelneighbor };
+    end;
+    params = { params{:} 'method', opt.fieldtrip.method, 'naccu', opt.fieldtrip.naccu 'mcorrect' opt.fieldtrip.mcorrect 'alpha' opt.fieldtrip.alpha 'numrandomization' opt.fieldtrip.naccu };
+        
+    if strcmpi(opt.condstats, 'on') && nc > 1
+        for g = 1:ng
+            [F df pval] = statcondfieldtrip(data(:,g), 'paired', opt.paired{1}, params{:});
+            pcond{g} = squeeze(pval);
+        end;
+    else
+        pcond = {};
+    end;
+    if strcmpi(opt.groupstats, 'on') && ng > 1
+        for c = 1:nc
+            [F df pval] = statcondfieldtrip(data(c,:), 'paired', opt.paired{2}, params{:});
+            pgroup{c} = squeeze(pval);
+        end;
+    else
+        pgroup = {};
+    end;
+    if ( strcmpi(opt.groupstats, 'on') && strcmpi(opt.condstats, 'on') ) & ng > 1 & nc > 1
+        opt.paired = sort(opt.paired); % put 'off' first if present
+        [F df pval] = statcondfieldtrip(data, 'paired', opt.paired{1}, params{:});
+        for index = 1:length(pval)
+            pinter{index} = squeeze(pval{index});
+        end;
+    else
+        pinter = {};
+    end;
+end;
+
+% apply stat threshold to data
+% ----------------------------
+function newdata = applythreshold(data, threshold)
+
+threshold = sort(threshold);
+newdata = zeros(size(data));
+for index = 1:length(threshold)
+    inds = data < threshold(index);
+    data(inds)    = 1;
+    newdata(inds) = length(threshold)-index+1;
+end;
+
+% compute correction for multiple comparisons
+% -------------------------------------------
+function pvals = mcorrect(pvals, method);
+
+switch method
+    case 'none', return;
+    case 'bonferoni', pvals = pvals*prod(size(pvals));
+    case 'holms',     [tmp ind] = sort(pvals(:)); [tmp ind2] = sort(ind); pvals(:) = pvals(:).*(prod(size(pvals))-ind2+1);
+    case 'fdr',       pvals = fdr(pvals);
+    otherwise error(['Unknown method ''' method ''' for correction for multiple comparisons' ]);
+end;        
+        
