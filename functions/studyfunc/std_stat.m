78e559c731ed62bfc0c27214ede3cfcb52b26635
Arnaud Delorme <arno@ucsd.edu>
2012-07-14 22:28:20 +0000

    adding 3 outputs to return statistics in addition to p-values
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@9711 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/std_stat.m
+++ functions/studyfunc/std_stat.m
@@ -55,10 +55,17 @@
 %   'statistics' - now 'method'  
 %
 % Outputs:
-%  pcond        - [cell] condition pvalues or mask (0 or 1). One element per group.
-%  pgroup       - [cell] group pvalues or mask (0 or 1). One element per condition.
+%  pcond        - [cell] condition pvalues or mask (0 or 1) if an alpha value
+%                 is selected. One element per group.
+%  pgroup       - [cell] group pvalues or mask (0 or 1). One element per 
+%                 condition.
 %  pinter       - [cell] three elements, condition pvalues (group pooled),
 %                 group pvalues (condition pooled) and interaction pvalues.
+%  statcond     - [cell] condition statistic values (F or T).
+%  statgroup    - [cell] group pvalues or mask (0 or 1). One element per 
+%                 condition.
+%  statinter    - [cell] three elements, condition statistics (group pooled),
+%                 group statistics (condition pooled) and interaction F statistics.
 %
 % Author: Arnaud Delorme, CERCO, CNRS, 2006-
 %
@@ -80,7 +87,7 @@
 % along with this program; if not, write to the Free Software
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-function [pcond, pgroup, pinter] = std_stat(data, varargin)
+function [pcond, pgroup, pinter, statscond, statsgroup, statsinter] = std_stat(data, varargin)
 
 pgroup = {};
 pcond  = {};
@@ -113,33 +120,37 @@ ng = size(data,2);
 
 % compute significance mask
 % -------------------------
+pcond  = {};
+pgroup = {};
+pinter = {};
+statscond  = {};
+statsgroup = {};
+statsinter = {};
 if strcmpi(opt.mode, 'eeglab')
     % EEGLAB statistics
     % -----------------
     if strcmpi(opt.condstats, 'on') && nc > 1
         for g = 1:ng
             [F df pval] = statcond(data(:,g), 'method', opt.eeglab.method, 'naccu', opt.eeglab.naccu, 'paired', opt.paired{1});
-            pcond{g} = squeeze(pval);
+            pcond{g}     = squeeze(pval);
+            statscond{g} = squeeze(F);
         end;
-    else
-        pcond = {};
     end;
     if strcmpi(opt.groupstats, 'on') && ng > 1
         for c = 1:nc
             [F df pval] = statcond(data(c,:), 'method', opt.eeglab.method, 'naccu', opt.eeglab.naccu, 'paired', opt.paired{2});
-            pgroup{c} = squeeze(pval);
+            pgroup{c}     = squeeze(pval);
+            statsgroup{c} = squeeze(F);
         end;
     else
-        pgroup = {};
     end;
     if ( strcmpi(opt.groupstats, 'on') && strcmpi(opt.condstats, 'on') ) & ng > 1 & nc > 1
         opt.paired = sort(opt.paired); % put 'off' first if present
         [F df pval] = statcond(data, 'method', opt.eeglab.method, 'naccu', opt.eeglab.naccu, 'paired', opt.paired{1});
         for index = 1:length(pval)
             pinter{index} = squeeze(pval{index});
+            statsinter{index} = squeeze(F{index});
         end;
-    else
-        pinter = {};
     end;
     
     if ~isempty(opt.groupstats) || ~isempty(opt.condstats)
@@ -174,6 +185,7 @@ else
         for g = 1:ng
             [F df pval] = statcondfieldtrip(data(:,g), 'paired', opt.paired{1}, params{:});
             pcond{g} = squeeze(pval);
+            statscond{g} = squeeze(F);
         end;
     else
         pcond = {};
@@ -182,6 +194,7 @@ else
         for c = 1:nc
             [F df pval] = statcondfieldtrip(data(c,:), 'paired', opt.paired{2}, params{:});
             pgroup{c} = squeeze(pval);
+            statsgroup{c} = squeeze(F);
         end;
     else
         pgroup = {};
@@ -191,6 +204,7 @@ else
         [F df pval] = statcondfieldtrip(data, 'paired', opt.paired{1}, params{:});
         for index = 1:length(pval)
             pinter{index} = squeeze(pval{index});
+            statsinter{index} = squeeze(F{index});
         end;
     else
         pinter = {};
