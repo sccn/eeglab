216c60dfafd087d1db5ea4efb4ab59a8aebd98a0
Arnaud Delorme <arno@ucsd.edu>
2012-09-17 19:59:27 +0000

    new method of computation without using switch command
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@9786 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/statistics/anova2_cell.m
+++ functions/statistics/anova2_cell.m
@@ -71,6 +71,7 @@ function [FC, FR, FI, freeC, freeR, freeI] = anova2_cell(data)
     b = size(data,2);
     nd = myndims( data{1} );
     c  = size(data{1}, nd);
+    sz = size(data{1});
     
     % dataabs if for complex data only
     % --------------------------------
@@ -83,90 +84,123 @@ function [FC, FR, FI, freeC, freeR, freeI] = anova2_cell(data)
         end;
     end;
     
-    if nd == 1
-        
-        VE = 0;
-        m  = zeros( size(data), 'single' );
-        for i = 1:a
-            for ii = 1:b
-                m(i,ii) = mymean(data{i,ii});
-                VE      = VE+sum( (dataabs{i,ii}-m(i,ii)).^2 );
+    VE = zeros( [ sz(1:end-1) 1], 'single' );
+    m  = zeros( [ sz(1:end-1) size(data) ], 'single' );
+    for i = 1:a
+        for ii = 1:b
+            tmpm = mymean(data{i,ii}, nd);
+            switch nd
+                case 1, m(i,ii) = tmpm;
+                case 2, m(:,i,ii) = tmpm;
+                case 3, m(:,:,i,ii) = tmpm;
+                case 4, m(:,:,:,i,ii) = tmpm;
+                case 5, m(:,:,:,:,i,ii) = tmpm;
+                case 6, m(:,:,:,:,:,i,ii) = tmpm;
+                case 7, m(:,:,:,:,:,:,i,ii) = tmpm;
+                otherwise error('Dimension not supported');
             end;
+            VE = VE+sum( bsxfun(@minus, dataabs{i,ii}, tmpm).^2, nd);
         end;
-        X  = mean(mean(m));
-        Xj = mean(m,2);
-        Xk = mean(m,1);
-        VR = b*c*sum( (Xj-X).^2 );
-        VC = a*c*sum( (Xk-X).^2 );
-        
-        Xj = repmat(Xj, [1 size(m,2) ]);
-        Xk = repmat(Xk, [size(m,1)  1]);
-        VI = c*sum( sum( ( m - Xj - Xk + X ).^2 ) );
-        
-    elseif nd == 2
-
-        VE = zeros( size(data{1},1),1, 'single');
-        m  = zeros( [ size(data{1},1) size(data) ], 'single' );
-        for i = 1:a
-            for ii = 1:b
-                tmpm = mymean(data{i,ii}, 2);
-                m(:,i,ii) = tmpm;
-                VE        = VE+sum( (dataabs{i,ii}-repmat(tmpm, [1 size(data{i,ii},2)])).^2, 2);
-            end;
-        end;
-        X  = mean(mean(m,3),2);
-        Xj = mean(m,3);
-        Xk = mean(m,2);
-        VR = b*c*sum( (Xj-repmat(X, [1 size(Xj,2)])).^2, 2 );
-        VC = a*c*sum( (Xk-repmat(X, [1 1 size(Xk,3)])).^2, 3 );
-        
-        Xj = repmat(Xj, [1 1 size(m,3) ]);
-        Xk = repmat(Xk, [1 size(m,2)  1]);
-        VI = c*sum( sum( ( m - Xj - Xk + repmat(X, [1 size(m,2) size(m,3)]) ).^2, 3), 2 );
-        
-    elseif nd == 3
-        
-        VE = zeros( size(data{1},1), size(data{1},2), 'single' );
-        m  = zeros( [ size(data{1},1) size(data{1},2) size(data) ], 'single' );
-        for i = 1:a
-            for ii = 1:b
-                tmpm = mymean(data{i,ii}, 3);
-                m(:,:,i,ii) = tmpm;
-                VE          = VE+sum( (dataabs{i,ii}-repmat(tmpm, [1 1 size(data{i,ii},3)])).^2, 3);
-            end;
-        end;
-        X  = mean(mean(m,4),3);
-        Xj = mean(m,4);
-        Xk = mean(m,3);
-        VR = b*c*sum( (Xj-repmat(X, [1 1 size(Xj,3)  ])).^2, 3 );
-        VC = a*c*sum( (Xk-repmat(X, [1 1 1 size(Xk,4)])).^2, 4 );
-        
-        Xj = repmat(Xj, [1 1 1 size(m,4) ]);
-        Xk = repmat(Xk, [1 1 size(m,3)  1]);
-        VI = c*sum( sum( ( m - Xj - Xk + repmat(X, [1 1 size(m,3) size(m,4)]) ).^2, 4 ), 3 );
-                
-    else % nd == 4
-        
-        VE = zeros( size(data{1},1), size(data{1},2), size(data{1},3), 'single' );
-        m  = zeros( [ size(data{1},1) size(data{1},2) size(data{1},3) size(data) ], 'single' );
-        for i = 1:a
-            for ii = 1:b
-                tmpm = mymean(data{i,ii}, 4);
-                m(:,:,:,i,ii) = tmpm;
-                VE            = VE+sum( (dataabs{i,ii}-repmat(tmpm, [1 1 1 size(data{i,ii},4)])).^2, 4);
-            end;
-        end;
-        X  = mean(mean(m,5),4);
-        Xj = mean(m,5);
-        Xk = mean(m,4);
-        VR = b*c*sum( (Xj-repmat(X, [1 1 1 size(Xj,4)  ])).^2, 4 );
-        VC = a*c*sum( (Xk-repmat(X, [1 1 1 1 size(Xk,5)])).^2, 5 );
-        
-        Xj = repmat(Xj, [1 1 1 1 size(m,5) ]);
-        Xk = repmat(Xk, [1 1 1 size(m,4)  1]);
-        VI = c*sum( sum( ( m - Xj - Xk + repmat(X, [1 1 1 size(m,4) size(m,5)]) ).^2, 5 ), 4 );
-                
     end;
+    X  = mean(mean(m,nd+1),nd);
+    Xj = mean(m,nd+1);
+    Xk = mean(m,nd);
+    VR = b*c*sum( bsxfun(@minus, Xj, X).^2, nd);
+    VC = a*c*sum( bsxfun(@minus, Xk, X).^2, nd+1 );
+    VI = c*sum( sum( bsxfun(@plus, bsxfun(@minus, bsxfun(@minus, m, Xj), Xk), X).^2, nd+1 ), nd );
+        
+% before bsxfun
+%     VR = b*c*sum( (Xj-repmat(X, [ones(1,nd-1) size(Xj,nd  )])).^2, nd   );
+%     VC = a*c*sum( (Xk-repmat(X, [ones(1,nd  ) size(Xk,nd+1)])).^2, nd+1 );
+%     
+%     Xj = repmat(Xj, [ones(1,nd)   size(m,nd+1) ]);
+%     Xk = repmat(Xk, [ones(1,nd-1) size(m,nd)  1]);
+%     VI = c*sum( sum( ( m - Xj - Xk + repmat(X, [ones(1,nd-1) size(m,nd) size(m,nd+1)]) ).^2, nd+1 ), nd );
+    
+%     if nd == 1
+%         
+%         VE = 0;
+%         m  = zeros( size(data), 'single' );
+%         for i = 1:a
+%             for ii = 1:b
+%                 m(i,ii) = mymean(data{i,ii});
+%                 VE      = VE+sum( (dataabs{i,ii}-m(i,ii)).^2 );
+%             end;
+%         end;
+%         X  = mean(mean(m));
+%         Xj = mean(m,2);
+%         Xk = mean(m,1);
+%         VR = b*c*sum( (Xj-X).^2 );
+%         VC = a*c*sum( (Xk-X).^2 );
+%         
+%         Xj = repmat(Xj, [1 size(m,2) ]);
+%         Xk = repmat(Xk, [size(m,1)  1]);
+%         VI = c*sum( sum( ( m - Xj - Xk + X ).^2 ) );
+%         
+%     elseif nd == 2
+% 
+%         VE = zeros( size(data{1},1),1, 'single');
+%         m  = zeros( [ size(data{1},1) size(data) ], 'single' );
+%         for i = 1:a
+%             for ii = 1:b
+%                 tmpm = mymean(data{i,ii}, 2);
+%                 m(:,i,ii) = tmpm;
+%                 VE        = VE+sum( (dataabs{i,ii}-repmat(tmpm, [1 size(data{i,ii},2)])).^2, 2);
+%             end;
+%         end;
+%         X  = mean(mean(m,3),2);
+%         Xj = mean(m,3);
+%         Xk = mean(m,2);
+%         VR = b*c*sum( (Xj-repmat(X, [1 size(Xj,2)])).^2, 2 );
+%         VC = a*c*sum( (Xk-repmat(X, [1 1 size(Xk,3)])).^2, 3 );
+%         
+%         Xj = repmat(Xj, [1 1 size(m,3) ]);
+%         Xk = repmat(Xk, [1 size(m,2)  1]);
+%         VI = c*sum( sum( ( m - Xj - Xk + repmat(X, [1 size(m,2) size(m,3)]) ).^2, 3), 2 );
+%         
+%     elseif nd == 3
+%         
+%         VE = zeros( size(data{1},1), size(data{1},2), 'single' );
+%         m  = zeros( [ size(data{1},1) size(data{1},2) size(data) ], 'single' );
+%         for i = 1:a
+%             for ii = 1:b
+%                 tmpm = mymean(data{i,ii}, 3);
+%                 m(:,:,i,ii) = tmpm;
+%                 VE          = VE+sum( (dataabs{i,ii}-repmat(tmpm, [1 1 size(data{i,ii},3)])).^2, 3);
+%             end;
+%         end;
+%         X  = mean(mean(m,4),3);
+%         Xj = mean(m,4);
+%         Xk = mean(m,3);
+%         VR = b*c*sum( (Xj-repmat(X, [1 1 size(Xj,3)  ])).^2, 3 );
+%         VC = a*c*sum( (Xk-repmat(X, [1 1 1 size(Xk,4)])).^2, 4 );
+%         
+%         Xj = repmat(Xj, [1 1 1 size(m,4) ]);
+%         Xk = repmat(Xk, [1 1 size(m,3)  1]);
+%         VI = c*sum( sum( ( m - Xj - Xk + repmat(X, [1 1 size(m,3) size(m,4)]) ).^2, 4 ), 3 );
+%                 
+%     else % nd == 4
+%         
+%         VE = zeros( size(data{1},1), size(data{1},2), size(data{1},3), 'single' );
+%         m  = zeros( [ size(data{1},1) size(data{1},2) size(data{1},3) size(data) ], 'single' );
+%         for i = 1:a
+%             for ii = 1:b
+%                 tmpm = mymean(data{i,ii}, 4);
+%                 m(:,:,:,i,ii) = tmpm;
+%                 VE            = VE+sum( (dataabs{i,ii}-repmat(tmpm, [1 1 1 size(data{i,ii},4)])).^2, 4);
+%             end;
+%         end;
+%         X  = mean(mean(m,5),4);
+%         Xj = mean(m,5);
+%         Xk = mean(m,4);
+%         VR = b*c*sum( (Xj-repmat(X, [1 1 1 size(Xj,4)  ])).^2, 4 );
+%         VC = a*c*sum( (Xk-repmat(X, [1 1 1 1 size(Xk,5)])).^2, 5 );
+%         
+%         Xj = repmat(Xj, [1 1 1 1 size(m,5) ]);
+%         Xk = repmat(Xk, [1 1 1 size(m,4)  1]);
+%         VI = c*sum( sum( ( m - Xj - Xk + repmat(X, [1 1 1 size(m,4) size(m,5)]) ).^2, 5 ), 4 );
+%                 
+%     end;
     
     SR2 = VR/(a-1);
     SC2 = VC/(b-1);
