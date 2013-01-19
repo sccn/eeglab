39126bc39bde678b9f502872339f6f69e26bab04
Arnaud Delorme <arno@ucsd.edu>
2013-01-19 05:21:55 +0000

    fix setting parameters
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@9910 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/pop_erpimparams.m
+++ functions/studyfunc/pop_erpimparams.m
@@ -101,11 +101,13 @@ else
             if ~isempty(strmatch(varargin{index}, allfields, 'exact'))
                 STUDY.etc.erpimparams = setfield(STUDY.etc.erpimparams, varargin{index}, varargin{index+1});
             else
-                inderpimopt = strmatch(varargin{index}, STUDY.etc.erpimparams.erpimageopt(1:2:end), 'exact');
-                if ~isempty(inderpimopt)
-                    STUDY.etc.erpimparams.erpimageopt{inderpimopt+1} = varargin{index+1};
-                else
-                    STUDY.etc.erpimparams.erpimageopt = { STUDY.etc.erpimparams.erpimageopt{:} varargin{index}, varargin{index+1} };
+                if ~isequal(varargin{index}, 'datatype') && ~isequal(varargin{index}, 'channels')
+                    inderpimopt = strmatch(varargin{index}, STUDY.etc.erpimparams.erpimageopt(1:2:end), 'exact');
+                    if ~isempty(inderpimopt)
+                        STUDY.etc.erpimparams.erpimageopt{inderpimopt+1} = varargin{index+1};
+                    else
+                        STUDY.etc.erpimparams.erpimageopt = { STUDY.etc.erpimparams.erpimageopt{:} varargin{index}, varargin{index+1} };
+                    end;
                 end;
             end;
         end;
