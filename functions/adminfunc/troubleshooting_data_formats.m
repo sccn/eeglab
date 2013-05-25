6fb8babbf242981c4d0226dc9b68eae1b9c0bf2f
Arnaud Delorme <arno@ucsd.edu>
2013-05-25 22:38:35 +0000

    new help function for data formats
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@10075 1e58238c-3a72-4c0f-aabe-36d030380f39

---
+++ functions/adminfunc/troubleshooting_data_formats.m
@@ -0,0 +1,16 @@
+%Several EEGLAB plugins (not included by default) are available to import 
+%specific data formats. To download these plugins use EEGLAB menu
+%
+%File > Manage Plugins > Manage data import plugins
+%
+%The FILEIO and BIOSIG toolboxes interface also allow to import in EEGLAB a wide variety
+%of EEG/MEG data file formats -- including those imported using EEGLAB native menus
+%
+%For EEGLAB, FILE-IO and BIOSIG suported data formats
+%http://sccn.ucsd.edu/wiki/A01:_Importing_Continuous_and_Epoched_Data
+%
+%For FILE-IO see in addition
+%http://www2.ru.nl/fcdonders/fieldtrip/doku.php?id=fieldtrip:dataformat
+%
+%For BIOSIG see in addition
+%http://biosig.sourceforge.net/SupportedSystems.html
