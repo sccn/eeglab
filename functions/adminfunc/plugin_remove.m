36410fb8153b676c2341f9bd59bb6e0e29190341
Arnaud Delorme <arno@ucsd.edu>
2013-05-25 22:35:19 +0000

    fix deletting plugin
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@10072 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/plugin_remove.m
+++ functions/adminfunc/plugin_remove.m
@@ -2,22 +2,11 @@ function plugin_remove(foldername);
 
     % get plugin path
     % ---------------
-    fullpluginfolder = fullfile(fileparts(which('eeglab.m')), 'plugins', foldername);
+    fullpluginfolder = fullfile(fileparts(which('eeglab.m')), 'deactivatedplugins', foldername);
     if ~exist(fullpluginfolder)
-        error([ 'Could not find folder ' foldername 'in plugin folder' ]);
+        error([ 'Could not find folder ' foldername ' in deactivatedplugins folder' ]);
     end;
 
-    disp('Removing plugin from path');
-    allPaths = path;
-    indSep   = [0 find(allPaths == ':') length(allPaths)+1];
-    for index = 1:length(indSep)-1
-        tmpPath = allPaths(indSep(index)+1:indSep(index+1)-1);
-        if ~isempty(strfind(tmpPath, fullpluginfolder))
-            rmpath(tmpPath);
-            fprintf('Removing path %s\n', tmpPath);
-        end;
-    end;
-    
     disp([ 'Removing plugin folder ' foldername ]);
     rmdir(fullpluginfolder, 's');
     
