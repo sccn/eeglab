235bc1034d30ef367bff803cc8500598ccc0b013
Arnaud Delorme <arno@ucsd.edu>
2013-09-21 21:25:34 +0000

    fix 'no' option for multiple comparisons
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@10122 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/std_stat.m
+++ functions/studyfunc/std_stat.m
@@ -243,7 +243,7 @@ end;
 function pvals = mcorrect(pvals, method);
 
 switch method
-    case 'none', return;
+    case {'no' 'none'}, return;
     case 'bonferoni', pvals = pvals*prod(size(pvals));
     case 'holms',     [tmp ind] = sort(pvals(:)); [tmp ind2] = sort(ind); pvals(:) = pvals(:).*(prod(size(pvals))-ind2+1);
     case 'fdr',       pvals = fdr(pvals);
