e3f98cec27a52cce0bd8d96d3434a9db19d143a8
Arnaud Delorme <arno@ucsd.edu>
2013-09-28 20:14:08 +0000

    removing all plugins except for DIPFIT
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@10124 1e58238c-3a72-4c0f-aabe-36d030380f39

--- plugins/MutualInfoClustering1.00/eegplugin_miclust.m
+++
@@ -1,4 +0,0 @@
-function eegplugin_miclust( fig, try_strings, catch_strings);
-plotmenu = findobj(fig, 'tag', 'plot');
-uimenu(plotmenu, 'label', 'Cluster dataset ICs', 'callback', ...
-    [try_strings.check_ica '[EEG LASTCOM]= pop_miclust(EEG);' catch_strings.store_and_hist '[ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG, CURRENTSET);' ]);
