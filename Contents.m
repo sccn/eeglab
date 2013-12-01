4029c296ab6db23e113f0519dd539c64ebf28b6a
Arnaud Delorme <arno@ucsd.edu>
2013-12-01 17:32:41 +0000

    development version
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@10199 1e58238c-3a72-4c0f-aabe-36d030380f39

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % EEGLAB Toolbox to process EEG data
-% Version 13.0.0
+% Version 13.x dev
 %
 % eeglab() - Matlab graphic user interface environment for 
 %   electrophysiological data analysis incorporating the ICA/EEG toolbox 
