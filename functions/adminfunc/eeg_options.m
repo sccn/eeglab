891bd59e1834805603ee814e600c1ac0a86fbfbe
Arnaud Delorme <arno@ucsd.edu>
2014-01-09 21:58:41 +0000

    removing option to save ICA activity
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@10255 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/eeg_options.m
+++ functions/adminfunc/eeg_options.m
@@ -6,7 +6,6 @@
 % STUDY options (set these checkboxes if you intend to work with studies)
 option_storedisk = 0 ; % If set, keep at most one dataset in memory. This allows processing hundreds of datasets within studies.
 option_savetwofiles = 1 ; % If set, save not one but two files for each dataset (header and data). This allows faster data loading in studies.
-option_saveica = 0 ; % If set, write ICA activations to disk. This speeds up loading ICA components when dealing with studies.
 % Memory options 
 option_single = 1 ; % If set, use single precision under Matlab 7.x. This saves RAM but can lead to rare numerical imprecisions.
 option_memmapdata = 0 ; % If set, use memory mapped array under Matlab 7.x. This may slow down some computation.
