feae01f4ffe5aca6de98e2e48b606e20b56aa2e9
Arnaud Delorme <arno@ucsd.edu>
2014-02-06 16:37:54 +0000

    remove history and fix (again) the code for selecting default filebase name
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@10286 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/studyfunc/std_makedesign.m
+++ functions/studyfunc/std_makedesign.m
@@ -274,22 +274,18 @@ for n2 = 1:nf2, [ dats2{n2} dattrials2{n2} ] = std_selectdataset( STUDY, ALLEEG,
 % detect files from old format
 % ----------------------------
 if ~strcmpi(opt.defaultdesign, 'forceoff') && isempty(opt.filepath)
-    if strcmpi(opt.defaultdesign, 'off') && designind == 1
-        if ~isfield(STUDY.design, 'cell') || ~isfield(STUDY.design(1).cell, 'filebase')
-             opt.defaultdesign = 'on';
-        else
-             newfilename = ~isempty(findstr('design', STUDY.design(1).cell(1).filebase));
-             if newfilename && isempty(dir(fullfile(STUDY.design(1).cell(1).filebase, '*.dat*')))
-                opt.defaultdesign = 'on';
-             elseif ~newfilename
-                opt.defaultdesign = 'on';
-             end;
+    if designind == 1
+        if strcmpi(opt.defaultdesign, 'off')
+            if isfield(STUDY, 'design') && ( ~isfield(STUDY.design, 'cell') || ~isfield(STUDY.design(1).cell, 'filebase') )
+                 opt.defaultdesign = 'on';
+            end;
         end;
-    end;
-    if strcmpi(opt.defaultdesign, 'on')
-        if isempty(dir(fullfile(ALLEEG(1).filepath, '*.dat*'))) && isempty(dir(fullfile(ALLEEG(1).filepath, '*.ica*')))
+        if isempty(dir(fullfile(ALLEEG(1).filepath, [ ALLEEG(1).filename(1:end-4) '.dat*' ]))) && ...
+                isempty(dir(fullfile(ALLEEG(1).filepath, [ ALLEEG(1).filename(1:end-4) '.ica*' ])))
             opt.defaultdesign = 'off';
         end;
+    else
+        opt.defaultdesign = 'off';
     end;
 else
     opt.defaultdesign = 'off';
@@ -388,11 +384,7 @@ STUDY = std_selectdesign(STUDY, ALLEEG, designind);
 
 % build output command
 % --------------------
-if strcmpi(opt.defaultdesign, 'on')
-    com = sprintf('STUDY = std_makedesign(STUDY, ALLEEG);' );
-else
-    com = sprintf('STUDY = std_makedesign(STUDY, ALLEEG, %d, %s);', designind, vararg2str( listcom ) );
-end;
+com = sprintf('STUDY = std_makedesign(STUDY, ALLEEG, %d, %s);', designind, vararg2str( listcom ) );
 
 % ---------------------------------------------------
 
