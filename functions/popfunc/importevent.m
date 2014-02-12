e5f7ee9dc7df0190bfa090964a06a3dd2e21d854
Arnaud Delorme <arno@ucsd.edu>
2014-02-12 20:37:29 +0000

    implementing fix so that event array is not transposed systematically when it has very few rows - bug 1563
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@10293 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/importevent.m
+++ functions/popfunc/importevent.m
@@ -110,6 +110,16 @@ if ~isempty(g.event)
     event = g.event;
     if ~isstr(event)
         if size(event,2) > size(event,1), event = event'; end;
+        if iscell(event)
+            eventStr = cellfun(@ischar, event);
+            eventNum = cellfun(@isnumeric, event);
+            if all(eventStr(1,:)) && ~all(eventStr(:,1))
+                event = event';
+            end;
+            if all(eventNum(1,:)) && ~all(eventNum(:,1))
+                event = event';
+            end;
+        end;
     end;
 end;
 g.event = event;
