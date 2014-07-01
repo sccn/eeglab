cdabe31e5f90f4e0984d94ce93a8d8d400f4df88
Ramon Martinez <nucleuscub@gmail.com>
2014-07-01 01:21:44 +0000

    +-5% for the color limits were added
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@10379 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/topoplot.m
+++ functions/sigprocfunc/topoplot.m
@@ -1197,7 +1197,11 @@ if ~strcmpi(STYLE,'blank') % if draw interpolated scalp map
   %
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Set color axis  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   %
-  caxis([amin amax]); % set coloraxis
+%   caxis([amin amax]); % set coloraxis
+
+% 7/30/2014 Ramon: +-5% for the color limits were added
+cax_sgn = sign([amin amax]);                                                  % getting sign
+caxis([amin+cax_sgn(1)*(0.05*abs(amin)) amax+cax_sgn(2)*(0.05*abs(amax))]);   % Adding 5% to the color limits
 
 else % if STYLE 'blank'
 %
