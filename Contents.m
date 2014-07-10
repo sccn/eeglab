2f714988f0e23b7de80183f9e046d97cd3769c05
Ramon Martinez <nucleuscub@gmail.com>
2014-07-10 22:52:25 +0000

    Update number of version to 12.0.2.6b
    git-svn-id: https://sccn.ucsd.edu/svn/software/branches/eeglab12@10396 1e58238c-3a72-4c0f-aabe-36d030380f39

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % EEGLAB Toolbox to process EEG data
-% Version 12.0.2.5b
+% Version 12.0.2.6b
 %
 % eeglab() - Matlab graphic user interface environment for 
 %   electrophysiological data analysis incorporating the ICA/EEG toolbox 
