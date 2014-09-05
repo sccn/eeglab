52a47552599d70a4213ab1cb982cf2645fcc9006
Ramon Martinez <nucleuscub@gmail.com>
2014-09-05 17:59:04 +0000

    More information added to the error message
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@10420 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/adminfunc/eeglab_error.m
+++ functions/adminfunc/eeglab_error.m
@@ -24,14 +24,17 @@
 % along with this program; if not, write to the Free Software
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-function eeglab_error;
+function eeglab_error
 
-    % handling errords
+    % handling errors
     % ----------------
     tmplasterr = lasterr;
     [iseeglaberror tmplasterr header] = testeeglaberror(tmplasterr);
     ft_error = false;
     if iseeglaberror
+        tmp = lasterror; % Add more information to eeglab errors JRI, RMC
+        tmplasterr = sprintf('%s,\n\n  (Error occurred in function %s() at line %d)',...
+          tmplasterr, tmp.stack(1).name, tmp.stack(1).line);
         errordlg2(tmplasterr, header);
     else
         try
@@ -44,29 +47,28 @@ function eeglab_error;
             tmperr = [ 'EEGLAB error:' 10 10 tmplasterr ];
         end;
         if ~ft_error
-            tmperr = [ tmperr 10 10 'If you think this is a bug, please submit a detailed' 10 ...
-                'description of how to reproduce the problem (and upload' 10 ...
-                'a small dataset) at http://sccn.ucsd.edu/eeglab/bugzilla' ];
+            tmperr = [ tmperr 10 10 'If you think this is a bug, please submit a detailed ' 10 ...
+                                    'description of how to reproduce the problem (and upload' 10 ...
+                                    'a small dataset) at http://sccn.ucsd.edu/eeglab/bugzilla' ];
         else
             tmperr = [ tmperr 10 10 'This is a problem with FIELDTRIP. The Fieldtrip version you downloaded' 10 ...
-                'is corrupted. Please manually replace Fieldtrip with an earlier version' 10 ...
-                'and/or email the Fieldtrip developpers so they can fix the issue.' ];
+                                    'is corrupted. Please manually replace Fieldtrip with an earlier version' 10 ...
+                                    'and/or email the Fieldtrip developpers so they can fix the issue.' ];
         end;
         errordlg2(tmperr, header);
     end;
     
-    function [ val str header ] = testeeglaberror(str);
+    function [ val str header ] = testeeglaberror(str)
         header = 'EEGLAB error';
         val = 0;
         try,
             if strcmp(str(1:5), 'Error'), 
                 val = 1; 
-                str = str(17:end);
-                indspace = find(str == ' ');
-                funcname = str(1:indspace(1));
+                str = str(12:end); %Corrected extraction of function name, occurs after 'Error using ' JRI, RMC
                 indendofline = find(str == 10);
+                funcname = str(1:indendofline(1)-1);
                 str = str(indendofline(1)+1:end);
                 header = [ funcname ' error' ];
             end;
-        catch, end;
-    
+        catch
+        end;
