5e7df0a37567f422a67680992f5745c454606098
Ramon Martinez <nucleuscub@gmail.com>
2014-11-12 23:56:12 +0000

    Fix to force points out of range to be in the valid range.This is necsesary since the double multiplication could lead to shift in points
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@10431 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/popfunc/eeg_lat2point.m
+++ functions/popfunc/eeg_lat2point.m
@@ -4,6 +4,8 @@
 % Usage:
 %       >> [newlat] = eeg_lat2point( lat_array, epoch_array,...
 %                                 srate, timelimits, timeunit);
+%       >> [newlat] = eeg_lat2point( lat_array, epoch_array,...
+%                                 srate, timelimits, '','outrange',1);
 % Inputs:
 %   lat_array   - latency array in 'timeunit' units (see below)
 %   epoch_array - epoch number for each latency
@@ -11,9 +13,15 @@
 %   timelimits  - [min max] epoch timelimits in 'timeunit' units (see below)
 %   timeunit    - time unit relative to seconds. Default is 1 = seconds.
 %
+% Optional inputs:
+%   outrange    - [1/0] Replace the points out of the range with the value of
+%                 the maximun point in the valid range or raise an error.
+%                 Default [1] : Replace point.
+%
 % Outputs:
 %   newlat      - converted latency values in points assuming concatenated
 %                 data epochs (see eeglab() event structure)
+%   flag        - 1 if any point out of range was replaced.
 %
 % Author: Arnaud Delorme, CNL / Salk Institute, 2 Mai 2002
 %
@@ -35,13 +43,28 @@
 % along with this program; if not, write to the Free Software
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-function newlat = eeg_lat2point( lat_array, epoch_array, srate, timewin, timeunit);
+function [newlat,flag] = eeg_lat2point( lat_array, epoch_array, srate, timewin, timeunit, varargin);
+% -------------------------------------------------------------------------
+try
+    options = varargin;
+    if ~isempty( varargin ),
+        for i = 1:2:numel(options)
+            g.(options{i}) = options{i+1};
+        end
+    else g= []; end;
+catch
+    error('std_checkdatasession() error: calling convention {''key'', value, ... } error');
+end;
+ try, g.outrange; catch, g.outrange = 1; end; %
+ 
+ flag = 0;
+% -------------------------------------------------------------------------
 
 if nargin <4
     help eeg_lat2point;
     return;
 end;    
-if nargin <5
+if nargin <5 | isempty(timeunit)
 	timeunit = 1;
 end;
 
@@ -65,3 +88,18 @@ end
 timewin = timewin*timeunit;
 pnts = (timewin(2)-timewin(1))*srate+1;
 newlat  = (lat_array*timeunit-timewin(1))*srate+1 + (epoch_array-1)*pnts;
+
+% Detecting points out of range (RMC)
+% Note: This is neccesary since the double precision  multiplication could lead to the
+% shifting in one sample out of the valid range
+
+if max(newlat(:)) > pnts
+    if g.outrange == 1
+        IndxOut = find(newlat(:) > pnts);
+        newlat(IndxOut) = pnts;
+        flag = 1;
+        warning('eeg_lat2point(): Points out of range detected. Points replaced with maximum value');
+    elseif g.outrange == 0
+        error('Error in eeg_lat2point(): Points out of range detected');
+    end
+end
