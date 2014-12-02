674747776d5d58dbe7e81267cbb91b4c676bdbd1
Arnaud Delorme <arno@ucsd.edu>
2014-12-02 22:36:56 +0000

    add new function to call the Matlab rotate function in Dipfit
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@10465 1e58238c-3a72-4c0f-aabe-36d030380f39

---
+++ functions/miscfunc/rotatematlab.m
@@ -0,0 +1,6 @@
+% This function calls the Matlab rotate function
+% This prevents the issue with the function in the private folder of Dipfit
+
+function rotatematlab(varargin)
+
+rotate(varargin{:});
