367a1bb17ea93a69aaace4c0cc4626c984571f9c
Arnaud Delorme <arno@ucsd.edu>
2014-12-02 22:35:23 +0000

    adding option to plot electrodes as disk
    git-svn-id: https://sccn.ucsd.edu/svn/software/eeglab@10463 1e58238c-3a72-4c0f-aabe-36d030380f39

--- functions/sigprocfunc/headplot.m
+++ functions/sigprocfunc/headplot.m
@@ -85,6 +85,7 @@
 %                      {default = 'absmax'}
 %   'lights'     - (3,N) matrix whose rows give [x y z] pos. of each of
 %                   N lights {default: four lights at corners}
+%   'electrode3d' - ['on'|'off'] plot electrodes in 3-D. Default is 'off'.
 %   'lighting'   - 'off' = show wire frame head {default 'on'} 
 %   'material'   - [see material function] {default 'dull'}
 %   'colormap'   -  3-column colormap matrix {default: jet(64)}
@@ -163,7 +164,8 @@ ElectDFac  = 1.06;  % plot electrode marker dots out from head surface
 plotelecopt.NamesDFac  = 1.05;  % plot electrode names/numbers out from markers
 plotelecopt.NamesColor = 'k'; % 'r';
 plotelecopt.NamesSize  =  10;   % FontSize for electrode names
-plotelecopt.MarkerColor= 'k';
+plotelecopt.MarkerColor= [0.5 0.5 0.5];
+plotelecopt.electrodes3d = 'off';
 
 sqaxis     = 1;     % if non-zero, make head proportions anatomical
 title_font = 18;
@@ -443,10 +445,12 @@ else
        'transform'  'real'   []              [];
        'meshfile'   {'string','struct' } []  DEFAULT_MESH;
        'electrodes' 'string' { 'on','off' }  'on';            
+       'electrodes3d' 'string' { 'on','off' }  'off';            
        'material'     'string'            [] 'dull';
        'orilocs'    { 'string','struct' } [] '';            
        'labels'     'integer' [0 1 2]        0 }, 'headplot');
    if isstr(g) error(g); end;
+   plotelecopt.electrodes3d = g.electrodes3d;
 
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   % Open head mesh and electrode spline files
@@ -726,9 +730,10 @@ function plotelec(newElect, ElectrodeNames, HeadCenter, opt);
     newNames = newElect*opt.NamesDFac; % Calculate electrode label positions
     for i = 1:size(newElect,1)
         if newElect(i,:) ~= [0 0 0]  % plot radial lines to electrode sites
-            line([newElect(i,1) HeadCenter(1)],[newElect(i,2) HeadCenter(2)],...
-                 [newElect(i,3) HeadCenter(3)],'color',opt.MarkerColor,'linewidth',1);
-            
+            if strcmpi(opt.electrodes3d, 'off')
+                line([newElect(i,1) HeadCenter(1)],[newElect(i,2) HeadCenter(2)],...
+                     [newElect(i,3) HeadCenter(3)],'color',opt.MarkerColor,'linewidth',1);
+            end;
             if opt.labelflag == 1        % plot electrode numbers
                 t=text(newNames(i,1),newNames(i,2),newNames(i,3),int2str(i)); 
                 set(t,'Color',opt.NamesColor,'FontSize',opt.NamesSize,'FontWeight','bold',...
@@ -744,8 +749,36 @@ function plotelec(newElect, ElectrodeNames, HeadCenter, opt);
                     fprintf('Variable ElectrodeNames not read from spline file.\n');
                 end
             else               % plot electrode markers
-                line(newElect(:,1),newElect(:,2),newElect(:,3),'marker',...
-                     '.','markersize',20,'color',opt.MarkerColor,'linestyle','none');
+                
+                if strcmpi(opt.electrodes3d, 'off')
+                    line(newElect(:,1),newElect(:,2),newElect(:,3),'marker',...
+                          '.','markersize',20,'color',opt.MarkerColor,'linestyle','none');
+                else
+                    [xc yc zc] = cylinder( 2, 10);
+                    [xs ys zs] = sphere(10);
+                    xc = [ xc; -xs(7:11,:)*2  ];
+                    yc = [ yc; -ys(7:11,:)*2  ];
+                    zc = [ zc; zs(7:11,:)*0.2+1 ];
+
+                    hold on;
+                    cylinderSize = 3;
+                    colorarray = repmat(reshape(opt.MarkerColor, 1,1,3), [size(zc,1) size(zc,2) 1]);
+                    handles = surf(xc*cylinderSize, yc*cylinderSize, zc*cylinderSize, colorarray, 'edgecolor', 'none', ...
+                        'backfacelighting', 'lit', 'facecolor', 'interp', 'facelighting', ...
+                        'phong', 'ambientstrength', 0.3);
+
+                    cylnderHeight = 1;
+                    if newElect(i,3) < 10, addZ = -30; else addZ = 0; end;
+                    if newElect(i,3) < -20, addZ = -60; else addZ = 0; end;
+                    xx   = newElect(i,1) - ( newElect(i,1)-HeadCenter(1) ) * 0.01 * cylnderHeight;
+                    xxo1 = newElect(i,1) + ( newElect(i,1)-HeadCenter(1) ) * 0.01 * cylnderHeight;
+                    yy   = newElect(i,2) - ( newElect(i,2)-HeadCenter(2) ) * 0.01 * cylnderHeight;
+                    yyo1 = newElect(i,2) + ( newElect(i,2)-HeadCenter(2) ) * 0.01 * cylnderHeight;
+                    zz   = newElect(i,3) - ( newElect(i,3)-HeadCenter(3)-addZ ) * 0.01 * cylnderHeight;
+                    zzo1 = newElect(i,3) + ( newElect(i,3)-HeadCenter(3)-addZ ) * 0.01 * cylnderHeight;
+                    [xc yc zc] = adjustcylinder2( handles, [xx yy zz], [xxo1 yyo1 zzo1] );
+                end;
+                
             end
         end
     end;
