49c898b6f3c9dca1c7bf9dea1fcadc2a22c49b54
Arnaud Delorme <arno@ucsd.edu>
2015-07-14 15:13:58 -0700

    Computing expected channel location structure for LIMO

--- functions/studyfunc/pop_limo.m
+++ functions/studyfunc/pop_limo.m
@@ -1,3 +1,38 @@
+% pop_limo() - prepare and convert EEGLAB data and structure to be
+%              processed by LIMO.
+%
+% Usage:
+%   >> STUDY = pop_limo( STUDY, ALLEEG, 'key', val)
+%
+% Inputs:
+%   STUDY        - an EEGLAB STUDY set of loaded EEG structures
+%   ALLEEG       - ALLEEG vector of one or more loaded EEG dataset structures
+%
+% Optional inputs: same as std_eeglab2limo()
+%
+% Outputs:
+%   STUDY       - an EEGLAB STUDY set of loaded EEG structures
+%
+% Author: Arnaud Delorme, SCCN, UCSD, 2015-
+%
+% See also: std_eeglab2limo()
+
+% Copyright (C) Arnaud Delorme
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 function [STUDY,com] = pop_limo(STUDY, ALLEEG, varargin)
 
 if nargin < 2
@@ -17,7 +52,8 @@ if nargin < 3
         {'style' 'popupmenu'  'string' dataMeasures 'tag' 'measure' } ...
         {'style' 'text'       'string' 'Optimization method' } ...
         {'style' 'popupmenu'  'string' methods 'tag' 'method' } ...
-        {'style' 'checkbox'   'string' 'Erase all previous models for this design' 'tag' 'erase' } };
+        {'style' 'checkbox'   'string' 'Erase previous model for this design and measure' 'tag' 'erase' 'value' true } ...
+        };
     
     cline = [1.1 1.1];
     geometry = { 1 1 1 cline cline 1 };
@@ -37,4 +73,4 @@ if strcmp(fastif(res.measure, 'on', 'off'), 'on')
     end
 end
 [tmp,STUDY] = std_eeglab2limo(STUDY, ALLEEG, options{:});
-com = sprintf('std_eeglab2limo(STUDY, ALLEEG, %s);', vararg2str(options));
+com = sprintf('pop_limo(STUDY, ALLEEG, %s);', vararg2str(options));
