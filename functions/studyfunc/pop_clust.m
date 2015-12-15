8af4b52811db0b13d1e23de6826203a625b70146
Arnaud Delorme <arno@ucsd.edu>
2015-12-15 14:52:08 -0800

    Fix calling cluster edit function

--- functions/studyfunc/pop_clust.m
+++ functions/studyfunc/pop_clust.m
@@ -229,11 +229,10 @@ if isempty(varargin) %GUI call
            command(end:end+1) = ');';
         end
            
-       % Call menu to plot clusters (use EEGLAB menu which include
-       % std_envtopo) - this crashed the hisotry
-       %eval( [ get(findobj(findobj('tag', 'EEGLAB'), 'Label', 'Edit/plot clusters'), 'callback') ] );
-       [STUDY LASTCOM] = pop_clustedit(STUDY, ALLEEG); 
-       command = [ command LASTCOM ];
+       % Call menu to plot clusters (use EEGLAB menu which include std_envtopo)
+       LASTCOM = command;
+       eval([ get(findobj(findobj('tag', 'EEGLAB'), 'Label', 'Edit/plot clusters'), 'callback') ] );
+       %[STUDY com] = pop_clustedit(STUDY, ALLEEG); 
 	end
     
 else %command line call
