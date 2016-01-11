1dc494b859aae8172f9469bd62e5ef21fb71ffcd
Arnaud Delorme <arno@ucsd.edu>
2016-01-10 23:29:59 -0800

    Add single trial option to call to std_readfile

--- functions/studyfunc/std_readerp.m
+++ functions/studyfunc/std_readerp.m
@@ -153,6 +153,7 @@ for ind = 1:length(finalinds) % scan channels or components
         if strcmpi(dtype, 'erp'), opts = { 'timelimits', opt.timerange };
         else                      opts = { 'freqlimits', opt.freqrange };
         end;
+        opts = { opts{:} 'singletrials' opt.singletrials };
         
         % read the data and select channels
         % ---------------------------------
