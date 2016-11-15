779d15dbd78d58fbb8324fda28d50de88603ae0f
Arnaud Delorme <arno@ucsd.edu>
2016-11-15 12:24:17 -0800

    Add internal function in case the mapping toolbox is absent

--- functions/timefreqfunc/angtimewarp.m
+++ functions/timefreqfunc/angtimewarp.m
@@ -110,4 +110,30 @@ function angdataw=angtimewarp(evLatency, newLatency, angdata)
 %       end
     end
   end
-  angdataw = wrapToPi(angdataw);
+  angdataw = wrap2pi(angdataw);
+
+  function a = wrap2pi(a, a_center )
+% function a = wrap(a,a_center)
+%
+% Wraps angles to a range of 2*pi.
+% Inverse of Matlab's "unwrap", and better than wrapToPi ( which has
+% redundant [-pi,pi])
+% Optional input "a_center" defines the center angle.  Default is 0, giving
+% angles from (-pi,pi], chosen to match angle(complex(-1,0)).  Maximum
+% possible value is pi.
+
+% T.Hilmer, UH
+% 2010.10.18 version 2
+%   removed code from version 1. Have not bug-checked second input
+%   "a_center"
+
+if nargin < 2, a_center = 0; end
+
+% new way
+a = mod(a,2*pi); % [0 2pi)
+
+% shift
+j = a > pi - a_center;
+a(j) = a(j) - 2*pi;
+j = a < a_center - pi;
+a(j) = a(j) + 2*pi;
