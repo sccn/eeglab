c6a67937be61c107a670902e660a07e91ec27e45
Arnaud Delorme <arno@ucsd.edu>
2017-06-06 09:32:45 -0700

    Change revision number

--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % EEGLAB Toolbox to process EEG data
-% Version 14.1.0
+% Version 14.1.1
 %
 % eeglab() - Matlab graphic user interface environment for 
 %   electrophysiological data analysis incorporating the ICA/EEG toolbox 
