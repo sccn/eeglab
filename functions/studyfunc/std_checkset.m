e71292a5ddd51b2089939db9e827f1846aede9e1
Arnaud Delorme <arno@ucsd.edu>
2017-10-08 15:31:31 +0200

    std_checkset.m, remove ";" after "end"

--- functions/studyfunc/std_checkset.m
+++ functions/studyfunc/std_checkset.m
@@ -34,36 +34,36 @@ function [STUDY, ALLEEG, command] = std_checkset(STUDY, ALLEEG, options)
 if nargin < 2
     help std_checkset;
     return;
-end;
+end
 command  = '';
 
-if isempty(STUDY), return; end;
+if isempty(STUDY), return; end
 studywasempty = 0;
 
 modif = 0;
-if ~isfield(STUDY, 'name'),  STUDY.name  = ''; modif = 1; end;
-if ~isfield(STUDY, 'task'),  STUDY.task  = ''; modif = 1; end;
-if ~isfield(STUDY, 'notes'), STUDY.notes = ''; modif = 1; end;
-if ~isfield(STUDY, 'filename'),  STUDY.filename  = ''; modif = 1; end;
-if ~isfield(STUDY, 'filepath'),  STUDY.filepath  = ''; modif = 1; end;
-if ~isfield(STUDY, 'history'),   STUDY.history   = ''; modif = 1; end;
-if ~isfield(STUDY, 'subject'),   STUDY.subject   = {}; modif = 1; end;
-if ~isfield(STUDY, 'group'),     STUDY.group     = {}; modif = 1; end;
-if ~isfield(STUDY, 'session'),   STUDY.session   = {}; modif = 1; end;
-if ~isfield(STUDY, 'condition'), STUDY.condition = {}; modif = 1; end;
-if ~isfield(STUDY, 'setind'),    STUDY.setind    = {}; modif = 1; end;
-if ~isfield(STUDY, 'etc'),       STUDY.etc       = []; modif = 1; end;
-if ~isfield(STUDY, 'cache'),     STUDY.cache     = []; modif = 1; end;
-if ~isfield(STUDY, 'etc.warnmemory'), STUDY.etc.warnmemory = 1; modif = 1; end;
-if ~isfield(STUDY, 'preclust'),  STUDY.preclust  = []; modif = 1; end;
-if ~isfield(STUDY, 'datasetinfo'), STUDY.datasetinfo = []; modif = 1; end;
-if ~isfield(STUDY.etc, 'version'), STUDY.etc.version = []; modif = 1; end;
-if ~isfield(STUDY.preclust, 'erpclusttimes' ),  STUDY.preclust.erpclusttimes = []; modif = 1; end;
-if ~isfield(STUDY.preclust, 'specclustfreqs' ), STUDY.preclust.specclustfreqs = []; modif = 1; end;
-if ~isfield(STUDY.preclust, 'erspclustfreqs' ), STUDY.preclust.erspclustfreqs = []; modif = 1; end;
-if ~isfield(STUDY.preclust, 'erspclusttimes' ), STUDY.preclust.erspclusttimes = []; modif = 1; end;
-if ~isfield(STUDY.datasetinfo, 'comps') & ~isempty(STUDY.datasetinfo), STUDY.datasetinfo(1).comps = []; modif = 1; end;
-if ~isfield(STUDY.datasetinfo, 'index') & ~isempty(STUDY.datasetinfo), STUDY.datasetinfo(1).index = []; modif = 1; end;
+if ~isfield(STUDY, 'name'),  STUDY.name  = ''; modif = 1; end
+if ~isfield(STUDY, 'task'),  STUDY.task  = ''; modif = 1; end
+if ~isfield(STUDY, 'notes'), STUDY.notes = ''; modif = 1; end
+if ~isfield(STUDY, 'filename'),  STUDY.filename  = ''; modif = 1; end
+if ~isfield(STUDY, 'filepath'),  STUDY.filepath  = ''; modif = 1; end
+if ~isfield(STUDY, 'history'),   STUDY.history   = ''; modif = 1; end
+if ~isfield(STUDY, 'subject'),   STUDY.subject   = {}; modif = 1; end
+if ~isfield(STUDY, 'group'),     STUDY.group     = {}; modif = 1; end
+if ~isfield(STUDY, 'session'),   STUDY.session   = {}; modif = 1; end
+if ~isfield(STUDY, 'condition'), STUDY.condition = {}; modif = 1; end
+if ~isfield(STUDY, 'setind'),    STUDY.setind    = {}; modif = 1; end
+if ~isfield(STUDY, 'etc'),       STUDY.etc       = []; modif = 1; end
+if ~isfield(STUDY, 'cache'),     STUDY.cache     = []; modif = 1; end
+if ~isfield(STUDY, 'etc.warnmemory'), STUDY.etc.warnmemory = 1; modif = 1; end
+if ~isfield(STUDY, 'preclust'),  STUDY.preclust  = []; modif = 1; end
+if ~isfield(STUDY, 'datasetinfo'), STUDY.datasetinfo = []; modif = 1; end
+if ~isfield(STUDY.etc, 'version'), STUDY.etc.version = []; modif = 1; end
+if ~isfield(STUDY.preclust, 'erpclusttimes' ),  STUDY.preclust.erpclusttimes = []; modif = 1; end
+if ~isfield(STUDY.preclust, 'specclustfreqs' ), STUDY.preclust.specclustfreqs = []; modif = 1; end
+if ~isfield(STUDY.preclust, 'erspclustfreqs' ), STUDY.preclust.erspclustfreqs = []; modif = 1; end
+if ~isfield(STUDY.preclust, 'erspclusttimes' ), STUDY.preclust.erspclusttimes = []; modif = 1; end
+if ~isfield(STUDY.datasetinfo, 'comps') && ~isempty(STUDY.datasetinfo), STUDY.datasetinfo(1).comps = []; modif = 1; end
+if ~isfield(STUDY.datasetinfo, 'index') && ~isempty(STUDY.datasetinfo), STUDY.datasetinfo(1).index = []; modif = 1; end
 
 % all summary fields
 % ------------------
@@ -71,26 +71,26 @@ try, subject = unique_bc({ STUDY.datasetinfo.subject });
 catch,
     subject = '';
     disp('Important warning: some datasets do not have subject codes; some functions may crash!');
-end;
+end
 try, group = unique_bc({ STUDY.datasetinfo.group });
 catch,
     group = {};
     % disp('Important warning: some datasets do not have group codes; some functions may crash!');
-end;
+end
 try, condition = unique_bc({ STUDY.datasetinfo.condition });
 catch,
     condition = {};
     disp('Important warning: some datasets do not have condition codes; some functions may crash!');
-end;
+end
 try, session = unique_bc([STUDY.datasetinfo.session]);
 catch,
     session = '';
     % disp('Important warning: some datasets do not have session numbers; some functions may crash!');
-end;
-if ~isequal(STUDY.subject,   subject  ), STUDY.subject   = subject;   modif = 1; end;
-if ~isequal(STUDY.group,     group    ), STUDY.group     = group;     modif = 1; end;
-if ~isequal(STUDY.condition, condition), STUDY.condition = condition; modif = 1; end;
-if ~isequal(STUDY.session,   session  ), STUDY.session   = session;   modif = 1; end;
+end
+if ~isequal(STUDY.subject,   subject  ), STUDY.subject   = subject;   modif = 1; end
+if ~isequal(STUDY.group,     group    ), STUDY.group     = group;     modif = 1; end
+if ~isequal(STUDY.condition, condition), STUDY.condition = condition; modif = 1; end
+if ~isequal(STUDY.session,   session  ), STUDY.session   = session;   modif = 1; end
 
 % check dataset info consistency
 % ------------------------------
@@ -99,8 +99,8 @@ for k = 1:length(STUDY.datasetinfo)
         STUDY.datasetinfo(k).filename = ALLEEG(k).filename; modif = 1;
         fprintf('Warning: file name has changed for dataset %d and the study has been updated\n', k);
         fprintf('         to discard this change in the study, reload it from disk\n');
-    end;
-end;
+    end
+end
 
 % recompute setind array (setind is deprecated but we keep it anyway)
 % -------------------------------------------------------------------
@@ -108,12 +108,12 @@ setind  = [];
 sameica = std_findsameica(ALLEEG);
 for index = 1:length(sameica)
     setind(length(sameica{index}):-1:1,index) = sameica{index}';
-end;
+end
 setind(find(setind == 0)) = NaN;
 if any(isnan(setind))
     warndlg('Warning: non-uniform set of dataset, some function might not work');
 end
-if ~isequal(setind, STUDY.setind), STUDY.setind = setind; modif = 1; end;
+if ~isequal(setind, STUDY.setind), STUDY.setind = setind; modif = 1; end
 
 % check that dipfit is present in all datasets
 % --------------------------------------------
@@ -122,18 +122,18 @@ for cc = 1:length(sameica)
     for tmpi = 1:length(sameica{cc})
         if isfield(ALLEEG(sameica{cc}(tmpi)).dipfit, 'model')
             idat = sameica{cc}(tmpi);
-        end;
-    end;
+        end
+    end
     if ~isempty(idat)
         for tmpi = 1:length(sameica{cc})
             if ~isfield(ALLEEG(sameica{cc}(tmpi)).dipfit, 'model')
                 ALLEEG(sameica{cc}(tmpi)).dipfit = ALLEEG(idat).dipfit;
                 ALLEEG(sameica{cc}(tmpi)).saved  = 'no';
                 fprintf('Warning: no ICA dipoles for dataset %d, using dipoles from dataset %d (same ICA)\n', sameica{cc}(tmpi), idat);
-            end;
-        end;
-    end;
-end;
+            end
+        end
+    end
+end
 
 % put in fake channels if channel labels are missing
 % --------------------------------------------------
@@ -145,15 +145,15 @@ if any(cellfun(@isempty, chanlabels))
         disp(' SOME OTHERs HAVE CHANNEL LABELS. GENERATING CHANNEL LABELS FOR ');
         disp(' THE FORMER DATASETS (THIS SHOULD PROBABLY BE FIXED BY THE USER).');
         disp('********************************************************************');
-    end;
+    end
     disp('Generating channel labels for all datasets...');
     for currentind = 1:length(ALLEEG)
         for ind = 1:ALLEEG(currentind).nbchan
             ALLEEG(currentind).chanlocs(ind).labels = int2str(ind);
-        end;
-    end;
+        end
+    end
     ALLEEG(currentind).saved = 'no';
-end;
+end
 
 if length( unique( [ ALLEEG.srate ] )) > 1
     disp('********************************************************************');
@@ -161,34 +161,34 @@ if length( unique( [ ALLEEG.srate ] )) > 1
     disp(' RATE AND THIS WILL MAKE MOST OF THE STUDY FUNCTIONS CRASH. THIS');
     disp(' SHOULD PROBABLY BE FIXED BY THE USER.');
     disp('********************************************************************');
-end;
+end
 
 % check cluster array
 % -------------------
 rebuild_design = 0;
-if ~isfield(STUDY, 'cluster'), STUDY.cluster = []; modif = 1; end;
-if ~isfield(STUDY, 'changrp'), STUDY.changrp = []; modif = 1; end;
+if ~isfield(STUDY, 'cluster'), STUDY.cluster = []; modif = 1; end
+if ~isfield(STUDY, 'changrp'), STUDY.changrp = []; modif = 1; end
 if isempty(STUDY.changrp) && isempty(STUDY.cluster)
     rebuild_design = 1;
-end;
+end
 if isfield(STUDY.cluster, 'sets'),
     if max(STUDY.cluster(1).sets(:)) > length(STUDY.datasetinfo)
         disp('Warning: Some datasets had been removed from the STUDY, clusters have been reinitialized');
         STUDY.cluster = [];
-    end;
-end;
+    end
+end
 if ~studywasempty
     if isempty(STUDY.cluster)
         modif = 1;
         [STUDY] = std_createclust(STUDY, ALLEEG, 'parentcluster', 'on');
-    end;
+    end
     if length(STUDY.cluster(1).child) == length(STUDY.cluster)-1 && length(STUDY.cluster) > 1
         newchild = { STUDY.cluster(2:end).name };
         if ~isequal(STUDY.cluster(1).child, newchild)
             STUDY.cluster(1).child = newchild;
-        end;
-    end;
-end;
+        end
+    end
+end
 
 % create STUDY design if it is not present
 % ----------------------------------------
@@ -199,8 +199,8 @@ if ~studywasempty
             disp('Rebuilding trial information structure for STUDY');
             STUDY  = std_maketrialinfo(STUDY, ALLEEG); % some dataset do not have trialinfo and
             % some other have it, remake it for everybody
-        end;
-    end;
+        end
+    end
     if ~isfield(STUDY, 'design') || isempty(STUDY.design) || ~isfield(STUDY.design, 'name')
         STUDY  = std_maketrialinfo(STUDY, ALLEEG);
         STUDY  = std_makedesign(STUDY, ALLEEG);
@@ -209,14 +209,14 @@ if ~studywasempty
     else
         if isfield(STUDY.design, 'indvar1')
             STUDY  = std_convertdesign(STUDY, ALLEEG);
-        end;
+        end
         
         % check independent variable
         setFields   = fieldnames(STUDY.datasetinfo);
         if isempty(ALLEEG(1).event)
              trialFields = [];
         else trialFields = fieldnames(ALLEEG(1).event);
-        end;
+        end
         rmInd       = [];
         for indDes = 1:length(STUDY.design)
             for indVar = 1:length(STUDY.design(indDes).variable)
@@ -232,19 +232,19 @@ if ~studywasempty
                                 res = questdlg2(  textVal, 'Issue with studies', 'No', 'Yes', 'Yes');
                                 if strcmpi(res, 'Yes')
                                     rmInd = [ rmInd indDes ];
-                                end;
+                                end
                             else
                                 disp([textVal1 textVal2]);
-                            end;
+                            end
                         else
                             rmInd = [ rmInd indDes ];
-                        end;
-                end;
-            end;
-        end;
+                        end
+                end
+            end
+        end
         if ~isempty(rmInd)
             STUDY.design(rmInd) = [];
-        end;
+        end
         
         % convert combined independent variable values
         % between dash to cell array of strings
@@ -256,33 +256,33 @@ if ~studywasempty
                 % add pairing info in case it is missing
                 if isempty(STUDY.design(inddes).variable(indvar).label)
                     rmVar = [ rmVar indvar ];
-                end;
+                end
                 if ~isfield(STUDY.design(inddes).variable, 'pairing') || isempty(STUDY.design(inddes).variable(indvar).pairing)
                     if strcmpi(STUDY.design(inddes).variable(indvar).label, 'group')
                         STUDY.design(inddes).variable(indvar).pairing = 'off';
                     else
                         STUDY.design(inddes).variable(indvar).pairing = 'on';
-                    end;
-                end;
+                    end
+                end
                 
                 for indval = 1:length(STUDY.design(inddes).variable(indvar).value)
                     STUDY.design(inddes).variable(indvar).value{indval} = convertindvarval(STUDY.design(inddes).variable(indvar).value{indval});
-                end;
-            end;
+                end
+            end
             STUDY.design(inddes).variable(rmVar) = [];
             for indinclude = 1:length(STUDY.design(inddes).include)
                 if iscell(STUDY.design(inddes).include{indinclude})
                     for indval = 1:length(STUDY.design(inddes).include{indinclude})
                         STUDY.design(inddes).include{indinclude}{indval} = convertindvarval(STUDY.design(inddes).include{indinclude}{indval});
-                    end;
-                end;
-            end;
-        end;
-    end;
+                    end
+                end
+            end
+        end
+    end
     
     if rebuild_design % in case datasets have been added or removed
         STUDY = std_rebuilddesign(STUDY, ALLEEG);
-    end;
+    end
     
     % scan design to fix old pairing format
     % ------------------------------------
@@ -293,10 +293,10 @@ if ~studywasempty
                     STUDY.design(design).variable(var).pairing = 'on';
                 elseif strcmpi(STUDY.design(design).variable(var).pairing, 'unpaired')
                     STUDY.design(design).variable(var).pairing = 'off';
-                end;
-            end;
-        end;
-    end;
+                end
+            end
+        end
+    end
     
     % add filepath field if absent
     for ind = 1:length(STUDY.design)
@@ -304,8 +304,8 @@ if ~studywasempty
             STUDY.design(ind).filepath = '';
             STUDY.saved = 'no';
             modif = 1;
-        end;
-    end;
+        end
+    end
     
     % check that ICA is present and if it is update STUDY.datasetinfo
     allcompsSTUDY  = { STUDY.datasetinfo.comps };
@@ -313,16 +313,16 @@ if ~studywasempty
     if all(cellfun(@isempty, allcompsSTUDY)) && ~all(cellfun(@isempty, allcompsALLEEG))
         for index = 1:length(STUDY.datasetinfo)
             STUDY.datasetinfo(index).comps = [1:size(ALLEEG(index).icaweights,1)];
-        end;
-    end;
+        end
+    end
     
     % make channel groups
     % -------------------
     if ~isfield(STUDY, 'changrp') || isempty(STUDY.changrp)
         STUDY = std_changroup(STUDY, ALLEEG);
         modif = 1;
-    end;
-end;
+    end
+end
 
 % adapt STUDY design to new framework
 % -----------------------------------
@@ -330,39 +330,39 @@ if ~isempty(STUDY.design) && ~isfield(STUDY.design(1).variable, 'vartype')
     for inddes = 1:length(STUDY.design)
         for indvar = 1:length(STUDY.design(inddes).variable)
             STUDY.design(inddes).variable(indvar).vartype = 'categorical';
-        end;
-    end;
-end;
+        end
+    end
+end
 if ~isempty(STUDY.design) && isfield(STUDY.design(1), 'cell')
     STUDY.design = rmfield(STUDY.design, 'cell');
-end;
+end
 
 % determine if there has been any change
 % --------------------------------------
-if modif;
+if modif
     STUDY.saved = 'no';
     command = '[STUDY ALLEEG] = std_checkset(STUDY, ALLEEG);';
     addToHistory = true;
     % check duplicate
     if length(STUDY.history) >= length(command) && strcmpi(STUDY.history(end-length(command)+1:end), command)
         addToHistory = false;
-    end;
+    end
     if addToHistory
         STUDY.history =  sprintf('%s\n%s',  STUDY.history, command);
-    end;
-end;
+    end
+end
 
 % convert combined independent variables
 % --------------------------------------
-function val = convertindvarval(val);
+function val = convertindvarval(val)
 if isstr(val)
     inddash = findstr(' - ', val);
-    if isempty(inddash), return; end;
+    if isempty(inddash), return; end
     inddash = [ -2 inddash length(val)+1];
     for ind = 1:length(inddash)-1
         newval{ind} = val(inddash(ind)+3:inddash(ind+1)-1);
-    end;
+    end
     val = newval;
-end;
+end
 
 
