6116301255c5e9ce6d7d63cee4e72f70d590b319
Arnaud Delorme <arno@ucsd.edu>
2017-12-01 08:21:14 -0800

    pop_rejcont.m, change command line history

--- functions/popfunc/pop_rejcont.m
+++ functions/popfunc/pop_rejcont.m
@@ -75,7 +75,7 @@ com = '';
 if nargin < 1
     help pop_rejcont;
     return;
-end;
+end
 
 if nargin < 2
     firstelec = 'EXG1';                % first non EEG channel
@@ -87,10 +87,10 @@ if nargin < 2
         
         if isempty(indelec), elecrange = 1:EEG.nbchan;
         else                 elecrange = 1:(indelec-1);
-        end;
+        end
     else
         elecrange = 1:EEG.nbchan;
-    end;
+    end
     elecrange = deblank(vararg2str(elecrange));
     %elecrange = elecrange(2:end-1);
     
@@ -120,7 +120,7 @@ if nargin < 2
                   };
     geom = { [2 1] [2 1] [2 1] [2 1] [2 1] [2 1] [2 1] };
     result = inputgui('uilist', uilist, 'geometry', geom);
-    if length( result ) == 0 return; end;
+    if length( result ) == 0 return; end
     
     options = { 'elecrange'     str2num(result{1}) ...
                 'freqlimit'     str2num(result{2}) ...
@@ -131,7 +131,7 @@ if nargin < 2
                 'taper'         fastif(result{7}, 'hamming', 'none') };
 else
     options = varargin;
-end;
+end
 
 opt = finputcheck(options, { 'threshold'     { 'real';'cell' }  []    10;
                              'freqlimit'     { 'real';'cell' }  []    [35 128];
@@ -146,13 +146,13 @@ opt = finputcheck(options, { 'threshold'     { 'real';'cell' }  []    10;
                              'taper'         'string' { 'none' 'hamming' } 'none';
                              'overlap'       'real'   []    0.25;
                              'epochlength'   'real'   []    0.5 }, 'pop_rejcont');
-if isstr(opt), error(opt); end;
+if isstr(opt), error(opt); end
 if ~iscell(opt.threshold) && length(opt.threshold) == 2 && ...
     iscell(opt.freqlimit) && length(opt.freqlimit) == 2
     opt.threshold = { opt.threshold(1) opt.threshold(2) };
-end;
-if ~iscell(opt.threshold), opt.threshold = { opt.threshold }; end;
-if ~iscell(opt.freqlimit), opt.freqlimit = { opt.freqlimit }; end;
+end
+if ~iscell(opt.threshold), opt.threshold = { opt.threshold }; end
+if ~iscell(opt.freqlimit), opt.freqlimit = { opt.freqlimit }; end
 
 %EEG.event = [];
 grouplen  = opt.contiguous/2*opt.epochlength*EEG.srate+1; % maximum number of points for grouping regions
@@ -174,8 +174,8 @@ if isempty(opt.precompstruct)
         else
             boundEvent = strmatch('boundary', { tmpevent.type }, 'exact');
             NEWEEG.event = NEWEEG.event(boundEvent);
-        end;
-    end;
+        end
+    end
 
     [TMPNEWEEG] = eeg_regepochs(NEWEEG, opt.overlap, [0 opt.epochlength], NaN);
     %[TMPNEWEEG indices] = pop_rejspec(TMPNEWEEG, 1, [1:64], -100, 15, 30, 45, 0, 0);
@@ -184,7 +184,7 @@ if isempty(opt.precompstruct)
     tmpdata = TMPNEWEEG.data;
     if strcmpi(opt.taper, 'hamming'), 
         tmpdata = bsxfun(@times, tmpdata, hamming(size(TMPNEWEEG.data,2))');
-    end;
+    end
     tmp   = fft(tmpdata, [], 2);
     freqs = linspace(0, TMPNEWEEG.srate/2, size(tmp,2)/2);
     freqspectrum = freqs(2:end); % remove DC (match the output of PSD)
@@ -206,7 +206,7 @@ else
     freqspectrum  = opt.precompstruct.freqs;
     TMPNEWEEG     = opt.precompstruct.EEG;
     precompstruct = opt.precompstruct;
-end;
+end
 
 % apply threshold to average of all electrodes
 % --------------------------------------------
@@ -214,20 +214,20 @@ rejepoch = [];
 for iReject = 1:length(opt.threshold)
     threshold = opt.threshold{iReject};
     freqLim   = opt.freqlimit{iReject};
-    if length(threshold) == 1, threshold = [ -100 threshold ]; end;
+    if length(threshold) == 1, threshold = [ -100 threshold ]; end
     [I1 tmpRejEpoch NS Erej] = eegthresh( meanspectrum, size(meanspectrum,2), 1, threshold(1), threshold(2), [freqspectrum(1) freqspectrum(end)], freqLim(1), freqLim(2));
     rejepoch = union_bc(rejepoch, tmpRejEpoch);
     if strcmpi(opt.verbose, 'on')
         fprintf('%d regions selected for rejection, threshold %3.2f-%3.2f dB, frequency limits %3.1f-%3.1f\n', length(tmpRejEpoch), threshold(1), threshold(2), freqLim(1), freqLim(2));
-    end;
-end;
+    end
+end
 
 % build the winrej array for eegplot
 % ----------------------------------
 winrej   = [];
 if ~isempty(find(cellfun(@isempty, { TMPNEWEEG.event.epoch }) == 1))
     error('Some events are not associated with any epoch');
-end;
+end
 tmpevent = TMPNEWEEG.event;
 allepoch = [ tmpevent.epoch ];
 if ~isempty(rejepoch)
@@ -239,10 +239,10 @@ if ~isempty(rejepoch)
             winrej  = [ winrej; lat lat+opt.epochlength*TMPNEWEEG.srate-1 color ]; %Erej(:,index)'];
         else
             error('Wrong type for epoch');
-        end;
-    end;
+        end
+    end
     winrej(:,6:6+length(opt.elecrange)-1) = 0;
-end;
+end
 
 % remove isolated regions and merge others
 % ----------------------------------------
@@ -260,19 +260,19 @@ for index = size(winrej,1):-1:1
         winrej(index,2) = winrej(index+1,2);
         winrej(index+1,:) = [];
         merged = merged + 1;
-    end;
-end;
+    end
+end
 if strcmpi(opt.verbose, 'on')
     fprintf('%d regions merged\n', merged);
     fprintf('%d regions removed\n', isolated);
-end;
+end
 
 % add time before and after each region
 % -------------------------------------
 for index = 1:size(winrej,1)
     winrej(index,1) = max(1,         winrej(index,1)-opt.addlength*EEG.srate);
     winrej(index,2) = min(EEG.pnts,  winrej(index,2)+opt.addlength*EEG.srate);
-end;
+end
 
 % plot result
 % -----------
@@ -286,8 +286,8 @@ if ~isempty(winrej)
                 winrej(end+1,1:2) = opt.rejectori(iRow,:);
                 winrej(end  ,4)   = 1; % color
                 winrej(end  ,5)   = 1; % color
-            end;
-        end;
+            end
+        end
         
         command = '[EEG LASTCOM] = pop_select(EEG, ''nopoint'', TMPREJ(:,1:2)); eegh(LASTCOM); [ALLEEG EEG CURRENTSET LASTCOM] = pop_newset(ALLEEG, EEG, CURRENTSET, ''study'', ~isempty(STUDY)+0); eegh(LASTCOM); eeglab redraw';
         if nargin < 2 || strcmpi(opt.eegplot, 'on')
@@ -297,17 +297,17 @@ if ~isempty(winrej)
             disp('Yellow is AUTOMATIC rejection');
         else
             NEWEEG = pop_select(EEG, 'nopoint', round(selectedregions));
-        end;
+        end
         EEG = NEWEEG;
     else
         EEG = [];
-    end;
+    end
 else
     selectedregions = [];
     if strcmpi(opt.verbose, 'on')
         disp('No region removed');
-    end;
-end;
+    end
+end
 if nargout > 3
-    com = sprintf('EEG = pop_rejcont(EEG, %s);', vararg2str(options));
-end;
+    com = sprintf('%% the command below does the automated rejection (the pop_select command after that includes manual editing)\n%% EEG = pop_rejcont(EEG, %s);', vararg2str(options));
+end
