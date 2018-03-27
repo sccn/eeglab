588a7e8e746daa3cd535c233a8f327b72fc6a2c3
Arnaud Delorme <arno@ucsd.edu>
2018-03-27 16:01:12 -0700

    fieldtrip2eeglab.m, now calls file-io for conversion

--- functions/miscfunc/fieldtrip2eeglab.m
+++ functions/miscfunc/fieldtrip2eeglab.m
@@ -1,52 +1,21 @@
-% load data file ('dataf') preprocessed with fieldtrip
-% and show in eeglab viewer
+% fieldtrip2eeglab - convert Fieldtrip structures to EEGLAB dataset
 %
-% This function is provided as is. It only works for some specific type of
-% data. This is a simple function to help the developer and by no mean
-% an all purpose function.
+% EEG = fieldtrip2eeglab(header, data, evt);
+%
+% Inputs:
+%    header   - Fieldtrip data header 
+%    data     - Fieldtrip raw data
+%    evt      - Fieldtrip event structure (optional)
+%
+% Output:
+%    EEG     - EEGLAB structure
+%
+% Author: Arnaud Delorme, UCSD
 
-function [EEG] = fieldtrip2eeglab(dataf)
+function EEG = fieldtrip2eeglab(header,data,evt)
 
-[ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
-
-if exist(dataf,'file')
-  load(dataf)
+if nargin < 3
+    evt = [];
 end
 
-% load chanlocs.mat
-% EEG.chanlocs = chanlocs;
-EEG.chanlocs = [];
-
-for i=1:size(data.trial,2)
-  EEG.data(:,:,i) = single(data.trial{i});
-end
-
-EEG.setname    = dataf; %data.cfg.dataset;
-EEG.filename   = '';
-EEG.filepath   = '';
-EEG.subject    = '';
-EEG.group      = '';
-EEG.condition  = '';
-EEG.session    = [];
-EEG.comments   = 'preprocessed with fieldtrip';
-EEG.nbchan     = size(data.trial{1},1);
-EEG.trials     = size(data.trial,2);
-EEG.pnts       = size(data.trial{1},2);
-EEG.srate      = data.fsample;
-EEG.xmin       = data.time{1}(1);
-EEG.xmax       = data.time{1}(end);
-EEG.times      = data.time{1};
-EEG.ref        = []; %'common';
-EEG.event      = [];
-EEG.epoch      = [];
-EEG.icawinv    = [];
-EEG.icasphere  = [];
-EEG.icaweights = [];
-EEG.icaact     = [];
-EEG.saved      = 'no';
-
-[ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG);
-eeglab redraw
-pop_eegplot( EEG, 1, 1, 1);
-
- 
+EEG = pop_fileio(header, data, evt);
