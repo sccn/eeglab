77d862b78f693923f5a1ffd919501c0c3fa3f2a7
Arnaud Delorme <arno@ucsd.edu>
2018-05-10 11:30:38 -0700

    eeg_store.m, change header documentatio

--- functions/adminfunc/eeg_store.m
+++ functions/adminfunc/eeg_store.m
@@ -48,8 +48,6 @@
 % along with this program; if not, write to the Free Software
 % Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-% uses the global variable EEG ALLEEG CURRENTSET 
-
 % store set
 % ------------------
 function [ALLEEG, EEG, storeSetIndex] = eeg_store(ALLEEG, EEG, storeSetIndex, varargin);
