6749d55f35ee7eea4f8a111c16cfa7e9aac04668
arno <>
2018-07-06 16:35:01 +0200

    std_limodesign.m, now takes into account split regression and interactions

--- functions/studyfunc/std_limodesign.m
+++ functions/studyfunc/std_limodesign.m
@@ -2,8 +2,8 @@
 %                    continuous variables.
 %
 % Usage:
-%   >> std_limosavedesignfiles(factors, trialinfo)
-%   >> std_limosavedesignfiles(factors, trialinfo, 'key', val)
+%   >> [catMat,contMat,description] = std_limosavedesignfiles(factors, trialinfo)
+%   >> [catMat,contMat,description] = std_limosavedesignfiles(factors, trialinfo, 'key', val)
 %
 % Inputs:
 %   factors      - [struct] list of factors (structure containing fields
@@ -16,6 +16,18 @@
 % Optional inputs:
 %   filepath     - [string] file path. If not empty, categorical_variables.txt
 %                  and continuous_variables.txt files are saved.
+%   'splitreg'   - ['on'|'off'] split regression for different categorical
+%                  factors. Default is 'off'.
+%   'interaction' - ['on'|'off'] compute interaction when using different
+%                  categorical variables. This allows computing interactions
+%                  between these variables at the second level. Default 
+%                  is 'on'.
+%   'desconly'    - ['on'|'off'] only output description
+%
+%  Outputs:
+%    catMat      - [array] design matrix for categorical variables
+%    contMat     - [array] design matrix for continuous variables
+%    description - [cell] what each columns contains
 %
 % Author: Arnaud Delorme, SCCN, UCSD, 2018-
 %
@@ -48,55 +60,111 @@ end
 % -----------------------
 opt = finputcheck( varargin, ...
     { 'splitreg'       'string'  { 'on','off' }   'off';
+      'interaction'    'string'  { 'on','off' }   'on';
+      'desconly'       'string'  { 'on','off' }   'off';
       'filepath'       'string'  {            }   ''    }, 'std_limosavedesignfiles');
 if isstr(opt), error(opt); end;
 
 % number of categorical and continuous var
 % ----------------------------------------
+catMat  = [];
+contMat = [];
 catVar  = sort(find(cellfun(@(x)strcmpi(x, 'categorical' ), { factors.vartype })));
 contVar = find(cellfun(@(x)strcmpi(x, 'continuous' ), { factors.vartype }));
 catVarLabel = unique({ factors(catVar).label });
 
-% categorical file/matrix
-% -----------------------
-col = 1;
-catMat = zeros(length(trialinfo),1);
+% find all values for each cat. indep. var.
+% -----------------------------------------
+alloptions = {};
 for iVar = 1:length(catVarLabel)
-    % find all values for this label
     indVals = find(cellfun(@(x)strcmpi(x, catVarLabel{iVar} ), { factors.label }));
     values = { factors(indVals).value };
-    limodesign.categorival(iVar).label = catVarLabel{iVar};
-    limodesign.categorival(iVar).value = values;
-    
-    % build design matrix
-    for iVal = 1:length(values)
-        [trialindsx, eventvals] = std_gettrialsind(trialinfo, catVarLabel{iVar}, values{iVal});
-        catMat(trialindsx, iVar) = iVal;
-    end
+    alloptions{iVar} = cellfun(@(x){catVarLabel{iVar} x}, values, 'uniformoutput', false);
 end
-catMat(catMat == 0) = NaN;
-if ~isempty(opt.filepath)
-    save(fullfile(opt.filepath, 'categorical_variables.txt'),'catMat','-ascii');
+
+% compute interactions if necessary
+% ---------------------------------
+alloptionsinter = inter(alloptions);
+for iOpt = 1:length(alloptionsinter)
+    alloptionsinter{iOpt} = [ alloptionsinter{iOpt}{:} ];
+end
+alloptionsinter = { alloptionsinter }; % 1 condition only
+if strcmpi(opt.interaction, 'on')
+    limodesign.categorival = alloptionsinter;
+else
+    limodesign.categorival = alloptions;
+end
+
+% build design matrix for categorical var.
+% ----------------------------------------
+if strcmpi(opt.desconly, 'off')
+    col = 1;
+    catMat = zeros(length(trialinfo),1);
+    for iVar = 1:length(alloptions)
+        for iVal = 1:length(alloptions{iVar})
+            [trialindsx, eventvals] = std_gettrialsind(trialinfo, alloptions{iVar}{iVal}{:});
+            catMat(trialindsx, iVar) = iVal;
+        end
+    end
+    catMat(catMat == 0) = NaN;
+    if ~isempty(opt.filepath)
+        save(fullfile(opt.filepath, 'categorical_variables.txt'),'catMat','-ascii');
+    end
 end
 
 % continuous file/matrix
 % ----------------------
-contMat = [];
 if ~isempty(contVar)
     contMat = zeros(length(trialinfo),1);
     
-    % build design matrix
     for iVar = 1:length(contVar)
-        limodesign.continuous(iVar).label = factors(contVar(iVar)).label;
-        [trialindsx, eventvals] = std_gettrialsind(trialinfo, factors(contVar(iVar)).label);
-        contMat(:         ,iVar) = NaN;
-        contMat(trialindsx,iVar) = eventvals;
+        limodesign.continuous{iVar} = { factors(contVar(iVar)).label '' };
     end
+    
+    % split variables if necessary
     if strcmpi(opt.splitreg, 'on')
-        % splits the continuous design based on categorical var. values
-        contMat = limo_split_continuous(contMat, catMat);
+        limodesign.continuous = inter( { limodesign.continuous, alloptionsinter{:} } );
+        for iOpt = 1:length(limodesign.continuous)
+            limodesign.continuous{iOpt} = [ limodesign.continuous{iOpt}{:} ];
+        end
     end
-    if ~isempty(opt.filepath)
-        save(fullfile(filepath, 'continuous_variables.txt'),'contMat','-ascii');
+    
+    if strcmpi(opt.desconly, 'off')
+        
+        % build continuous design matrix
+        for iVar = 1:length(limodesign.continuous)
+            [trialindsx, eventvals] = std_gettrialsind(trialinfo, limodesign.continuous{iVar}{:});
+            contMat(:         ,iVar) = NaN;
+            contMat(trialindsx,iVar) = eventvals;
+        end
+
+        % z-score columns if they are split - same as limo_split_continuous(catMat, contMat)
+        if strcmpi(opt.splitreg, 'on')
+            for iVar = 1:size(contMat,2)
+                nonNan = ~isnan(contMat(:,iVar));
+                contMat(nonNan,iVar) = zscore(contMat(nonNan,iVar));
+                contMat(~nonNan,iVar) = 0;
+            end
+        end
+        if ~isempty(opt.filepath)
+            save(fullfile(opt.filepath, 'continuous_variables.txt'),'contMat','-ascii');
+        end
     end
 end
+
+% compute interactions in a recursive maner
+% ex: res = inter( { { {1 2} {3 4} } { { 5 6 } { 7 8 } { 9 10 } } { { 11 12 } {13 14} } })
+% -----------------------------------------
+function b = inter(a)
+
+if length(a) == 1
+    b = a{1};
+else
+    c = inter(a(2:end));
+    for iVal1 = 1:length(a{1})
+        for iVal2 = 1:length(c)
+            b{(iVal1-1)*length(c)+iVal2} = { a{1}{iVal1} c{iVal2} };
+        end
+    end
+end
+
