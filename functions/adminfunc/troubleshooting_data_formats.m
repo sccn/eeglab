8be6eaf5af8450dfb606906c40ba7c3abcd3f20f
Robert Oostenveld <r.oostenveld@gmail.com>
2018-08-06 09:43:16 +0200

    updated link to fileio documentation

--- functions/adminfunc/troubleshooting_data_formats.m
+++ functions/adminfunc/troubleshooting_data_formats.m
@@ -6,11 +6,11 @@
 %The FILEIO and BIOSIG toolboxes interface also allow to import in EEGLAB a wide variety
 %of EEG/MEG data file formats -- including those imported using EEGLAB native menus
 %
-%For EEGLAB, FILE-IO and BIOSIG suported data formats
+%For EEGLAB, FILEIO and BIOSIG suported data formats
 %http://sccn.ucsd.edu/wiki/A01:_Importing_Continuous_and_Epoched_Data
 %
-%For FILE-IO see in addition
-%http://www2.ru.nl/fcdonders/fieldtrip/doku.php?id=fieldtrip:dataformat
+%For FILEIO see in addition
+%http://www.fieldtriptoolbox.org/development/fileio
 %
 %For BIOSIG see in addition
 %http://biosig.sourceforge.net/SupportedSystems.html
