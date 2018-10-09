dd21ca30c24f3d915e08eaa902d357fa09c6624b
arno <>
2018-10-09 00:36:38 -0700

    topoplot.m, fix issue pertaining to logical or and and

--- functions/sigprocfunc/topoplot.m
+++ functions/sigprocfunc/topoplot.m
@@ -524,7 +524,7 @@ if nargs > 2
                 end
             case 'plotrad'
                 plotrad = Value;
-                if ischar(plotrad) || (plotrad < MINPLOTRAD || plotrad > 1)
+                if ~isempty(plotrad) && (ischar(plotrad) || (plotrad < MINPLOTRAD || plotrad > 1))
                     error('plotrad argument should be a number between 0.15 and 1.0');
                 end
             case 'headrad'
