87776ecff859c6fa4c69e4d96ebc54db7242dfd6
arno <arnodelorme@gmail.com>
2019-04-03 18:01:10 -0700

    eeg_countepochs.m, add copyright back to file

--- functions/popfunc/eeg_countepochs.m
+++ functions/popfunc/eeg_countepochs.m
@@ -22,6 +22,8 @@
 %
 %  Author: Stephen Politzer-Ahles, University of Kansas, 2013
 
+% Copyright: Stephen Politzer-Ahles, University of Kansas, 2013
+%
 % This program is free software; you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation; either version 2 of the License, or
