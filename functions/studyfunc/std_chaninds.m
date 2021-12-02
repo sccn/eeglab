ea8b2b51cb2db0e50e2f2a8f53fae3ba85238c3e
Arnaud Delorme <arnodelorme@gmail.com>
2021-12-02 10:54:27 -1000

    fix formating

--- functions/studyfunc/std_chaninds.m
+++ functions/studyfunc/std_chaninds.m
@@ -42,7 +42,7 @@
 % ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 % THE POSSIBILITY OF SUCH DAMAGE.
 
-function finalinds = std_chaninds(instruct, channames);
+function finalinds = std_chaninds(instruct, channames)
 
     finalinds   = [];
     if isfield(instruct, 'chanlocs')
