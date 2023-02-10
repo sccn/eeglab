20c4e70fc85765d09e108477d02f21ab71205092
Arnaud Delorme <arnodelorme@gmail.com>
2023-02-09 16:15:29 -0800

    update version number

--- functions/adminfunc/eeg_getversion.m
+++ functions/adminfunc/eeg_getversion.m
@@ -41,8 +41,8 @@
 
 function [vers, versnum, releaseDate] = eeg_getversion
 
-vers        = '2022.1';
-releaseDate = '1-Aug-22 14:55:42'; % unix date -> date +"%d-%b-%y %T"
+vers        = '2023.0';
+releaseDate = '9-Feb-23 14:55:42'; % unix date -> date +"%d-%b-%y %T"
 
 % get numerical version number
 tmpvers = vers;
