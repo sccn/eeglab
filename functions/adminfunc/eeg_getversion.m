3d1bfc3ec2017814342142722844963b5b0c15f1
Arnaud Delorme <arnodelorme@gmail.com>
2023-02-14 15:50:01 -0800

    change version number

--- functions/adminfunc/eeg_getversion.m
+++ functions/adminfunc/eeg_getversion.m
@@ -41,7 +41,7 @@
 
 function [vers, versnum, releaseDate] = eeg_getversion
 
-vers        = '2023.0_rc';
+vers        = '2023.0';
 releaseDate = '9-Feb-23 14:55:42'; % unix date -> date +"%d-%b-%y %T"
 
 % get numerical version number
