4aa69ebaefd83ad6520825edbf96df284d789366
Arnaud Delorme <arnodelorme@gmail.com>
2023-03-07 18:59:01 -1000

    back to dev version

--- functions/adminfunc/eeg_getversion.m
+++ functions/adminfunc/eeg_getversion.m
@@ -41,7 +41,7 @@
 
 function [vers, versnum, releaseDate] = eeg_getversion
 
-vers        = '2023.0';
+vers        = 'dev';
 releaseDate = '17-Feb-23 14:55:42'; % unix date -> date +"%d-%b-%y %T"
 
 % get numerical version number
