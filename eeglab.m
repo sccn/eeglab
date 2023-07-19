0c8d8622a26fb83c14e3f85fa3eb207e43563ab0
Arnaud Delorme <arnodelorme@gmail.com>
2023-07-18 18:50:30 -1000

    fix issue with ~

--- eeglab.m
+++ eeglab.m
@@ -623,17 +623,17 @@ cb_rmbase      = [ check      '[EEG LASTCOM] = pop_rmbase(EEG);'   e_newset];
 cb_runica      = [ check      '[EEG LASTCOM] = pop_runica(EEG);'   e_store e_check_study];
 cb_subcomp     = [ checkica   '[EEG LASTCOM] = pop_subcomp(EEG);'  e_newset];
 %cb_chanrej     = [ check      'pop_rejchan(EEG); LASTCOM = '''';'  e_hist];
-cb_chanrej     = [ check      '[EEG ~ ~ LASTCOM] = pop_rejchan(EEG);'  e_hist];
-cb_autorej     = [ checkepoch '[EEG ~ LASTCOM] = pop_autorej(EEG);'    e_hist];
-cb_rejcont     = [ check      '[EEG ~ ~ LASTCOM] = pop_rejcont(EEG);'  e_hist];
+cb_chanrej     = [ check      '[EEG, ~, ~, LASTCOM] = pop_rejchan(EEG);'  e_hist];
+cb_autorej     = [ checkepoch '[EEG, ~, LASTCOM] = pop_autorej(EEG);'    e_hist];
+cb_rejcont     = [ check      '[EEG, ~, ~, LASTCOM] = pop_rejcont(EEG);'  e_hist];
 
 cb_rejmenu1    = [ check      'pop_rejmenu(EEG, 1); LASTCOM = '''';'    e_hist];
 cb_eegplotrej1 = [ check      '[LASTCOM] = pop_eegplot(EEG, 1);'        e_hist];
-cb_eegthresh1  = [ checkepoch '[~ LASTCOM] = pop_eegthresh(EEG, 1);'    e_hist];
+cb_eegthresh1  = [ checkepoch '[EEG,~,LASTCOM] = pop_eegthresh(EEG, 1);'    e_hist];
 cb_rejtrend1   = [ checkepoch '[EEG LASTCOM] = pop_rejtrend(EEG, 1);'   e_store];
 cb_jointprob1  = [ checkepoch '[EEG LASTCOM] = pop_jointprob(EEG, 1);'  e_store];
 cb_rejkurt1    = [ checkepoch '[EEG LASTCOM] = pop_rejkurt(EEG, 1);'    e_store];
-cb_rejspec1    = [ checkepoch '[EEG ~ LASTCOM] = pop_rejspec(EEG, 1);'  e_store];
+cb_rejspec1    = [ checkepoch '[EEG, ~, LASTCOM] = pop_rejspec(EEG, 1);'  e_store];
 cb_rejsup1     = [ checkepochica '[EEG LASTCOM] = eeg_rejsuperpose(EEG, 1,1,1,1,1,1,1,1); eegh(LASTCOM);' ...
                      'LASTCOM = ''EEG.reject.icarejmanual = EEG.reject.rejglobal;''; eval(LASTCOM);' e_store ];
 cb_rejsup2     = [ checkepoch '[EEG LASTCOM] = eeg_rejsuperpose(EEG, 1,1,1,1,1,1,1,1); EEG = eegh(LASTCOM, EEG);' ...
@@ -1567,7 +1567,11 @@ if exist_study
     tmp_m = eegmenu( false,  set_m, 'Label', 'Select the study set', 'Enable', 'on', 'userdata', 'study:on');
     set(tmp_m, 'enable', 'on', 'callback', cb_select, 'separator', 'on');        
 else 
-    delete( findobj('label', 'Select the study set') );
+    try
+        delete( findobj('label', 'Select the study set') );
+    catch
+        fprintf(2, 'Note: Issue with updating menu')
+    end
 end
 
 EEGUSERDAT{2} = EEGMENU;
