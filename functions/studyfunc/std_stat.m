def9d435678de78dfb6f49f0c71183039a5e35d1
Arnaud Delorme <arnodelorme@gmail.com>
2024-07-10 09:39:55 -0700

    update stats info

--- functions/studyfunc/std_stat.m
+++ functions/studyfunc/std_stat.m
@@ -42,7 +42,8 @@
 %  'fieldtripnaccu'   - 'numrandomization' Fieldtrip parameter
 %  'fieldtripalpha'   - 'alpha' Fieldtrip parameter. Default is 0.05.
 %  'fieldtripmethod'  - 'method' Fieldtrip parameter. Default is 'analytic'
-%  'fieldtripmcorrect' - 'mcorrect' Fieldtrip parameter. Default is 'none'.
+%  'fieldtripmcorrect' - ['cluster'|'max'|'fdr'|'holms'|'bonferoni'|'none']. 
+%                        Default is 'none'.
 %  'fieldtripclusterparam' - string or cell array for optional parameters
 %                            for cluster correction method, see function
 %                            ft_statistics_montecarlo for more information.
