ff988bba2e3cb74a70f462060c57fbaf4a4ce978
Arnaud Delorme <arnodelorme@gmail.com>
2024-07-10 16:33:35 -0700

    update version

--- functions/adminfunc/eeg_getversion.m
+++ functions/adminfunc/eeg_getversion.m
@@ -41,7 +41,7 @@
 
 function [vers, versnum, releaseDate] = eeg_getversion()
 
-vers        = '2024.1';
+vers        = 'dev'; %'2024.1';
 releaseDate = '10-Jul-24 14:55:42'; % unix date -> date +"%d-%b-%y %T"
 
 % get numerical version number
