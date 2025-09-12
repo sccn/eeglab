62a9dc8849ffdb071ac4bf743df3ab098b977484
Arnaud Delorme <arnodelorme@gmail.com>
2025-09-11 21:39:22 -0700

    fixing titles for 2x2 designs

--- functions/studyfunc/std_figtitle.m
+++ functions/studyfunc/std_figtitle.m
@@ -264,10 +264,10 @@ if strcmpi(opt.factor1stat, 'on')
     rown = rown+1;
     if strcmpi(opt.effect, 'marginal')
         for c2 = 1:length(opt.factor2vals)
-            all_titles{rown, c2} = [ value2str(opt.factor2vals{c2}) ' ' basicstat ];
+            all_titles{rown, c2} = [ opt.factor1 ', ' value2str(opt.factor2vals{c2}) ' ' basicstat ];
         end
     else
-        all_titles{rown, 1} = [ opt.factor2 ' ' basicstat ]; 
+        all_titles{rown, 1} = [ opt.factor1 ' ' basicstat ]; 
     end
 end
 coln = size(all_titles,2);
@@ -275,10 +275,10 @@ if strcmpi(opt.factor2stat, 'on')
     coln = coln+1;
     if strcmpi(opt.effect, 'marginal')
         for c1 = 1:length(opt.factor1vals)
-            all_titles{c1, coln} = [ value2str(opt.factor1vals{c1}) ' ' basicstat ];   
+            all_titles{c1, coln} = [ opt.factor2 ', ' value2str(opt.factor1vals{c1}) ' ' basicstat ];   
         end
     else
-        all_titles{1, coln} = [ opt.factor1 ' ' basicstat ];   
+        all_titles{1, coln} = [ opt.factor2 ' ' basicstat ];   
     end
 end
 if ~strcmpi(opt.effect, 'marginal') && (strcmpi(opt.factor1stat, 'on') && strcmpi(opt.factor2stat, 'on') && rown > 1 && coln > 1)
