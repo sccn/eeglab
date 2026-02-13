ac599be73ef559070727f1bea09d22abb849183a
Arnaud Delorme <arnodelorme@gmail.com>
2026-02-13 13:49:57 -0800

    fix early return issue

--- functions/adminfunc/plugin_askinstall.m
+++ functions/adminfunc/plugin_askinstall.m
@@ -54,7 +54,11 @@ if nargin < 2 || ~exist(char(pluginFunc))
         installRes = 0;
 
         % check is deactivated
-        try, PLUGINLIST = evalin('base', 'PLUGINLIST'); catch, PLUGINLIST = []; end
+        try
+            PLUGINLIST = evalin('base', 'PLUGINLIST');
+        catch
+            PLUGINLIST = []; 
+        end
         if ~isempty(PLUGINLIST) && isfield(PLUGINLIST, 'plugin')
             indPlugin = strmatch(lower(pluginName), lower({ PLUGINLIST.plugin }), 'exact');
             if ~isempty(indPlugin) && strcmpi(PLUGINLIST(indPlugin(1)).status, 'deactivated')
@@ -65,8 +69,6 @@ if nargin < 2 || ~exist(char(pluginFunc))
                 installRes = 1;
                 return;
             end
-            fprintf(['Plugin "' pluginName '" already installed\n']);
-            return;
         end
 
         % check for installing
