7921348d5b4e73c70ac0023e9942d24641715bee
Arnaud Delorme <arnodelorme@gmail.com>
2026-05-17 12:53:41 -0700

    fix bug in computing event latency

--- functions/sigprocfunc/epoch.m
+++ functions/sigprocfunc/epoch.m
@@ -123,7 +123,7 @@ indexes = zeros(length(events),1);
 alleventout = {};
 alllatencyout = {};
 for index = 1:length(events)
-	pos0 = floor(events(index)*g.srate); % offset of time locking event
+	pos0 = floor(double(events(index))*g.srate); % offset of time locking event (double cast prevents int64 arithmetic bugs)
 	posinit = pos0+reallim(1); % compute offset
 	posend  = pos0+reallim(2); % compute offset
    
